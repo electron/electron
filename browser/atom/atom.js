3c96007131aebdfbd8ffff9d24ed3d6a704d5424
Cheng Zhao <zcbenz@gmail.com>
2013-04-13 23:38:29 +0800

    Don't quit browser when get fatal js error.

--- browser/atom/atom.js
+++ browser/atom/atom.js
@@ -1,1 +1,7 @@
-console.log(process.argv);
+process.on('uncaughtException', function(error) {
+  console.error('uncaughtException:');
+  if (error.stack)
+    console.error(error.stack);
+  else
+    console.error(error.name + ': ' + error.message);
+});
