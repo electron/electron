f3a6037d007f96e387832360c6afc2eb4bdeaa13
Cheng Zhao <zcbenz@gmail.com>
2013-05-18 10:38:56 +0800

    Add Edit menu.

--- browser/default_app/main.js
+++ browser/default_app/main.js
@@ -66,6 +66,44 @@ delegate.browserMainParts.preMainMessageLoopRun = function() {
       ]
     },
     {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'Command+Z',
+          selector: 'undo:'
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+Command+Z',
+          selector: 'redo:'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Cut',
+          accelerator: 'Command+X',
+          selector: 'cut:'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'Command+C',
+          selector: 'copy:'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'Command+V',
+          selector: 'paste:'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'Command+A',
+          selector: 'selectAll:'
+        },
+      ]
+    },
+    {
       label: 'View',
       submenu: [
         {
