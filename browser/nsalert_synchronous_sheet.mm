a897d5b715be970f091a2ccb62f856aa27412549
Cheng Zhao <zcbenz@gmail.com>
2013-06-07 15:58:36 +0800

    Return button's index for [NSAlert runModalSheetForWindow].

--- browser/nsalert_synchronous_sheet.mm
+++ browser/nsalert_synchronous_sheet.mm
@@ -12,54 +12,39 @@
 @implementation NSAlert (SynchronousSheet)
 
 -(NSInteger) runModalSheetForWindow:(NSWindow *)aWindow {
-    // Set ourselves as the target for button clicks
-    for (NSButton *button in [self buttons]) {
-        [button setTarget:self];
-        [button setAction:@selector(BE_stopSynchronousSheet:)];
-    }
+  // Set ourselves as the target for button clicks
+  for (NSButton *button in [self buttons]) {
+    [button setTarget:self];
+    [button setAction:@selector(BE_stopSynchronousSheet:)];
+  }
 
-    // Bring up the sheet and wait until stopSynchronousSheet is triggered by a button click
-    [self performSelectorOnMainThread:@selector(BE_beginSheetModalForWindow:) withObject:aWindow waitUntilDone:YES];
-    NSInteger modalCode = [NSApp runModalForWindow:[self window]];
+  // Bring up the sheet and wait until stopSynchronousSheet is triggered by a button click
+  [self performSelectorOnMainThread:@selector(BE_beginSheetModalForWindow:) withObject:aWindow waitUntilDone:YES];
+  NSInteger modalCode = [NSApp runModalForWindow:[self window]];
 
-    // This is called only after stopSynchronousSheet is called (that is,
-    // one of the buttons is clicked)
-    [NSApp performSelectorOnMainThread:@selector(endSheet:) withObject:[self window] waitUntilDone:YES];
+  // This is called only after stopSynchronousSheet is called (that is,
+  // one of the buttons is clicked)
+  [NSApp performSelectorOnMainThread:@selector(endSheet:) withObject:[self window] waitUntilDone:YES];
 
-    // Remove the sheet from the screen
-    [[self window] performSelectorOnMainThread:@selector(orderOut:) withObject:self waitUntilDone:YES];
+  // Remove the sheet from the screen
+  [[self window] performSelectorOnMainThread:@selector(orderOut:) withObject:self waitUntilDone:YES];
 
-    return modalCode;
+  return modalCode;
 }
 
 -(NSInteger) runModalSheet {
-    return [self runModalSheetForWindow:[NSApp mainWindow]];
+  return [self runModalSheetForWindow:[NSApp mainWindow]];
 }
 
 
 #pragma mark Private methods
 
 -(IBAction) BE_stopSynchronousSheet:(id)sender {
-    // See which of the buttons was clicked
-    NSUInteger clickedButtonIndex = [[self buttons] indexOfObject:sender];
-
-    // Be consistent with Apple's documentation (see NSAlert's addButtonWithTitle) so that
-    // the fourth button is numbered NSAlertThirdButtonReturn + 1, and so on
-    NSInteger modalCode = 0;
-    if (clickedButtonIndex == NSAlertFirstButtonReturn)
-        modalCode = NSAlertFirstButtonReturn;
-    else if (clickedButtonIndex == NSAlertSecondButtonReturn)
-        modalCode = NSAlertSecondButtonReturn;
-    else if (clickedButtonIndex == NSAlertThirdButtonReturn)
-        modalCode = NSAlertThirdButtonReturn;
-    else
-        modalCode = NSAlertThirdButtonReturn + (clickedButtonIndex - 2);
-
-    [NSApp stopModalWithCode:modalCode];
+  [NSApp stopModalWithCode:[sender  tag]];
 }
 
 -(void) BE_beginSheetModalForWindow:(NSWindow *)aWindow {
-    [self beginSheetModalForWindow:aWindow modalDelegate:nil didEndSelector:nil contextInfo:nil];
+  [self beginSheetModalForWindow:aWindow modalDelegate:nil didEndSelector:nil contextInfo:nil];
 }
 
 @end
