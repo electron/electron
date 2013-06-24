5c4c86c6ef5d288c1162356e6f0d94c9b85a1127
Cheng Zhao <zcbenz@gmail.com>
2013-06-24 15:36:38 +0800

    Rewrite build script in python.

---
+++ script/build.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import subprocess
+import sys
+
+
+CONFIGURATIONS = ['Release', 'Debug']
+
+
+def main():
+  args = parse_args()
+  for config in args.configuration:
+    build_path = os.path.join('out', config)
+    subprocess.check_call(['ninja', '-C', build_path])
+
+
+def parse_args():
+  parser = argparse.ArgumentParser(description='Build atom-shell')
+  parser.add_argument('-c', '--configuration',
+                      help='Build with Release or Debug configuration',
+                      nargs='+',
+                      default=CONFIGURATIONS,
+                      required=False)
+  return parser.parse_args()
+
+
+if __name__ == '__main__':
+  sys.exit(main())
