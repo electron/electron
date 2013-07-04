f146c1fe3baaf3a02612ce0a49ae40305b686fac
Cheng Zhao <zcbenz@gmail.com>
2013-07-04 15:54:34 +0800

    Seperate implementation of crash reporter to standalone class.

--- browser/api/atom_api_crash_reporter.cc
+++ browser/api/atom_api_crash_reporter.cc
@@ -4,9 +4,7 @@
 
 #include "browser/api/atom_api_crash_reporter.h"
 
-#import <Quincy/BWQuincyManager.h>
-
-#include "base/strings/sys_string_conversions.h"
+#include "browser/crash_reporter.h"
 #include "vendor/node/src/node.h"
 #include "vendor/node/src/node_internals.h"
 
@@ -14,37 +12,24 @@ namespace atom {
 
 namespace api {
 
-namespace {
-
-// Converts a V8 value to a string16.
-string16 V8ValueToUTF16(v8::Handle<v8::Value> value) {
-  v8::String::Value s(value);
-  return string16(reinterpret_cast<const char16*>(*s), s.length());
-}
-
-}  // namespace
-
 // static
 v8::Handle<v8::Value> CrashReporter::SetCompanyName(const v8::Arguments &args) {
-  BWQuincyManager *manager = [BWQuincyManager sharedQuincyManager];
-  string16 str(V8ValueToUTF16(args[0]));
-  [manager setCompanyName:base::SysUTF16ToNSString(str)];
+  std::string name(*v8::String::Utf8Value(args[0]));
+  crash_reporter::CrashReporter::SetCompanyName(name);
   return v8::Undefined();
 }
 
 // static
 v8::Handle<v8::Value> CrashReporter::SetSubmissionURL(
     const v8::Arguments &args) {
-  BWQuincyManager *manager = [BWQuincyManager sharedQuincyManager];
-  string16 str(V8ValueToUTF16(args[0]));
-  [manager setSubmissionURL:base::SysUTF16ToNSString(str)];
+  std::string url(*v8::String::Utf8Value(args[0]));
+  crash_reporter::CrashReporter::SetSubmissionURL(url);
   return v8::Undefined();
 }
 
 // static
 v8::Handle<v8::Value> CrashReporter::SetAutoSubmit(const v8::Arguments &args) {
-  BWQuincyManager *manager = [BWQuincyManager sharedQuincyManager];
-  [manager setAutoSubmitCrashReport:args[0]->BooleanValue()];
+  crash_reporter::CrashReporter::SetAutoSubmit(args[0]->BooleanValue());
   return v8::Undefined();
 }
 
