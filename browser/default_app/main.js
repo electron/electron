a7557a9626c59a63eb002e2ec108bcf3c1bca46a
Cheng Zhao <zcbenz@gmail.com>
2013-07-24 15:20:59 +0800

    Do not end the process when other exception occurs.

--- browser/default_app/main.js
+++ browser/default_app/main.js
@@ -5,11 +5,16 @@ var path = require('path');
 // Start the specified app if there is one specified in command line, otherwise
 // start the default app.
 if (argv._.length > 0) {
-  process.on('uncaughtException', function() {
-    process.exit(1);
-  });
-
-  require(path.resolve(argv._[0]));
+  try {
+    require(path.resolve(argv._[0]));
+  } catch(e) {
+    if (e.code == 'MODULE_NOT_FOUND') {
+      console.error('Specified app is invalid');
+      process.exit(1);
+    } else {
+      throw e;
+    }
+  }
 } else {
   require('./default_app.js');
 }
