f3eef6cc2e29d66b5c05970cf2d1ea604b73fddc
Cheng Zhao <zcbenz@gmail.com>
2013-09-27 11:01:40 +0800

    Do not warn about 80 columns in coffeelint, it's not required.

--- script/coffeelint.py
+++ script/coffeelint.py
@@ -14,16 +14,17 @@ def main():
 
   coffeelint = os.path.join(SOURCE_ROOT, 'node_modules', 'coffeelint', 'bin',
                             'coffeelint')
+  settings = ['--quiet', '-f', os.path.join('script', 'coffeelint.json')]
   files = glob.glob('browser/api/lib/*.coffee') + \
           glob.glob('renderer/api/lib/*.coffee') + \
           glob.glob('common/api/lib/*.coffee') + \
           glob.glob('browser/atom/*.coffee')
 
   if sys.platform in ['win32', 'cygwin']:
-    subprocess.check_call(['node', coffeelint] + files,
+    subprocess.check_call(['node', coffeelint] + settings + files,
                           executable='C:/Program Files/nodejs/node.exe')
   else:
-    subprocess.check_call(['node', coffeelint] + files)
+    subprocess.check_call(['node', coffeelint] + settings + files)
 
 
 if __name__ == '__main__':
