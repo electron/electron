cb684c5c6eaf1c9ab2bfd4ada0859f1cc1f01ffa
Adam Roben <adam@roben.org>
2013-11-17 18:24:36 -0500

    Fix cpplint errors in inspectable_web_contents_view_mac.h

--- brightray/browser/inspectable_web_contents_view_mac.h
+++ brightray/browser/inspectable_web_contents_view_mac.h
@@ -1,9 +1,11 @@
 #ifndef BRIGHTRAY_BROWSER_INSPECTABLE_WEB_CONTENTS_VIEW_MAC_H_
 #define BRIGHTRAY_BROWSER_INSPECTABLE_WEB_CONTENTS_VIEW_MAC_H_
 
-#import "browser/inspectable_web_contents_view.h"
+#include "browser/inspectable_web_contents_view.h"
 
-#import "base/mac/scoped_nsobject.h"
+#include <string>
+
+#include "base/mac/scoped_nsobject.h"
 
 @class BRYInspectableWebContentsView;
 
@@ -12,20 +14,23 @@ namespace brightray {
 class InspectableWebContentsImpl;
 
 class InspectableWebContentsViewMac : public InspectableWebContentsView {
-public:
-  InspectableWebContentsViewMac(InspectableWebContentsImpl*);
-  
+ public:
+  explicit InspectableWebContentsViewMac(
+      InspectableWebContentsImpl* inspectable_web_contents_impl);
+
   virtual gfx::NativeView GetNativeView() const OVERRIDE;
   virtual void ShowDevTools() OVERRIDE;
   virtual void CloseDevTools() OVERRIDE;
   virtual bool SetDockSide(const std::string& side) OVERRIDE;
 
-  InspectableWebContentsImpl* inspectable_web_contents() { return inspectable_web_contents_; }
+  InspectableWebContentsImpl* inspectable_web_contents() {
+    return inspectable_web_contents_;
+  }
 
-private:
+ private:
   // Owns us.
   InspectableWebContentsImpl* inspectable_web_contents_;
-  
+
   base::scoped_nsobject<BRYInspectableWebContentsView> view_;
 
   DISALLOW_COPY_AND_ASSIGN(InspectableWebContentsViewMac);
