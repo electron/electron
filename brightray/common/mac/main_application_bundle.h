2c1e1b039dc0b311dbdfb0365b5126aae11e23d0
Adam Roben <adam@roben.org>
2013-11-17 18:57:25 -0500

    Fix cpplint errors in main_application_bundle.h

--- brightray/common/mac/main_application_bundle.h
+++ brightray/common/mac/main_application_bundle.h
@@ -9,12 +9,13 @@ class FilePath;
 
 namespace brightray {
 
-// The "main" application bundle is the outermost bundle for this logical application. E.g., if you
-// have MyApp.app and MyApp.app/Contents/Frameworks/MyApp Helper.app, the main application bundle is
-// MyApp.app, no matter which executable is currently running.
+// The "main" application bundle is the outermost bundle for this logical
+// application. E.g., if you have MyApp.app and
+// MyApp.app/Contents/Frameworks/MyApp Helper.app, the main application bundle
+// is MyApp.app, no matter which executable is currently running.
 NSBundle* MainApplicationBundle();
 base::FilePath MainApplicationBundlePath();
 
-}
+}  // namespace brightray
 
 #endif
