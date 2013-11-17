32f4862e56e911dd6be9d4cae1ac2de1c6b87755
Adam Roben <adam@roben.org>
2013-11-17 18:57:54 -0500

    Fix cpplint errors in main_delegate.h

--- brightray/common/main_delegate.h
+++ brightray/common/main_delegate.h
@@ -5,10 +5,11 @@
 #ifndef BRIGHTRAY_COMMON_MAIN_DELEGATE_H_
 #define BRIGHTRAY_COMMON_MAIN_DELEGATE_H_
 
+#include <vector>
+
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
 #include "content/public/app/content_main_delegate.h"
-#include <vector>
 
 namespace base {
 class FilePath;
@@ -19,21 +20,23 @@ namespace brightray {
 class ContentClient;
 
 class MainDelegate : public content::ContentMainDelegate {
-public:
+ public:
   MainDelegate();
   ~MainDelegate();
 
-protected:
-  // Subclasses can override this to provide their own ContentClient implementation.
+ protected:
+  // Subclasses can override this to provide their own ContentClient
+  // implementation.
   virtual scoped_ptr<ContentClient> CreateContentClient();
 
-  // Subclasses can override this to provide additional .pak files to be included in the ui::ResourceBundle.
+  // Subclasses can override this to provide additional .pak files to be
+  // included in the ui::ResourceBundle.
   virtual void AddPakPaths(std::vector<base::FilePath>* pak_paths) {}
 
   virtual bool BasicStartupComplete(int* exit_code) OVERRIDE;
   virtual void PreSandboxStartup() OVERRIDE;
 
-private:
+ private:
   void InitializeResourceBundle();
 #if defined(OS_MACOSX)
   static base::FilePath GetResourcesPakFilePath();
