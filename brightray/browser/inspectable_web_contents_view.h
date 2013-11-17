ea689b31a4c558736d8a44ab2b6b380d660041dd
Adam Roben <adam@roben.org>
2013-11-17 18:23:13 -0500

    Fix cpplint errors in inspectable_web_contents_view.h

--- brightray/browser/inspectable_web_contents_view.h
+++ brightray/browser/inspectable_web_contents_view.h
@@ -1,12 +1,14 @@
 #ifndef BRIGHTRAY_BROWSER_INSPECTABLE_WEB_CONTENTS_VIEW_H_
 #define BRIGHTRAY_BROWSER_INSPECTABLE_WEB_CONTENTS_VIEW_H_
 
+#include <string>
+
 #include "ui/gfx/native_widget_types.h"
 
 namespace brightray {
 
 class InspectableWebContentsView {
-public:
+ public:
   virtual ~InspectableWebContentsView() {}
 
   virtual gfx::NativeView GetNativeView() const = 0;
