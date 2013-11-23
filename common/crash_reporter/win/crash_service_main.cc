4bab284f2e29e8750dfd6c96f51b2ce6e8f6650a
Cheng Zhao <zcbenz@gmail.com>
2013-11-23 20:26:47 +0800

    Add crash_service on Windows.

---
+++ common/crash_reporter/win/crash_service_main.cc
@@ -0,0 +1,13 @@
+// Copyright (c) 2013 GitHub, Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "common/crash_reporter/win/crash_service_main.h"
+
+namespace crash_service {
+
+int Main() {
+  return 0;
+}
+
+}  // namespace crash_service
