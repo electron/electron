c56480fd8905332e54649dac0ade95c825e8ba23
Cheng Zhao <zcbenz@gmail.com>
2014-02-20 18:20:29 +0800

    Update libchromiumcontent to use the thin version.

--- script/lib/config.py
+++ script/lib/config.py
@@ -2,4 +2,4 @@
 
 NODE_VERSION = 'v0.11.10'
 BASE_URL = 'https://gh-contractor-zcbenz.s3.amazonaws.com/libchromiumcontent'
-LIBCHROMIUMCONTENT_COMMIT = 'b27290717c08f8c6a58067d3c3725d68b4e6a2e5'
+LIBCHROMIUMCONTENT_COMMIT = 'fe05f53f3080889ced2696b2741d93953e654b49'
