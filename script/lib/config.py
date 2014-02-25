30b3657c0e017d34e4121a96554ec6d5184ec1d7
Cheng Zhao <zcbenz@gmail.com>
2014-02-25 08:53:17 +0800

    Update libchromiumcontent to 1df8e7cdac8aa74c91c19ae0691ce512d560ab3e.

--- script/lib/config.py
+++ script/lib/config.py
@@ -2,4 +2,4 @@
 
 NODE_VERSION = 'v0.11.10'
 BASE_URL = 'https://gh-contractor-zcbenz.s3.amazonaws.com/libchromiumcontent'
-LIBCHROMIUMCONTENT_COMMIT = 'fe05f53f3080889ced2696b2741d93953e654b49'
+LIBCHROMIUMCONTENT_COMMIT = '1df8e7cdac8aa74c91c19ae0691ce512d560ab3e'
