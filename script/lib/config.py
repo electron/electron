eca911a1b1623368f991dbf47002c0b59abc15db
Cheng Zhao <zcbenz@gmail.com>
2014-03-01 22:07:00 +0800

    Update libchromiumcontent: Suppress CFAllocator warning.

--- script/lib/config.py
+++ script/lib/config.py
@@ -2,4 +2,4 @@
 
 NODE_VERSION = 'v0.11.10'
 BASE_URL = 'https://gh-contractor-zcbenz.s3.amazonaws.com/libchromiumcontent'
-LIBCHROMIUMCONTENT_COMMIT = '1df8e7cdac8aa74c91c19ae0691ce512d560ab3e'
+LIBCHROMIUMCONTENT_COMMIT = 'aa4874a6bcc51fdd87ca7ae0928514ce83645988'
