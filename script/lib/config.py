1965a5ee508a5a8e2b35145c69f98908629fa290
Cheng Zhao <zcbenz@gmail.com>
2014-07-03 20:26:05 +0800

    Upgrade libchromiumcontent to 93f4be4bc30f98a445e4db2a2759057f0f01de01.

--- script/lib/config.py
+++ script/lib/config.py
@@ -5,7 +5,7 @@ import sys
 
 NODE_VERSION = 'v0.11.13'
 BASE_URL = 'https://gh-contractor-zcbenz.s3.amazonaws.com/libchromiumcontent'
-LIBCHROMIUMCONTENT_COMMIT = 'e6aebd2156034b12e869d8905b22c088b2dc42ea'
+LIBCHROMIUMCONTENT_COMMIT = '93f4be4bc30f98a445e4db2a2759057f0f01de01'
 
 ARCH = {
     'cygwin': '32bit',
