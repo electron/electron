e443898941dcdf9c017de0cbb5f540b83ba7d756
Chris Soh <soh1988@gmail.com>
2014-08-22 09:45:08 +0800

    Cleaning Up
    Sorry did not notice

--- atom/browser/native_window.cc
+++ atom/browser/native_window.cc
@@ -142,8 +142,8 @@ void NativeWindow::InitFromOptions(const mate::Dictionary& options) {
     Center();
   }
   int min_height = 0, min_width = 0;
--  if (options.Get(switches::kMinHeight, &min_height) |
--      options.Get(switches::kMinWidth, &min_width)) {
+- if (options.Get(switches::kMinHeight, &min_height) |
+-     options.Get(switches::kMinWidth, &min_width)) {
     SetMinimumSize(gfx::Size(min_width, min_height));
   }
   int max_height = -1, max_width = -1;
