ed68bd18b88f349a7348006a2e14cdddbc993da7
Cheng Zhao <zcbenz@gmail.com>
2014-09-01 13:33:03 +0800

    Upgrade libchromiumcontent to Chrome 37.

--- script/lib/config.py
+++ script/lib/config.py
@@ -5,7 +5,7 @@ import sys
 
 NODE_VERSION = 'v0.11.13'
 BASE_URL = 'https://gh-contractor-zcbenz.s3.amazonaws.com/libchromiumcontent'
-LIBCHROMIUMCONTENT_COMMIT = 'afb4570ceee2ad10f3caf5a81335a2ee11ec68a5'
+LIBCHROMIUMCONTENT_COMMIT = 'ea1a7e85a3de1878e5656110c76f4d2d8af41c6e'
 
 ARCH = {
     'cygwin': '32bit',
