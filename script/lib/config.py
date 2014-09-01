d874ba80db5bedb67b0b50cea431321c77b10f5d
Cheng Zhao <zcbenz@gmail.com>
2014-09-01 16:08:08 +0800

    Upgrade libchromiumcontent for gin headers.

--- script/lib/config.py
+++ script/lib/config.py
@@ -5,7 +5,7 @@ import sys
 
 NODE_VERSION = 'v0.11.13'
 BASE_URL = 'https://gh-contractor-zcbenz.s3.amazonaws.com/libchromiumcontent'
-LIBCHROMIUMCONTENT_COMMIT = 'ea1a7e85a3de1878e5656110c76f4d2d8af41c6e'
+LIBCHROMIUMCONTENT_COMMIT = '2cf80c1743e370c12eb7bf078eb425f3cc355383'
 
 ARCH = {
     'cygwin': '32bit',
