9e87037d34e158ce011c41bf0266c9bcee67abec
Cheng Zhao <zcbenz@gmail.com>
2014-10-13 10:09:58 +0800

    Upgrade to libchromiumcontent 44c71d8

--- script/lib/config.py
+++ script/lib/config.py
@@ -4,7 +4,7 @@ import platform
 import sys
 
 BASE_URL = 'https://gh-contractor-zcbenz.s3.amazonaws.com/libchromiumcontent'
-LIBCHROMIUMCONTENT_COMMIT = '70fe9677634a8f13fd5c67b8c83c056f2d9c8d46'
+LIBCHROMIUMCONTENT_COMMIT = '44c71d88d9c098ece5dbf3e1fcc93ab87d8193cd'
 
 ARCH = {
     'cygwin': '32bit',
