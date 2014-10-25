e7825da0f8467717aac9857bbc046d946aa2ce66
Cheng Zhao <zcbenz@gmail.com>
2014-10-25 18:53:24 +0800

    Upgrade libchromiumcontent to discard iframe security settings

--- script/lib/config.py
+++ script/lib/config.py
@@ -4,7 +4,7 @@ import platform
 import sys
 
 BASE_URL = 'https://gh-contractor-zcbenz.s3.amazonaws.com/libchromiumcontent'
-LIBCHROMIUMCONTENT_COMMIT = '56984fa0e4c3c745652510f342c0fb2724d846c2'
+LIBCHROMIUMCONTENT_COMMIT = '2dfdf169b582e3f051e1fec3dd7df2bc179e1aa6'
 
 ARCH = {
     'cygwin': '32bit',
