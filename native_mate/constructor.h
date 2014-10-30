b1f7cfa9da1f53158a4992428b40a780c9617632
Cheng Zhao <zcbenz@gmail.com>
2014-10-30 21:31:53 +0800

    Don't throw error in Constructor

--- native_mate/constructor.h
+++ native_mate/constructor.h
@@ -154,8 +154,6 @@ class Constructor {
     Wrappable* object = internal::InvokeFactory(args, factory);
     if (object)
       object->Wrap(isolate, args->GetThis());
-    else
-      args->ThrowError();
 
     MATE_METHOD_RETURN_UNDEFINED();
   }
