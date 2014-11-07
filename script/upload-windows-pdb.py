caa0634df88949985bd1c18ec99d8a07960c4407
Cheng Zhao <zcbenz@gmail.com>
2014-11-07 20:51:25 +0800

    Upload symbols to S3

--- script/upload-windows-pdb.py
+++ script/upload-windows-pdb.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import os
+import glob
 
-from lib.util import execute, rm_rf, safe_mkdir
+from lib.util import execute, rm_rf, safe_mkdir, s3put, s3_config
 
 
 SOURCE_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
@@ -21,9 +22,17 @@ def main():
   for pdb in PDB_LIST:
     run_symstore(pdb, SYMBOLS_DIR, 'AtomShell')
 
+  bucket, access_key, secret_key = s3_config()
+  files = glob.glob(SYMBOLS_DIR + '/*.pdb/*/*.pdb')
+  upload_symbols(bucket, access_key, secret_key, files)
+
 
 def run_symstore(pdb, dest, product):
   execute(['symstore', 'add', '/r', '/f', pdb, '/s', dest, '/t', product])
+
+
+def upload_symbols(bucket, access_key, secret_key, files):
+  s3put(bucket, access_key, secret_key, SYMBOLS_DIR, 'atom-shell/symbols', files)
 
 
 if __name__ == '__main__':
