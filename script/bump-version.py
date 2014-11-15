9c8b3e3c2ed1c6ec22455f878501f6b730981f5f
Cheng Zhao <zcbenz@gmail.com>
2014-11-15 20:37:52 +0800

    No . in commit message

--- script/bump-version.py
+++ script/bump-version.py
@@ -114,7 +114,7 @@ def update_info_plist(version):
 
 
 def tag_version(version):
-  execute(['git', 'commit', '-a', '-m', 'Bump v{0}.'.format(version)])
+  execute(['git', 'commit', '-a', '-m', 'Bump v{0}'.format(version)])
   execute(['git', 'tag', 'v{0}'.format(version)])
 
 
