654d21100ff2908af6c44e2d2d71ba92b1e96722
Cheng Zhao <zcbenz@gmail.com>
2014-12-12 16:42:07 -0800

    Upgrade libchromiumcontent to remove dom storage quota
    Closes #897.

--- script/lib/config.py
+++ script/lib/config.py
@@ -4,7 +4,7 @@ import platform
 import sys
 
 BASE_URL = 'https://gh-contractor-zcbenz.s3.amazonaws.com/libchromiumcontent'
-LIBCHROMIUMCONTENT_COMMIT = 'aa87035cc012ce0d533bb56b947bca81a6e71b82'
+LIBCHROMIUMCONTENT_COMMIT = 'e375124044f9044ac88076eba0cd17361ee0997c'
 
 ARCH = {
     'cygwin': '32bit',
