895ccf69a7fb3ab3601bbb394a0d3ffcf20ba66b
Cheng Zhao <zcbenz@gmail.com>
2015-01-04 22:45:51 -0800

    Avoid using app API in renderer process for #907

--- atom/common/api/atom_api_screen.cc
+++ atom/common/api/atom_api_screen.cc
@@ -2,41 +2,21 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
-#include "atom/browser/browser.h"
 #include "atom/common/native_mate_converters/gfx_converter.h"
 #include "atom/common/node_includes.h"
 
 namespace {
 
-const char* kRequireAppReadyMessage =
-    "APIs of \"screen\" module can only be used after the \"ready\" event of "
-    "\"app\" module gets emitted.";
-
-gfx::Point GetCursorScreenPoint(mate::Arguments* args) {
-  if (!atom::Browser::Get()->is_ready()) {
-    args->ThrowError(kRequireAppReadyMessage);
-    return gfx::Point();
-  }
-
-  gfx::Screen* screen = gfx::Screen::GetNativeScreen();
-  return screen->GetCursorScreenPoint();
-}
-
-gfx::Display GetPrimaryDisplay(mate::Arguments* args) {
-  if (!atom::Browser::Get()->is_ready()) {
-    args->ThrowError(kRequireAppReadyMessage);
-    return gfx::Display();
-  }
-
-  gfx::Screen* screen = gfx::Screen::GetNativeScreen();
-  return screen->GetPrimaryDisplay();
-}
-
 void Initialize(v8::Handle<v8::Object> exports, v8::Handle<v8::Value> unused,
                 v8::Handle<v8::Context> context, void* priv) {
+  gfx::Screen* screen = gfx::Screen::GetNativeScreen();
   mate::Dictionary dict(context->GetIsolate(), exports);
-  dict.SetMethod("getCursorScreenPoint", &GetCursorScreenPoint);
-  dict.SetMethod("getPrimaryDisplay", &GetPrimaryDisplay);
+  dict.SetMethod("getCursorScreenPoint",
+                 base::Bind(&gfx::Screen::GetCursorScreenPoint,
+                            base::Unretained(screen)));
+  dict.SetMethod("getPrimaryDisplay",
+                 base::Bind(&gfx::Screen::GetPrimaryDisplay,
+                            base::Unretained(screen)));
 }
 
 }  // namespace
