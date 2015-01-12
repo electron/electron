7ab5db1adb11c41e309a6c2885ec3e65c352fbda
Cheng Zhao <zcbenz@gmail.com>
2015-01-12 14:10:22 -0800

    Use http in framework download url, fixes #998

--- script/update-external-binaries.py
+++ script/update-external-binaries.py
@@ -9,7 +9,7 @@ from lib.util import safe_mkdir, rm_rf, extract_zip, tempdir, download
 
 VERSION = 'v0.3.0'
 SOURCE_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-FRAMEWORKS_URL = 'https://github.com/atom/atom-shell-frameworks/releases' \
+FRAMEWORKS_URL = 'http://github.com/atom/atom-shell-frameworks/releases' \
                  '/download/' + VERSION
 
 
