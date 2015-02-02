b9fced9f6b9c3488b6da11985b9cda7cc59da4cf
Max Ogden <max@maxogden.com>
2015-02-01 17:21:54 -0800

    0.20.1

--- npm/install.js
+++ npm/install.js
@@ -12,7 +12,7 @@ if ('win32' === platform) {
   // 64-bit is not available under windows.
   arch = 'ia32';
 }
-var version = '0.20.0'
+var version = '0.20.1'
 var name = 'atom-shell-v'+version+'-'+platform+'-'+arch+'.zip'
 var url = 'https://github.com/atom/atom-shell/releases/download/v'+version+'/atom-shell-v'+version+'-'+platform+'-'+arch+'.zip'
 
