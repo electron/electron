414c91a6377ffab37ea162eb9a9d16fdfb72457c
Cheng Zhao <zcbenz@gmail.com>
2015-03-23 09:51:50 +0800

    Cleanup #1275

--- tools/win/register_msdia80_dll.js
+++ tools/win/register_msdia80_dll.js
@@ -2,16 +2,11 @@ var fs = require('fs');
 var path = require('path');
 var runas = require('runas');
 
-/* FIXME: C:\\Program Files\\ should comes from env variable
- And for 32 bit msdia80 is should be placed at %ProgramFiles(x86)% */
 var source = path.resolve(__dirname, '..', '..', 'vendor', 'breakpad', 'msdia80.dll');
 var target = 'C:\\Program Files\\Common Files\\Microsoft Shared\\VC\\msdia80.dll';
 if (fs.existsSync(target))
   return;
 
-runas('cmd', ['/K',
-        'copy', source, target,
-        '&', 'regsvr32', '/s', target,
-        '&', 'exit'],
-        {admin:true}
-);
+runas('cmd',
+      ['/K', 'copy', source, target, '&', 'regsvr32', '/s', target, '&', 'exit'],
+      {admin: true});
