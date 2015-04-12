cb161234e50ff37573075195c86c36a4d13e1ef4
Mathias Buus <mathiasbuus@gmail.com>
2015-04-12 01:36:42 -0700

    v0.23.0

--- npm/install.js
+++ npm/install.js
@@ -8,11 +8,7 @@ var fs = require('fs')
 
 var platform = os.platform()
 var arch = os.arch()
-if (platform === 'win32') {
-  // 64-bit is not available under windows.
-  arch = 'ia32'
-}
-var version = '0.22.3'
+var version = '0.23.0'
 var filename = 'atom-shell-v' + version + '-' + platform + '-' + arch + '.zip'
 var url = 'https://github.com/atom/atom-shell/releases/download/v' + version + '/atom-shell-v' + version + '-' + platform + '-' + arch + '.zip'
 
