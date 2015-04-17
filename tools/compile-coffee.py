e4415f00212de791b669595368f4b7814acee3be
Andy Dill <andy.dill@gmail.com>
2015-04-17 09:33:00 -0700

    compile-coffee.py searches for node even on non-windows systems

--- tools/compile-coffee.py
+++ tools/compile-coffee.py
@@ -10,6 +10,11 @@ WINDOWS_NODE_PATHs = [
   'C:/Program Files (x86)/nodejs',
   'C:/Program Files/nodejs',
 ] + os.environ['PATH'].split(os.pathsep)
+NIX_NODE_PATHs = [
+  '/usr/local/bin',
+  '/usr/bin',
+  '/bin',
+] + os.environ['PATH'].split(os.pathsep)
 
 
 def main():
@@ -18,20 +23,24 @@ def main():
 
   coffee = os.path.join(SOURCE_ROOT, 'node_modules', 'coffee-script', 'bin',
                         'coffee')
+
+  node = 'node'
   if sys.platform in ['win32', 'cygwin']:
-    node = find_node()
-    if not node:
-      print 'Node.js not found in PATH for building atom-shell'
-      return 1
-    subprocess.check_call(['node', coffee, '-c', '-o', output_dir, input_file],
-                          executable=node)
+    node = find_node(WINDOWS_NODE_PATHs, 'node.exe')
   else:
-    subprocess.check_call(['node', coffee, '-c', '-o', output_dir, input_file])
+    node = find_node(NIX_NODE_PATHs, 'node')
+
+  if not node:
+    print 'Node.js not found in PATH for building electron'
+    return 1
+
+  subprocess.check_call(['node', coffee, '-c', '-o', output_dir, input_file],
+                        executable=node)
 
 
-def find_node():
-  for path in WINDOWS_NODE_PATHs:
-    full_path = os.path.join(path, 'node.exe')
+def find_node(paths, target):
+  for path in paths:
+    full_path = os.path.join(path, target)
     if os.path.exists(full_path):
       return full_path
   return None
