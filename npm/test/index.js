3c8af7564b5b792c63f822f8a40408d012ac86ac
RnbWd <dwisner6@gmail.com>
2015-05-06 17:50:49 -0700

    fs.existsSync() is depreciated in io.js

--- npm/test/index.js
+++ npm/test/index.js
@@ -1,8 +1,8 @@
 var tape = require('tape')
 var electron = require('../')
-var fs = require('fs')
+var pathExists = require('path-exists')
 
 tape('has binary', function(t) {
-  t.ok(fs.existsSync(electron), 'electron was downloaded')
+  t.ok(pathExists.sync(electron), 'electron was downloaded')
   t.end()
 })
