7864197291c0957f64f472f1671a1b8ae064b797
RnbWd <dwisner6@gmail.com>
2015-05-06 21:44:10 -0700

    added cache

--- npm/install.js
+++ npm/install.js
@@ -2,10 +2,12 @@
 
 var os = require('os')
 var path = require('path')
+var pathExists = require('path-exists')
+var mkdir = require('mkdirp')
 var nugget = require('nugget')
 var extract = require('extract-zip')
 var fs = require('fs')
-
+var getHomePath = require('home-path')()
 var platform = os.platform()
 var arch = os.arch()
 var version = '0.25.2'
@@ -22,12 +24,28 @@ var paths = {
   win32: path.join(__dirname, './dist/electron.exe')
 }
 
+var cache = {
+  darwin: path.join(getHomePath, './.electron'),
+  linux: path.join(getHomePath, './.electron'),
+  win32: path.join(getHomePath, './.electron')
+}
+
 if (!paths[platform]) throw new Error('Unknown platform: ' + platform)
 
-nugget(url, {target: filename, dir: __dirname, resume: true, verbose: true}, function (err) {
+// use cache if possible
+if (pathExists.sync(path.join(cache[platform], filename))) {
+  extractFile()
+} else {
+  mkdir(cache[platform], function(err) {
+    if (err) return onerror(err)
+    nugget(url, {target: filename, dir: cache[platform], resume: true, verbose: true}, extractFile)
+  })
+}
+
+function extractFile (err) {
   if (err) return onerror(err)
   fs.writeFileSync(path.join(__dirname, 'path.txt'), paths[platform])
-  extract(path.join(__dirname, filename), {dir: path.join(__dirname, 'dist')}, function (err) {
+  extract(path.join(cache[platform], filename), {dir: path.join(__dirname, 'dist')}, function (err) {
     if (err) return onerror(err)
   })
-})
+}
