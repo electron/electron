e2fc1a0fb1bbcfb34e44492ab5646da21bbe2a5b
Max Ogden <max@maxogden.com>
2015-05-10 13:54:06 -0700

    refactor to use electron-download

--- npm/install.js
+++ npm/install.js
@@ -1,18 +1,15 @@
 #!/usr/bin/env node
 
+// maintainer note - update this manually when doing new releases:
+var version = '0.25.3'
+
+var fs = require('fs')
 var os = require('os')
 var path = require('path')
-var pathExists = require('path-exists')
-var mkdir = require('mkdirp')
-var nugget = require('nugget')
 var extract = require('extract-zip')
-var fs = require('fs')
-var getHomePath = require('home-path')()
+var download = require('electron-download')
+
 var platform = os.platform()
-var arch = os.arch()
-var version = '0.25.3'
-var filename = 'electron-v' + version + '-' + platform + '-' + arch + '.zip'
-var url = 'https://github.com/atom/electron/releases/download/v' + version + '/electron-v' + version + '-' + platform + '-' + arch + '.zip'
 
 function onerror (err) {
   throw err
@@ -24,24 +21,18 @@ var paths = {
   win32: path.join(__dirname, './dist/electron.exe')
 }
 
-var cache = path.join(getHomePath, './.electron')
-
 if (!paths[platform]) throw new Error('Unknown platform: ' + platform)
 
-// use cache if possible
-if (pathExists.sync(path.join(cache, filename))) {
-  extractFile()
-} else {
-  mkdir(cache, function(err) {
-    if (err) return onerror(err)
-    nugget(url, {target: filename, dir: cache, resume: true, verbose: true}, extractFile)
-  })
-}
+// downloads if not cached
+download({version: version}, extractFile)
 
-function extractFile (err) {
+// unzips and makes path.txt point at the correct executable
+function extractFile (err, zipPath) {
   if (err) return onerror(err)
-  fs.writeFileSync(path.join(__dirname, 'path.txt'), paths[platform])
-  extract(path.join(cache, filename), {dir: path.join(__dirname, 'dist')}, function (err) {
+  fs.writeFile(path.join(__dirname, 'path.txt'), paths[platform], function (err) {
     if (err) return onerror(err)
+    extract(zipPath, {dir: path.join(__dirname, 'dist')}, function (err) {
+      if (err) return onerror(err)
+    })
   })
 }
