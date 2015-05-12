add7f8a4aae7199d942b1f85a4f9a3b2465ef035
deepak1556 <hop2deep@gmail.com>
2015-05-12 18:03:00 +0530

    add test

--- atom/browser/atom_browser_client.cc
+++ atom/browser/atom_browser_client.cc
@@ -20,7 +20,6 @@
 #include "chrome/browser/renderer_host/pepper/chrome_browser_pepper_host_factory.h"
 #include "chrome/browser/speech/tts_message_filter.h"
 #include "content/public/browser/browser_ppapi_host.h"
-#include "content/public/browser/quota_permission_context.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/resource_dispatcher_host.h"
