e95ee4775e2063602b05927e2f8ca28d7fc4b8ba
Cheng Zhao <zcbenz@gmail.com>
2015-07-03 13:59:02 +0800

    Ignore Windows errors in clean.py

--- script/clean.py
+++ script/clean.py
@@ -11,11 +11,14 @@ SOURCE_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 def main():
   os.chdir(SOURCE_ROOT)
-  rm_rf('node_modules')
-  rm_rf('dist')
-  rm_rf('out')
-  rm_rf('spec/node_modules')
-  rm_rf('vendor/brightray/vendor/download/libchromiumcontent')
+  try:
+    rm_rf('node_modules')
+    rm_rf('dist')
+    rm_rf('out')
+    rm_rf('spec/node_modules')
+    rm_rf('vendor/brightray/vendor/download/libchromiumcontent')
+  except WindowsError:  # pylint: disable=E0602
+    pass
 
 
 if __name__ == '__main__':
