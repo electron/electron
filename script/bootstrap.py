26525d6b0fbde4d6c6432b713af3d9154d8c757e
Cheng Zhao <zcbenz@gmail.com>
2015-07-03 20:56:49 +0800

    Set openssl_fips in config.gypi

--- script/bootstrap.py
+++ script/bootstrap.py
@@ -174,7 +174,7 @@ def create_chrome_version_h():
 def touch_config_gypi():
   config_gypi = os.path.join(SOURCE_ROOT, 'vendor', 'node', 'config.gypi')
   with open(config_gypi, 'w+') as f:
-    content = '\n{}'
+    content = "{\n  'variables': {\n    'openssl_fips': ''\n  }\n}"
     if f.read() != content:
       f.write(content)
 
