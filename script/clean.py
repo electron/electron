6b7d3a070ae3a45138384711bd1026b3c90233f9
Cheng Zhao <zcbenz@gmail.com>
2015-07-03 13:52:13 +0800

    Clean before and after cibuild
    Our CI machine is out of disk space

--- script/clean.py
+++ script/clean.py
@@ -12,8 +12,10 @@ SOURCE_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 def main():
   os.chdir(SOURCE_ROOT)
   rm_rf('node_modules')
+  rm_rf('dist')
   rm_rf('out')
   rm_rf('spec/node_modules')
+  rm_rf('vendor/brightray/vendor/download/libchromiumcontent')
 
 
 if __name__ == '__main__':
