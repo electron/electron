76b7cd0908d23a6a95b4d18d7782c9f46c0c0fec
Robert Hencke <robert.hencke@gmail.com>
2015-09-19 17:11:29 -0400

    Fix small typo in debug message.

--- atom/browser/web_contents_preferences.cc
+++ atom/browser/web_contents_preferences.cc
@@ -94,7 +94,7 @@ void WebContentsPreferences::AppendExtraCommandLineSwitches(
     if (base::FilePath(preload).IsAbsolute())
       command_line->AppendSwitchNative(switches::kPreloadScript, preload);
     else
-      LOG(ERROR) << "preload script must have abosulute path.";
+      LOG(ERROR) << "preload script must have absolute path.";
   } else if (web_preferences.GetString(switches::kPreloadUrl, &preload)) {
     // Translate to file path if there is "preload-url" option.
     base::FilePath preload_path;
