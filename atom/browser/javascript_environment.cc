664dcabfa0b245106cb3eca1368441442c5da7a8
Shawn K <skhameneh@gmail.com>
2015-11-12 01:00:41 -0800

    Add --js-flags support for main thread.
    Allow use of flags that must be set before V8 is initialized, such as
    "--harmont_proxies", e.g. --js-flags="--harmony_proxies
    --harmony_collections"

--- atom/browser/javascript_environment.cc
+++ atom/browser/javascript_environment.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
+#include <string>
+
 #include "atom/browser/javascript_environment.h"
 
 #include "base/command_line.h"
