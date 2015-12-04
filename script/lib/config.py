4279d13fbd2204376a2a3e7f3e992c8ebd5ae240
Gabriel Handford <gabrielh@gmail.com>
2015-12-04 12:06:36 -0800

    Use https for downloading libchromiumcontent

--- script/lib/config.py
+++ script/lib/config.py
@@ -7,7 +7,7 @@ import sys
 
 
 BASE_URL = os.getenv('LIBCHROMIUMCONTENT_MIRROR') or \
-    'http://gh-contractor-zcbenz.s3.amazonaws.com/libchromiumcontent'
+    'https://s3.amazonaws.com/gh-contractor-zcbenz/libchromiumcontent'
 LIBCHROMIUMCONTENT_COMMIT = '17a4337f7948a45b5ea4b8f391df152ba8db5979'
 
 PLATFORM = {
