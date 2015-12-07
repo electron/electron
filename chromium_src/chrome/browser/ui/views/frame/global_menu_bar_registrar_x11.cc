9a0cecf943a0b2985ea6508539245eb911f0f1ab
Paul Betts <paul@paulbetts.org>
2015-12-07 14:29:11 -0800

    Rig GlobalMenuBarRegistrarX11 for Atom

--- chromium_src/chrome/browser/ui/views/frame/global_menu_bar_registrar_x11.cc
+++ chromium_src/chrome/browser/ui/views/frame/global_menu_bar_registrar_x11.cc
@@ -70,7 +70,7 @@ GlobalMenuBarRegistrarX11::~GlobalMenuBarRegistrarX11() {
 
 void GlobalMenuBarRegistrarX11::RegisterXID(unsigned long xid) {
   DCHECK(registrar_proxy_);
-  std::string path = GlobalMenuBarX11::GetPathForWindow(xid);
+  std::string path = atom::GlobalMenuBarX11::GetPathForWindow(xid);
 
   ANNOTATE_SCOPED_MEMORY_LEAK; // http://crbug.com/314087
   // TODO(erg): The mozilla implementation goes to a lot of callback trouble
@@ -91,7 +91,7 @@ void GlobalMenuBarRegistrarX11::RegisterXID(unsigned long xid) {
 
 void GlobalMenuBarRegistrarX11::UnregisterXID(unsigned long xid) {
   DCHECK(registrar_proxy_);
-  std::string path = GlobalMenuBarX11::GetPathForWindow(xid);
+  std::string path = atom::GlobalMenuBarX11::GetPathForWindow(xid);
 
   ANNOTATE_SCOPED_MEMORY_LEAK; // http://crbug.com/314087
   // TODO(erg): The mozilla implementation goes to a lot of callback trouble
