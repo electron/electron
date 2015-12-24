6f81d1e29fca788f0f92c9d71a79c07d75d7d9ae
Cheng Zhao <zcbenz@gmail.com>
2015-12-24 22:20:47 +0800

    Expose NotificationPresenter to public

--- brightray/browser/platform_notification_service.cc
+++ brightray/browser/platform_notification_service.cc
@@ -2,68 +2,48 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE-CHROMIUM file.
 
-#include "browser/platform_notification_service_impl.h"
+#include "browser/platform_notification_service.h"
 
+#include "browser/browser_client.h"
 #include "browser/notification_presenter.h"
-
 #include "content/public/browser/desktop_notification_delegate.h"
-
-#if defined(OS_WIN)
-#include "base/win/windows_version.h"
-#endif
 
 namespace brightray {
 
-// static
-PlatformNotificationServiceImpl*
-PlatformNotificationServiceImpl::GetInstance() {
-  return base::Singleton<PlatformNotificationServiceImpl>::get();
+PlatformNotificationService::PlatformNotificationService(
+    BrowserClient* browser_client)
+    : browser_client_(browser_client) {
 }
 
-PlatformNotificationServiceImpl::PlatformNotificationServiceImpl() {}
-PlatformNotificationServiceImpl::~PlatformNotificationServiceImpl() {}
+PlatformNotificationService::~PlatformNotificationService() {}
 
-NotificationPresenter* PlatformNotificationServiceImpl::notification_presenter() {
-  #if defined(OS_WIN)
-  // Bail out if on Windows 7 or even lower, no operating will follow
-  if (base::win::GetVersion() < base::win::VERSION_WIN8)
-    return nullptr;
-  #endif
-
-  if (!notification_presenter_) {
-    // Create a new presenter if on OS X, Linux, or Windows 8+
-    notification_presenter_.reset(NotificationPresenter::Create());
-  }
-  return notification_presenter_.get();
-}
-
-blink::WebNotificationPermission PlatformNotificationServiceImpl::CheckPermissionOnUIThread(
+blink::WebNotificationPermission PlatformNotificationService::CheckPermissionOnUIThread(
     content::BrowserContext* browser_context,
     const GURL& origin,
     int render_process_id) {
   return blink::WebNotificationPermissionAllowed;
 }
 
-blink::WebNotificationPermission PlatformNotificationServiceImpl::CheckPermissionOnIOThread(
+blink::WebNotificationPermission PlatformNotificationService::CheckPermissionOnIOThread(
     content::ResourceContext* resource_context,
     const GURL& origin,
     int render_process_id) {
   return blink::WebNotificationPermissionAllowed;
 }
 
-void PlatformNotificationServiceImpl::DisplayNotification(
+void PlatformNotificationService::DisplayNotification(
     content::BrowserContext* browser_context,
     const GURL& origin,
     const SkBitmap& icon,
     const content::PlatformNotificationData& notification_data,
     scoped_ptr<content::DesktopNotificationDelegate> delegate,
     base::Closure* cancel_callback) {
-  auto presenter = notification_presenter();
+  auto presenter = browser_client_->GetNotificationPresenter();
   if (presenter)
     presenter->ShowNotification(notification_data, icon, delegate.Pass(), cancel_callback);
 }
 
-void PlatformNotificationServiceImpl::DisplayPersistentNotification(
+void PlatformNotificationService::DisplayPersistentNotification(
     content::BrowserContext* browser_context,
     int64_t service_worker_registration_id,
     const GURL& origin,
@@ -71,12 +51,12 @@ void PlatformNotificationServiceImpl::DisplayPersistentNotification(
     const content::PlatformNotificationData& notification_data) {
 }
 
-void PlatformNotificationServiceImpl::ClosePersistentNotification(
+void PlatformNotificationService::ClosePersistentNotification(
     content::BrowserContext* browser_context,
     int64_t persistent_notification_id) {
 }
 
-bool PlatformNotificationServiceImpl::GetDisplayedPersistentNotifications(
+bool PlatformNotificationService::GetDisplayedPersistentNotifications(
     content::BrowserContext* browser_context,
     std::set<std::string>* displayed_notifications) {
   return false;
