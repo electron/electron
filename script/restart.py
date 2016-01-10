54030833b4ee17ebbb91701dddfc57245fc6e432
leethomas <leet944@gmail.com>
2016-01-09 21:36:06 -0800

    add basic restart script

---
+++ script/restart.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import os
+import platform
+import subprocess
+import sys
+import time
+
+def main():
+  pid         = int(sys.argv[1])
+  timeout     = 5000 #int(sys.argv[2])
+  system_os   = platform.system()
+  startup_cmd = sys.argv[3]
+
+  if system_os == 'Darwin' or system_os == 'Linux':
+    if wait_for_exit_unix(pid, timeout):
+      print(startup_cmd)
+      subprocess.Popen(startup_cmd, close_fds=True)
+      return 0
+    else:
+      print('Timeout exceeded.')
+      return 1
+  elif system_os == 'Windows':
+    pass
+  else:
+    return 1
+
+
+def wait_for_exit_unix(pid, timeout):
+  wait_time = timeout
+  while(wait_time > 0):
+    try:
+      os.kill(pid, 0)
+    except OSError:
+      return True
+    time.sleep(1)
+    wait_time -= 1000
+
+  return False
+
+def wait_for_exit_windows(timeout):
+  time.sleep(1)
+  return False
+
+if __name__ == '__main__':
+  sys.exit(main())
