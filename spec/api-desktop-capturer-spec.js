fb3ab9e1f1f32bdbd40c8bce56a3818a0c311cfe
Kevin Sawicki <kevinsawicki@gmail.com>
2016-01-19 10:54:12 -0800

    Use const and remove extra returns

--- spec/api-desktop-capturer-spec.js
+++ spec/api-desktop-capturer-spec.js
@@ -1,17 +1,14 @@
-var assert, desktopCapturer;
-
-assert = require('assert');
-
-desktopCapturer = require('electron').desktopCapturer;
+const assert = require('assert');
+const desktopCapturer = require('electron').desktopCapturer;
 
 describe('desktopCapturer', function() {
-  return it('should return a non-empty array of sources', function(done) {
-    return desktopCapturer.getSources({
+  it('should return a non-empty array of sources', function(done) {
+    desktopCapturer.getSources({
       types: ['window', 'screen']
     }, function(error, sources) {
       assert.equal(error, null);
       assert.notEqual(sources.length, 0);
-      return done();
+      done();
     });
   });
 });
