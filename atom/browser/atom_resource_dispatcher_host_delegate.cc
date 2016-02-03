b27f62bde0ca223308af047d15823be5d4ef5917
Ben Gotow <bengotow@gmail.com>
2016-02-03 11:35:01 -0800

    Fix linter error

--- atom/browser/atom_resource_dispatcher_host_delegate.cc
+++ atom/browser/atom_resource_dispatcher_host_delegate.cc
@@ -26,7 +26,8 @@ bool AtomResourceDispatcherHostDelegate::HandleExternalProtocol(
     bool has_user_gesture) {
   GURL escaped_url(net::EscapeExternalHandlerValue(url.spec()));
   BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
-      base::Bind(base::IgnoreResult(platform_util::OpenExternal), escaped_url, false));
+      base::Bind(
+          base::IgnoreResult(platform_util::OpenExternal), escaped_url, false));
   return true;
 }
 
