fee56df40074b6730e71591db964477c8fce8b91
Dongjoon Hyun <dongjoon@apache.org>
2016-02-24 11:15:21 -0800

    Rename function `FilterAccecelator` to `FilterAccelerator` in `submenu_button.cc`.

--- atom/browser/ui/views/submenu_button.cc
+++ atom/browser/ui/views/submenu_button.cc
@@ -15,7 +15,7 @@ namespace atom {
 namespace {
 
 // Filter out the "&" in menu label.
-base::string16 FilterAccecelator(const base::string16& label) {
+base::string16 FilterAccelerator(const base::string16& label) {
   base::string16 out;
   base::RemoveChars(label, base::ASCIIToUTF16("&").c_str(), &out);
   return out;
@@ -26,7 +26,7 @@ base::string16 FilterAccecelator(const base::string16& label) {
 SubmenuButton::SubmenuButton(views::ButtonListener* listener,
                              const base::string16& title,
                              views::MenuButtonListener* menu_button_listener)
-    : views::MenuButton(listener, FilterAccecelator(title),
+    : views::MenuButton(listener, FilterAccelerator(title),
                         menu_button_listener, false),
       accelerator_(0),
       show_underline_(false),
