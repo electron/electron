8f820e09be43616ffd65ffc00cd43e6153b1a615
Kevin Sawicki <kevinsawicki@gmail.com>
2016-03-04 14:54:12 -0800

    Use template string and arrow functions

--- spec/native-image-spec.js
+++ spec/native-image-spec.js
@@ -1,11 +1,13 @@
+'use strict';
+
 const assert = require('assert');
 const nativeImage = require('electron').nativeImage;
 const path = require('path');
 
-describe('nativeImage module', function () {
-  describe('createFromPath(path)', function () {
-    it('normalizes the path', function () {
-      const nonAbsolutePath = path.join(__dirname, 'fixtures', 'api') + path.sep + '..' + path.sep + path.join('assets', 'logo.png');
+describe('nativeImage module', () => {
+  describe('createFromPath(path)', () => {
+    it('normalizes the path', () => {
+      const nonAbsolutePath = `${path.join(__dirname, 'fixtures', 'api')}${path.sep}..${path.sep}${path.join('assets', 'logo.png')}`;
       const image = nativeImage.createFromPath(nonAbsolutePath);
       assert(!image.isEmpty());
     });
