97930fcd84402674960b1a3503c5ea6df7012492
Kevin Sawicki <kevinsawicki@gmail.com>
2016-03-07 14:19:24 -0800

    Add specs for more image path cases

--- spec/api-native-image-spec.js
+++ spec/api-native-image-spec.js
@@ -6,9 +6,30 @@ const path = require('path');
 
 describe('nativeImage module', () => {
   describe('createFromPath(path)', () => {
-    it('normalizes the path', () => {
-      const nonAbsolutePath = `${path.join(__dirname, 'fixtures', 'api')}${path.sep}..${path.sep}${path.join('assets', 'logo.png')}`;
-      const image = nativeImage.createFromPath(nonAbsolutePath);
+    it('returns an empty image for invalid paths', () => {
+      assert(nativeImage.createFromPath('').isEmpty());
+      assert(nativeImage.createFromPath('does-not-exist.png').isEmpty());
+    });
+
+    it('loads images from paths relative to the current working directory', () => {
+      const imagePath = `.${path.sep}${path.join('spec', 'fixtures', 'assets', 'logo.png')}`;
+      const image = nativeImage.createFromPath(imagePath);
+      assert(!image.isEmpty());
+      assert.equal(image.getSize().height, 190);
+      assert.equal(image.getSize().width, 538);
+    })
+
+    it('loads images from paths with `.` segments', () => {
+      const imagePath = `${path.join(__dirname, 'fixtures')}${path.sep}.${path.sep}${path.join('assets', 'logo.png')}`;
+      const image = nativeImage.createFromPath(imagePath);
+      assert(!image.isEmpty());
+      assert.equal(image.getSize().height, 190);
+      assert.equal(image.getSize().width, 538);
+    });
+
+    it('loads images from path with `..` segments', () => {
+      const imagePath = `${path.join(__dirname, 'fixtures', 'api')}${path.sep}..${path.sep}${path.join('assets', 'logo.png')}`;
+      const image = nativeImage.createFromPath(imagePath);
       assert(!image.isEmpty());
       assert.equal(image.getSize().height, 190);
       assert.equal(image.getSize().width, 538);
