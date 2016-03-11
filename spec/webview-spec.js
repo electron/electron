d564727583b405fdeec53a320ffdc41bc7ed5c28
Cheng Zhao <zcbenz@gmail.com>
2016-03-11 11:17:02 +0900

    spec: Increase timeout for executeJavaScript

--- spec/webview-spec.js
+++ spec/webview-spec.js
@@ -568,6 +568,7 @@ describe('<webview> tag', function() {
     });
 
     it('can return the result of the executed script', function(done) {
+      this.timeout(50000);
       var listener = function() {
         var jsScript = "'4'+2";
         webview.executeJavaScript(jsScript, false, function(result) {
