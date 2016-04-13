a22dc8676f24899e01a18d85b430ff53df2b739c
Marco Trevisan (Treviño) <mail@3v1n0.net>
2016-04-13 04:42:01 +0200

    LibnotifyLoader: add notify_get_server_caps support

--- brightray/browser/linux/libnotify_loader.cc
+++ brightray/browser/linux/libnotify_loader.cc
@@ -47,6 +47,15 @@ bool LibNotifyLoader::Load(const std::string& library_name) {
     return false;
   }
 
+  notify_get_server_caps =
+      reinterpret_cast<decltype(this->notify_get_server_caps)>(
+          dlsym(library_, "notify_get_server_caps"));
+  notify_get_server_caps = &::notify_get_server_caps;
+  if (!notify_get_server_caps) {
+    CleanUp(true);
+    return false;
+  }
+
   notify_notification_new =
       reinterpret_cast<decltype(this->notify_notification_new)>(
           dlsym(library_, "notify_notification_new"));
@@ -114,6 +123,7 @@ void LibNotifyLoader::CleanUp(bool unload) {
   notify_is_initted = NULL;
   notify_init = NULL;
   notify_get_server_info = NULL;
+  notify_get_server_caps = NULL;
   notify_notification_new = NULL;
   notify_notification_add_action = NULL;
   notify_notification_set_image_from_pixbuf = NULL;
