02d72c81af73bdd765beb3af9b7915209e18a747
Brian R. Bondy <netzen@gmail.com>
2016-04-28 20:07:16 -0400

    Fix linking problem with IPC::MessageT
    IPC::MessageT<ChromeUtilityHostMsg_ProcessStarted_Meta, std::__1::tuple<>, void>::MessageT(IPC::Routing)

--- atom/common/common_message_generator.h
+++ atom/common/common_message_generator.h
@@ -8,3 +8,4 @@
 #include "chrome/common/print_messages.h"
 #include "chrome/common/tts_messages.h"
 #include "chrome/common/widevine_cdm_messages.h"
+#include "chrome/common/chrome_utility_messages.h"
