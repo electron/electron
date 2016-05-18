a45af78b15fb3beb8e0b9d8cff276794373599b5
Cheng Zhao <zcbenz@gmail.com>
2016-05-18 09:42:14 +0900

    Remove unneeded lines when importing code

--- brightray/browser/linux/libnotify_loader.cc
+++ brightray/browser/linux/libnotify_loader.cc
@@ -23,7 +23,6 @@ bool LibNotifyLoader::Load(const std::string& library_name) {
   notify_is_initted =
       reinterpret_cast<decltype(this->notify_is_initted)>(
           dlsym(library_, "notify_is_initted"));
-  notify_is_initted = &::notify_is_initted;
   if (!notify_is_initted) {
     CleanUp(true);
     return false;
@@ -32,7 +31,6 @@ bool LibNotifyLoader::Load(const std::string& library_name) {
   notify_init =
       reinterpret_cast<decltype(this->notify_init)>(
           dlsym(library_, "notify_init"));
-  notify_init = &::notify_init;
   if (!notify_init) {
     CleanUp(true);
     return false;
@@ -41,7 +39,6 @@ bool LibNotifyLoader::Load(const std::string& library_name) {
   notify_get_server_info =
       reinterpret_cast<decltype(this->notify_get_server_info)>(
           dlsym(library_, "notify_get_server_info"));
-  notify_get_server_info = &::notify_get_server_info;
   if (!notify_get_server_info) {
     CleanUp(true);
     return false;
@@ -50,7 +47,6 @@ bool LibNotifyLoader::Load(const std::string& library_name) {
   notify_get_server_caps =
       reinterpret_cast<decltype(this->notify_get_server_caps)>(
           dlsym(library_, "notify_get_server_caps"));
-  notify_get_server_caps = &::notify_get_server_caps;
   if (!notify_get_server_caps) {
     CleanUp(true);
     return false;
@@ -59,7 +55,6 @@ bool LibNotifyLoader::Load(const std::string& library_name) {
   notify_notification_new =
       reinterpret_cast<decltype(this->notify_notification_new)>(
           dlsym(library_, "notify_notification_new"));
-  notify_notification_new = &::notify_notification_new;
   if (!notify_notification_new) {
     CleanUp(true);
     return false;
@@ -68,7 +63,6 @@ bool LibNotifyLoader::Load(const std::string& library_name) {
   notify_notification_add_action =
       reinterpret_cast<decltype(this->notify_notification_add_action)>(
           dlsym(library_, "notify_notification_add_action"));
-  notify_notification_add_action = &::notify_notification_add_action;
   if (!notify_notification_add_action) {
     CleanUp(true);
     return false;
@@ -77,7 +71,6 @@ bool LibNotifyLoader::Load(const std::string& library_name) {
   notify_notification_set_image_from_pixbuf =
       reinterpret_cast<decltype(this->notify_notification_set_image_from_pixbuf)>(
           dlsym(library_, "notify_notification_set_image_from_pixbuf"));
-  notify_notification_set_image_from_pixbuf = &::notify_notification_set_image_from_pixbuf;
   if (!notify_notification_set_image_from_pixbuf) {
     CleanUp(true);
     return false;
@@ -86,7 +79,6 @@ bool LibNotifyLoader::Load(const std::string& library_name) {
   notify_notification_set_timeout =
       reinterpret_cast<decltype(this->notify_notification_set_timeout)>(
           dlsym(library_, "notify_notification_set_timeout"));
-  notify_notification_set_timeout = &::notify_notification_set_timeout;
   if (!notify_notification_set_timeout) {
     CleanUp(true);
     return false;
@@ -95,7 +87,6 @@ bool LibNotifyLoader::Load(const std::string& library_name) {
   notify_notification_set_hint_string =
       reinterpret_cast<decltype(this->notify_notification_set_hint_string)>(
           dlsym(library_, "notify_notification_set_hint_string"));
-  notify_notification_set_hint_string = &::notify_notification_set_hint_string;
   if (!notify_notification_set_hint_string) {
     CleanUp(true);
     return false;
@@ -104,7 +95,6 @@ bool LibNotifyLoader::Load(const std::string& library_name) {
   notify_notification_show =
       reinterpret_cast<decltype(this->notify_notification_show)>(
           dlsym(library_, "notify_notification_show"));
-  notify_notification_show = &::notify_notification_show;
   if (!notify_notification_show) {
     CleanUp(true);
     return false;
@@ -113,7 +103,6 @@ bool LibNotifyLoader::Load(const std::string& library_name) {
   notify_notification_close =
       reinterpret_cast<decltype(this->notify_notification_close)>(
           dlsym(library_, "notify_notification_close"));
-  notify_notification_close = &::notify_notification_close;
   if (!notify_notification_close) {
     CleanUp(true);
     return false;
