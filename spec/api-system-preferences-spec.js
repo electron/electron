e377298cd2e6184ef57bc5f4cfaa939b9b571ff4
Charlie Hess <charlie@slack-corp.com>
2016-06-01 19:29:24 -0700

    Be consistent in spec organization.

--- spec/api-system-preferences-spec.js
+++ spec/api-system-preferences-spec.js
@@ -7,14 +7,16 @@ describe('systemPreferences module', function () {
     return
   }
 
-  it('returns values for known user defaults', function () {
-    let locale = systemPreferences.getUserDefault('AppleLocale', 'string')
-    assert.notEqual(locale, null)
-    assert(locale.length > 0)
+  describe('systemPreferences.getUserDefault(key, type)', function () {
+    it('returns values for known user defaults', function () {
+      let locale = systemPreferences.getUserDefault('AppleLocale', 'string')
+      assert.notEqual(locale, null)
+      assert(locale.length > 0)
 
-    let languages = systemPreferences.getUserDefault('AppleLanguages', 'array')
-    assert.notEqual(languages, null)
-    assert(languages.length > 0)
+      let languages = systemPreferences.getUserDefault('AppleLanguages', 'array')
+      assert.notEqual(languages, null)
+      assert(languages.length > 0)
+    })
   })
 
 })
