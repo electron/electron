f7b0bb2bf3b15df34543b963979945c7528c8263
Kevin Sawicki <kevinsawicki@gmail.com>
2016-06-07 16:23:09 -0700

    Add TODO about blinkFeatures -> enableBlinkFeatures

--- atom/common/options_switches.cc
+++ atom/common/options_switches.cc
@@ -107,6 +107,7 @@ const char kOpenerID[] = "openerId";
 const char kScrollBounce[] = "scrollBounce";
 
 // Enable blink features.
+// TODO(kevinsawicki) Rename to enableBlinkFeatures in 2.0
 const char kBlinkFeatures[] = "blinkFeatures";
 
 // Disable blink features.
