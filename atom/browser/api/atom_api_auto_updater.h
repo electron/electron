a3786f66c916abf22d876d195d8ae3c9649c7d30
Cheng Zhao <zcbenz@gmail.com>
2016-06-13 09:32:24 +0900

    Do not add JS wrapper for setFeedURL
    When possible we should avoid adding wrappers in JS, it usually makes
    code more difficult to mantain

--- atom/browser/api/atom_api_auto_updater.h
+++ atom/browser/api/atom_api_auto_updater.h
@@ -10,6 +10,7 @@
 #include "atom/browser/api/event_emitter.h"
 #include "atom/browser/auto_updater.h"
 #include "atom/browser/window_list_observer.h"
+#include "native_mate/arguments.h"
 #include "native_mate/handle.h"
 
 namespace atom {
@@ -43,6 +44,7 @@ class AutoUpdater : public mate::EventEmitter<AutoUpdater>,
   void OnWindowAllClosed() override;
 
  private:
+  void SetFeedURL(const std::string& url, mate::Arguments* args);
   void QuitAndInstall();
 
   DISALLOW_COPY_AND_ASSIGN(AutoUpdater);
