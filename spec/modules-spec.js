b273b70eee6a15808faad6f3fd5ad5d85d36e4d4
Kevin Sawicki <kevinsawicki@gmail.com>
2016-06-23 15:27:45 -0700

    Filter existing search paths instead reimplementing

--- spec/modules-spec.js
+++ spec/modules-spec.js
@@ -89,8 +89,7 @@ describe('Module._nodeModulePaths', function () {
     it('includes paths outside of the resources path', function () {
       let modulePath = path.resolve('/foo')
       assert.deepEqual(Module._nodeModulePaths(modulePath), [
-        path.join(modulePath, 'node_modules'),
-        path.join('node_modules')
+        path.join(modulePath, 'node_modules')
       ])
     })
   })
