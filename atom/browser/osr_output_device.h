504c79cae73ac6f1da6fa268b6fc004a5b4e1753
gellert <gellihegyi@gmail.com>
2016-07-20 11:32:59 +0200

    removes commented includes

--- atom/browser/osr_output_device.h
+++ atom/browser/osr_output_device.h
@@ -5,20 +5,6 @@
 #ifndef ATOM_BROWSER_OSR_OUTPUT_DEVICE_H_
 #define ATOM_BROWSER_OSR_OUTPUT_DEVICE_H_
 
-// #include "content/browser/renderer_host/render_widget_host_view_base.h"
-// #include "content/browser/renderer_host/delegated_frame_host.h"
-// #include "content/browser/renderer_host/resize_lock.h"
-// #include "third_party/WebKit/public/platform/WebVector.h"
-// #include "cc/scheduler/begin_frame_source.h"
-// #include "content/browser/renderer_host/render_widget_host_impl.h"
-// #include "cc/output/compositor_frame.h"
-// #include "ui/gfx/geometry/point.h"
-// #include "base/threading/thread.h"
-// #include "ui/compositor/compositor.h"
-// #include "ui/compositor/layer_delegate.h"
-// #include "ui/compositor/layer_owner.h"
-// #include "ui/base/ime/text_input_client.h"
-
 #include "cc/output/software_output_device.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkCanvas.h"
@@ -31,7 +17,6 @@ class OffScreenOutputDevice : public cc::SoftwareOutputDevice {
 public:
   OffScreenOutputDevice();
   ~OffScreenOutputDevice();
-
 
   // void saveSkBitmapToBMPFile(const SkBitmap& skBitmap, const char* path);
   void Resize(const gfx::Size& pixel_size, float scale_factor) override;
@@ -47,7 +32,6 @@ private:
 
   DISALLOW_COPY_AND_ASSIGN(OffScreenOutputDevice);
 };
-
 
 }  // namespace atom
 
