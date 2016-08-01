7c9145301f4d85dc03343604b46e97a00f435885
yan <yan@mit.edu>
2016-08-01 15:40:39 -0700

    Don't suppress renderer restart for chrome-extension
    The renderer process needs to be restarted for PDFJS to load PDFs without
    encountering a CORS error. Brave was calling webview.reload in order to force
    renderer process restart on non-navigation PDF loads, but this was broken by
    02ec0b2cb9613ba21672ebcaac7b4a64ec0a4b14. This commit fixes it but
    a better solution would be for renderer reload to automatically happen
    on non-navigation chrome-extension loads.
    
    cf https://github.com/brave/browser-laptop/issues/2715
    
    Auditors: @bridiver

--- atom/browser/atom_browser_client.cc
+++ atom/browser/atom_browser_client.cc
@@ -42,6 +42,10 @@
 #include "ui/base/l10n/l10n_util.h"
 #include "v8/include/v8.h"
 
+#if defined(ENABLE_EXTENSIONS)
+#include "extensions/common/constants.h"
+#endif
+
 namespace atom {
 
 namespace {
@@ -155,6 +159,11 @@ void AtomBrowserClient::OverrideSiteInstanceForNavigation(
   //     return;
   //   }
   // }
+#if defined(ENABLE_EXTENSIONS)
+  if (url.SchemeIs(extensions::kExtensionScheme)) {
+    g_suppress_renderer_process_restart = false;
+  }
+#endif
 
   if (g_suppress_renderer_process_restart) {
     g_suppress_renderer_process_restart = false;
