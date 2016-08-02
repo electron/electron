9872fa0f52a2dec9ec5702481c7476f007965588
gellert <gellihegyi@gmail.com>
2016-08-02 15:17:50 +0200

    restoring default app

--- default_app/default_app.js
+++ default_app/default_app.js
@@ -3,9 +3,6 @@ const path = require('path')
 
 let mainWindow = null
 
-app.commandLine.appendSwitch('--disable-gpu');
-app.commandLine.appendSwitch('--disable-gpu-compositing');
-
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   app.quit()
@@ -13,42 +10,19 @@ app.on('window-all-closed', () => {
 
 exports.load = (appUrl) => {
   app.on('ready', () => {
-    mainWindow = new BrowserWindow({
+    const options = {
       width: 800,
       height: 600,
-      show: false,
       autoHideMenuBar: true,
       backgroundColor: '#FFFFFF',
-      useContentSize: true,
-      webPreferences: {
-        offscreen: true,
-        nodeIntegration: false,
-        backgroundThrottling: false
-      }
-    })
+      useContentSize: true
+    }
+    if (process.platform === 'linux') {
+      options.icon = path.join(__dirname, 'icon.png')
+    }
+
+    mainWindow = new BrowserWindow(options)
     mainWindow.loadURL(appUrl)
     mainWindow.focus()
-
-    mainWindow.webContents.on('dom-ready', () => {
-      mainWindow.webContents.setFrameRate(10)
-      let ping = true
-      setInterval(() => {
-        if (ping) {
-          mainWindow.webContents.startPainting()
-        } else {
-          mainWindow.webContents.stopPainting()
-        }
-
-        ping = !ping
-      }, 3000)
-    })
-
-    setInterval(() => {
-      console.log(mainWindow.webContents.isPainting())
-    }, 500)
-
-    mainWindow.webContents.on('paint', (e, rect, data) => {
-      console.log('painting', data.length)
-    })
   })
 }
