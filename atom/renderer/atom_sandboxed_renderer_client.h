524bab530acdff646fdc4d1168a450f621099268
Thiago de Arruda <tpadilha84@gmail.com>
2016-08-21 07:30:21 -0300

    Improve AtomSandboxedRendererClient to support preload scripts.
    Add RenderFrameObserver/RenderViewObserver subclasses that implement the
    necessary support for preload scripts in sandboxed renderers.

--- atom/renderer/atom_sandboxed_renderer_client.h
+++ atom/renderer/atom_sandboxed_renderer_client.h
@@ -4,7 +4,11 @@
 #ifndef ATOM_RENDERER_ATOM_SANDBOXED_RENDERER_CLIENT_H_
 #define ATOM_RENDERER_ATOM_SANDBOXED_RENDERER_CLIENT_H_
 
+#include <string>
+#include <vector>
+
 #include "content/public/renderer/content_renderer_client.h"
+#include "content/public/renderer/render_frame.h"
 
 namespace atom {
 
@@ -12,6 +16,17 @@ class AtomSandboxedRendererClient : public content::ContentRendererClient {
  public:
   AtomSandboxedRendererClient();
   virtual ~AtomSandboxedRendererClient();
+
+  void DidCreateScriptContext(
+      v8::Handle<v8::Context> context, content::RenderFrame* render_frame);
+  void WillReleaseScriptContext(
+      v8::Handle<v8::Context> context, content::RenderFrame* render_frame);
+  void InvokeBindingCallback(v8::Handle<v8::Context> context,
+                             std::string callback_name,
+                             std::vector<v8::Handle<v8::Value>> args);
+  // content::ContentRendererClient:
+  void RenderFrameCreated(content::RenderFrame*) override;
+  void RenderViewCreated(content::RenderView*) override;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(AtomSandboxedRendererClient);
