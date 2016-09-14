4244a72f95a0974acb68089ad89f17328c3f30c3
Kevin Sawicki <kevinsawicki@gmail.com>
2016-09-14 11:16:44 -0700

    Allow spec to be run multiple times in same runner

--- spec/api-ipc-spec.js
+++ spec/api-ipc-spec.js
@@ -70,7 +70,7 @@ describe('ipc module', function () {
       assert.ok(Object.keys(a.foo).includes('nested'))
       assert.ok(Object.keys(a.foo).includes('method1'))
 
-      a = remote.require(path.join(fixtures, 'module', 'function-with-missing-properties.js'))
+      a = remote.require(path.join(fixtures, 'module', 'function-with-missing-properties.js')).setup()
       assert.equal(a.bar(), true)
       assert.equal(typeof a.bar.baz, 'function')
     })
--- spec/fixtures/module/function-with-missing-properties.js
+++ spec/fixtures/module/function-with-missing-properties.js
@@ -1,11 +1,13 @@
-const foo = {}
+exports.setup = function () {
+  const foo = {}
 
-foo.bar = function () {
-  return delete foo.bar.baz && delete foo.bar
+  foo.bar = function () {
+    return delete foo.bar.baz && delete foo.bar
+  }
+
+  foo.bar.baz = function () {
+    return 3
+  }
+
+  return foo
 }
-
-foo.bar.baz = function () {
-  return 3
-}
-
-module.exports = foo
