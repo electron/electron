fb2f4ae5282a4c97fa445ec4868eecf13b97baca
Kevin Sawicki <kevinsawicki@gmail.com>
2016-09-19 14:32:11 -0700

    Set quiet to true when loglevel is silent

--- npm/install.js
+++ npm/install.js
@@ -36,7 +36,13 @@ if (installedVersion === version && fs.existsSync(path.join(__dirname, paths[pla
 }
 
 // downloads if not cached
-download({version: version, platform: process.env.npm_config_platform, arch: process.env.npm_config_arch, strictSSL: process.env.npm_config_strict_ssl === 'true'}, extractFile)
+download({
+  version: version,
+  platform: process.env.npm_config_platform,
+  arch: process.env.npm_config_arch,
+  strictSSL: process.env.npm_config_strict_ssl === 'true',
+  quiet: process.env.npm_config_loglevel === 'silent',
+}, extractFile)
 
 // unzips and makes path.txt point at the correct executable
 function extractFile (err, zipPath) {
