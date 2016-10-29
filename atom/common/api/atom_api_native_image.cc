1d24a3a1751ea3c5020a636e9c6020eb2376dabe
Yury Solovyov <biohazard707@gmail.com>
2016-10-29 14:38:23 +0300

    Add callback converters

--- atom/common/api/atom_api_native_image.cc
+++ atom/common/api/atom_api_native_image.cc
@@ -12,6 +12,7 @@
 #include "atom/common/native_mate_converters/gfx_converter.h"
 #include "atom/common/native_mate_converters/gurl_converter.h"
 #include "atom/common/native_mate_converters/value_converter.h"
+#include "atom/common/native_mate_converters/callback.h"
 #include "base/base64.h"
 #include "base/files/file_util.h"
 #include "base/strings/pattern.h"
