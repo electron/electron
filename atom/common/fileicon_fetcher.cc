602aba872327df664b53b506c70fcba7107977bd
Yury Solovyov <biohazard707@gmail.com>
2016-10-30 15:31:30 +0300

    Include proper header

--- atom/common/fileicon_fetcher.cc
+++ atom/common/fileicon_fetcher.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/webui/fileicon_source.h"
+#include "atom/common/fileicon_fetcher.h"
 
 #include "base/bind.h"
 #include "base/callback.h"
