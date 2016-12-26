5c8ebb296c017252bbeee52304c07833e30c8804
Samuel Attard <samuel.r.attard@gmail.com>
2016-12-27 12:17:48 +1300

    Update index.js

--- npm/index.js
+++ npm/index.js
@@ -1,4 +1,10 @@
 var fs = require('fs')
 var path = require('path')
 
-module.exports = path.join(__dirname, fs.readFileSync(path.join(__dirname, 'path.txt'), 'utf-8'))
+const pathFile = path.join(__dirname, 'path.txt')
+
+if (fs.existsSync(pathFile)) {
+  module.exports = path.join(__dirname, fs.readFileSync(pathFile, 'utf-8'))
+} else {
+  throw new Error('Electron failed to install correctly, please delete node_modules/electron and try installing again');
+}
