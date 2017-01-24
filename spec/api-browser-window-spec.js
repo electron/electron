284ca3114bce8e096b2f86f315f6c214d4a2212f
Kevin Sawicki <kevinsawicki@gmail.com>
2017-01-24 08:29:50 -0800

    Wait for window to transition before leaving full screen

--- spec/api-browser-window-spec.js
+++ spec/api-browser-window-spec.js
@@ -1437,13 +1437,16 @@ describe('BrowserWindow module', function () {
       // Only implemented on macOS.
       if (process.platform !== 'darwin') return
 
-      it('can be changed with setFullScreen method', function () {
+      it('can be changed with setFullScreen method', function (done) {
         w.destroy()
         w = new BrowserWindow()
+        w.once('enter-full-screen', () => {
+          w.setFullScreen(false)
+          assert.equal(w.isFullScreen(), false)
+          done()
+        })
         w.setFullScreen(true)
         assert.equal(w.isFullScreen(), true)
-        w.setFullScreen(false)
-        assert.equal(w.isFullScreen(), false)
       })
 
       it('should not be changed by setKiosk method', function () {
