836865954e93e81c77b078ec11a9127b2a4b4356
Kevin Sawicki <kevinsawicki@gmail.com>
2017-01-26 09:57:53 -0800

    Clear interval before setting new one

--- spec/api-browser-window-spec.js
+++ spec/api-browser-window-spec.js
@@ -1679,6 +1679,8 @@ describe('BrowserWindow module', function () {
 
     const showLastDevToolsPanel = () => {
       w.webContents.once('devtools-opened', function () {
+        clearInterval(showPanelIntervalId)
+
         showPanelIntervalId = setInterval(function () {
           if (w == null || w.isDestroyed())  {
             clearInterval(showLastDevToolsPanel)
