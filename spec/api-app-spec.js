ec38083ec0ab2b6b881c384b7b60c269e905764c
Yury Solovyov <biohazard707@gmail.com>
2017-02-08 22:47:02 +0300

    Run fileicon specs

--- spec/api-app-spec.js
+++ spec/api-app-spec.js
@@ -9,8 +9,6 @@ const {closeWindow} = require('./window-helpers')
 
 const {app, BrowserWindow, ipcMain} = remote
 
-const isCI = remote.getGlobal('isCi')
-
 describe('electron module', function () {
   it('does not expose internal modules to require', function () {
     assert.throws(function () {
@@ -460,9 +458,6 @@ describe('app module', function () {
   })
 
   describe('getFileIcon() API', function () {
-    // FIXME Get these specs running on Linux CI
-    if (process.platform === 'linux' && isCI) return
-
     const iconPath = path.join(__dirname, 'fixtures/assets/icon.ico')
     const sizes = {
       small: 16,
