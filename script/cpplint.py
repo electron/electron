d4d3c78701f00ceec05ee14ab1145ffd008e53ad
Kevin Sawicki <kevinsawicki@gmail.com>
2017-02-27 10:43:44 -0800

    Ignore lint warnings in Objective-C header

--- script/cpplint.py
+++ script/cpplint.py
@@ -9,6 +9,8 @@ from lib.util import execute
 IGNORE_FILES = [
   os.path.join('atom', 'browser', 'mac', 'atom_application.h'),
   os.path.join('atom', 'browser', 'mac', 'atom_application_delegate.h'),
+  os.path.join('atom', 'browser', 'ui', 'cocoa',
+               'touch_bar_forward_declarations.h'),
   os.path.join('atom', 'browser', 'resources', 'win', 'resource.h'),
   os.path.join('atom', 'browser', 'ui', 'cocoa', 'atom_menu_controller.h'),
   os.path.join('atom', 'common', 'api', 'api_messages.h'),
