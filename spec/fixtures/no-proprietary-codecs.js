6b8f840da05e9cc326770c8c7105f3b5d8ce395b
Kevin Sawicki <kevinsawicki@gmail.com>
2017-03-29 16:23:30 -0700

    Add script to verify ffmpeg without proprietary codecs

---
+++ spec/fixtures/no-proprietary-codecs.js
@@ -0,0 +1,36 @@
+const {app, BrowserWindow, ipcMain} = require('electron')
+const path = require('path')
+const url = require('url')
+
+const MEDIA_ERR_SRC_NOT_SUPPORTED = 4
+
+let window
+
+app.once('ready', () => {
+  window = new BrowserWindow({
+    show: false
+  })
+
+  window.loadURL(url.format({
+    protocol: 'file',
+    slashed: true,
+    pathname: path.resolve(__dirname, 'asar', 'video.asar', 'index.html')
+  }))
+
+  ipcMain.on('asar-video', function (event, message, error) {
+    if (message === 'ended') {
+      console.log('Video played, proprietary codecs are included')
+      app.exit(1)
+      return
+    }
+
+    if (message === 'error' && error === MEDIA_ERR_SRC_NOT_SUPPORTED) {
+      console.log('Video format not supported, proprietary codecs are not included')
+      app.exit(0)
+      return
+    }
+
+    console.log(`Unexpected error: ${error}`)
+    app.exit(1)
+  })
+})
