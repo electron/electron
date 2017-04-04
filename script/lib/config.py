453cb2c0b40e63196d4eeb4c8ca119c985ee23b7
Cheng Zhao <zcbenz@gmail.com>
2017-04-04 12:16:57 +0900

    Upgrade to Chrome 58.0.3029.40

--- script/lib/config.py
+++ script/lib/config.py
@@ -9,7 +9,7 @@ import sys
 BASE_URL = os.getenv('LIBCHROMIUMCONTENT_MIRROR') or \
     'https://s3.amazonaws.com/github-janky-artifacts/libchromiumcontent'
 LIBCHROMIUMCONTENT_COMMIT = os.getenv('LIBCHROMIUMCONTENT_COMMIT') or \
-    '4a0e32606e52c12c50c2e3a0973d015d8cdff494'
+    '8c3e532385811b5cea80e969c1beb1bf3ffb6777'
 
 PLATFORM = {
   'cygwin': 'win32',
