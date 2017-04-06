4f40b8a42c1c12063ede9fd1f7e2f0edcac5404c
Brendan Forster <brendan@github.com>
2017-04-06 11:01:58 +1000

    stub the windows source file

---
+++ atom/browser/ui/certificate_trust_win.cc
@@ -0,0 +1,12 @@
+// Copyright (c) 2013 GitHub, Inc.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#include "atom/browser/ui/certificate_trust.h"
+
+#include <windows.h>  // windows.h must be included first
+
+namespace certificate_trust {
+
+
+}  // namespace certificate_trust
