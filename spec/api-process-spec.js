3c71f542a8fa66e6234cc720e9faf52fa3152009
Hari Krishna Reddy Juturu <jhreddy@microsoft.com>
2017-05-04 11:10:57 -0700

    Fixing UT on mac

--- spec/api-process-spec.js
+++ spec/api-process-spec.js
@@ -12,12 +12,12 @@ describe('process module', function () {
   describe('process.getIOCounters()', function () {
     it('returns an io counters object', function () {
       var ioCounters = process.getIOCounters()
-      assert.ok(ioCounters.readOperationCount > 0, 'read operation count not > 0')
-      assert.ok(ioCounters.writeOperationCount > 0, 'write operation count not > 0')
-      assert.ok(ioCounters.otherOperationCount > 0, 'other operation count not > 0')
-      assert.ok(ioCounters.readTransferCount > 0, 'read transfer count not > 0')
-      assert.ok(ioCounters.writeTransferCount > 0, 'write transfer count not > 0')
-      assert.ok(ioCounters.otherTransferCount > 0, 'other transfer count not > 0')
+      assert.equal(typeof ioCounters.readOperationCount, 'number')
+      assert.equal(typeof ioCounters.writeOperationCount, 'number')
+      assert.equal(typeof ioCounters.otherOperationCount, 'number')
+      assert.equal(typeof ioCounters.readTransferCount, 'number')
+      assert.equal(typeof ioCounters.writeTransferCount, 'number')
+      assert.equal(typeof ioCounters.otherTransferCount, 'number')
     })
   })
 })
