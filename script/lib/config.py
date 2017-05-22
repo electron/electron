433a827cc366fd5d9732e0c19ee59b2b3251241c
Kevin Sawicki <kevinsawicki@gmail.com>
2017-05-22 09:04:23 -0700

    Upgrade libcc to electron/libchromiumcontent@a3cff0522

--- script/lib/config.py
+++ script/lib/config.py
@@ -9,7 +9,7 @@ import sys
 BASE_URL = os.getenv('LIBCHROMIUMCONTENT_MIRROR') or \
     'https://s3.amazonaws.com/github-janky-artifacts/libchromiumcontent'
 LIBCHROMIUMCONTENT_COMMIT = os.getenv('LIBCHROMIUMCONTENT_COMMIT') or \
-    '549af32d00045c7b9dbb2760c090586c8d3a2638'
+    'a3cff052273b659702c6a8728e28a3b45659cd76'
 
 PLATFORM = {
   'cygwin': 'win32',
