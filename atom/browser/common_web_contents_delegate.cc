4e52fd48053d557acfd076cf130be7aa0052371b
Aleksei Kuzmin <alkuzmin@microsoft.com>
2017-06-16 23:50:03 +0300

    Remove base::StringValue
    https://codereview.chromium.org/2664753002

--- atom/browser/common_web_contents_delegate.cc
+++ atom/browser/common_web_contents_delegate.cc
@@ -300,7 +300,7 @@ void CommonWebContentsDelegate::DevToolsSaveToFile(
     settings.title = url;
     settings.default_path = base::FilePath::FromUTF8Unsafe(url);
     if (!file_dialog::ShowSaveDialog(settings, &path)) {
-      base::StringValue url_value(url);
+      base::Value url_value(url);
       web_contents_->CallClientFunction(
           "DevToolsAPI.canceledSaveURL", &url_value, nullptr, nullptr);
       return;
@@ -404,7 +404,7 @@ void CommonWebContentsDelegate::DevToolsRemoveFileSystem(
   DictionaryPrefUpdate update(pref_service, prefs::kDevToolsFileSystemPaths);
   update.Get()->RemoveWithoutPathExpansion(path, nullptr);
 
-  base::StringValue file_system_path_value(path);
+  base::Value file_system_path_value(path);
   web_contents_->CallClientFunction("DevToolsAPI.fileSystemRemoved",
                                     &file_system_path_value,
                                     nullptr, nullptr);
@@ -468,7 +468,7 @@ void CommonWebContentsDelegate::DevToolsSearchInPath(
 void CommonWebContentsDelegate::OnDevToolsSaveToFile(
     const std::string& url) {
   // Notify DevTools.
-  base::StringValue url_value(url);
+  base::Value url_value(url);
   web_contents_->CallClientFunction(
       "DevToolsAPI.savedURL", &url_value, nullptr, nullptr);
 }
@@ -476,7 +476,7 @@ void CommonWebContentsDelegate::OnDevToolsSaveToFile(
 void CommonWebContentsDelegate::OnDevToolsAppendToFile(
     const std::string& url) {
   // Notify DevTools.
-  base::StringValue url_value(url);
+  base::Value url_value(url);
   web_contents_->CallClientFunction(
       "DevToolsAPI.appendedToURL", &url_value, nullptr, nullptr);
 }
@@ -486,7 +486,7 @@ void CommonWebContentsDelegate::OnDevToolsIndexingWorkCalculated(
     const std::string& file_system_path,
     int total_work) {
   base::Value request_id_value(request_id);
-  base::StringValue file_system_path_value(file_system_path);
+  base::Value file_system_path_value(file_system_path);
   base::Value total_work_value(total_work);
   web_contents_->CallClientFunction("DevToolsAPI.indexingTotalWorkCalculated",
                                     &request_id_value,
@@ -499,7 +499,7 @@ void CommonWebContentsDelegate::OnDevToolsIndexingWorked(
     const std::string& file_system_path,
     int worked) {
   base::Value request_id_value(request_id);
-  base::StringValue file_system_path_value(file_system_path);
+  base::Value file_system_path_value(file_system_path);
   base::Value worked_value(worked);
   web_contents_->CallClientFunction("DevToolsAPI.indexingWorked",
                                     &request_id_value,
@@ -512,7 +512,7 @@ void CommonWebContentsDelegate::OnDevToolsIndexingDone(
     const std::string& file_system_path) {
   devtools_indexing_jobs_.erase(request_id);
   base::Value request_id_value(request_id);
-  base::StringValue file_system_path_value(file_system_path);
+  base::Value file_system_path_value(file_system_path);
   web_contents_->CallClientFunction("DevToolsAPI.indexingDone",
                                     &request_id_value,
                                     &file_system_path_value,
@@ -528,7 +528,7 @@ void CommonWebContentsDelegate::OnDevToolsSearchCompleted(
     file_paths_value.AppendString(file_path);
   }
   base::Value request_id_value(request_id);
-  base::StringValue file_system_path_value(file_system_path);
+  base::Value file_system_path_value(file_system_path);
   web_contents_->CallClientFunction("DevToolsAPI.searchCompleted",
                                     &request_id_value,
                                     &file_system_path_value,
