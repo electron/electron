55253db6c87906f535fce019981ac6c1042a69a4
Aleksei Kuzmin <alkuzmin@microsoft.com>
2017-06-17 03:20:31 +0300

    Fix cc/paint skia type mismatches
    https://codereview.chromium.org/2717943002

--- chromium_src/chrome/renderer/printing/print_web_view_helper.cc
+++ chromium_src/chrome/renderer/printing/print_web_view_helper.cc
@@ -354,7 +354,7 @@ float PrintWebViewHelper::RenderPageContent(blink::WebFrame* frame,
                                             const gfx::Rect& content_area,
                                             double scale_factor,
                                             blink::WebCanvas* canvas) {
-  SkAutoCanvasRestore auto_restore(canvas, true);
+  cc::PaintCanvasAutoRestore auto_restore(canvas, true);
   canvas->translate((content_area.x() - canvas_area.x()) / scale_factor,
                     (content_area.y() - canvas_area.y()) / scale_factor);
   return frame->PrintPage(page_number, canvas);
