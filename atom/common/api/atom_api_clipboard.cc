5f2dd2ef6c39cf96abdbd1d6f57d15d4662779c5
Vladimir Vainer <str3l0k@gmail.com>
2017-06-29 16:11:57 +0300

    Fix #8017: Crash copying croped image to clipboard

--- atom/common/api/atom_api_clipboard.cc
+++ atom/common/api/atom_api_clipboard.cc
@@ -7,6 +7,7 @@
 #include "atom/common/native_mate_converters/image_converter.h"
 #include "atom/common/native_mate_converters/string16_converter.h"
 #include "base/strings/utf_string_conversions.h"
+#include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/base/clipboard/scoped_clipboard_writer.h"
 
 #include "atom/common/node_includes.h"
@@ -166,7 +167,12 @@ gfx::Image Clipboard::ReadImage(mate::Arguments* args) {
 
 void Clipboard::WriteImage(const gfx::Image& image, mate::Arguments* args) {
   ui::ScopedClipboardWriter writer(GetClipboardType(args));
-  writer.WriteImage(image.AsBitmap());
+  SkBitmap bmp;
+  if (image.AsBitmap().deepCopyTo(&bmp)) {
+    writer.WriteImage(bmp);
+  } else {
+    writer.WriteImage(image.AsBitmap());
+  }
 }
 
 #if !defined(OS_MACOSX)
