b407834ed79f6ec2b1bd1be0024dcf6d7646aad8
Kevin Sawicki <kevinsawicki@gmail.com>
2017-07-21 14:37:06 -0700

    Remove default

--- tools/xvfb-init.sh
+++ tools/xvfb-init.sh
@@ -1,6 +1,6 @@
 XVFB=/usr/bin/Xvfb
 XVFBARGS="$DISPLAY -ac -screen 0 1024x768x16 +extension RANDR"
-PIDFILE=/var/xvfb_${DISPLAY:1}.pid
+PIDFILE=/var/xvfb_$DISPLAY.pid
 case "$1" in
   start)
     echo -n "Starting virtual X frame buffer: Xvfb"
