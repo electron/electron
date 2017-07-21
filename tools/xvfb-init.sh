26fa785692c43ecb00b24fb84b5f399270ba4d96
Kevin Sawicki <kevinsawicki@gmail.com>
2017-07-21 14:38:48 -0700

    Simplify xvfb init script

--- tools/xvfb-init.sh
+++ tools/xvfb-init.sh
@@ -1,23 +1,3 @@
 XVFB=/usr/bin/Xvfb
 XVFBARGS="$DISPLAY -ac -screen 0 1024x768x16 +extension RANDR"
-PIDFILE=/var/xvfb_$DISPLAY.pid
-case "$1" in
-  start)
-    echo -n "Starting virtual X frame buffer: Xvfb"
-    /sbin/start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background --exec $XVFB -- $XVFBARGS
-    echo "."
-    ;;
-  stop)
-    echo -n "Stopping virtual X frame buffer: Xvfb"
-    /sbin/start-stop-daemon --stop --quiet --pidfile $PIDFILE
-    echo "."
-    ;;
-  restart)
-    $0 stop
-    $0 start
-    ;;
-  *)
-  echo "Usage: /etc/init.d/xvfb {start|stop|restart}"
-  exit 1
-esac
-exit 0
+/sbin/start-stop-daemon --start --quiet --background --exec $XVFB -- $XVFBARGS
