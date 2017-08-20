f00da21dd95bad770400f6f99ab50398d41f161f
Aleksei Kuzmin <alkuzmin@microsoft.com>
2017-08-21 00:35:04 +0300

    Refactor client cert private key handling.
    https://codereview.chromium.org/2898573002

--- atom/browser/atom_browser_client.h
+++ atom/browser/atom_browser_client.h
@@ -12,6 +12,7 @@
 
 #include "brightray/browser/browser_client.h"
 #include "content/public/browser/render_process_host_observer.h"
+#include "net/ssl/client_cert_identity.h"
 
 namespace content {
 class QuotaPermissionContext;
@@ -77,7 +78,7 @@ class AtomBrowserClient : public brightray::BrowserClient,
   void SelectClientCertificate(
       content::WebContents* web_contents,
       net::SSLCertRequestInfo* cert_request_info,
-      net::CertificateList client_certs,
+      net::ClientCertIdentityList client_certs,
       std::unique_ptr<content::ClientCertificateDelegate> delegate) override;
   void ResourceDispatcherHostCreated() override;
   bool CanCreateWindow(
