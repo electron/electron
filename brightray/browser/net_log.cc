0ad967c9a57868acde937811f00e01917e0b3974
Tony Ganch <tonyganch@gmail.com>
2017-08-24 15:08:05 +0200

    Don't specify URLRequestContext when starting logging
    Related CL: https://codereview.chromium.org/2698143004

--- brightray/browser/net_log.cc
+++ brightray/browser/net_log.cc
@@ -11,6 +11,7 @@
 #include "base/memory/ptr_util.h"
 #include "base/values.h"
 #include "content/public/common/content_switches.h"
+#include "net/log/file_net_log_observer.h"
 #include "net/log/net_log_util.h"
 
 namespace brightray {
@@ -38,30 +39,20 @@ NetLog::NetLog() {
 NetLog::~NetLog() {
 }
 
-void NetLog::StartLogging(net::URLRequestContext* url_request_context) {
+void NetLog::StartLogging() {
   auto command_line = base::CommandLine::ForCurrentProcess();
   if (!command_line->HasSwitch(switches::kLogNetLog))
     return;
 
   base::FilePath log_path =
       command_line->GetSwitchValuePath(switches::kLogNetLog);
-#if defined(OS_WIN)
-  log_file_.reset(_wfopen(log_path.value().c_str(), L"w"));
-#elif defined(OS_POSIX)
-  log_file_.reset(fopen(log_path.value().c_str(), "w"));
-#endif
-
-  if (!log_file_) {
-    LOG(ERROR) << "Could not open file: " << log_path.value()
-               << "for net logging";
-    return;
-  }
-
   std::unique_ptr<base::Value> constants(GetConstants());
-  write_to_file_observer_.StartObserving(this,
-                                         std::move(log_file_),
-                                         constants.get(),
-                                         url_request_context);
+  net::NetLogCaptureMode capture_mode =
+      net::NetLogCaptureMode::IncludeCookiesAndCredentials();
+
+  std::unique_ptr<net::FileNetLogObserver> file_net_log_observer_ =
+      net::FileNetLogObserver::CreateUnbounded(log_path, std::move(constants));
+  file_net_log_observer_->StartObserving(this, capture_mode);
 }
 
 }  // namespace brightray
