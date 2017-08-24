fb7661d2d26f57dcd46d3cd861074a4b8fc26930
Samuel Attard <samuel.r.attard@gmail.com>
2017-08-24 10:30:56 +1000

    Merge pull request #10232 from electron/drag-browser-view
    Add -webkit-app-region support to BrowserView

--- atom/browser/native_browser_view_mac.h
+++ atom/browser/native_browser_view_mac.h
@@ -6,8 +6,11 @@
 #define ATOM_BROWSER_NATIVE_BROWSER_VIEW_MAC_H_
 
 #import <Cocoa/Cocoa.h>
+#include <vector>
 
 #include "atom/browser/native_browser_view.h"
+#include "atom/common/draggable_region.h"
+#include "base/mac/scoped_nsobject.h"
 
 namespace atom {
 
@@ -20,6 +23,8 @@ class NativeBrowserViewMac : public NativeBrowserView {
   void SetAutoResizeFlags(uint8_t flags) override;
   void SetBounds(const gfx::Rect& bounds) override;
   void SetBackgroundColor(SkColor color) override;
+  void UpdateDraggableRegions(
+      const std::vector<gfx::Rect>& system_drag_exclude_areas) override;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(NativeBrowserViewMac);
