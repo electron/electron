ebc6e54b27835ba367dcdda41927215a9558f085
Arnav Borborah <arnavborborah11@gmail.com>
2017-08-25 12:23:00 -0400

    :art: Changed a = a + b + c to a += b + c

--- atom/browser/atom_web_ui_controller_factory.cc
+++ atom/browser/atom_web_ui_controller_factory.cc
@@ -63,7 +63,7 @@ AtomWebUIControllerFactory::CreateWebUIControllerForURL(content::WebUI* web_ui,
       }
     }
     if (url.has_ref()) {
-      src = src + '#' + url.ref();
+      src += '#' + url.ref();
     }
     auto browser_context = web_ui->GetWebContents()->GetBrowserContext();
     return new PdfViewerUI(browser_context, web_ui, src);
