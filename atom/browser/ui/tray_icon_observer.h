0b205019b6ff068bfff9508434e7c8bf7379c261
Sean Bannigan <bannigan@meetmaestro.com>
2017-08-26 14:04:58 -0400

    add mouse-move event and click event position for tray

--- atom/browser/ui/tray_icon_observer.h
+++ atom/browser/ui/tray_icon_observer.h
@@ -17,7 +17,7 @@ namespace atom {
 
 class TrayIconObserver {
  public:
-  virtual void OnClicked(const gfx::Rect& bounds, int modifiers) {}
+  virtual void OnClicked(const gfx::Rect& bounds, const gfx::Point& location, int modifiers) {}
   virtual void OnDoubleClicked(const gfx::Rect& bounds, int modifiers) {}
   virtual void OnBalloonShow() {}
   virtual void OnBalloonClicked() {}
@@ -31,6 +31,7 @@ class TrayIconObserver {
   virtual void OnDragEnded() {}
   virtual void OnMouseEntered(const gfx::Point& location, int modifiers) {}
   virtual void OnMouseExited(const gfx::Point& location, int modifiers) {}
+  virtual void OnMouseMoved(const gfx::Point& location, int modifiers) {}
 
  protected:
   virtual ~TrayIconObserver() {}
