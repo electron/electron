d51cf0f9de513b374c52bf9576a9bed511a4c0f6
Arnav Borborah <arnavborborah11@gmail.com>
2017-08-28 14:23:17 -0400

    :art: Changed return to break for clarity

--- atom/browser/lib/bluetooth_chooser.cc
+++ atom/browser/lib/bluetooth_chooser.cc
@@ -31,11 +31,10 @@ const int kMaxScanRetries = 5;
 void OnDeviceChosen(
     const content::BluetoothChooser::EventHandler& handler,
     const std::string& device_id) {
-  if (device_id.empty()) {
+  if (device_id.empty())
     handler.Run(content::BluetoothChooser::Event::CANCELLED, device_id);
-  } else {
+  else
     handler.Run(content::BluetoothChooser::Event::SELECTED, device_id);
-  }
 }
 
 }  // namespace
@@ -103,7 +102,7 @@ void BluetoothChooser::RemoveDevice(const std::string& device_id) {
   for (auto it = device_list_.begin(); it != device_list_.end(); ++it) {
     if (it->device_id == device_id) {
       device_list_.erase(it);
-      return;
+      break;
     }
   }
 }
