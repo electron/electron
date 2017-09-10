79e59a88b486c556677189d6c5c6a47799ccacd5
Aleksei Kuzmin <alkuzmin@microsoft.com>
2017-09-10 16:29:05 +0200

    Rename ScopedComPtr::Receive to ScopedComPtr::GetAddressOf
    https://codereview.chromium.org/2870263002

--- atom/common/platform_util_win.cc
+++ atom/common/platform_util_win.cc
@@ -243,7 +243,7 @@ bool ShowItemInFolder(const base::FilePath& full_path) {
   }
 
   base::win::ScopedComPtr<IShellFolder> desktop;
-  HRESULT hr = SHGetDesktopFolder(desktop.Receive());
+  HRESULT hr = SHGetDesktopFolder(desktop.GetAddressOf());
   if (FAILED(hr))
     return false;
 
@@ -356,9 +356,10 @@ bool MoveItemToTrash(const base::FilePath& path) {
 
   // Create an IShellItem from the supplied source path.
   base::win::ScopedComPtr<IShellItem> delete_item;
-  if (FAILED(SHCreateItemFromParsingName(path.value().c_str(),
-                                         NULL,
-                                         IID_PPV_ARGS(delete_item.Receive()))))
+  if (FAILED(SHCreateItemFromParsingName(
+      path.value().c_str(),
+      NULL,
+      IID_PPV_ARGS(delete_item.GetAddressOf()))))
     return false;
 
   base::win::ScopedComPtr<IFileOperationProgressSink> delete_sink(
