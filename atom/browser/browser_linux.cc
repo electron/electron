a1347f67a13cd51cf6e00453156541274dc5c894
Shelley Vohr <shelley.vohr@gmail.com>
2017-10-06 23:40:58 -0400

    remove a space from a comment wow thanks linter

--- atom/browser/browser_linux.cc
+++ atom/browser/browser_linux.cc
@@ -121,7 +121,7 @@ bool Browser::IsDefaultProtocolClient(const std::string& protocol,
   #endif
 }
 
-// Todo implement 
+// Todo implement
 bool Browser::RemoveAsDefaultProtocolClient(const std::string& protocol,
                                             mate::Arguments* args) {
   return false;
