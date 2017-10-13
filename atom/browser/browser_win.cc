4cd1912ad8f9c7bf71bc6f864f7ad5d060751450
Robin Andersson <robin.andersson@digiexam.se>
2017-10-13 12:35:29 +0200

    Fixed the application level focus issues with Windows 10

--- atom/browser/browser_win.cc
+++ atom/browser/browser_win.cc
@@ -36,7 +36,23 @@ BOOL CALLBACK WindowsEnumerationHandler(HWND hwnd, LPARAM param) {
 
   GetWindowThreadProcessId(hwnd, &process_id);
   if (process_id == target_process_id) {
+    // To unlock SetForegroundWindow we need to imitate pressing the Alt key
+    // This circumvents the ForegroundLockTimeout in Windows 10
+    bool bPressed = false;
+    if((GetAsyncKeyState(VK_MENU) & 0x8000) == 0)
+    {
+        bPressed = true;
+        keybd_event(VK_MENU, 0, KEYEVENTF_EXTENDEDKEY | 0, 0);
+    }
+
+    SetForegroundWindow(hwnd);
     SetFocus(hwnd);
+
+    if(bPressed)
+    {
+        keybd_event(VK_MENU, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+    }
+
     return FALSE;
   }
 
