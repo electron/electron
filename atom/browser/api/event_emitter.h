77a26882a30a3dbd20987b5fc98ec34575c618d2
Matt Crocker <mcrocker@slack-corp.com>
2017-10-23 22:58:43 -0700

    Make linter happy

--- atom/browser/api/event_emitter.h
+++ atom/browser/api/event_emitter.h
@@ -80,9 +80,10 @@ class EventEmitter : public Wrappable<T> {
     v8::Locker locker(isolate());
     v8::HandleScope handle_scope(isolate());
     v8::Local<v8::Object> wrapper = GetWrapper();
-    if (wrapper.IsEmpty())
+    if (wrapper.IsEmpty()) {
       return false;
-     v8::Local<v8::Object> event = internal::CreateJSEvent(
+    }
+    v8::Local<v8::Object> event = internal::CreateJSEvent(
         isolate(), wrapper, sender, message);
     return EmitWithEvent(name, event, args...);
   }
