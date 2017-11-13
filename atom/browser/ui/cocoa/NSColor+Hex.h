a4905b3c958b206ad209a8fb8aadbc9813a1c547
Sebastien Bramille <sebastien.bramille@gmail.com>
2017-11-13 17:22:01 +0000

    Lint

--- atom/browser/ui/cocoa/NSColor+Hex.h
+++ atom/browser/ui/cocoa/NSColor+Hex.h
@@ -5,12 +5,17 @@
 //  Created by Mathias Leppich on 03/02/14.
 //  Copyright (c) 2014 Bit Bar. All rights reserved.
 //
+#ifndef ATOM_BROWSER_UI_COCOA_NSCOLOR_HEX_H_
+#define ATOM_BROWSER_UI_COCOA_NSCOLOR_HEX_H_
 
 #import <Cocoa/Cocoa.h>
+#import <string>
 
-@interface NSColor (Hex)
+@interface NSColor(Hex)
 
 + (NSColor*) colorWithWebColorString:(NSString*)color;
 + (NSColor*) colorWithHexColorString:(NSString*)hex;
 
 @end
+
+#endif  // ATOM_BROWSER_UI_COCOA_NSCOLOR_HEX_H_
--- atom/browser/ui/cocoa/NSString+ANSI.h
+++ atom/browser/ui/cocoa/NSString+ANSI.h
@@ -6,11 +6,16 @@
 //  Copyright © 2016 Bit Bar. All rights reserved.
 //
 
+#ifndef ATOM_BROWSER_UI_COCOA_NSSTRING_ANSI_H_
+#define ATOM_BROWSER_UI_COCOA_NSSTRING_ANSI_H_
+
 #import <Foundation/Foundation.h>
 
-@interface NSString (ANSI)
+@interface NSString(ANSI)
 
 - (BOOL)containsANSICodes;
 - (NSMutableAttributedString*)attributedStringParsingANSICodes;
 
 @end
+
+#endif  // ATOM_BROWSER_UI_COCOA_NSSTRING_ANSI_H_
