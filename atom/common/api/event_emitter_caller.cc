3c0b233d043e75d257217cf025b25cef40260e7b
Samuel Attard <samuel.r.attard@gmail.com>
2017-11-13 11:42:42 +1100

    Fix line length in caller.cc

--- atom/common/api/event_emitter_caller.cc
+++ atom/common/api/event_emitter_caller.cc
@@ -20,8 +20,9 @@ v8::Local<v8::Value> CallMethodWithArgs(v8::Isolate* isolate,
                                    v8::MicrotasksScope::kRunMicrotasks);
   // Use node::MakeCallback to call the callback, and it will also run pending
   // tasks in Node.js.
-  v8::MaybeLocal<v8::Value> ret = node::MakeCallback(isolate, obj, method, args->size(), &args->front(),
-                            {0, 0});
+  v8::MaybeLocal<v8::Value> ret = node::MakeCallback(isolate, obj, method,
+                                                     args->size(),
+                                                     &args->front(), {0, 0});
   if (ret.IsEmpty()) {
     return v8::Boolean::New(isolate, false);
   }
