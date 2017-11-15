8a8f1696285994e31f8a428d9ae98441b27adaa1
Samuel Attard <samuel.r.attard@gmail.com>
2017-11-15 18:21:47 +1100

    Add comments and use ToLocal instead of ToLocalChecked

--- atom/common/api/event_emitter_caller.cc
+++ atom/common/api/event_emitter_caller.cc
@@ -20,13 +20,22 @@ v8::Local<v8::Value> CallMethodWithArgs(v8::Isolate* isolate,
                                    v8::MicrotasksScope::kRunMicrotasks);
   // Use node::MakeCallback to call the callback, and it will also run pending
   // tasks in Node.js.
-  v8::MaybeLocal<v8::Value> ret = node::MakeCallback(isolate, obj, method,
-                                                     args->size(),
-                                                     &args->front(), {0, 0});
+  v8::MaybeLocal<v8::Value> ret = node::MakeCallback(isolate, obj, method, args->size(), &args->front(),
+                            {0, 0});
+  // If the JS function throws an exception (doesn't return a value) the result
+  // of MakeCallback will be empty, in this case we need to return "false" as
+  // that indicates that the event emitter did not handle the event
   if (ret.IsEmpty()) {
     return v8::Boolean::New(isolate, false);
   }
-  return ret.ToLocalChecked();
+
+  v8::Local<v8::Value> localRet;
+  if (ret.ToLocal(&localRet)) {
+    return localRet;
+  }
+  // Should be unreachable, but the compiler complains if we don't check
+  // the result of ToLocal
+  return v8::Undefined(isolate);
 }
 
 }  // namespace internal
