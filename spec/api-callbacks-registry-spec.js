e5983eacd6f68b1ba27129dc0a17e97395b347e6
Shelley Vohr <shelley.vohr@gmail.com>
2017-11-16 00:04:33 -0500

    add callbacks registry spec

---
+++ spec/api-callbacks-registry-spec.js
@@ -0,0 +1,42 @@
+const assert = require('assert')
+const {CallbacksRegistry} = require('electron')
+
+describe.only('CallbacksRegistry module', () => {
+  let registry = null
+
+  beforeEach(() => {
+    registry = new CallbacksRegistry()
+  })
+
+  it('adds a callback to the registry', () => {
+    const cb = () => [1, 2, 3, 4, 5]
+    const id = registry.add(cb)
+    assert.equal(id, 1)
+  })
+
+  it('returns a specified callback if it is in the registry', () => {
+    const cb = () => [1, 2, 3, 4, 5]
+    registry.add(cb)
+
+    const callback = registry.get(1)
+    assert.equal(callback.toString(), cb.toString())
+  })
+
+  it('returns an empty function if the cb doesnt exist', () => {
+    const callback = registry.get(1)
+    assert.equal(callback.toString(), 'function () {}')
+  })
+
+  it('removes a callback to the registry', () => {
+    const cb = () => [1, 2, 3, 4, 5]
+    const id = registry.add(cb)
+    assert.equal(id, 1)
+
+    const beforeCB = registry.get(1)
+    assert.equal(beforeCB.toString(), cb.toString())
+
+    registry.remove(1)
+    const afterCB = registry.get(1)
+    assert.equal(afterCB.toString(), 'function () {}')
+  })
+})
