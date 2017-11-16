933c7330a551d00b43907f73cbcd288ffc3eafcd
Shelley Vohr <shelley.vohr@gmail.com>
2017-11-16 13:39:07 -0500

    changes from review

--- spec/api-callbacks-registry-spec.js
+++ spec/api-callbacks-registry-spec.js
@@ -1,7 +1,7 @@
-const assert = require('assert')
+const {assert} = require('chai')
 const {CallbacksRegistry} = require('electron')
 
-describe('CallbacksRegistry module', () => {
+describe.only('CallbacksRegistry module', () => {
   let registry = null
 
   beforeEach(() => {
@@ -10,33 +10,39 @@ describe('CallbacksRegistry module', () => {
 
   it('adds a callback to the registry', () => {
     const cb = () => [1, 2, 3, 4, 5]
-    const id = registry.add(cb)
-    assert.equal(id, 1)
+    const key = registry.add(cb)
+
+    assert.exists(key)
   })
 
   it('returns a specified callback if it is in the registry', () => {
     const cb = () => [1, 2, 3, 4, 5]
-    registry.add(cb)
+    const key = registry.add(cb)
+    const callback = registry.get(key)
 
-    const callback = registry.get(1)
     assert.equal(callback.toString(), cb.toString())
   })
 
   it('returns an empty function if the cb doesnt exist', () => {
     const callback = registry.get(1)
-    assert.equal(callback.toString(), 'function () {}')
+
+    assert.isFunction(callback)
   })
 
   it('removes a callback to the registry', () => {
     const cb = () => [1, 2, 3, 4, 5]
-    const id = registry.add(cb)
-    assert.equal(id, 1)
+    const key = registry.add(cb)
 
-    const beforeCB = registry.get(1)
+    assert.exists(key)
+
+    const beforeCB = registry.get(key)
+
     assert.equal(beforeCB.toString(), cb.toString())
 
-    registry.remove(1)
-    const afterCB = registry.get(1)
-    assert.equal(afterCB.toString(), 'function () {}')
+    registry.remove(key)
+    const afterCB = registry.get(key)
+
+    assert.isFunction(afterCB)
+    assert.notEqual(afterCB.toString(), cb.toString())
   })
 })
