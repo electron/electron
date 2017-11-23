9193e3fdbbac03a732da0ae1fa6da7fb73253488
Heilig Benedek <benecene@gmail.com>
2017-11-23 20:58:37 +0100

    update tests to reflect new behaviour

--- spec/api-browser-window-spec.js
+++ spec/api-browser-window-spec.js
@@ -2177,8 +2177,11 @@ describe('BrowserWindow module', () => {
         assert.equal(w.isMaximizable(), true)
         w.setFullScreenable(false)
         assert.equal(w.isMaximizable(), true)
+      })
+
+      it('is set to false when resizable state is set to false', () => {
         w.setResizable(false)
-        assert.equal(w.isMaximizable(), true)
+        assert.equal(w.isMaximizable(), false)
       })
     })
 
