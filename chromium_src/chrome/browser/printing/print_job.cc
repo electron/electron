024f84f1beafb2f827559b2b9580863713e1b8c5
Tomas Rycl <torycl@microsoft.com>
2017-11-23 14:14:01 +0000

    chromium_src print_job updated to match current chromium implementation in Quit method

--- chromium_src/chrome/browser/printing/print_job.cc
+++ chromium_src/chrome/browser/printing/print_job.cc
@@ -459,7 +459,7 @@ void PrintJob::HoldUntilStopIsCalled() {
 }
 
 void PrintJob::Quit() {
-  base::MessageLoop::current()->QuitWhenIdle();
+  base::RunLoop::QuitCurrentWhenIdleDeprecated();
 }
 
 // Takes settings_ ownership and will be deleted in the receiving thread.
