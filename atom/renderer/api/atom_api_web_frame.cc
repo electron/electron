f1740b65954855ab8a4517845e510e6bb22eaa20
Aleksei Kuzmin <alkuzmin@microsoft.com>
2017-11-23 21:12:06 +0100

    Rename CompositionUnderline to ImeTextSpan
    https://chromium-review.googlesource.com/609524

--- atom/renderer/api/atom_api_web_frame.cc
+++ atom/renderer/api/atom_api_web_frame.cc
@@ -20,6 +20,7 @@
 #include "third_party/WebKit/public/platform/WebCache.h"
 #include "third_party/WebKit/public/web/WebDocument.h"
 #include "third_party/WebKit/public/web/WebFrameWidget.h"
+#include "third_party/WebKit/public/web/WebImeTextSpan.h"
 #include "third_party/WebKit/public/web/WebInputMethodController.h"
 #include "third_party/WebKit/public/web/WebLocalFrame.h"
 #include "third_party/WebKit/public/web/WebScriptExecutionCallback.h"
@@ -263,7 +264,7 @@ void WebFrame::InsertText(const std::string& text) {
   web_frame_->FrameWidget()
             ->GetActiveWebInputMethodController()
             ->CommitText(blink::WebString::FromUTF8(text),
-                         blink::WebVector<blink::WebCompositionUnderline>(),
+                         blink::WebVector<blink::WebImeTextSpan>(),
                          blink::WebRange(),
                          0);
 }
