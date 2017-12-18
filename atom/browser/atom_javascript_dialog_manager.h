ce755483ba175b3d9a98e2ffdde9d810be726c93
Samuel Attard <samuel.r.attard@gmail.com>
2017-12-18 12:24:13 +1100

    DialogClosedCallback are not const references anymore

--- atom/browser/atom_javascript_dialog_manager.h
+++ atom/browser/atom_javascript_dialog_manager.h
@@ -27,12 +27,12 @@ class AtomJavaScriptDialogManager : public content::JavaScriptDialogManager {
       content::JavaScriptDialogType dialog_type,
       const base::string16& message_text,
       const base::string16& default_prompt_text,
-      const DialogClosedCallback& callback,
+      DialogClosedCallback callback,
       bool* did_suppress_message) override;
   void RunBeforeUnloadDialog(
       content::WebContents* web_contents,
       bool is_reload,
-      const DialogClosedCallback& callback) override;
+      DialogClosedCallback callback) override;
   void CancelDialogs(content::WebContents* web_contents,
                      bool reset_state) override;
 
