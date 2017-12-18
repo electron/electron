e65815e34c882140bc044b2b676bbc053898f084
Samuel Attard <samuel.r.attard@gmail.com>
2017-12-18 12:40:29 +1100

    Implement GetBackgroundFetchDelegate in brightray::BrowserContext

--- brightray/browser/browser_context.cc
+++ brightray/browser/browser_context.cc
@@ -199,6 +199,11 @@ content::PermissionManager* BrowserContext::GetPermissionManager() {
   return permission_manager_.get();
 }
 
+content::BackgroundFetchDelegate*
+BrowserContext::GetBackgroundFetchDelegate() {
+  return nullptr;
+}
+
 content::BackgroundSyncController*
 BrowserContext::GetBackgroundSyncController() {
   return nullptr;
