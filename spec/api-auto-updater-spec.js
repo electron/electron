e847615baa7ee0352364541aaade0f0f7148e1d2
Shelley Vohr <shelley.vohr@gmail.com>
2017-12-18 22:44:20 -0500

    formatting cleanup for autoupdater spec

--- spec/api-auto-updater-spec.js
+++ spec/api-auto-updater-spec.js
@@ -2,62 +2,53 @@ const assert = require('assert')
 const {autoUpdater} = require('electron').remote
 const {ipcRenderer} = require('electron')
 
-describe('autoUpdater module', function () {
-  // XXX(alexeykuzmin): Calling `.skip()` in a 'before' hook
-  // doesn't affect nested 'describe's
+describe.only('autoUpdater module', () => {
   beforeEach(function () {
-    // Skip autoUpdater tests in MAS build.
     if (process.mas) {
       this.skip()
     }
   })
 
-  describe('checkForUpdates', function () {
-    it('emits an error on Windows when called the feed URL is not set', function (done) {
-      if (process.platform !== 'win32') {
-        // FIXME(alexeykuzmin): Skip the test.
-        // this.skip()
-        return done()
-      }
+  describe('checkForUpdates', () => {
+    it('emits an error on Windows when called the feed URL is not set', (done) => {
+      if (process.platform !== 'win32') return done()
 
-      ipcRenderer.once('auto-updater-error', function (event, message) {
+      ipcRenderer.once('auto-updater-error', (event, message) => {
         assert.equal(message, 'Update URL is not set')
         done()
       })
+
       autoUpdater.setFeedURL('')
       autoUpdater.checkForUpdates()
     })
   })
 
-  describe('setFeedURL', function () {
-    describe('on Mac', function () {
+  describe('setFeedURL', () => {
+    describe('on Mac', () => {
       before(function () {
         if (process.platform !== 'darwin') {
           this.skip()
         }
       })
 
-      it('emits an error when the application is unsigned', function (done) {
-        ipcRenderer.once('auto-updater-error', function (event, message) {
+      it('emits an error when the application is unsigned', (done) => {
+        ipcRenderer.once('auto-updater-error', (event, message) => {
           assert.equal(message, 'Could not get code signature for running application')
           done()
         })
+
         autoUpdater.setFeedURL('')
       })
     })
   })
 
-  describe('getFeedURL', function () {
-    it('returns a falsey value by default', function () {
+  describe('getFeedURL', () => {
+    it('returns a falsey value by default', () => {
       assert.ok(!autoUpdater.getFeedURL())
     })
 
-    it('correctly fetches the previously set FeedURL', function (done) {
-      if (process.platform !== 'win32') {
-        // FIXME(alexeykuzmin): Skip the test.
-        // this.skip()
-        return done()
-      }
+    it('correctly fetches the previously set FeedURL', (done) => {
+      if (process.platform !== 'win32') return done()
 
       const updateURL = 'https://fake-update.electron.io'
       autoUpdater.setFeedURL(updateURL)
@@ -66,31 +57,24 @@ describe('autoUpdater module', function () {
     })
   })
 
-  describe('quitAndInstall', function () {
-    it('emits an error on Windows when no update is available', function (done) {
-      if (process.platform !== 'win32') {
-        // FIXME(alexeykuzmin): Skip the test.
-        // this.skip()
-        return done()
-      }
+  describe('quitAndInstall', () => {
+    it('emits an error on Windows when no update is available', (done) => {
+      if (process.platform !== 'win32') return done()
 
-      ipcRenderer.once('auto-updater-error', function (event, message) {
+      ipcRenderer.once('auto-updater-error', (event, message) => {
         assert.equal(message, 'No update available, can\'t quit and install')
         done()
       })
+
       autoUpdater.quitAndInstall()
     })
   })
 
-  describe('error event', function () {
-    it('serializes correctly over the remote module', function (done) {
-      if (process.platform === 'linux') {
-        // FIXME(alexeykuzmin): Skip the test.
-        // this.skip()
-        return done()
-      }
+  describe('error event', () => {
+    it('serializes correctly over the remote module', (done) => {
+      if (process.platform === 'linux') return done()
 
-      autoUpdater.once('error', function (error) {
+      autoUpdater.once('error', (error) => {
         assert.equal(error instanceof Error, true)
         assert.deepEqual(Object.getOwnPropertyNames(error), ['stack', 'message', 'name'])
         done()
