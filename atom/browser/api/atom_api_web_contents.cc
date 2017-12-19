809d44630fe9e53b8a75e87bbd35eb75a3b8cfb8
Samuel Attard <samuel.r.attard@gmail.com>
2017-12-19 11:57:10 +1100

    Temporarily disable OnCursorChange
    ScopedProfile is removed
    https://chromium-review.googlesource.com/c/chromium/src/+/655147

--- atom/browser/api/atom_api_web_contents.cc
+++ atom/browser/api/atom_api_web_contents.cc
@@ -967,7 +967,7 @@ bool WebContents::OnMessageReceived(const IPC::Message& message) {
     IPC_MESSAGE_HANDLER_DELAY_REPLY(AtomViewHostMsg_GetZoomLevel,
                                     OnGetZoomLevel)
     IPC_MESSAGE_HANDLER_CODE(ViewHostMsg_SetCursor, OnCursorChange,
-      handled = false)
+                             handled = false)
     IPC_MESSAGE_UNHANDLED(handled = false)
   IPC_END_MESSAGE_MAP()
 
