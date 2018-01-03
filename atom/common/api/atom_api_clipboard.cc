107d3934c59487ec3402f614749a55b25f35b248
Shelley Vohr <shelley.vohr@gmail.com>
2018-01-03 11:43:36 -0800

    NODE_MODULE_CONTEXT_AWARE_BUILTIN -> NODE_BUILTIN_MODULE_CONTEXT_AWARE

--- atom/common/api/atom_api_clipboard.cc
+++ atom/common/api/atom_api_clipboard.cc
@@ -261,4 +261,4 @@ void Initialize(v8::Local<v8::Object> exports, v8::Local<v8::Value> unused,
 
 }  // namespace
 
-NODE_MODULE_CONTEXT_AWARE_BUILTIN(atom_common_clipboard, Initialize)
+NODE_BUILTIN_MODULE_CONTEXT_AWARE(atom_common_clipboard, Initialize)
