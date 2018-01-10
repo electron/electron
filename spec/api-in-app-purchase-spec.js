bdeb979d412e727bf5727da4fe053bcf94420966
Cheng Zhao <zcbenz@gmail.com>
2018-01-10 15:21:53 +0900

    spec: Simple tests for inAppPurchase module

---
+++ spec/api-in-app-purchase-spec.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const assert = require('assert')
+
+const {remote} = require('electron')
+const {inAppPurchase} = remote
+
+describe('inAppPurchase module', () => {
+  if (process.platform !== 'darwin') return
+
+  it('canMakePayments() does not throw', () => {
+    inAppPurchase.canMakePayments()
+  })
+
+  it('getReceiptURL() returns receipt URL', () => {
+    assert.ok(inAppPurchase.getReceiptURL().endsWith('_MASReceipt/receipt'))
+  })
+
+  it('purchaseProduct() fails when buying invalid product', (done) => {
+    inAppPurchase.purchaseProduct('non-exist', 1, (success) => {
+      assert.ok(!success)
+      done()
+    })
+  })
+})
