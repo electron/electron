c934ee37d8422b727a6e5c2d089407b477e04be7
Nitish Sakhawalkar <nitsakh@icloud.com>
2018-01-13 17:56:25 -0800

    Fix macos Full Screen Toggle menu item label

--- atom/browser/native_window_mac.mm
+++ atom/browser/native_window_mac.mm
@@ -732,7 +732,7 @@ enum {
     [super performClose:sender];
 }
 
-- (void)toggleFullScreen:(id)sender {
+- (void)toggleFullScreenMode:(id)sender {
   if (shell_->simple_fullscreen())
     shell_->SetSimpleFullScreen(!shell_->IsSimpleFullScreen());
   else
@@ -1178,7 +1178,7 @@ void NativeWindowMac::SetFullScreen(bool fullscreen) {
   if (fullscreen == IsFullscreen())
     return;
 
-  [window_ toggleFullScreen:nil];
+  [window_ toggleFullScreenMode:nil];
 }
 
 bool NativeWindowMac::IsFullscreen() const {
