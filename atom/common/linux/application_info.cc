e01cdb2345d440cd5d9b3e368430ac4ed015437a
Charles Kerr <ckerr@github.com>
2018-02-20 08:29:22 -0600

    on Linux, have GetApplicationVersion() use brightray version API

--- atom/common/linux/application_info.cc
+++ atom/common/linux/application_info.cc
@@ -2,9 +2,12 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
+#include "brightray/common/application_info.h"
+
 #include <string>
 
 #include "atom/common/atom_version.h"
+#include "base/log.h"
 
 namespace brightray {
 
@@ -13,7 +16,23 @@ std::string GetApplicationName() {
 }
 
 std::string GetApplicationVersion() {
-  return ATOM_VERSION_STRING;
+  std::string ret;
+
+  // ensure ATOM_PRODUCT_NAME and ATOM_PRODUCT_STRING match up
+  if (GetApplicationName() == ATOM_PRODUCT_NAME)
+    ret = ATOM_VERSION_STRING;
+
+  // try to use the string set in app.setVersion()
+  if (ret.empty())
+    ret = GetOverriddenApplicationVersion();
+
+  // no known version number; return some safe fallback
+  if (ret.empty()) {
+    LOG(WARNING) << "No version found. Was app.setVersion() called?";
+    ret = "0.0";
+  }
+
+  return ret;
 }
 
 }  // namespace brightray
