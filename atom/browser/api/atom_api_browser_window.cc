c2aa312e0cbcaab72b3b2f4dbd6b487c23968aec
Cheng Zhao <zcbenz@gmail.com>
2018-02-22 15:10:33 +0900

    Check weak ptr before using it

--- atom/browser/api/atom_api_browser_window.cc
+++ atom/browser/api/atom_api_browser_window.cc
@@ -205,7 +205,8 @@ void BrowserWindow::DidFirstVisuallyNonEmptyPaint() {
   base::ThreadTaskRunnerHandle::Get()->PostTask(
       FROM_HERE,
       base::Bind([](base::WeakPtr<BrowserWindow> self) {
-        self->Emit("ready-to-show");
+        if (self)
+          self->Emit("ready-to-show");
       }, GetWeakPtr()));
 }
 
