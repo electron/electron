a5e8be034991368e22433ce438d7cde24bb6594b
Shelley Vohr <shelley.vohr@gmail.com>
2018-02-23 00:08:53 -0500

    use urllib2 bnut force TLS1.2

--- script/lib/util.py
+++ script/lib/util.py
@@ -11,7 +11,7 @@ import subprocess
 import sys
 import tarfile
 import tempfile
-import urllib3
+import urllib2
 import os
 import zipfile
 
@@ -82,7 +82,8 @@ def download(text, url, path):
     if hasattr(ssl, '_create_unverified_context'):
       ssl._create_default_https_context = ssl._create_unverified_context
 
-    web_file = urllib3.urlopen(url)
+    ctx = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    web_file = urllib2.urlopen(url, context=ctx)
     file_size = int(web_file.info().getheaders("Content-Length")[0])
     downloaded_size = 0
     block_size = 128
