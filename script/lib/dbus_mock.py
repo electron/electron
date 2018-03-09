1f0568a77c28a3aea6b183c6cd9856dbd8cbdd74
Charles Kerr <ckerr@github.com>
2018-03-09 16:44:59 +0900

    DBusMock now honors verbose mode flag

--- script/lib/dbus_mock.py
+++ script/lib/dbus_mock.py
@@ -1,6 +1,9 @@
+from config import is_verbose_mode
 from dbusmock import DBusTestCase
 
 import atexit
+import os
+import sys
 
 
 def cleanup():
@@ -10,8 +13,10 @@ def cleanup():
 
 atexit.register(cleanup)
 
+dbusmock_log = sys.stdout if is_verbose_mode() else open(os.devnull, 'w')
+
 DBusTestCase.start_system_bus()
-DBusTestCase.spawn_server_template('logind')
+DBusTestCase.spawn_server_template('logind', None, dbusmock_log)
 
 DBusTestCase.start_session_bus()
-DBusTestCase.spawn_server_template('notification_daemon')
+DBusTestCase.spawn_server_template('notification_daemon', None, dbusmock_log)
