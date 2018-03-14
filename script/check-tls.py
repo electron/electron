86653fb2fbf7a87ad87654e57bf0fbd096c63ed3
Felix Rieseberg <felix@felixrieseberg.com>
2018-03-14 13:00:03 -0700

    :wrench: Make the linter happy

--- script/check-tls.py
+++ script/check-tls.py
@@ -5,7 +5,8 @@ import urllib2
 import sys
 
 def main():
-  tls = json.load(urllib2.urlopen('https://www.howsmyssl.com/a/check'))['tls_version']
+  response = json.load(urllib2.urlopen('https://www.howsmyssl.com/a/check'))
+  tls = response['tls_version']
 
   if sys.platform == "linux" or sys.platform == "linux2":
     tutorial = "./docs/development/build-instructions-linux.md"
@@ -14,15 +15,18 @@ def main():
   elif sys.platform == "win32":
     tutorial = "./docs/development/build-instructions-windows.md"
   else:
-    tutorial = "build instructions for your operation system in ./docs/development/"
+    tutorial = "build instructions for your operation system" \
+      + "in ./docs/development/"
 
   if tls == "TLS 1.0":
-    print "Your system/python combination is using an outdated security protocol and will not"
-    print "be able to compile Electron. Please see " + tutorial + "."
-    print "for instructions on how to update Python."
+    print "Your system/python combination is using an outdated security" \
+      + "protocol and will not be able to compile Electron. Please see " \
+      + tutorial + "." \
+      + "for instructions on how to update Python."
     sys.exit(1)
   else:
-    print "Your Python is using " + tls + ", which is sufficient for building Electron."
+    print "Your Python is using " + tls + ", which is sufficient for" \
+      + "building Electron."
     sys.exit(0)
 
 if __name__ == '__main__':
