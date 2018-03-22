24c21a549b0edaca2bb22d17975fa815ff70947d
Hiroki Takami <htk3@outlook.jp>
2018-03-23 00:38:13 +0900

    add compatibility with old app-command name

--- atom/browser/api/atom_api_top_level_window.cc
+++ atom/browser/api/atom_api_top_level_window.cc
@@ -243,6 +243,8 @@ void TopLevelWindow::OnWindowLeaveHtmlFullScreen() {
 }
 
 void TopLevelWindow::OnExecuteWindowsCommand(const std::string& command_name) {
+  if (command_name == "media-play-pause")
+    Emit("app-command", "media-play_pause");
   Emit("app-command", command_name);
 }
 
