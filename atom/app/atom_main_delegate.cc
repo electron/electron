e6e4b75a8151abc56f763eea59fb51aadd5ba052
deepak1556 <hop2deep@gmail.com>
2018-03-28 15:02:24 +0530

    AtomBrowserClient::GetGeolocationApiKey returns the right default

--- atom/app/atom_main_delegate.cc
+++ atom/app/atom_main_delegate.cc
@@ -128,11 +128,6 @@ bool AtomMainDelegate::BasicStartupComplete(int* exit_code) {
 void AtomMainDelegate::PreSandboxStartup() {
   brightray::MainDelegate::PreSandboxStartup();
 
-  // Set google API key.
-  std::unique_ptr<base::Environment> env(base::Environment::Create());
-  if (!env->HasVar("GOOGLE_API_KEY"))
-    env->SetVar("GOOGLE_API_KEY", GOOGLEAPIS_API_KEY);
-
   auto command_line = base::CommandLine::ForCurrentProcess();
   std::string process_type = command_line->GetSwitchValueASCII(
       ::switches::kProcessType);
