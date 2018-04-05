e2e26dd9749974edc359f40e1d099edadaa10a21
Aleksei Kuzmin <alkuzmin@microsoft.com>
2018-04-05 23:09:37 +0200

    Adds ability for BrowserMainParts::PreEarlyInitialization() to fail
    https://chromium-review.googlesource.com/862964

--- atom/browser/atom_browser_main_parts.cc
+++ atom/browser/atom_browser_main_parts.cc
@@ -20,6 +20,7 @@
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/browser_process.h"
 #include "content/public/browser/child_process_security_policy.h"
+#include "content/public/common/result_codes.h"
 #include "device/geolocation/geolocation_delegate.h"
 #include "device/geolocation/geolocation_provider.h"
 #include "ui/base/idle/idle.h"
@@ -126,11 +127,16 @@ void AtomBrowserMainParts::RegisterDestructionCallback(
   destructors_.insert(destructors_.begin(), std::move(callback));
 }
 
-void AtomBrowserMainParts::PreEarlyInitialization() {
-  brightray::BrowserMainParts::PreEarlyInitialization();
+int AtomBrowserMainParts::PreEarlyInitialization() {
+  const int result = brightray::BrowserMainParts::PreEarlyInitialization();
+  if (result != content::RESULT_CODE_NORMAL_EXIT)
+    return result;
+
 #if defined(OS_POSIX)
   HandleSIGCHLD();
 #endif
+
+  return content::RESULT_CODE_NORMAL_EXIT;
 }
 
 void AtomBrowserMainParts::PostEarlyInitialization() {
