167ae82524cc645d54fa8e4ef95795aea42d98ca
Samuel Attard <samuel.r.attard@gmail.com>
2018-04-10 01:07:39 +1000

    fix linting

--- atom/browser/web_contents_preferences.cc
+++ atom/browser/web_contents_preferences.cc
@@ -66,7 +66,8 @@ WebContentsPreferences::WebContentsPreferences(
   SetDefaultBoolIfUndefined("webgl", true);
   bool webSecurity = true;
   SetDefaultBoolIfUndefined("webSecurity", webSecurity);
-  // If webSecurity was explicity set to false, let's inherit that into insureContent
+  // If webSecurity was explicity set to false, let's inherit that into
+  // insecureContent
   if (web_preferences.GetBoolean("webSecurity", &webSecurity) && !webSecurity) {
     SetDefaultBoolIfUndefined("allowRunningInsecureContent", true);
   } else {
