680b28750b51f978761f8fbd53261fc007d438f1
Aleksei Kuzmin <alkuzmin@microsoft.com>
2018-04-11 11:21:23 +0200

    Remove base::Value::GetType()
    https://chromium-review.googlesource.com/718458

--- atom/common/native_mate_converters/v8_value_converter.cc
+++ atom/common/native_mate_converters/v8_value_converter.cc
@@ -164,7 +164,7 @@ base::Value* V8ValueConverter::FromV8Value(
 v8::Local<v8::Value> V8ValueConverter::ToV8ValueImpl(
     v8::Isolate* isolate,
     const base::Value* value) const {
-  switch (value->GetType()) {
+  switch (value->type()) {
     case base::Value::Type::NONE:
       return v8::Null(isolate);
 
@@ -204,7 +204,7 @@ v8::Local<v8::Value> V8ValueConverter::ToV8ValueImpl(
       return ToArrayBuffer(isolate, static_cast<const base::Value*>(value));
 
     default:
-      LOG(ERROR) << "Unexpected value type: " << value->GetType();
+      LOG(ERROR) << "Unexpected value type: " << value->type();
       return v8::Null(isolate);
   }
 }
