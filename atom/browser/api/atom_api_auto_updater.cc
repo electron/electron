43a9092c1646db962c1361527497045e3cc1b980
Shelley Vohr <shelley.vohr@gmail.com>
2018-04-14 21:24:19 -0400

    remove accidental file change

--- atom/browser/api/atom_api_auto_updater.cc
+++ atom/browser/api/atom_api_auto_updater.cc
@@ -104,8 +104,6 @@ void AutoUpdater::SetFeedURL(mate::Arguments* args) {
 }
 
 void AutoUpdater::QuitAndInstall() {
-  Emit("before-quit-for-update");
-
   // If we don't have any window then quitAndInstall immediately.
   if (WindowList::IsEmpty()) {
     auto_updater::AutoUpdater::QuitAndInstall();
