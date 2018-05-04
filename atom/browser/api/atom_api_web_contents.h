8cd280d8125cc40f0190214c2d8183dae9595d8f
Milan Burda <milan.burda@gmail.com>
2018-05-04 17:03:04 +0200

    Pass preload script source to lib/sandboxed_renderer/init.js
    Don't use remote fs to load the preload script.
    Load it in the native code instead and pass the script source code as an argument to the init function.
    This would allow the preload script to work even if access to fs is disabled by filtering remote.require requests in rpc-server.js for security reasons.

--- atom/browser/api/atom_api_web_contents.h
+++ atom/browser/api/atom_api_web_contents.h
@@ -429,6 +429,9 @@ class WebContents : public mate::TrackableObject<WebContents>,
   void InitZoomController(content::WebContents* web_contents,
                           const mate::Dictionary& options);
 
+  void OnGetPreloadScript(content::RenderFrameHost* frame_host,
+                          IPC::Message* reply_msg);
+
   v8::Global<v8::Value> session_;
   v8::Global<v8::Value> devtools_web_contents_;
   v8::Global<v8::Value> debugger_;
--- atom/renderer/atom_sandboxed_renderer_client.cc
+++ atom/renderer/atom_sandboxed_renderer_client.cc
@@ -165,7 +165,7 @@ void AtomSandboxedRendererClient::DidCreateScriptContext(
   v8::Context::Scope context_scope(context);
   // Wrap the bundle into a function that receives the binding object and the
   // preload script path as arguments.
-  std::string left = "(function(binding, preloadPath, require) {\n";
+  std::string left = "(function(binding, preloadPath, preloadSrc, require) {\n";
   std::string right = "\n})";
   // Compile the wrapper and run it to get the function object
   auto script = v8::Script::Compile(v8::String::Concat(
@@ -174,14 +174,25 @@ void AtomSandboxedRendererClient::DidCreateScriptContext(
                          mate::ConvertToV8(isolate, right)->ToString())));
   auto func =
       v8::Handle<v8::Function>::Cast(script->Run(context).ToLocalChecked());
+  // Load the preload script if specified
+  std::string preload_script;
+  if (!preload_script_path.empty()) {
+    IPC::SyncMessage* message = new AtomViewHostMsg_GetPreloadScript(
+        render_frame->GetRoutingID(), &preload_script);
+    if (!render_frame->Send(message)) {
+      PLOG(WARNING) << "Unable to send AtomViewHostMsg_GetPreloadScript";
+    }
+  }
   // Create and initialize the binding object
   auto binding = v8::Object::New(isolate);
   InitializeBindings(binding, context);
   AddRenderBindings(isolate, binding);
   v8::Local<v8::Value> args[] = {
-      binding, mate::ConvertToV8(isolate, preload_script_path.value())};
+      binding, mate::ConvertToV8(isolate, preload_script_path.value()),
+      mate::ConvertToV8(isolate, preload_script)};
   // Execute the function with proper arguments
-  ignore_result(func->Call(context, v8::Null(isolate), 2, args));
+  ignore_result(
+      func->Call(context, v8::Null(isolate), node::arraysize(args), args));
 }
 
 void AtomSandboxedRendererClient::WillReleaseScriptContext(
