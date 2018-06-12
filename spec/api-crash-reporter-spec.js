4a5de16fee311bbd1a999ace78cea85dcf1ff42e
Aleksei Kuzmin <alkuzmin@microsoft.com>
2018-06-12 19:10:01 +0200

    fixup! Skip crashReported tests on linux

--- spec/api-crash-reporter-spec.js
+++ spec/api-crash-reporter-spec.js
@@ -14,6 +14,7 @@ const {app, BrowserWindow, crashReporter} = remote.require('electron')
 describe('crashReporter module', () => {
   if (process.mas || process.env.DISABLE_CRASH_REPORTER_TESTS) return
 
+  // TODO(alexeykuzmin): [Ch66] Fails. Fix it and enable back.
   if (process.platform === 'linux') return
 
   let originalTempDirectory = null
