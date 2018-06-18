daeb733cc169c1e594d181643d23da99c3a4df4b
Zhuo Lu <sethlu@mintkit.net>
2018-06-18 09:10:35 -0700

    Update copyright

--- atom/browser/api/atom_api_net_log.h
+++ atom/browser/api/atom_api_net_log.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Slack Technologies, Inc.
+// Copyright (c) 2018 GitHub, Inc.
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
