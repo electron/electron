12ac342666f45570179a685b7b7fe2c6b97512a3
Cheng Zhao <zcbenz@gmail.com>
2018-07-04 15:29:05 +0900

    implement devtools showItemInFolder message

--- brightray/browser/inspectable_web_contents_impl.h
+++ brightray/browser/inspectable_web_contents_impl.h
@@ -83,6 +83,7 @@ class InspectableWebContentsImpl
                            int stream_id) override;
   void SetIsDocked(const DispatchCallback& callback, bool is_docked) override;
   void OpenInNewTab(const std::string& url) override;
+  void ShowItemInFolder(const std::string& file_system_path) override;
   void SaveToFile(const std::string& url,
                   const std::string& content,
                   bool save_as) override;
