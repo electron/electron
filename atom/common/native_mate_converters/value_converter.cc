8ae8413f13a0fb92b6b4be75ab213b319f1cedeb
Shelley Vohr <shelley.vohr@gmail.com>
2018-07-17 11:39:02 -0700

    add new native_mate converters for base::Value

--- atom/common/native_mate_converters/value_converter.cc
+++ atom/common/native_mate_converters/value_converter.cc
@@ -30,6 +30,27 @@ v8::Local<v8::Value> Converter<base::DictionaryValue>::ToV8(
   return converter.ToV8Value(&val, isolate->GetCurrentContext());
 }
 
+bool Converter<base::Value>::FromV8(v8::Isolate* isolate,
+                                              v8::Local<v8::Value> val,
+                                              base::Value* out) {
+  atom::V8ValueConverter converter;
+  std::unique_ptr<base::Value> value(
+      converter.FromV8Value(val, isolate->GetCurrentContext()));
+  if (value) {
+    out->static_cast<base::Value*>(value.get());
+    return true;
+  } else {
+    return false;
+  }
+}
+
+v8::Local<v8::Value> Converter<base::Value>::ToV8(
+    v8::Isolate* isolate,
+    const base::Value& val) {
+  atom::V8ValueConverter converter;
+  return converter.ToV8Value(&val, isolate->GetCurrentContext());
+}
+
 bool Converter<base::ListValue>::FromV8(v8::Isolate* isolate,
                                         v8::Local<v8::Value> val,
                                         base::ListValue* out) {
