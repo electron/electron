c0460a90af8cc7d3a8d1f34b8b639aa9465912b7
Samuel Attard <samuel.r.attard@gmail.com>
2018-08-16 09:26:53 -0700

    build: nightly is nightly, beta is beta

--- script/prepare-release.js
+++ script/prepare-release.js
@@ -206,14 +206,14 @@ async function createRelease (branchToTarget, isBeta) {
   githubOpts.name = `electron ${newVersion}`
   if (isBeta) {
     if (versionType === 'nightly') {
-      githubOpts.body = `Note: This is a beta release.  Please file new issues ` +
-        `for any bugs you find in it.\n \n This release is published to npm ` +
-        `under the beta tag and can be installed via npm install electron@beta, ` +
-        `or npm i electron@${newVersion.substr(1)}.\n \n ${releaseNotes}`
-    } else {
       githubOpts.body = `Note: This is a nightly release.  Please file new issues ` +
         `for any bugs you find in it.\n \n This release is published to npm ` +
         `under the nightly tag and can be installed via npm install electron@nightly, ` +
+        `or npm i electron@${newVersion.substr(1)}.\n \n ${releaseNotes}`
+    } else {
+      githubOpts.body = `Note: This is a beta release.  Please file new issues ` +
+        `for any bugs you find in it.\n \n This release is published to npm ` +
+        `under the beta tag and can be installed via npm install electron@beta, ` +
         `or npm i electron@${newVersion.substr(1)}.\n \n ${releaseNotes}`
     }
     githubOpts.name = `${githubOpts.name}`
