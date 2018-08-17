electron/electron

commit 2e5cb930de80e79000887ba02372960fe1efbb20

Provide an easy way to use a local build of Electron (#12426)
* Provide an easy way to use a local build of Electron

For instance from ~/projects/electron/out/D

* document ELECTRON_OVERRIDE_DIST_PATH

* Make the linter happy

* Tweak ELECTRON_OVERRIDE_DIST_PATH docs

npm/install.js
@@ -52,12 +52,12 @@ function getPlatformPath () {
 
   switch (platform) {
     case 'darwin':
-      return 'dist/Electron.app/Contents/MacOS/Electron'
+      return 'Electron.app/Contents/MacOS/Electron'
     case 'freebsd':
     case 'linux':
-      return 'dist/electron'
+      return 'electron'
     case 'win32':
-      return 'dist/electron.exe'
+      return 'electron.exe'
     default:
       throw new Error('Electron builds are not available on platform: ' + platform)
   }

.gitignore
@@ -43,3 +43,8 @@
 node_modules/
 SHASUMS256.txt
 **/package-lock.json
+**/yarn.lock
+
+# npm package
+/npm/dist
+/npm/path.txt

npm/index.js
@@ -3,8 +3,16 @@ var path = require('path')
 
 var pathFile = path.join(__dirname, 'path.txt')
 
-if (fs.existsSync(pathFile)) {
-  module.exports = path.join(__dirname, fs.readFileSync(pathFile, 'utf-8'))
-} else {
-  throw new Error('Electron failed to install correctly, please delete node_modules/electron and try installing again')
+function getElectronPath () {
+  if (fs.existsSync(pathFile)) {
+    var executablePath = fs.readFileSync(pathFile, 'utf-8')
+    if (process.env.ELECTRON_OVERRIDE_DIST_PATH) {
+      return path.join(process.env.ELECTRON_OVERRIDE_DIST_PATH, executablePath)
+    }
+    return path.join(__dirname, 'dist', executablePath)
+  } else {
+    throw new Error('Electron failed to install correctly, please delete node_modules/electron and try installing again')
+  }
 }
+
+module.exports = getElectronPath()

commit 3c6e8da0cb5f86c297587ec886ca8901a6012fa4

fix #14127 issue caused by #12426

npm/install.js
@@ -17,7 +17,7 @@ try {
 
 var platformPath = getPlatformPath()
 
-if (installedVersion === version && fs.existsSync(path.join(__dirname, platformPath))) {
+if (installedVersion === version && fs.existsSync(path.join(__dirname, 'dist', platformPath))) {
   process.exit(0)
 }
 