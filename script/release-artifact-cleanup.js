94e3f10c4a1dde882c220aeeb17f9f7d7159b98c
Shelley Vohr <shelley.vohr@gmail.com>
2018-08-22 09:18:49 -0700

    add function to delete incomplete release draft

--- script/release-artifact-cleanup.js
+++ script/release-artifact-cleanup.js
@@ -22,71 +22,75 @@ github.authenticate({
 })
 
 async function getCurrentBranch (gitDir) {
-  console.log(`Determining current git branch`)
-  let gitArgs = ['rev-parse', '--abbrev-ref', 'HEAD']
-  let branchDetails = await GitProcess.exec(gitArgs, gitDir)
+  const gitArgs = ['rev-parse', '--abbrev-ref', 'HEAD']
+  const branchDetails = await GitProcess.exec(gitArgs, gitDir)
   if (branchDetails.exitCode === 0) {
-    let currentBranch = branchDetails.stdout.trim()
-    console.log(`${pass} Successfully determined current git branch is ` +
-      `${currentBranch}`)
-    return currentBranch
+    return branchDetails.stdout.trim()
   } else {
-    let error = GitProcess.parseError(branchDetails.stderr)
-    console.log(`${fail} Could not get details for the current branch,
-    error was ${branchDetails.stderr}`, error)
+    const error = GitProcess.parseError(branchDetails.stderr)
+    console.log(`${fail} Couldn't get current branch: ${branchDetails.stderr}`, error)
     process.exit(1)
   }
 }
 
 async function revertBumpCommit (tag) {
-  let branch = getCurrentBranch()
-  let hashToRevert = await exec(`git log | grep 'Bump v[0-9.]*' | grep -o '\\w\\ {8,\\}' | head -n 1`)
+  const branch = getCurrentBranch()
+  const hashToRevert = await exec(`git log | grep 'Bump v[0-9.]*' | grep -o '\\w\\ {8,\\}' | head -n 1`)
   await GitProcess.exec(['revert', hashToRevert], gitDir)
-  let pushDetails = await GitProcess.exec(['push', 'origin', `HEAD:${branch}`, '--follow-tags'], gitDir)
+  const pushDetails = await GitProcess.exec(['push', 'origin', `HEAD:${branch}`, '--follow-tags'], gitDir)
   if (pushDetails.exitCode === 0) {
     console.log(`${pass} Successfully reverted release commit.`)
   } else {
-    console.log(`${fail} Failed to push release commit: ` +
-      `${pushDetails.stderr}`)
+    console.log(`${fail} Failed to push release commit: ${pushDetails.stderr}`)
     process.exit(1)
   }
 }
 
-// async function deleteDraft (tag) {
-
-// }
+async function deleteDraft (tag, repo) {
+  try {
+    const result = await github.repos.getReleaseByTag({
+      owner: 'electron',
+      repo,
+      tag
+    })
+    await github.repos.deleteRelease({
+      owner: 'electron',
+      repo,
+      release_id: result.id
+    })
+    console.log(`Successfully deleted draft with tag ${tag}`)
+  } catch (err) {
+    console.log(`Couldn't delete draft: ${err}`)
+    process.exit(1)
+  }
+}
 
 async function deleteTag (tag, targetRepo) {
-  const result = await (github.gitdata.deleteReference({
-    owner: 'electron',
-    repo: targetRepo,
-    ref: tag
-  }))
-
-  if (result.exitCode === 0) {
+  try {
+    await github.gitdata.deleteReference({
+      owner: 'electron',
+      repo: targetRepo,
+      ref: tag
+    })
     console.log(`Successfully deleted tag ${tag} from 'electron/electron'`)
-  } else {
+  } catch (err) {
     console.log(`Couldn't delete tag ${tag} from 'electron/electron'`)
+    process.exit(1)
   }
 }
 
 async function cleanReleaseArtifacts () {
   const tag = args.tag
-
-  // delete tag from nightlies repo
   const lastBumpCommit = execSync(`git log | grep 'Bump v[0-9.]*' | head -n 1`).toString()
 
   if (lastBumpCommit.indexOf('nightly' > 0)) {
+    await deleteDraft(tag, 'nightlies')
     await deleteTag(tag, 'nightlies')
+  } else {
+    await deleteDraft(tag, 'electron')
   }
 
-  // delete tag from `electron/electron`
   await deleteTag(tag, 'electron')
-
-  // delete release
-  // await deleteDraft(tag)
-
-  // revert commit in `electron/electron`
   await revertBumpCommit()
 
   console.log('Failed release artifact cleanup complete')
