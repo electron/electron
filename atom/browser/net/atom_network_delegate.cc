9ec75ae4d63b575ef4b0e97d08521956be1c5cce
Charles Kerr <ckerr@github.com>
2018-09-12 14:01:16 -0500

    fix: include order

--- atom/browser/net/atom_network_delegate.cc
+++ atom/browser/net/atom_network_delegate.cc
@@ -2,10 +2,10 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
+#include "atom/browser/net/atom_network_delegate.h"
+
 #include <memory>
 #include <utility>
-
-#include "atom/browser/net/atom_network_delegate.h"
 
 #include "atom/browser/api/atom_api_web_contents.h"
 #include "atom/browser/login_handler.h"
