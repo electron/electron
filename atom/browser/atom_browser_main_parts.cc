0eefad1ff44fa7e27337b14e963abfeefd84fc81
Charles Kerr <ckerr@github.com>
2018-09-12 14:07:42 -0500

    fix: include order

--- atom/browser/atom_browser_main_parts.cc
+++ atom/browser/atom_browser_main_parts.cc
@@ -2,9 +2,9 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
-#include <utility>
-
 #include "atom/browser/atom_browser_main_parts.h"
+
+#include <utility>
 
 #include "atom/browser/api/atom_api_app.h"
 #include "atom/browser/api/trackable_object.h"
