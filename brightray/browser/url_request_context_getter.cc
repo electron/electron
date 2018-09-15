e4c760bb251228d481da95c7cd21ba3750866e41
Jeremy Apthorp <nornagon@nornagon.net>
2018-09-14 17:31:00 -0700

    FIXME: certificate_transparency moved out of net/
    We should figure out if we want to keep doing CT?

--- brightray/browser/url_request_context_getter.cc
+++ brightray/browser/url_request_context_getter.cc
@@ -18,6 +18,7 @@
 #include "brightray/browser/net/require_ct_delegate.h"
 #include "brightray/browser/net_log.h"
 #include "brightray/common/switches.h"
+#include "components/certificate_transparency/ct_known_logs.h"
 #include "components/network_session_configurator/common/network_switches.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/cookie_store_factory.h"
@@ -25,7 +26,6 @@
 #include "content/public/browser/resource_context.h"
 #include "net/base/host_mapping_rules.h"
 #include "net/cert/cert_verifier.h"
-#include "net/cert/ct_known_logs.h"
 #include "net/cert/ct_log_verifier.h"
 #include "net/cert/ct_policy_enforcer.h"
 #include "net/cert/multi_log_ct_verifier.h"
@@ -347,11 +347,13 @@ net::URLRequestContext* URLRequestContextGetter::GetURLRequestContext() {
         new net::HttpServerPropertiesImpl);
     storage_->set_http_server_properties(std::move(server_properties));
 
+    // FIXME(jeremy): decide what to do about certificate transparency.
     std::unique_ptr<net::MultiLogCTVerifier> ct_verifier =
         std::make_unique<net::MultiLogCTVerifier>();
-    ct_verifier->AddLogs(net::ct::CreateLogVerifiersForKnownLogs());
+    // ct_verifier->AddLogs(net::ct::CreateLogVerifiersForKnownLogs());
     storage_->set_cert_transparency_verifier(std::move(ct_verifier));
-    storage_->set_ct_policy_enforcer(std::make_unique<net::CTPolicyEnforcer>());
+    storage_->set_ct_policy_enforcer(
+        std::make_unique<net::DefaultCTPolicyEnforcer>());
 
     net::HttpNetworkSession::Params network_session_params;
     network_session_params.ignore_certificate_errors = false;
