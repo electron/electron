ca36efc4dd5a5173b4ff2f45cd325031e64c6601
Jeremy Apthorp <nornagon@nornagon.net>
2018-09-18 15:11:03 -0700

    ViewsDelegate::CreateWebContents went away
    see https://chromium-review.googlesource.com/c/chromium/src/+/1031314

--- brightray/browser/views/views_delegate.h
+++ brightray/browser/views/views_delegate.h
@@ -46,9 +46,6 @@ class ViewsDelegate : public views::ViewsDelegate {
       views::Widget* widget) override;
   void AddRef() override;
   void ReleaseRef() override;
-  content::WebContents* CreateWebContents(
-      content::BrowserContext* browser_context,
-      content::SiteInstance* site_instance) override;
   void OnBeforeWidgetInit(
       views::Widget::InitParams* params,
       views::internal::NativeWidgetDelegate* delegate) override;
