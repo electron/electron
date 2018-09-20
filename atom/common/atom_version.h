977e287cfaf34ea36d24a20adacdb9eba5afec33
Jeremy Apthorp <nornagon@nornagon.net>
2018-09-19 23:55:07 -0700

    chore: turn off clang-format for ATOM_PRE_RELEASE_VERSION (#14711)

--- atom/common/atom_version.h
+++ atom/common/atom_version.h
@@ -8,7 +8,9 @@
 #define ATOM_MAJOR_VERSION 4
 #define ATOM_MINOR_VERSION 0
 #define ATOM_PATCH_VERSION 0
+// clang-format off
 #define ATOM_PRE_RELEASE_VERSION -nightly.20180905
+// clang-format on
 
 #ifndef ATOM_STRINGIFY
 #define ATOM_STRINGIFY(n) ATOM_STRINGIFY_HELPER(n)
