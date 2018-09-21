2875371ffd8f614aa07ef4fb5397d928915b0e19
John Kleinschmidt <jkleinsc@github.com>
2018-09-21 15:35:59 -0400

    Use chromium 's generate_breakpad_symbols.py

--- script/dump-symbols.py
+++ script/dump-symbols.py
@@ -1,40 +1,42 @@
 #!/usr/bin/env python
 
+import argparse
 import os
 import sys
 
 from lib.config import PLATFORM
-from lib.util import electron_gyp, execute, rm_rf
+from lib.gn import gn
+from lib.util import execute, rm_rf
 
+ELECTRON_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+SOURCE_ROOT = os.path.abspath(os.path.dirname(ELECTRON_ROOT))
+RELEASE_PATH = os.path.join('out', 'Release')
 
-SOURCE_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-DIST_DIR = os.path.join(SOURCE_ROOT, 'dist')
-OUT_DIR = os.path.join(SOURCE_ROOT, 'out', 'R')
-CHROMIUM_DIR = os.path.join(SOURCE_ROOT, 'vendor', 'download',
-                            'libchromiumcontent', 'static_library')
-
-
-def main(destination):
-  rm_rf(destination)
-  (project_name, product_name) = get_names_from_gyp()
+def main():
+  args = parse_args()
+  rm_rf(args.destination)
+  source_root = os.path.abspath(args.source_root)
+  build_path = os.path.join(source_root, args.build_dir)
+  product_name = gn(build_path).args().get_string('electron_product_name')
+  project_name = gn(build_path).args().get_string('electron_project_name')
 
   if PLATFORM in ['darwin', 'linux']:
-    generate_breakpad_symbols = os.path.join(SOURCE_ROOT, 'tools', 'posix',
+    generate_breakpad_symbols = os.path.join(source_root, 'components', 'crash',
+                                             'content', 'tools',
                                              'generate_breakpad_symbols.py')
     if PLATFORM == 'darwin':
       #macOS has an additional helper app; provide the path to that binary also
-      main_app = os.path.join(OUT_DIR, '{0}.app'.format(product_name),
+      main_app = os.path.join(build_path, '{0}.app'.format(product_name),
                             'Contents', 'MacOS', product_name)
       helper_name = product_name + " Helper"
-      helper_app = os.path.join(OUT_DIR, '{0}.app'.format(helper_name),
+      helper_app = os.path.join(build_path, '{0}.app'.format(helper_name),
                             'Contents', 'MacOS', product_name + " Helper")
       binaries = [main_app, helper_app]
     else:
-      binaries = [os.path.join(OUT_DIR, project_name)]
+      binaries = [os.path.join(build_path, project_name)]
     args = [
-      '--build-dir={0}'.format(OUT_DIR),
-      '--symbols-dir={0}'.format(destination),
-      '--libchromiumcontent-dir={0}'.format(CHROMIUM_DIR),
+      '--build-dir={0}'.format(build_path),
+      '--symbols-dir={0}'.format(args.destination),
       '--clear',
       '--jobs=16',
     ]
@@ -42,21 +44,32 @@ def main(destination):
       args += '--binary={0}'.format(binary),
 
   else:
-    generate_breakpad_symbols = os.path.join(SOURCE_ROOT, 'tools', 'win',
+    generate_breakpad_symbols = os.path.join(ELECTRON_ROOT, 'tools', 'win',
                                              'generate_breakpad_symbols.py')
     args = [
-      '--symbols-dir={0}'.format(destination),
+      '--symbols-dir={0}'.format(args.destination),
       '--jobs=16',
-      os.path.relpath(OUT_DIR),
+      os.path.relpath(build_path),
     ]
-
+  print [sys.executable, generate_breakpad_symbols] + args
   execute([sys.executable, generate_breakpad_symbols] + args)
 
-
-def get_names_from_gyp():
-  variables = electron_gyp()
-  return (variables['project_name%'], variables['product_name%'])
-
+def parse_args():
+  parser = argparse.ArgumentParser(description='Create breakpad symbols')
+  parser.add_argument('-b', '--build-dir',
+                      help='Path to an Electron build folder. \
+                          Relative to the --source-root.',
+                      default=RELEASE_PATH,
+                      required=False)
+  parser.add_argument('-d', '--destination',
+                      help='Path to save symbols to.',
+                      default=None,
+                      required=True)
+  parser.add_argument('-s', '--source-root',
+                      help='Path to the src folder.',
+                      default=SOURCE_ROOT,
+                      required=False)
+  return parser.parse_args()
 
 if __name__ == '__main__':
-  sys.exit(main(sys.argv[1]))
+  sys.exit(main())
