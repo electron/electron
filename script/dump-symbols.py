44777546c6243475491f300e960937f3d0f1d08c
John Kleinschmidt <jkleinsc@github.com>
2018-09-24 12:16:38 -0400

    Add breakpad symbol gen to CI.

--- script/dump-symbols.py
+++ script/dump-symbols.py
@@ -4,7 +4,7 @@ import argparse
 import os
 import sys
 
-from lib.config import PLATFORM
+from lib.config import PLATFORM, enable_verbose_mode, is_verbose_mode
 from lib.gn import gn
 from lib.util import execute, rm_rf
 
@@ -14,6 +14,8 @@ RELEASE_PATH = os.path.join('out', 'Release')
 
 def main():
   args = parse_args()
+  if args.verbose:
+    enable_verbose_mode()
   rm_rf(args.destination)
   source_root = os.path.abspath(args.source_root)
   build_path = os.path.join(source_root, args.build_dir)
@@ -21,9 +23,7 @@ def main():
   project_name = gn(build_path).args().get_string('electron_project_name')
 
   if PLATFORM in ['darwin', 'linux']:
-    generate_breakpad_symbols = os.path.join(source_root, 'components', 'crash',
-                                             'content', 'tools',
-                                             'generate_breakpad_symbols.py')
+
     if PLATFORM == 'darwin':
       #macOS has an additional helper app; provide the path to that binary also
       main_app = os.path.join(build_path, '{0}.app'.format(product_name),
@@ -32,16 +32,13 @@ def main():
       helper_app = os.path.join(build_path, '{0}.app'.format(helper_name),
                             'Contents', 'MacOS', product_name + " Helper")
       binaries = [main_app, helper_app]
+      for binary in binaries:        
+        generate_posix_symbols(binary, source_root, build_path, 
+                                        args.destination)
     else:
-      binaries = [os.path.join(build_path, project_name)]
-    args = [
-      '--build-dir={0}'.format(build_path),
-      '--symbols-dir={0}'.format(args.destination),
-      '--clear',
-      '--jobs=16',
-    ]
-    for binary in binaries:
-      args += '--binary={0}'.format(binary),
+      binary = os.path.join(build_path, project_name)
+      generate_posix_symbols(binary, source_root, build_path, 
+                                      args.destination)
 
   else:
     generate_breakpad_symbols = os.path.join(ELECTRON_ROOT, 'tools', 'win',
@@ -50,9 +47,22 @@ def main():
       '--symbols-dir={0}'.format(args.destination),
       '--jobs=16',
       os.path.relpath(build_path),
-    ]
-  print [sys.executable, generate_breakpad_symbols] + args
-  execute([sys.executable, generate_breakpad_symbols] + args)
+    ]  
+    execute([sys.executable, generate_breakpad_symbols] + args)
+
+def generate_posix_symbols(binary, source_root, build_dir, destination):
+  generate_breakpad_symbols = os.path.join(source_root, 'components', 'crash',
+                                          'content', 'tools',
+                                          'generate_breakpad_symbols.py')
+  args = [
+    '--build-dir={0}'.format(build_dir),
+    '--symbols-dir={0}'.format(destination),
+    '--jobs=16',
+    '--binary={0}'.format(binary),
+  ]
+  if is_verbose_mode(): 
+    args += ['-v']
+  execute([sys.executable, generate_breakpad_symbols] + args)    
 
 def parse_args():
   parser = argparse.ArgumentParser(description='Create breakpad symbols')
@@ -69,6 +79,9 @@ def parse_args():
                       help='Path to the src folder.',
                       default=SOURCE_ROOT,
                       required=False)
+  parser.add_argument('-v', '--verbose',
+                      action='store_true',
+                      help='Prints the output of the subprocesses')                      
   return parser.parse_args()
 
 if __name__ == '__main__':
