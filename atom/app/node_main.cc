169c4060ac0fa019a1770b48fe040c0e5c0fba14
deepak1556 <hop2deep@gmail.com>
2018-09-27 20:40:32 +0530

    refactor: switch to BUILDFLAG(ENABLE_RUN_AS_NODE)

--- atom/app/node_main.cc
+++ atom/app/node_main.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
-#ifdef ENABLE_RUN_AS_NODE
-
 #include "atom/app/node_main.h"
 
 #include <memory>
@@ -119,5 +117,3 @@ int NodeMain(int argc, char* argv[]) {
 }
 
 }  // namespace atom
-
-#endif  // ENABLE_RUN_AS_NODE
