0e1365fc94732e76c49ff55c6e0911c1f54e668e
Troy <ColonelTJ120@gmail.com>
2018-09-28 04:59:00 -0700

    fix: enable key accelerator flags for Windows and Linux (#14830)
    * Fix Accelerator Flags for Windows and Linux
    
    * Lint fix
    
    * Lint whitespace

--- atom/browser/ui/accelerator_util.cc
+++ atom/browser/ui/accelerator_util.cc
@@ -91,7 +91,9 @@ bool TriggerAcceleratorTableCommand(AcceleratorTable* table,
   if (base::ContainsKey(*table, accelerator)) {
     const accelerator_util::MenuItem& item = (*table)[accelerator];
     if (item.model->IsEnabledAt(item.position)) {
-      item.model->ActivatedAt(item.position);
+      const auto event_flags =
+        accelerator.MaskOutKeyEventFlags(accelerator.modifiers());
+      item.model->ActivatedAt(item.position, event_flags);
       return true;
     }
   }
