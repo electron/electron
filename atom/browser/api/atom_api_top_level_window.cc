428214df31655ebcf52a1bd0ee589ada23e744c0
Troy Smith <Troy.L.Smith-1@ou.edu>
2018-10-02 22:58:10 +0000

    Inital setSize check

--- atom/browser/api/atom_api_top_level_window.cc
+++ atom/browser/api/atom_api_top_level_window.cc
@@ -376,6 +376,11 @@ gfx::Rect TopLevelWindow::GetContentBounds() {
 
 void TopLevelWindow::SetSize(int width, int height, mate::Arguments* args) {
   bool animate = false;
+  gfx::Size size = window_->GetSize();
+  if ((size.height() <= height) |
+      (size.width() <= width)) {
+    //throw dev warning
+  }
   args->GetNext(&animate);
   window_->SetSize(gfx::Size(width, height), animate);
 }
