97f5c793f3981185f07e526f2a29683393b187df
Troy Smith <Troy.L.Smith-1@ou.edu>
2018-10-03 21:40:58 +0000

    lint

--- atom/browser/api/atom_api_top_level_window.cc
+++ atom/browser/api/atom_api_top_level_window.cc
@@ -399,12 +399,10 @@ gfx::Rect TopLevelWindow::GetContentBounds() {
 void TopLevelWindow::SetSize(int width, int height, mate::Arguments* args) {
   bool animate = false;
   gfx::Size size = window_->GetMinimumSize();
-  if ((size.height() <= height) |
-      (size.width() <= width)) {
+  if ((size.height() <= height) | (size.width() <= width)) {
     args->GetNext(&animate);
     window_->SetSize(gfx::Size(width, height), animate);
   }
-  
 }
 
 std::vector<int> TopLevelWindow::GetSize() {
