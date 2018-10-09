30850f491f6804dcb6b4739e09db581dcb4ed10b
Jeremy Apthorp <nornagon@nornagon.net>
2018-10-09 14:15:19 -0700

    fix: [mac] restore SetBackgroundColor functionality

--- atom/browser/native_window_mac.mm
+++ atom/browser/native_window_mac.mm
@@ -997,20 +997,15 @@ bool NativeWindowMac::IsKiosk() {
 }
 
 void NativeWindowMac::SetBackgroundColor(SkColor color) {
-  NOTIMPLEMENTED() << "TODO";
-  /*
   base::ScopedCFTypeRef<CGColorRef> cgcolor(
       skia::CGColorCreateFromSkColor(color));
   // views::Widget adds a layer for the content view.
   auto* bridge = views::NativeWidgetMac::GetBridgeForNativeWindow(window_);
-  NSView* compositor_superview =
-      static_cast<ui::AcceleratedWidgetMacNSView*>(bridge)
-          ->AcceleratedWidgetGetNSView();
+  auto* compositor_superview = bridge->ns_view();
   [[compositor_superview layer] setBackgroundColor:cgcolor];
   // When using WebContents as content view, the contentView also has layer.
   if ([[window_ contentView] wantsLayer])
     [[[window_ contentView] layer] setBackgroundColor:cgcolor];
-    */
 }
 
 void NativeWindowMac::SetHasShadow(bool has_shadow) {
