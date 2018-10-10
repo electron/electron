19e02cd63ea086c8000548669bd6262636b6a698
Samuel Attard <sattard@atlassian.com>
2018-10-10 11:04:55 +1100

    chore: add logging to zip-symbols.py to help debugging timeouts

--- script/zip-symbols.py
+++ script/zip-symbols.py
@@ -13,6 +13,7 @@ PROJECT_NAME = get_electron_branding()['project_name']
 OUT_DIR = get_out_dir()
 
 def main():
+  print('Zipping Symbols')
   if get_target_arch() == 'mips64el':
     return
 
@@ -22,18 +23,23 @@ def main():
 
   with scoped_cwd(OUT_DIR):
     dirs = ['{0}.breakpad.syms'.format(PROJECT_NAME)]
+    print('Making symbol zip: ' + zip_file)
     make_zip(zip_file, licenses, dirs)
 
   if PLATFORM == 'darwin':
     dsym_name = 'dsym.zip'
     with scoped_cwd(OUT_DIR):
       dsyms = glob.glob('*.dSYM')
-      make_zip(os.path.join(OUT_DIR, dsym_name), licenses, dsyms)
+      dsym_zip_file = os.path.join(OUT_DIR, dsym_name)
+      print('Making dsym zip: ' + dsym_zip_file)
+      make_zip(dsym_zip_file, licenses, dsyms)
   elif PLATFORM == 'win32':
     pdb_name = 'pdb.zip'
     with scoped_cwd(OUT_DIR):
       pdbs = glob.glob('*.pdb')
-      make_zip(os.path.join(OUT_DIR, pdb_name), pdbs + licenses, [])
+      pdb_zip_file = os.path.join(OUT_DIR, pdb_name)
+      print('Making pdb zip: ' + pdb_zip_file)
+      make_zip(pdb_zip_file, pdbs + licenses, [])
 
 if __name__ == '__main__':
   sys.exit(main())
