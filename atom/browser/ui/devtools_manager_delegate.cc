e8e7edf0177642c0d790c42fc8dfe9381f1ff497
Milan Burda <milan.burda@gmail.com>
2018-10-22 19:04:23 +0200

    refactor: eliminate brightray::ContentClient (#15303)

--- atom/browser/ui/devtools_manager_delegate.cc
+++ atom/browser/ui/devtools_manager_delegate.cc
@@ -13,7 +13,6 @@
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
-#include "brightray/common/content_client.h"
 #include "content/public/browser/devtools_agent_host.h"
 #include "content/public/browser/devtools_frontend_host.h"
 #include "content/public/browser/devtools_socket_factory.h"
--- brightray/common/main_delegate.h
+++ brightray/common/main_delegate.h
@@ -22,7 +22,6 @@ class ResourceBundle;
 namespace brightray {
 
 class BrowserClient;
-class ContentClient;
 
 void LoadResourceBundle(const std::string& locale);
 void LoadCommonResources();
@@ -33,10 +32,6 @@ class MainDelegate : public content::ContentMainDelegate {
   ~MainDelegate() override;
 
  protected:
-  // Subclasses can override this to provide their own ContentClient
-  // implementation.
-  virtual std::unique_ptr<ContentClient> CreateContentClient();
-
   // Subclasses can override this to provide their own BrowserClient
   // implementation.
   virtual std::unique_ptr<BrowserClient> CreateBrowserClient();
@@ -54,7 +49,6 @@ class MainDelegate : public content::ContentMainDelegate {
  private:
   content::ContentBrowserClient* CreateContentBrowserClient() override;
 
-  std::unique_ptr<ContentClient> content_client_;
   std::unique_ptr<BrowserClient> browser_client_;
 
   DISALLOW_COPY_AND_ASSIGN(MainDelegate);
