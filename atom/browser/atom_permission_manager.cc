4179cedf376f0af16d22d823762bacf0bb14a8f1
deepak1556 <hop2deep@gmail.com>
2018-10-25 21:15:51 +0530

    Change SubscribePermissionStatusChange to use a RFH to query permissions
    https://chromium-review.googlesource.com/c/chromium/src/+/979735

--- atom/browser/atom_permission_manager.cc
+++ atom/browser/atom_permission_manager.cc
@@ -222,8 +222,8 @@ blink::mojom::PermissionStatus AtomPermissionManager::GetPermissionStatus(
 
 int AtomPermissionManager::SubscribePermissionStatusChange(
     content::PermissionType permission,
+    content::RenderFrameHost* render_frame_host,
     const GURL& requesting_origin,
-    const GURL& embedding_origin,
     const base::Callback<void(blink::mojom::PermissionStatus)>& callback) {
   return -1;
 }
