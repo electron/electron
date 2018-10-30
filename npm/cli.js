f40167e57ca9a327f0cdcdf6fe10a1bd9b225530
liudonghua <liudonghua123@gmail.com>
2018-10-30 11:00:07 +0800

    Update cli.js

--- npm/cli.js
+++ npm/cli.js
@@ -4,7 +4,7 @@ var electron = require('./')
 
 var proc = require('child_process')
 
-var options = parseInt(process.versions.node) >= 11 ? {stdio: 'inherit', windowsHide: false}: {stdio: 'inherit'};
+var options = parseInt(process.versions.node) >= 11 ? {stdio: 'inherit', windowsHide: false}: {stdio: 'inherit'}
 var child = proc.spawn(electron, process.argv.slice(2), options)
 child.on('close', function (code) {
   process.exit(code)
