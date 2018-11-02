084b9b16c7f5e73b1c3aa53c7c8e1baad0c5789d
Shelley Vohr <shelley.vohr@gmail.com>
2018-11-01 23:11:14 -0700

    add a spec for new/renamed fn

--- spec/api-global-shortcut-spec.js
+++ spec/api-global-shortcut-spec.js
@@ -18,19 +18,36 @@ describe('globalShortcut module', () => {
     globalShortcut.unregisterAll()
   })
 
-  it('can register and unregister accelerators', () => {
+  it('can register and unregister single accelerators', () => {
     const accelerator = 'CommandOrControl+A+B+C'
 
     expect(globalShortcut.isRegistered(accelerator)).to.be.false()
-    globalShortcut.register(accelerator, () => {})
+    globalShortcut.registerAccelerator(accelerator, () => {})
     expect(globalShortcut.isRegistered(accelerator)).to.be.true()
     globalShortcut.unregister(accelerator)
     expect(globalShortcut.isRegistered(accelerator)).to.be.false()
 
     expect(globalShortcut.isRegistered(accelerator)).to.be.false()
-    globalShortcut.register(accelerator, () => {})
+    globalShortcut.registerAccelerator(accelerator, () => {})
     expect(globalShortcut.isRegistered(accelerator)).to.be.true()
     globalShortcut.unregisterAll()
     expect(globalShortcut.isRegistered(accelerator)).to.be.false()
+  })
+
+  it('can register andunregistermultiple accelerators', () => {
+    const accelerators = ['CommandOrControl+X', 'CommandOrControl+Y']
+
+    expect(globalShortcut.isRegistered(accelerators[0])).to.be.false()
+    expect(globalShortcut.isRegistered(accelerators[1])).to.be.false()
+
+    globalShortcut.register(accelerators, () => {})
+
+    expect(globalShortcut.isRegistered(accelerators[0])).to.be.true()
+    expect(globalShortcut.isRegistered(accelerators[1])).to.be.true()
+
+    globalShortcut.unregisterAll()
+
+    expect(globalShortcut.isRegistered(accelerators[0])).to.be.false()
+    expect(globalShortcut.isRegistered(accelerators[1])).to.be.false()
   })
 })
