746beb0d8b28809ec96d6ce9ad1ee252402584a7
Cheng Zhao <zcbenz@github.com>
2018-11-09 00:57:28 +0900

    fix: destroy WebContents synchronously on shutdown (#15541)

---
+++ spec/fixtures/api/leak-exit-webcontentsview.js
@@ -0,0 +1,7 @@
+const { WebContentsView, app, webContents } = require('electron')
+app.on('ready', function () {
+  const web = webContents.create({})
+  new WebContentsView(web)  // eslint-disable-line
+
+  process.nextTick(() => app.quit())
+})
