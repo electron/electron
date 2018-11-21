0f1a2180da179a8465c1251a43691b5b7b6c05b7
John Kleinschmidt <jkleinsc@github.com>
2018-11-21 13:56:38 -0500

    Added comment on why snapshot_blob.bin is skipped on arm/arm64

--- build/zip.py
+++ build/zip.py
@@ -20,6 +20,12 @@ PATHS_TO_SKIP = [
 ]
 
 def skip_path(dep, dist_zip, target_cpu):
+  # Skip specific paths and extensions as well as the following special case:
+  # snapshot_blob.bin is a dependency of mksnapshot.zip because
+  # v8_context_generator needs it, but this file does not get generated for arm
+  # and arm 64 binaries of mksnapshot since they are built on x64 hardware.
+  # Consumers of arm and arm64 mksnapshot can generate snapshot_blob.bin
+  # themselves by running mksnapshot.
   should_skip = (
     any(dep.startswith(path) for path in PATHS_TO_SKIP) or
     any(dep.endswith(ext) for ext in EXTENSIONS_TO_SKIP) or
