d0f175ff387bd15abda9067fff3c5cdd30f21028
Shelley Vohr <shelley.vohr@gmail.com>
2018-12-05 11:35:11 -0800

    chore: convert bump script to js

--- script/get-last-major-for-master.js
+++ script/get-last-major-for-master.js
@@ -3,7 +3,7 @@ const path = require('path')
 const semver = require('semver')
 const gitDir = path.resolve(__dirname, '..')
 
-async function determineNextMajorForMaster () {
+async function getLastMajorForMaster () {
   let branchNames
   const result = await GitProcess.exec(['branch', '-a', '--remote', '--list', 'origin/[0-9]-[0-9]-x'], gitDir)
   if (result.exitCode === 0) {
@@ -17,13 +17,7 @@ async function determineNextMajorForMaster () {
 
 function getNextReleaseBranch (branches) {
   const converted = branches.map(b => b.replace(/-/g, '.').replace('x', '0'))
-  const next = converted.reduce((v1, v2) => {
-    return semver.gt(v1, v2) ? v1 : v2
-  })
-  return parseInt(next.split('.')[0], 10)
+  return converted.reduce((v1, v2) => semver.gt(v1, v2) ? v1 : v2)
 }
 
-determineNextMajorForMaster().then(console.info).catch((err) => {
-  console.error(err)
-  process.exit(1)
-})
+module.exports = { getLastMajorForMaster }
---
+++ spec/version-bump-spec.js
@@ -0,0 +1,80 @@
+const { expect } = require('chai')
+const { nextVersion } = require('../script/bump-version')
+
+describe('bump-version script', () => {
+  const nightlyPattern = /[0-9.]*(-nightly.(\d{4})(\d{2})(\d{2}))$/g
+  const betaPattern = /[0-9.]*(-beta[0-9.]*)/g
+
+  it('bumps to nightly from stable', async () => {
+    const version = 'v2.0.0'
+    const next = await nextVersion('nightly', version)
+    const matches = next.match(nightlyPattern)
+    expect(matches).to.have.lengthOf(1)
+  })
+
+  it('bumps to nightly from beta', async () => {
+    const version = 'v2.0.0-beta.1'
+    const next = await nextVersion('nightly', version)
+    const matches = next.match(nightlyPattern)
+    expect(matches).to.have.lengthOf(1)
+  })
+
+  it('bumps to nightly from nightly', async () => {
+    const version = 'v2.0.0-nightly.19950901'
+    const next = await nextVersion('nightly', version)
+    const matches = next.match(nightlyPattern)
+    expect(matches).to.have.lengthOf(1)
+  })
+
+  it('throws error when bumping to beta from stable', () => {
+    const version = 'v2.0.0'
+    return expect(
+      nextVersion('beta', version)
+    ).to.be.rejectedWith('Cannot bump to beta from stable.')
+  })
+
+  it('bumps to beta from nightly', async () => {
+    const version = 'v2.0.0-nightly.19950901'
+    const next = await nextVersion('beta', version)
+    const matches = next.match(betaPattern)
+    expect(matches).to.have.lengthOf(1)
+  })
+
+  it('bumps to beta from beta', async () => {
+    const version = 'v2.0.0-beta.8'
+    const next = await nextVersion('beta', version)
+    expect(next).to.equal('2.0.0-beta.9')
+  })
+
+  it('bumps to stable from beta', async () => {
+    const version = 'v2.0.0-beta.1'
+    const next = await nextVersion('stable', version)
+    expect(next).to.equal('2.0.0')
+  })
+
+  it('bumps to stable from stable', async () => {
+    const version = 'v2.0.0'
+    const next = await nextVersion('stable', version)
+    expect(next).to.equal('2.0.1')
+  })
+
+  it('bumps to stable from nightly', async () => {
+    const version = 'v2.0.0-nightly.19950901'
+    const next = await nextVersion('stable', version)
+    expect(next).to.equal('2.0.0')
+  })
+
+  it('throws on an invalid version', () => {
+    const version = 'vI.AM.INVALID'
+    return expect(
+      nextVersion('beta', version)
+    ).to.be.rejectedWith(`Invalid current version: ${version}`)
+  })
+
+  it('throws on an invalid bump type', () => {
+    const version = 'v2.0.0'
+    return expect(
+      nextVersion('WRONG', version)
+    ).to.be.rejectedWith('Invalid bump type.')
+  })
+})
