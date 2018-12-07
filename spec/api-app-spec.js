509e92ebd17cbd8c3b3553b2c11059f14db5d19f
Simone Cattaneo <simone@neo2it.net>
2018-12-07 12:55:30 +0100

    test: skip test for not supported setLoginItemSettings on mas platform

--- spec/api-app-spec.js
+++ spec/api-app-spec.js
@@ -450,7 +450,7 @@ describe('app module', () => {
     ]
 
     before(function () {
-      if (process.platform === 'linux') {
+      if (process.platform === 'linux' || process.mas) {
         this.skip()
       }
     })
