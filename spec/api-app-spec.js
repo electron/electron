ca218b63958891e79c6aad28d559801e47dc53a5
Shelley Vohr <codebytere@github.com>
2019-01-10 14:15:47 -0800

    test: allow retries for flaky mas loginitem specs (#16347)

--- spec/api-app-spec.js
+++ spec/api-app-spec.js
@@ -528,7 +528,10 @@ describe('app module', () => {
     })
   })
 
-  describe('app.get/setLoginItemSettings API', () => {
+  describe('app.get/setLoginItemSettings API', function () {
+    // allow up to three retries to account for flaky mas results
+    this.retries(3)
+
     const updateExe = path.resolve(path.dirname(process.execPath), '..', 'Update.exe')
     const processStartArgs = [
       '--processStart', `"${path.basename(process.execPath)}"`,
@@ -536,9 +539,7 @@ describe('app module', () => {
     ]
 
     before(function () {
-      if (process.platform === 'linux') {
-        this.skip()
-      }
+      if (process.platform === 'linux') this.skip()
     })
 
     beforeEach(() => {
@@ -609,11 +610,7 @@ describe('app module', () => {
     })
 
     it('allows you to pass a custom executable and arguments', function () {
-      if (process.platform !== 'win32') {
-        // FIXME(alexeykuzmin): Skip the test.
-        // this.skip()
-        return
-      }
+      if (process.platform !== 'win32') this.skip()
 
       app.setLoginItemSettings({ openAtLogin: true, path: updateExe, args: processStartArgs })
 
