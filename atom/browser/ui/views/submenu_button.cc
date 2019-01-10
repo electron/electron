ed5a865efb31245391251e1bc27d1f081a372fb8
deepak1556 <hop2deep@gmail.com>
2019-01-10 23:36:28 +0530

    Update GetAccessibleName() in view::ImageView and view::Button
    https://chromium-review.googlesource.com/c/chromium/src/+/1256308

--- atom/browser/ui/views/submenu_button.cc
+++ atom/browser/ui/views/submenu_button.cc
@@ -71,7 +71,7 @@ void SubmenuButton::SetUnderlineColor(SkColor color) {
 }
 
 void SubmenuButton::GetAccessibleNodeData(ui::AXNodeData* node_data) {
-  node_data->SetName(accessible_name());
+  node_data->SetName(GetAccessibleName());
   node_data->role = ax::mojom::Role::kPopUpButton;
 }
 
