eb02a422decfb44850f074963538d32d40665207
Milan Burda <milan.burda@gmail.com>
2019-01-10 14:32:03 +0100

    feat: add `fileMenu` / `viewMenu` / `appMenu` roles (#16328)

--- default_app/menu.js
+++ default_app/menu.js
@@ -1,163 +1,50 @@
 const { shell, Menu } = require('electron')
 
+const isMac = process.platform === 'darwin'
+
 const setDefaultApplicationMenu = () => {
   if (Menu.getApplicationMenu()) return
 
-  const template = [
-    {
-      label: 'Edit',
-      submenu: [
-        {
-          role: 'undo'
-        },
-        {
-          role: 'redo'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          role: 'cut'
-        },
-        {
-          role: 'copy'
-        },
-        {
-          role: 'paste'
-        },
-        {
-          role: 'pasteandmatchstyle'
-        },
-        {
-          role: 'delete'
-        },
-        {
-          role: 'selectall'
+  const helpMenu = {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click () {
+          shell.openExternal('https://electronjs.org')
         }
-      ]
-    },
-    {
-      label: 'View',
-      submenu: [
-        {
-          role: 'reload'
-        },
-        {
-          role: 'forcereload'
-        },
-        {
-          role: 'toggledevtools'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          role: 'resetzoom'
-        },
-        {
-          role: 'zoomin'
-        },
-        {
-          role: 'zoomout'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          role: 'togglefullscreen'
+      },
+      {
+        label: 'Documentation',
+        click () {
+          shell.openExternal(
+            `https://github.com/electron/electron/tree/v${process.versions.electron}/docs#readme`
+          )
         }
-      ]
-    },
-    {
-      role: 'windowMenu'
-    },
-    {
-      role: 'help',
-      submenu: [
-        {
-          label: 'Learn More',
-          click () {
-            shell.openExternal('https://electronjs.org')
-          }
-        },
-        {
-          label: 'Documentation',
-          click () {
-            shell.openExternal(
-              `https://github.com/electron/electron/tree/v${process.versions.electron}/docs#readme`
-            )
-          }
-        },
-        {
-          label: 'Community Discussions',
-          click () {
-            shell.openExternal('https://discuss.atom.io/c/electron')
-          }
-        },
-        {
-          label: 'Search Issues',
-          click () {
-            shell.openExternal('https://github.com/electron/electron/issues')
-          }
+      },
+      {
+        label: 'Community Discussions',
+        click () {
+          shell.openExternal('https://discuss.atom.io/c/electron')
         }
-      ]
-    }
-  ]
-
-  if (process.platform === 'darwin') {
-    template.unshift({
-      label: 'Electron',
-      submenu: [
-        {
-          role: 'about'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          role: 'services'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          role: 'hide'
-        },
-        {
-          role: 'hideothers'
-        },
-        {
-          role: 'unhide'
-        },
-        {
-          type: 'separator'
-        },
-        {
-          role: 'quit'
+      },
+      {
+        label: 'Search Issues',
+        click () {
+          shell.openExternal('https://github.com/electron/electron/issues')
         }
-      ]
-    })
-    template[1].submenu.push({
-      type: 'separator'
-    }, {
-      label: 'Speech',
-      submenu: [
-        {
-          role: 'startspeaking'
-        },
-        {
-          role: 'stopspeaking'
-        }
-      ]
-    })
-  } else {
-    template.unshift({
-      label: 'File',
-      submenu: [{
-        role: 'quit'
-      }]
-    })
+      }
+    ]
   }
+
+  const template = [
+    ...(isMac ? [{ role: 'appMenu' }] : []),
+    { role: 'fileMenu' },
+    { role: 'editMenu' },
+    { role: 'viewMenu' },
+    { role: 'windowMenu' },
+    helpMenu
+  ]
 
   const menu = Menu.buildFromTemplate(template)
   Menu.setApplicationMenu(menu)
