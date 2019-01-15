9c8b6ed540f30b10951ab19453df189092aaf33e
Shelley Vohr <shelley.vohr@gmail.com>
2019-01-15 08:50:32 -0800

    forgot one

--- atom/common/promise_util.cc
+++ atom/common/promise_util.cc
@@ -13,7 +13,6 @@ namespace util {
 
 Promise::Promise(v8::Isolate* isolate) {
   auto context = isolate->GetCurrentContext();
-
   auto resolver = v8::Promise::Resolver::New(context).ToLocalChecked();
   isolate_ = isolate;
 
@@ -24,6 +23,12 @@ Promise::Promise(v8::Isolate* isolate) {
 Promise::~Promise() = default;
 
 v8::Maybe<bool> Promise::RejectWithErrorMessage(const std::string& string) {
+  v8::HandleScope handle_scope(isolate());
+  v8::MicrotasksScope script_scope(isolate(),
+                                   v8::MicrotasksScope::kRunMicrotasks);
+  v8::Context::Scope context_scope(
+      v8::Local<v8::Context>::New(isolate(), GetContext()));
+
   v8::Local<v8::String> error_message =
       v8::String::NewFromUtf8(isolate(), string.c_str());
   v8::Local<v8::Value> error = v8::Exception::Error(error_message);
