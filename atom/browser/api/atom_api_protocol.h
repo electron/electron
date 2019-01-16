3d3cb1bf406fe0d63bcf9e3d55860ab014f30fe0
Nitish Sakhawalkar <nitsakh@icloud.com>
2019-01-15 16:39:20 -0800

    Updated api to take an array, still working on tests

--- atom/browser/api/atom_api_protocol.h
+++ atom/browser/api/atom_api_protocol.h
@@ -35,7 +35,7 @@ namespace api {
 
 std::vector<std::string> GetStandardSchemes();
 
-void RegisterSchemesAsPrivileged(const std::vector<std::string>& schemes,
+void RegisterSchemesAsPrivileged(v8::Local<v8::Value> val,
                                  mate::Arguments* args);
 
 class Protocol : public mate::TrackableObject<Protocol> {
