1a2df2f309f7c69c6f6795c3f8aef7dd761fc433
deepak1556 <hop2deep@gmail.com>
2019-01-21 23:44:57 +0530

    Change the source origin parameter type for CanCreateWindow from GURL to url::Origin.
    https://chromium-review.googlesource.com/c/chromium/src/+/1395066

--- atom/browser/api/atom_api_app.cc
+++ atom/browser/api/atom_api_app.cc
@@ -690,7 +690,7 @@ bool App::CanCreateWindow(
     content::RenderFrameHost* opener,
     const GURL& opener_url,
     const GURL& opener_top_level_frame_url,
-    const GURL& source_origin,
+    const url::Origin& source_origin,
     content::mojom::WindowContainerType container_type,
     const GURL& target_url,
     const content::Referrer& referrer,
