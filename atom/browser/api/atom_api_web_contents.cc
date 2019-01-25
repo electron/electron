770bb4cbce779e1acbc9745f42efd4e37ccc8944
Michael Tsu <michtsu@microsoft.com>
2019-01-25 14:28:58 -0800

    Add devtools API to inspect shared worker.

--- atom/browser/api/atom_api_web_contents.cc
+++ atom/browser/api/atom_api_web_contents.cc
@@ -1416,6 +1416,23 @@ void WebContents::InspectElement(int x, int y) {
   managed_web_contents()->InspectElement(x, y);
 }
 
+void WebContents::InspectSharedWorker() {
+  if (type_ == REMOTE)
+    return;
+
+  if (!enable_devtools_)
+    return;
+
+  for (const auto& agent_host : content::DevToolsAgentHost::GetOrCreateAll()) {
+    if (agent_host->GetType() ==
+        content::DevToolsAgentHost::kTypeSharedWorker) {
+      OpenDevTools(nullptr);
+      managed_web_contents()->AttachTo(agent_host);
+      break;
+    }
+  }
+}
+
 void WebContents::InspectServiceWorker() {
   if (type_ == REMOTE)
     return;
@@ -2178,6 +2195,7 @@ void WebContents::BuildPrototype(v8::Isolate* isolate,
       .SetMethod("unregisterServiceWorker",
                  &WebContents::UnregisterServiceWorker)
       .SetMethod("inspectServiceWorker", &WebContents::InspectServiceWorker)
+      .SetMethod("inspectSharedWorker", &WebContents::InspectSharedWorker)
 #if BUILDFLAG(ENABLE_PRINTING)
       .SetMethod("_print", &WebContents::Print)
       .SetMethod("_getPrinters", &WebContents::GetPrinterList)
--- atom/browser/api/atom_api_web_contents.h
+++ atom/browser/api/atom_api_web_contents.h
@@ -160,6 +160,7 @@ class WebContents : public mate::TrackableObject<WebContents>,
   void EnableDeviceEmulation(const blink::WebDeviceEmulationParams& params);
   void DisableDeviceEmulation();
   void InspectElement(int x, int y);
+  void InspectSharedWorker();
   void InspectServiceWorker();
   v8::Local<v8::Promise> HasServiceWorker();
   void UnregisterServiceWorker(const base::Callback<void(bool)>&);
