357fbfd7a4cfa686eafc07705456bf89541255c4
Samuel Attard <sattard@slack-corp.com>
2019-02-04 14:29:12 -0800

    chore: auto-format GN files in our precommit

---
+++ script/run-gn-format.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+import sys
+
+# Helper to run gn format on multiple files (gn only formats a single file at a time)
+def main():
+  for file in sys.argv[1:]:
+    subprocess.check_call(['gn', 'format', file])
+
+if __name__ == '__main__':
+  sys.exit(main())
