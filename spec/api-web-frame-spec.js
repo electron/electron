a9d9b7cf0ad4174b2815e5ff8f75b200db6025b5
Cheng Zhao <zcbenz@gmail.com>
2019-02-06 12:38:25 +0900

    fix: do not create native api::WebFrame in webFrame
    When reloading a page without restarting renderer process (for example
    sandbox mode), the blink::WebFrame is not destroyed, but api::WebFrame
    is always recreated for the new page context. This leaves a leak of
    api::WebFrame.

--- spec/api-web-frame-spec.js
+++ spec/api-web-frame-spec.js
@@ -158,4 +158,28 @@ describe('webFrame module', function () {
     const [, text] = await spellCheckerFeedback
     expect(text).to.equal(misspelledWord)
   })
+
+  it('top is self for top frame', () => {
+    expect(webFrame.top.context).to.equal(webFrame.context)
+  })
+
+  it('opener is null for top frame', () => {
+    expect(webFrame.opener).to.be.null()
+  })
+
+  it('firstChild is null for top frame', () => {
+    expect(webFrame.firstChild).to.be.null()
+  })
+
+  it('getFrameForSelector() does not crash when not found', () => {
+    expect(webFrame.getFrameForSelector('unexist-selector')).to.be.null()
+  })
+
+  it('findFrameByName() does not crash when not found', () => {
+    expect(webFrame.findFrameByName('unexist-name')).to.be.null()
+  })
+
+  it('findFrameByRoutingId() does not crash when not found', () => {
+    expect(webFrame.findFrameByRoutingId(-1)).to.be.null()
+  })
 })
