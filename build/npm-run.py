a8546f993a120be9f2725d46ae22fed9c47154e9
Samuel Attard <sattard@slack-corp.com>
2019-02-11 16:57:57 -0800

    chore: suppress output of npm_action unless it fails

--- build/npm-run.py
+++ build/npm-run.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 import os
+import subprocess
 import sys
 
 SOURCE_ROOT = os.path.dirname(os.path.dirname(__file__))
-args = ["npm", "run",
+cmd = "npm"
+if sys.platform == "win32":
+    cmd += ".cmd"
+args = [cmd, "run",
     "--prefix",
     SOURCE_ROOT
     ] + sys.argv[1:]
-os.execvp("npm", args)
+try:
+    subprocess.check_output(args, stderr=subprocess.STDOUT)
+except subprocess.CalledProcessError, e:
+    print("NPM script '" + sys.argv[2] + "' failed with code '" + str(e.returncode) + "':\n" + e.output)
+    sys.exit(e.returncode)
