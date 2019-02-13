b5affb57ba2c1e7a779754da2cf9bc37b507919e
Felix Rieseberg <felix@felixrieseberg.com>
2019-02-13 13:48:15 -0800

    refactor: Port renderer-internal-utils to TypeScript

--- lib/renderer/ipc-renderer-internal-utils.ts
+++ lib/renderer/ipc-renderer-internal-utils.ts
@@ -1,14 +1,14 @@
-'use strict'
-
-const ipcRenderer = require('@electron/internal/renderer/ipc-renderer-internal')
-const errorUtils = require('@electron/internal/common/error-utils')
+import * as ipcRenderer from '@electron/internal/renderer/ipc-renderer-internal'
+import * as errorUtils from '@electron/internal/common/error-utils'
 
 let nextId = 0
 
-exports.invoke = function (command, ...args) {
+export function invoke (command: string, ...args: any[]) {
   return new Promise((resolve, reject) => {
     const requestId = ++nextId
-    ipcRenderer.once(`${command}_RESPONSE_${requestId}`, (event, error, result) => {
+    ipcRenderer.once(`${command}_RESPONSE_${requestId}`, (
+      _event: Electron.Event, error: Electron.SerializedError, result: any
+    ) => {
       if (error) {
         reject(errorUtils.deserialize(error))
       } else {
@@ -19,7 +19,7 @@ exports.invoke = function (command, ...args) {
   })
 }
 
-exports.invokeSync = function (command, ...args) {
+export function invokeSync (command: string, ...args: any[]) {
   const [ error, result ] = ipcRenderer.sendSync(command, ...args)
 
   if (error) {
