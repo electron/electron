743728376225d1629425679ce32d5d4960eece38
Samuel Attard <sattard@slack-corp.com>
2019-02-13 13:17:30 -0800

    fix: fix paths for tsconfig / ts-smoke runner

--- spec/ts-smoke/runner.js
+++ spec/ts-smoke/runner.js
@@ -3,8 +3,8 @@ const childProcess = require('child_process')
 
 const typeCheck = () => {
   const tscExec = path.resolve(require.resolve('typescript'), '../../bin/tsc')
-  const tscChild = childProcess.spawn('node', [tscExec, '--project', '../../tsconfig.json'], {
-    cwd: path.resolve(__dirname, 'test-smoke/electron')
+  const tscChild = childProcess.spawn(process.execPath, [tscExec, '--project', './ts-smoke/tsconfig.json'], {
+    cwd: path.resolve(__dirname, '../')
   })
   tscChild.stdout.on('data', d => console.log(d.toString()))
   tscChild.stderr.on('data', d => console.error(d.toString()))
