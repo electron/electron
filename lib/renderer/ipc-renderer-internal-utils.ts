9112ad01bee544ec4e6317e43beb785e3f929756
Felix Rieseberg <felix@felixrieseberg.com>
2019-02-13 23:05:49 -0800

    refactor: Port renderer-internal-utils to TypeScript  (#16942)
    * chore: make aliasify work on .ts files as well
    
    * refactor: Port renderer-internal-utils to TypeScript
    
    * refactor: Implement feedback <3

---
+++ lib/renderer/ipc-renderer-internal-utils.ts
@@ -0,0 +1,30 @@
+import * as ipcRenderer from '@electron/internal/renderer/ipc-renderer-internal'
+import * as errorUtils from '@electron/internal/common/error-utils'
+
+let nextId = 0
+
+export function invoke<T> (command: string, ...args: any[]) {
+  return new Promise<T>((resolve, reject) => {
+    const requestId = ++nextId
+    ipcRenderer.once(`${command}_RESPONSE_${requestId}`, (
+      _event: Electron.Event, error: Electron.SerializedError, result: any
+    ) => {
+      if (error) {
+        reject(errorUtils.deserialize(error))
+      } else {
+        resolve(result)
+      }
+    })
+    ipcRenderer.send(command, requestId, ...args)
+  })
+}
+
+export function invokeSync<T> (command: string, ...args: any[]): T {
+  const [ error, result ] = ipcRenderer.sendSync(command, ...args)
+
+  if (error) {
+    throw errorUtils.deserialize(error)
+  } else {
+    return result
+  }
+}
