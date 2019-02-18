2b15eafb3ecc5579315695d5d5a1dd8d40bcfc1e
Felix Rieseberg <felix@felixrieseberg.com>
2019-02-18 14:24:01 +0000

    refactor: Port callbacks-registry to TypeScript

--- lib/renderer/callbacks-registry.ts
+++ lib/renderer/callbacks-registry.ts
@@ -1,16 +1,12 @@
-'use strict'
-
 const v8Util = process.atomBinding('v8_util')
 
-class CallbacksRegistry {
-  constructor () {
-    this.nextId = 0
-    this.callbacks = {}
-  }
+export class CallbacksRegistry {
+  public nextId = 0
+  public callbacks: Record<number, any> = {}
 
-  add (callback) {
+  public add (callback: (...args: Array<any>) => any) {
     // The callback is already added.
-    let id = v8Util.getHiddenValue(callback, 'callbackId')
+    let id: number | null = v8Util.getHiddenValue(callback, 'callbackId')
     if (id != null) return id
 
     id = this.nextId += 1
@@ -23,31 +19,35 @@ class CallbacksRegistry {
     let filenameAndLine
     let match
 
-    while ((match = regexp.exec(stackString)) !== null) {
+    while ((match = regexp.exec(stackString || '')) !== null) {
       const location = match[1]
       if (location.includes('(native)')) continue
       if (location.includes('(<anonymous>)')) continue
       if (location.includes('electron.asar')) continue
 
       const ref = /([^/^)]*)\)?$/gi.exec(location)
+
+      if (!ref) continue
       filenameAndLine = ref[1]
       break
     }
+
     this.callbacks[id] = callback
     v8Util.setHiddenValue(callback, 'callbackId', id)
     v8Util.setHiddenValue(callback, 'location', filenameAndLine)
+
     return id
   }
 
-  get (id) {
+  get (id: number) {
     return this.callbacks[id] || function () {}
   }
 
-  apply (id, ...args) {
+  apply (id: number, ...args: Array<any>) {
     return this.get(id).apply(global, ...args)
   }
 
-  remove (id) {
+  remove (id: number) {
     const callback = this.callbacks[id]
     if (callback) {
       v8Util.deleteHiddenValue(callback, 'callbackId')
@@ -55,5 +55,3 @@ class CallbacksRegistry {
     }
   }
 }
-
-module.exports = CallbacksRegistry
