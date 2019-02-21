d04d11cfb5a6534bd6eef6ab5a550e9859d4c811
deepak1556 <hop2deep@gmail.com>
2019-02-21 23:19:58 +0530

    [mojo] include mojo header
    https://chromium-review.googlesource.com/c/chromium/src/+/1447145

--- atom/browser/api/frame_subscriber.cc
+++ atom/browser/api/frame_subscriber.cc
@@ -10,6 +10,7 @@
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/render_widget_host_view.h"
+#include "media/capture/mojom/video_capture_types.mojom.h"
 #include "ui/gfx/skbitmap_operations.h"
 
 #include "atom/common/node_includes.h"
