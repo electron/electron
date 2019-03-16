2bee62c2d1a3c237e1a10be96271d5ab25b71015
Milan Burda <milan.burda@gmail.com>
2019-03-17 00:47:30 +0100

    fix: regressions introduced by adding world isolation to Chrome extension content scripts

--- spec/fixtures/extensions/chrome-api/main.js
+++ spec/fixtures/extensions/chrome-api/main.js
@@ -1,15 +1,28 @@
 /* global chrome */
 
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  sendResponse(message)
+})
+
 const testMap = {
   getManifest () {
     const manifest = chrome.runtime.getManifest()
     console.log(JSON.stringify(manifest))
+  },
+  sendMessage (message) {
+    chrome.runtime.sendMessage({ method: 'sendMessage', args: [message] }, response => {
+      console.log(JSON.stringify(response))
+    })
+  },
+  executeScript (code) {
+    chrome.runtime.sendMessage({ method: 'executeScript', args: [code] }, response => {
+      console.log(JSON.stringify(response))
+    })
   }
 }
 
 const dispatchTest = (event) => {
-  const testName = event.data
-  const test = testMap[testName]
-  test()
+  const { method, args = [] } = JSON.parse(event.data)
+  testMap[method](...args)
 }
 window.addEventListener('message', dispatchTest, false)
