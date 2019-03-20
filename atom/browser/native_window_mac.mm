b140a82fe88489910279f363d69358bf202070e1
Shelley Vohr <codebytere@github.com>
2019-03-20 15:33:59 -0700

    fix: take foreground visibility into account for isVisible on macOS (#17463)

--- atom/browser/native_window_mac.mm
+++ atom/browser/native_window_mac.mm
@@ -576,7 +576,12 @@ void NativeWindowMac::Hide() {
 }
 
 bool NativeWindowMac::IsVisible() {
-  return [window_ isVisible];
+  bool occluded = [window_ occlusionState] == NSWindowOcclusionStateVisible;
+
+  // For a window to be visible, it must be visible to the user in the
+  // foreground of the app, which means that it should not be minimized or
+  // occluded
+  return [window_ isVisible] && !occluded && !IsMinimized();
 }
 
 bool NativeWindowMac::IsEnabled() {
