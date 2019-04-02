fcfcd1bc8cfe3b999b76f1162692a048ddcadb58
Heilig Benedek <benecene@gmail.com>
2019-04-02 15:09:48 +0200

    Update osr_host_display_client_mac.mm

--- atom/browser/osr/osr_host_display_client_mac.mm
+++ atom/browser/osr/osr_host_display_client_mac.mm
@@ -14,14 +14,6 @@ void OffScreenHostDisplayClient::OnDisplayReceivedCALayerParams(
     base::ScopedCFTypeRef<IOSurfaceRef> io_surface(
         IOSurfaceLookupFromMachPort(ca_layer_params.io_surface_mach_port));
 
-    // IOReturn io_result = IOSurfaceLock(
-    //     io_surface, kIOSurfaceLockReadOnly | kIOSurfaceLockAvoidSync,
-    //     nullptr);
-    // if (io_result) {
-    //   DLOG(ERROR) << "Failed to lock IOSurface " << io_result;
-    //   return;
-    // }
-
     gfx::Size pixel_size_ = ca_layer_params.pixel_size;
     void* pixels = static_cast<void*>(IOSurfaceGetBaseAddress(io_surface));
     size_t stride = IOSurfaceGetBytesPerRow(io_surface);
@@ -35,15 +27,6 @@ void OffScreenHostDisplayClient::OnDisplayReceivedCALayerParams(
         SkImageInfo::MakeN32(pixel_size_.width(), pixel_size_.height(),
                              kPremul_SkAlphaType),
         pixels, stride);
-    // [](void*, void* context) {
-    //   auto* surface_pinner = static_cast<IOSurfacePinner*>(context);
-    //   IOReturn io_result = IOSurfaceUnlock(surface_pinner->io_surface,
-    //     kIOSurfaceLockAvoidSync, nullptr);
-    //   if (io_result)
-    //     DLOG(ERROR) << "Failed to unlock IOSurface " << io_result;
-    //   delete surface_pinner;
-    // },
-    // new IOSurfacePinner{io_surface});
     bitmap.setImmutable();
     callback_.Run(ca_layer_params.damage, bitmap);
   }
