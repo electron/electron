e4ac518b7b90a495bc9e4b80a64f3ec916ebfcb2
zp1996 <1179258516@qq.com>
2019-04-15 17:16:08 +0800

    fix: make parseFeaturesString support all webPreferences option

--- spec/internal-spec.js
+++ spec/internal-spec.js
@@ -2,13 +2,14 @@ const chai = require('chai')
 const { expect } = chai
 
 describe('feature-string parsing', () => {
+  const parseFeaturesString = require('@electron/internal/common/parse-features-string')
+  const checkParse = (string, parsed) => {
+    const features = {}
+    parseFeaturesString(string, (k, v) => { features[k] = v })
+    expect(features).to.deep.equal(parsed)
+  }
+
   it('is indifferent to whitespace around keys and values', () => {
-    const parseFeaturesString = require('@electron/internal/common/parse-features-string')
-    const checkParse = (string, parsed) => {
-      const features = {}
-      parseFeaturesString(string, (k, v) => { features[k] = v })
-      expect(features).to.deep.equal(parsed)
-    }
     checkParse('a=yes,c=d', { a: true, c: 'd' })
     checkParse('a=yes ,c=d', { a: true, c: 'd' })
     checkParse('a=yes, c=d', { a: true, c: 'd' })
@@ -19,5 +20,19 @@ describe('feature-string parsing', () => {
     checkParse(' a = yes , c =d', { a: true, c: 'd' })
     checkParse(' a = yes , c = d', { a: true, c: 'd' })
     checkParse(' a = yes , c = d ', { a: true, c: 'd' })
+    checkParse('a=1, c=d, additionalArguments=[--name=name test,--title=title]', {
+      additionalArguments: ['--name=name test', '--title=title'],
+      a: true,
+      c: 'd'
+    })
+    checkParse(' additionalArguments = [--title=title] ', {
+      additionalArguments: ['--title=title']
+    })
+  })
+
+  it('A name by itself is given a true boolean value', () => {
+    checkParse('a', { a: true })
+    checkParse('a, c', { a: true, c: true })
+    checkParse('a,c=1', { a: true, c: true })
   })
 })
