b7b9efa875f5e63dd182247909629c37c68d8788
Samuel Attard <samuel.r.attard@gmail.com>
2019-04-16 14:22:51 -0400

    fix: handle remote-debugging-port=0 correctly (#17800)
    By default the Chromedriver will send remote-debugging-port=0 to let the
    browser choose a free port to listen on.  The chosen port is written to
    a known file in the user data dir that is passed to the app through the
    CLI.
    
    This PR does two things.
    
    1. Correctly passes the USER_DATA_DIR to the remote debugging server so
    it knows where to write the file
    2. Adds support for --user-data-dir as we did not support that CLI
    argument and Chromedriver relies on being able to tell the "browser"
    where to write this file.
    
    Fixes #17354

--- typings/internal-electron.d.ts
+++ typings/internal-electron.d.ts
@@ -13,6 +13,7 @@ declare namespace Electron {
   }
 
   interface App {
+    _setDefaultAppPaths(packagePath: string | null): void;
     setVersion(version: string): void;
     setDesktopName(name: string): void;
     setAppPath(path: string | null): void;
