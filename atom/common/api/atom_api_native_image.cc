276ebca24cef50eb0ad405dfcc794e7738b55a01
Milan Burda <milan.burda@gmail.com>
2019-04-18 17:54:05 +0200

    fix: copy pixels in AddImageSkiaRepFromBuffer (#17843) (#17861)

--- atom/common/api/atom_api_native_image.cc
+++ atom/common/api/atom_api_native_image.cc
@@ -135,7 +135,7 @@ bool AddImageSkiaRepFromBuffer(gfx::ImageSkia* image,
 
   SkBitmap bitmap;
   bitmap.allocN32Pixels(width, height, false);
-  bitmap.setPixels(const_cast<void*>(reinterpret_cast<const void*>(data)));
+  bitmap.writePixels({info, data, bitmap.rowBytes()});
 
   image->AddRepresentation(gfx::ImageSkiaRep(bitmap, scale_factor));
   return true;
