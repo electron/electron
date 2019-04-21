bf1540c6bb3dcd2cdc793f4c4158d793805d8e29
Kirill Mikhaylov <kirill.mikhailov@gmail.com>
2019-04-21 19:38:44 +0300

    C++ opmization interface for CustomMediaStream

--- atom/renderer/api/custom_media_stream.cc
+++ atom/renderer/api/custom_media_stream.cc
@@ -28,6 +28,12 @@
 #include "native_mate/handle.h"
 #include "native_mate/wrappable.h"
 
+#include "atom/renderer/api/custom_media_stream.h"
+
+namespace {
+struct ControlObject;
+}
+
 namespace mate {
 template <>
 struct Converter<blink::MediaStreamComponent*> {
@@ -85,6 +91,28 @@ struct Converter<base::TimeTicks> {
     return false;
   }
 };
+
+template <>
+struct Converter<ControlObject*> {
+  static v8::Local<v8::Value> ToV8(v8::Isolate* isolate, ControlObject* v);
+
+  static bool FromV8(v8::Isolate* isolate,
+                     v8::Local<v8::Value> value,
+                     ControlObject** out) {
+    if (!value->IsObject())
+      return false;
+
+    v8::Local<v8::Object> obj = v8::Local<v8::Object>::Cast(value);
+
+    if (obj->InternalFieldCount() != 2)
+      return false;
+
+    *out =
+        static_cast<ControlObject*>(obj->GetAlignedPointerFromInternalField(0));
+    return true;
+  }
+};
+
 }  // namespace mate
 
 namespace {
@@ -172,8 +200,135 @@ struct Frame : mate::Wrappable<Frame> {
   scoped_refptr<media::VideoFrame> frame_;
 };
 
-struct ControlObject : mate::Wrappable<ControlObject> {
-  ControlObject(v8::Isolate* isolate) { Init(isolate); }
+struct NonGCFrame final : CustomMediaStream::VideoFrame {
+  NonGCFrame(scoped_refptr<media::VideoFrame> frame) : frame_(frame) {}
+
+  CustomMediaStream::Format format() override {
+    return {frame_->visible_rect().width(), frame_->visible_rect().height()};
+  }
+
+  unsigned stride(CustomMediaStream::Plane plane) override {
+    switch (plane) {
+      case CustomMediaStream::Plane::Y:
+        return frame_->stride(media::VideoFrame::kYPlane);
+      case CustomMediaStream::Plane::U:
+        return frame_->stride(media::VideoFrame::kUPlane);
+      case CustomMediaStream::Plane::V:
+        return frame_->stride(media::VideoFrame::kVPlane);
+      default:
+        return -1;
+    }
+  }
+
+  unsigned rows(CustomMediaStream::Plane plane) override {
+    switch (plane) {
+      case CustomMediaStream::Plane::Y:
+        return frame_->rows(media::VideoFrame::kYPlane);
+      case CustomMediaStream::Plane::U:
+        return frame_->rows(media::VideoFrame::kUPlane);
+      case CustomMediaStream::Plane::V:
+        return frame_->rows(media::VideoFrame::kVPlane);
+      default:
+        return -1;
+    }
+  }
+
+  void* data(CustomMediaStream::Plane plane) override {
+    switch (plane) {
+      case CustomMediaStream::Plane::Y:
+        return frame_->visible_data(media::VideoFrame::kYPlane);
+      case CustomMediaStream::Plane::U:
+        return frame_->visible_data(media::VideoFrame::kUPlane);
+      case CustomMediaStream::Plane::V:
+        return frame_->visible_data(media::VideoFrame::kVPlane);
+      default:
+        return nullptr;
+    }
+  }
+
+  scoped_refptr<media::VideoFrame> frame_;
+};
+
+struct ControlObject final
+    : CustomMediaStream::VideoFrameCallback {  // not dereving from
+                                               // mate::wrappable because we
+                                               // need 2 internal fields
+  ControlObject(v8::Isolate* isolate, gfx::Size resolution)
+      : isolate_(isolate), resolution_(resolution) {
+    auto templ = GetConstructor(isolate);
+
+    v8::Local<v8::Object> wrapper;
+    if (!templ->InstanceTemplate()
+             ->NewInstance(isolate->GetCurrentContext())
+             .ToLocal(&wrapper))
+      std::terminate();
+
+    wrapper_.Reset(isolate, wrapper);
+    wrapper_.SetWeak(this, FirstWeakCallback, v8::WeakCallbackType::kParameter);
+
+    auto w = this->wrapper();
+    w->SetAlignedPointerInInternalField(0, this);
+    w->SetAlignedPointerInInternalField(
+        1, static_cast<CustomMediaStream::VideoFrameCallback*>(this));
+  }
+
+  v8::Local<v8::Object> wrapper() const {
+    return v8::Local<v8::Object>::New(isolate_, wrapper_);
+  }
+
+  v8::Isolate* isolate() const { return isolate_; }
+
+  ~ControlObject() {
+    std::cerr << "~ControlObject" << std::endl;
+    if (wrapper_.IsEmpty())
+      return;
+
+    wrapper()->SetAlignedPointerInInternalField(0, nullptr);
+    wrapper()->SetAlignedPointerInInternalField(1, nullptr);
+    wrapper_.ClearWeak();
+    wrapper_.Reset();
+  }
+
+  static v8::Local<v8::FunctionTemplate> GetConstructor(v8::Isolate* isolate) {
+    auto* data = gin::PerIsolateData::From(isolate);
+    auto templ = data->GetFunctionTemplate(&kWrapperInfo);
+    if (templ.IsEmpty()) {
+      templ = v8::FunctionTemplate::New(isolate);
+      templ->InstanceTemplate()->SetInternalFieldCount(2);
+      BuildPrototype(isolate, templ);
+      data->SetFunctionTemplate(&kWrapperInfo, templ);
+    }
+    return templ;
+  }
+
+  static void FirstWeakCallback(
+      const v8::WeakCallbackInfo<ControlObject>& data) {
+    ControlObject* p = data.GetParameter();
+    p->wrapper_.Reset();
+    data.SetSecondPassCallback(SecondWeakCallback);
+  }
+
+  static void SecondWeakCallback(
+      const v8::WeakCallbackInfo<ControlObject>& data) {
+    ControlObject* c = data.GetParameter();
+    delete c;
+  }
+
+  static void BuildPrototype(v8::Isolate* isolate,
+                             v8::Local<v8::FunctionTemplate> prototype) {
+    prototype->SetClassName(
+        mate::StringToV8(isolate, "CustomMediaStreamController"));
+    mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
+        .SetMethod("allocateFrame", &ControlObject::allocate)
+        .SetMethod("queueFrame", &ControlObject::queue);
+  }
+
+  static std::pair<v8::Local<v8::Object>, ControlObject*> create(
+      v8::Isolate* isolate,
+      gfx::Size resolution) {
+    ControlObject* p = new ControlObject(isolate, resolution);
+    return std::make_pair(p->wrapper(), p);
+  }
 
   Frame* allocate(gfx::Size size, base::TimeDelta timestamp) {
     auto f = framePool_.CreateFrame(media::PIXEL_FORMAT_I420, size,
@@ -187,23 +342,38 @@ struct ControlObject : mate::Wrappable<ControlObject> {
     frame->frame_ = nullptr;
   }
 
-  static mate::Handle<ControlObject> create(v8::Isolate* isolate) {
-    return mate::CreateHandle(isolate, new ControlObject(isolate));
+  CustomMediaStream::VideoFrame* allocateFrame(
+      CustomMediaStream::Timestamp ts,
+      CustomMediaStream::Format const* format) override {
+    gfx::Size size =
+        format ? gfx::Size{format->width, format->height} : resolution_;
+    auto f = framePool_.CreateFrame(
+        media::PIXEL_FORMAT_I420, size, gfx::Rect(size), size,
+        base::TimeDelta::FromMillisecondsD(ts.milliseconds));
+    return new NonGCFrame(f);
   }
 
-  static void BuildPrototype(v8::Isolate* isolate,
-                             v8::Local<v8::FunctionTemplate> prototype) {
-    prototype->SetClassName(
-        mate::StringToV8(isolate, "CustomMediaStreamController"));
-    mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
-        .SetMethod("allocateFrame", &ControlObject::allocate)
-        .SetMethod("queueFrame", &ControlObject::queue);
+  void queueFrame(CustomMediaStream::Timestamp ts,
+                  CustomMediaStream::VideoFrame* frame) override {
+    NonGCFrame* f = static_cast<NonGCFrame*>(frame);
+    io_task_runner_->PostTask(
+        FROM_HERE,
+        base::Bind(deliver_, f->frame_,
+                   base::TimeTicks::UnixEpoch() +
+                       base::TimeDelta::FromMillisecondsD(ts.milliseconds)));
+    delete f;
   }
 
+  v8::Isolate* isolate_;
+  v8::Global<v8::Object> wrapper_;  // Weak
+
+  gfx::Size resolution_;
   content::VideoCaptureDeliverFrameCB deliver_;
   media::VideoFramePool framePool_;
   const scoped_refptr<base::SingleThreadTaskRunner> io_task_runner_ =
       content::RenderThread::Get()->GetIOTaskRunner();
+
+  static gin::WrapperInfo kWrapperInfo;
 };
 
 struct CustomCapturerSource : media::VideoCapturerSource {
@@ -216,9 +386,17 @@ struct CustomCapturerSource : media::VideoCapturerSource {
       : format_(resolution, framerate, media::PIXEL_FORMAT_I420),
         onStartCapture_(std::move(onStartCapture)),
         onStopCapture_(std::move(onStopCapture)) {
-    auto handle = ControlObject::create(isolate);
-    control_wrapper_ = v8::Global<v8::Value>(isolate, handle.ToV8());
-    control_ = handle.get();
+    auto r = ControlObject::create(isolate, resolution);
+    control_wrapper_ = v8::Global<v8::Value>(isolate, r.first);
+    control_ = r.second;
+  }
+
+  ~CustomCapturerSource() override {
+    std::cerr << "~CustomCapturerSource" << std::endl;
+    control_wrapper_.ClearWeak();
+    control_wrapper_.Reset();
+
+    control_->wrapper_.ClearWeak();
   }
 
   media::VideoCaptureFormats GetPreferredFormats() override {
@@ -301,5 +479,12 @@ void Initialize(v8::Local<v8::Object> exports,
 }
 
 }  // namespace
+
+// template<>
+v8::Local<v8::Value> mate::Converter<ControlObject*>::ToV8(v8::Isolate* isolate,
+                                                           ControlObject* v) {
+  return v->wrapper();
+}
+gin::WrapperInfo ControlObject::kWrapperInfo = {gin::kEmbedderNativeGin};
 
 NODE_BUILTIN_MODULE_CONTEXT_AWARE(atom_renderer_custom_media_stream, Initialize)
---
+++ atom/renderer/api/custom_media_stream.h
@@ -0,0 +1,41 @@
+#ifndef __CUSTOM_MEDIA_STREAM_H__
+#define __CUSTOM_MEDIA_STREAM_H__
+
+namespace CustomMediaStream {
+
+enum class Plane { Y, U, V };
+
+struct Format {
+  unsigned width;
+  unsigned height;
+};
+
+struct VideoFrame {
+  virtual Format format() = 0;
+  virtual unsigned stride(Plane) = 0;
+  virtual unsigned rows(Plane) = 0;
+  virtual void* data(Plane plane) = 0;
+};
+
+struct Timestamp {
+  double milliseconds;
+};
+
+struct VideoFrameCallback {
+  virtual VideoFrame* allocateFrame(Timestamp ts, Format const* format) = 0;
+  virtual void queueFrame(Timestamp ts, VideoFrame* frame) = 0;
+};
+
+VideoFrameCallback* unwrapCallback(v8::Handle<v8::Value> value) {
+  if (!value->IsObject())
+    return nullptr;
+  v8::Local<v8::Object> obj = v8::Local<v8::Object>::Cast(value);
+  if (obj->InternalFieldCount() != 2)
+    return nullptr;
+  return static_cast<VideoFrameCallback*>(
+      obj->GetAlignedPointerFromInternalField(1));
+}
+
+}  // namespace CustomMediaStream
+
+#endif
