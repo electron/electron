8355b48c2771b99c603bcdadc37e7934a8e31738
Cheng Zhao <zcbenz@gmail.com>
2019-04-22 15:28:45 +0900

    Feed dummy content to custom protocol

--- atom/browser/net/atom_url_loader_factory.cc
+++ atom/browser/net/atom_url_loader_factory.cc
@@ -4,8 +4,11 @@
 
 #include "atom/browser/net/atom_url_loader_factory.h"
 
+#include "content/public/browser/browser_thread.h"
 #include "services/network/public/cpp/url_loader_completion_status.h"
 #include "services/network/public/mojom/url_loader.mojom.h"
+
+using content::BrowserThread;
 
 namespace atom {
 
@@ -21,7 +24,23 @@ void AtomURLLoaderFactory::CreateLoaderAndStart(
     const network::ResourceRequest& request,
     network::mojom::URLLoaderClientPtr client,
     const net::MutableNetworkTrafficAnnotationTag& traffic_annotation) {
-  client->OnComplete(network::URLLoaderCompletionStatus(net::ERR_FAILED));
+  std::string contents = "Not Implemented";
+
+  uint32_t size = base::saturated_cast<uint32_t>(contents.size());
+  mojo::DataPipe pipe(size);
+  MojoResult result = pipe.producer_handle->WriteData(
+      contents.data(), &size, MOJO_WRITE_DATA_FLAG_NONE);
+  if (result != MOJO_RESULT_OK || size < contents.size()) {
+    client->OnComplete(network::URLLoaderCompletionStatus(net::ERR_FAILED));
+    return;
+  }
+
+  network::ResourceResponseHead head;
+  head.mime_type = "text/html";
+  head.charset = "utf-8";
+  client->OnReceiveResponse(head);
+  client->OnStartLoadingResponseBody(std::move(pipe.consumer_handle));
+  client->OnComplete(network::URLLoaderCompletionStatus(net::OK));
 }
 
 void AtomURLLoaderFactory::Clone(
