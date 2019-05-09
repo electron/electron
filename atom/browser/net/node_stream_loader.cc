fed93cf7608cfa04a93fd694358a8cc4c9262ac1
Cheng Zhao <zcbenz@gmail.com>
2019-05-09 15:59:06 +0900

    fix: handle the case when one write can not write all data

--- atom/browser/net/node_stream_loader.cc
+++ atom/browser/net/node_stream_loader.cc
@@ -41,28 +41,82 @@ NodeStreamLoader::~NodeStreamLoader() {
     node::MakeCallback(isolate_, emitter_.Get(isolate_), "removeListener",
                        node::arraysize(args), args, {0, 0});
   }
+
+  // Release references.
+  emitter_.Reset();
+  buffer_.Reset();
 }
 
 void NodeStreamLoader::Start(network::ResourceResponseHead head) {
+  mojo::ScopedDataPipeProducerHandle producer;
   mojo::ScopedDataPipeConsumerHandle consumer;
-  MojoResult rv = mojo::CreateDataPipe(nullptr, &producer_, &consumer);
+  MojoResult rv = mojo::CreateDataPipe(nullptr, &producer, &consumer);
   if (rv != MOJO_RESULT_OK) {
-    client_->OnComplete(
-        network::URLLoaderCompletionStatus(net::ERR_INSUFFICIENT_RESOURCES));
-    delete this;
+    NotifyComplete(net::ERR_INSUFFICIENT_RESOURCES);
     return;
   }
+
+  producer_ =
+      std::make_unique<mojo::StringDataPipeProducer>(std::move(producer));
 
   client_->OnReceiveResponse(head);
   client_->OnStartLoadingResponseBody(std::move(consumer));
 
   auto weak = weak_factory_.GetWeakPtr();
-  On("end", base::BindRepeating(&NodeStreamLoader::OnEnd, weak));
-  On("error", base::BindRepeating(&NodeStreamLoader::OnError, weak));
-  // Since every node::MakeCallback call has a micro scope itself, we have to
-  // subscribe |data| at last otherwise |end|'s listener won't be called when
-  // it is emitted in the same tick.
-  On("data", base::BindRepeating(&NodeStreamLoader::OnData, weak));
+  On("end",
+     base::BindRepeating(&NodeStreamLoader::NotifyComplete, weak, net::OK));
+  On("error", base::BindRepeating(&NodeStreamLoader::NotifyComplete, weak,
+                                  net::ERR_FAILED));
+  On("readable", base::BindRepeating(&NodeStreamLoader::ReadMore, weak));
+}
+
+void NodeStreamLoader::NotifyComplete(int result) {
+  // Wait until write finishes or fails.
+  if (is_writing_) {
+    ended_ = true;
+    result_ = result;
+    return;
+  }
+
+  client_->OnComplete(network::URLLoaderCompletionStatus(result));
+  delete this;
+}
+
+void NodeStreamLoader::ReadMore() {
+  // buffer = emitter.read()
+  v8::MaybeLocal<v8::Value> ret = node::MakeCallback(
+      isolate_, emitter_.Get(isolate_), "read", 0, nullptr, {0, 0});
+
+  // If there is no buffer read, wait until |readable| is emitted again.
+  v8::Local<v8::Value> buffer;
+  if (!ret.ToLocal(&buffer) || !node::Buffer::HasInstance(buffer))
+    return;
+
+  // Hold the buffer until the write is done.
+  buffer_.Reset(isolate_, buffer);
+
+  // Write buffer to mojo pipe asyncronously.
+  is_writing_ = true;
+  producer_->Write(
+      base::StringPiece(node::Buffer::Data(buffer),
+                        node::Buffer::Length(buffer)),
+      mojo::StringDataPipeProducer::AsyncWritingMode::
+          STRING_STAYS_VALID_UNTIL_COMPLETION,
+      base::BindOnce(&NodeStreamLoader::DidWrite, weak_factory_.GetWeakPtr()));
+}
+
+void NodeStreamLoader::DidWrite(MojoResult result) {
+  is_writing_ = false;
+  // We were told to end streaming.
+  if (ended_) {
+    NotifyComplete(result_);
+    return;
+  }
+
+  if (result == MOJO_RESULT_OK)
+    ReadMore();
+  else
+    NotifyComplete(net::ERR_FAILED);
 }
 
 void NodeStreamLoader::On(const char* event, EventCallback callback) {
@@ -78,34 +132,7 @@ void NodeStreamLoader::On(const char* event, EventCallback callback) {
   handlers_[event].Reset(isolate_, args[1]);
   node::MakeCallback(isolate_, emitter_.Get(isolate_), "on",
                      node::arraysize(args), args, {0, 0});
-}
-
-void NodeStreamLoader::OnData(mate::Arguments* args) {
-  v8::Local<v8::Value> buffer;
-  args->GetNext(&buffer);
-  if (!node::Buffer::HasInstance(buffer)) {
-    args->ThrowError("data must be Buffer");
-    return;
-  }
-
-  size_t ssize = node::Buffer::Length(buffer);
-  uint32_t size = base::saturated_cast<uint32_t>(ssize);
-  MojoResult result = producer_->WriteData(node::Buffer::Data(buffer), &size,
-                                           MOJO_WRITE_DATA_FLAG_NONE);
-  if (result != MOJO_RESULT_OK || size < ssize) {
-    OnError(nullptr);
-    return;
-  }
-}
-
-void NodeStreamLoader::OnEnd(mate::Arguments* args) {
-  client_->OnComplete(network::URLLoaderCompletionStatus(net::OK));
-  delete this;
-}
-
-void NodeStreamLoader::OnError(mate::Arguments* args) {
-  client_->OnComplete(network::URLLoaderCompletionStatus(net::ERR_FAILED));
-  delete this;
+  // No more code bellow, as this class may destruct when subscribing.
 }
 
 }  // namespace atom
