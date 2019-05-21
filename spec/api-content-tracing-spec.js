82e2063b0b2818fb8752f2fcb1b1cd8c8fe6ba06
Shelley Vohr <shelley.vohr@gmail.com>
2019-05-21 10:15:56 -0700

    test: add spec for contentTracing.stopRecording

--- spec/api-content-tracing-spec.js
+++ spec/api-content-tracing-spec.js
@@ -15,10 +15,6 @@ const timeout = async (milliseconds) => {
   })
 }
 
-const getPathInATempFolder = (filename) => {
-  return path.join(app.getPath('temp'), filename)
-}
-
 describe('contentTracing', () => {
   beforeEach(function () {
     // FIXME: The tests are skipped on arm/arm64.
@@ -38,7 +34,7 @@ describe('contentTracing', () => {
     return resultFilePath
   }
 
-  const outputFilePath = getPathInATempFolder('trace.json')
+  const outputFilePath = path.join(app.getPath('temp'), 'trace.json')
   beforeEach(() => {
     if (fs.existsSync(outputFilePath)) {
       fs.unlinkSync(outputFilePath)
@@ -114,6 +110,18 @@ describe('contentTracing', () => {
 
   describe('stopRecording', function () {
     this.timeout(5e3)
+
+    it('does not crash on empty string', async () => {
+      const options = {
+        categoryFilter: '*',
+        traceOptions: 'record-until-full,enable-sampling'
+      }
+
+      await contentTracing.startRecording(options)
+      const path = await contentTracing.stopRecording('')
+      expect(path).to.be.a('string').that.is.not.empty()
+      expect(fs.statSync(path).isFile()).to.be.true()
+    })
 
     it('calls its callback with a result file path', async () => {
       const resultFilePath = await record(/* options */ {}, outputFilePath)
