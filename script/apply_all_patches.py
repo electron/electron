6b6e1203ac798e7036054a1c9fa5c0722b8d94be
Samuel Attard <sattard@slack-corp.com>
2019-05-24 15:02:15 -0700

    build: add env var to allow easily triggering a 3way patch

--- script/apply_all_patches.py
+++ script/apply_all_patches.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+import os
 import sys
 
 from lib import git
@@ -9,8 +10,10 @@ from lib.patches import patch_from_dir
 
 
 def apply_patches(dirs):
+  threeway = os.environ.get("ELECTRON_USE_THREE_WAY_MERGE_FOR_PATCHES")
   for patch_dir, repo in dirs.iteritems():
     git.import_patches(repo=repo, patch_data=patch_from_dir(patch_dir),
+      threeway=threeway is not None,
       committer_name="Electron Scripts", committer_email="scripts@electron")
 
 
