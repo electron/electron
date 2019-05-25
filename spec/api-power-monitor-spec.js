b7693de5d7fc6572b566afb548ca3f0fe8491d82
deepak1556 <hop2deep@gmail.com>
2019-05-24 17:54:23 -0700

    Revert "skip more dbus tests"
    This reverts commit 68dbef48daa5ff499bb507842c72d8ceec82dcf3.

--- spec/api-power-monitor-spec.js
+++ spec/api-power-monitor-spec.js
@@ -14,8 +14,7 @@ const Promise = require('bluebird')
 const { expect } = chai
 chai.use(dirtyChai)
 
-const skip = process.platform !== 'linux' || !process.env.DBUS_SYSTEM_BUS_ADDRESS ||
-             true
+const skip = process.platform !== 'linux' || !process.env.DBUS_SYSTEM_BUS_ADDRESS
 
 describe('powerMonitor', () => {
   let logindMock, dbusMockPowerMonitor, getCalls, emitSignal, reset
