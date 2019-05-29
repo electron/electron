babe2b68fbaf8f2d6326706c698e3afe601b572b
Jeremy Apthorp <jeremya@chromium.org>
2019-05-29 13:38:14 -0700

    test: move beforeunload tests to main runner and fix flake (#18432)

--- spec/api-web-contents-spec.js
+++ spec/api-web-contents-spec.js
@@ -779,31 +779,6 @@ describe('webContents module', () => {
     })
   })
 
-  describe('will-prevent-unload event', () => {
-    it('does not emit if beforeunload returns undefined', (done) => {
-      w.once('closed', () => {
-        done()
-      })
-      w.webContents.on('will-prevent-unload', (e) => {
-        expect.fail('should not have fired')
-      })
-      w.loadFile(path.join(fixtures, 'api', 'close-beforeunload-undefined.html'))
-    })
-
-    it('emits if beforeunload returns false', (done) => {
-      w.webContents.on('will-prevent-unload', () => {
-        done()
-      })
-      w.loadFile(path.join(fixtures, 'api', 'close-beforeunload-false.html'))
-    })
-
-    it('supports calling preventDefault on will-prevent-unload events', (done) => {
-      ipcRenderer.send('prevent-next-will-prevent-unload', w.webContents.id)
-      w.once('closed', () => done())
-      w.loadFile(path.join(fixtures, 'api', 'close-beforeunload-false.html'))
-    })
-  })
-
   describe('render view deleted events', () => {
     let server = null
 
