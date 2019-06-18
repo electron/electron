d47c7f0ef5874aa123a3b1396f67683d7519e984
Micha Hanselmann <deermichel@github.com>
2019-06-18 11:09:31 -0700

    specify error msg in test

--- spec/api-protocol-spec.js
+++ spec/api-protocol-spec.js
@@ -240,7 +240,7 @@ describe('protocol module', () => {
         expect(() => callback({
           path: filePath,
           headers: { 'X-Great-Header': 42 }
-        })).to.throw()
+        })).to.throw(Error, 'Header value has to be a string')
         done()
       }
       registerFileProtocol(protocolName, handler).then(() => {
