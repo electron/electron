73285243b6d3ffab62c4fd844bd8b2b41a700efc
Samuel Attard <sattard@slack-corp.com>
2019-06-19 13:03:29 -0700

    chore: also import public key

--- script/codesign/generate-identity.sh
+++ script/codesign/generate-identity.sh
@@ -19,11 +19,13 @@ mkdir -p "$dir"
 # Generate Certs
 openssl req -newkey rsa:2048 -nodes -keyout "$dir"/private.pem -x509 -days 1 -out "$dir"/certificate.pem -extensions extended -config "$(dirname $0)"/codesign.cnf
 openssl x509 -inform PEM -in "$dir"/certificate.pem -outform DER -out "$dir"/certificate.cer
+openssl x509 -pubkey -noout -in "$dir"/certificate.pem > "$dir"/public.key
 rm -f "$dir"/certificate.pem
 
 # Import Certs
 security import "$dir"/certificate.cer -k $KEY_CHAIN
 security import "$dir"/private.pem -k $KEY_CHAIN
+security import "$dir"/public.key -k $KEY_CHAIN
 
 # Generate Trust Settings
 node "$(dirname $0)"/gen-trust.js "$dir"/certificate.cer "$dir"/trust.xml
