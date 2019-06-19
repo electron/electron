4575a4aae39ac266181da0d66da75fae9bede400
Milan Burda <milan.burda@gmail.com>
2019-06-20 01:39:12 +0200

    Revert "feat: only allow bundled preload scripts (#17308)" (#18091)
    This reverts commit 8cf15cc9310ca5983f15e511c3ecdac8e75e225d.

--- lib/common/path-utils.ts
+++
@@ -1,6 +0,0 @@
-import * as path from 'path'
-
-export const isParentDir = function (parent: string, dir: string) {
-  const relative = path.relative(parent, dir)
-  return !!relative && !relative.startsWith('..') && !path.isAbsolute(relative)
-}
