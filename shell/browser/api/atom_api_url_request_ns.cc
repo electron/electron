9da64d94eb6b611ea1c608428b545da1e2febf4c
Cheng Zhao <zcbenz@gmail.com>
2019-06-28 20:21:24 +0900

    Create SimpleURLLoader

--- shell/browser/api/atom_api_url_request_ns.cc
+++ shell/browser/api/atom_api_url_request_ns.cc
@@ -4,23 +4,156 @@
 
 #include "shell/browser/api/atom_api_url_request_ns.h"
 
+#include "content/public/browser/storage_partition.h"
+#include "native_mate/dictionary.h"
 #include "native_mate/object_template_builder.h"
+#include "shell/browser/api/atom_api_session.h"
+#include "shell/browser/atom_browser_context.h"
+#include "shell/common/native_mate_converters/gurl_converter.h"
 
 namespace electron {
 
 namespace api {
 
-URLRequestNS::URLRequestNS(v8::Isolate* isolate,
-                           v8::Local<v8::Object> wrapper) {
-  InitWith(isolate, wrapper);
+namespace {
+
+enum State {
+  STATE_STARTED = 1 << 0,
+  STATE_FINISHED = 1 << 1,
+  STATE_CANCELED = 1 << 2,
+  STATE_FAILED = 1 << 3,
+  STATE_CLOSED = 1 << 4,
+};
+
+const net::NetworkTrafficAnnotationTag kAnnotation =
+    net::DefineNetworkTrafficAnnotation("electron_net_module", R"(
+        semantics {
+          sender: "Electron Net module"
+          description:
+            "Issue HTTP/HTTPS requests using Chromium's native networking "
+            "library."
+          trigger: "Using the Net module"
+          data: "Anything the user wants to send."
+          destination: OTHER
+        }
+        policy {
+          cookies_allowed: YES
+          cookies_store: "user"
+          setting: "This feature cannot be disabled."
+        })");
+
+}  // namespace
+
+URLRequestNS::URLRequestNS(mate::Arguments* args) {
+  auto request = std::make_unique<network::ResourceRequest>();
+  request_ = request.get();  // ownership of |request| will be transferred.
+
+  mate::Dictionary dict;
+  if (args->GetNext(&dict)) {
+    dict.Get("method", &request_->method);
+    dict.Get("url", &request_->url);
+  }
+
+  std::string partition;
+  mate::Handle<api::Session> session;
+  if (!dict.Get("session", &session)) {
+    if (dict.Get("partition", &partition))
+      session = Session::FromPartition(args->isolate(), partition);
+    else  // default session
+      session = Session::FromPartition(args->isolate(), "");
+  }
+
+  auto* browser_context = session->browser_context();
+  url_loader_factory_ =
+      content::BrowserContext::GetDefaultStoragePartition(browser_context)
+          ->GetURLLoaderFactoryForBrowserProcess();
+  loader_ = network::SimpleURLLoader::Create(std::move(request), kAnnotation);
+
+  InitWith(args->isolate(), args->GetThis());
 }
 
 URLRequestNS::~URLRequestNS() {}
 
+bool URLRequestNS::NotStarted() const {
+  return request_state_ == 0;
+}
+
+bool URLRequestNS::Finished() const {
+  return request_state_ & STATE_FINISHED;
+}
+
+bool URLRequestNS::Canceled() const {
+  return request_state_ & STATE_CANCELED;
+}
+
+bool URLRequestNS::Failed() const {
+  return request_state_ & STATE_FAILED;
+}
+
+void URLRequestNS::Cancel() {
+  // TODO(zcbenz): Implement this.
+}
+
+void URLRequestNS::Close() {
+  // TODO(zcbenz): Implement this.
+}
+
+bool URLRequestNS::Write(const std::string& data, bool is_last) {
+  // TODO(zcbenz): Implement this.
+  return false;
+}
+
+void URLRequestNS::FollowRedirect() {
+  if (request_state_ & (STATE_CANCELED | STATE_CLOSED))
+    return;
+  // TODO(zcbenz): Implement this.
+}
+
+bool URLRequestNS::SetExtraHeader(const std::string& name,
+                                  const std::string& value) {
+  if (!request_)
+    return false;
+  if (!net::HttpUtil::IsValidHeaderName(name))
+    return false;
+  if (!net::HttpUtil::IsValidHeaderValue(value))
+    return false;
+  request_->headers.SetHeader(name, value);
+  return true;
+}
+
+void URLRequestNS::RemoveExtraHeader(const std::string& name) {
+  if (request_)
+    request_->headers.RemoveHeader(name);
+}
+
+void URLRequestNS::SetChunkedUpload(bool is_chunked_upload) {
+  // TODO(zcbenz): Implement this.
+}
+
+void URLRequestNS::SetLoadFlags(int flags) {
+  // TODO(zcbenz): Implement this.
+}
+
+void URLRequestNS::OnDataReceived(base::StringPiece string_piece,
+                                  base::OnceClosure resume) {}
+
+void URLRequestNS::OnRetry(base::OnceClosure start_retry) {}
+
+void URLRequestNS::OnComplete(bool success) {}
+
+void URLRequestNS::Pin() {
+  if (wrapper_.IsEmpty()) {
+    wrapper_.Reset(isolate(), GetWrapper());
+  }
+}
+
+void URLRequestNS::Unpin() {
+  wrapper_.Reset();
+}
+
 // static
 mate::WrappableBase* URLRequestNS::New(mate::Arguments* args) {
-  auto* url_request = new URLRequestNS(args->isolate(), args->GetThis());
-  return url_request;
+  return new URLRequestNS(args);
 }
 
 // static
--- shell/browser/api/atom_api_url_request_ns.h
+++ shell/browser/api/atom_api_url_request_ns.h
@@ -5,13 +5,18 @@
 #ifndef SHELL_BROWSER_API_ATOM_API_URL_REQUEST_NS_H_
 #define SHELL_BROWSER_API_ATOM_API_URL_REQUEST_NS_H_
 
+#include "mojo/public/cpp/system/string_data_pipe_producer.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/public/cpp/simple_url_loader.h"
+#include "services/network/public/cpp/simple_url_loader_stream_consumer.h"
 #include "shell/browser/api/event_emitter.h"
 
 namespace electron {
 
 namespace api {
 
-class URLRequestNS : public mate::EventEmitter<URLRequestNS> {
+class URLRequestNS : public mate::EventEmitter<URLRequestNS>,
+                     public network::SimpleURLLoaderStreamConsumer {
  public:
   static mate::WrappableBase* New(mate::Arguments* args);
 
@@ -19,10 +24,48 @@ class URLRequestNS : public mate::EventEmitter<URLRequestNS> {
                              v8::Local<v8::FunctionTemplate> prototype);
 
  protected:
-  URLRequestNS(v8::Isolate* isolate, v8::Local<v8::Object> wrapper);
+  explicit URLRequestNS(mate::Arguments* args);
   ~URLRequestNS() override;
 
+  bool NotStarted() const;
+  bool Finished() const;
+  bool Canceled() const;
+  bool Failed() const;
+
+  void Cancel();
+  void Close();
+
+  bool Write(const std::string& data, bool is_last);
+  void FollowRedirect();
+  bool SetExtraHeader(const std::string& name, const std::string& value);
+  void RemoveExtraHeader(const std::string& name);
+  void SetChunkedUpload(bool is_chunked_upload);
+  void SetLoadFlags(int flags);
+
+  // SimpleURLLoaderStreamConsumer:
+  void OnDataReceived(base::StringPiece string_piece,
+                      base::OnceClosure resume) override;
+  void OnComplete(bool success) override;
+  void OnRetry(base::OnceClosure start_retry) override;
+
  private:
+  void Pin();
+  void Unpin();
+
+  std::unique_ptr<mojo::StringDataPipeProducer> producer_;
+  std::unique_ptr<network::SimpleURLLoader> loader_;
+  scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
+
+  // Current status.
+  int request_state_ = 0;
+  int response_state_ = 0;
+
+  // Weak ref to request, which is managed by loader_.
+  network::ResourceRequest* request_;
+
+  // Used by pin/unpin to manage lifetime.
+  v8::Global<v8::Object> wrapper_;
+
   DISALLOW_COPY_AND_ASSIGN(URLRequestNS);
 };
 
