c2d78deeca3a9ea2034e97b688c51cdabfd86cb2
Jeremy Apthorp <jeremya@chromium.org>
2019-07-02 13:48:26 -0700

    test: attempt to fix flaky webview.clearHistory test (#19083)

--- spec/webview-spec.js
+++ spec/webview-spec.js
@@ -917,11 +917,12 @@ describe('<webview> tag', function () {
 
   describe('<webview>.clearHistory()', () => {
     it('should clear the navigation history', async () => {
-      loadWebView(webview, {
+      const message = waitForEvent(webview, 'ipc-message')
+      await loadWebView(webview, {
         nodeintegration: 'on',
         src: `file://${fixtures}/pages/history.html`
       })
-      const event = await waitForEvent(webview, 'ipc-message')
+      const event = await message
 
       expect(event.channel).to.equal('history')
       expect(event.args[0]).to.equal(2)
