c1a24af4596b490ea170ce323d34bf5d3cb2bbaf
Jeremy Apthorp <jeremya@chromium.org>
2019-07-02 15:10:30 -0700

    dump_syms.exe doesn't support -i on windows

--- build/dump_syms.py
+++ build/dump_syms.py
@@ -9,16 +9,15 @@ import sys
 BINARY_INFO = collections.namedtuple('BINARY_INFO',
                                      ['platform', 'arch', 'hash', 'name'])
 
-def get_module_info(dump_syms, binary):
-  header_info = subprocess.check_output([dump_syms, '-i', binary]).splitlines()[0]
+def get_module_info(header_info):
   # header info is of the form "MODULE $PLATFORM $ARCH $HASH $BINARY"
   info_split = header_info.strip().split(' ', 4)
   if len(info_split) != 5 or info_split[0] != 'MODULE':
     return None
   return BINARY_INFO(*info_split[1:])
 
-def get_symbol_path(dump_syms, binary):
-  module_info = get_module_info(dump_syms, binary)
+def get_symbol_path(symbol_data):
+  module_info = get_module_info(symbol_data[:symbol_data.index('\n')])
   if not module_info:
     raise Exception("Couldn't get module info for binary '{}'".format(binary))
   return os.path.join(module_info.name, module_info.hash, module_info.name + ".sym")
@@ -33,14 +32,17 @@ def mkdir_p(path):
     else: raise
 
 def main(dump_syms, binary, out_dir, stamp_file, dsym_file=None):
-  symbol_file = os.path.join(out_dir, get_symbol_path(dump_syms, binary))
-  mkdir_p(os.path.dirname(symbol_file))
-  with open(symbol_file, 'w') as outfileobj:
-    args = [dump_syms]
-    if dsym_file:
-      args += ["-g", dsym_file]
-    args += [binary]
-    subprocess.check_call(args, stdout=outfileobj)
+  args = [dump_syms]
+  if dsym_file:
+    args += ["-g", dsym_file]
+  args += [binary]
+
+  symbol_data = subprocess.check_output(args)
+  symbol_path = get_symbol_path(symbol_data)
+  mkdir_p(os.path.dirname(symbol_path))
+
+  with open(symbol_path, 'w') as out:
+    out.write(symbol_data)
 
   with open(stamp_file, 'w'):
     pass
