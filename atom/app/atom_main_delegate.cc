ecb42d07df938e64c312031218a5e98b2afe551b
Micha Hanselmann <deermichel@github.com>
2019-07-11 14:57:16 -0700

    check for sandbox param on root

--- atom/app/atom_main_delegate.cc
+++ atom/app/atom_main_delegate.cc
@@ -199,6 +199,14 @@ bool AtomMainDelegate::BasicStartupComplete(int* exit_code) {
     base::win::PinUser32();
 #endif
 
+#if defined(OS_LINUX)
+  // Check for --no-sandbox parameter when running as root.
+  if (getuid() == 0 && IsSandboxEnabled(command_line))
+    LOG(FATAL) << "Running as root without --"
+               << service_manager::switches::kNoSandbox
+               << " is not supported. See https://crbug.com/638180.";
+#endif
+
   content_client_ = std::make_unique<AtomContentClient>();
   SetContentClient(content_client_.get());
 
