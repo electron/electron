7abd11b8b9579559e3de3e6037ff3e32b9b01e07
Jeremy Apthorp <jeremya@chromium.org>
2019-07-12 16:48:09 -0700

    test: move MenuItem spec to main process

--- spec-main/ambient.d.ts
+++ spec-main/ambient.d.ts
@@ -1,1 +1,15 @@
 declare var isCI: boolean;
+
+declare namespace Electron {
+  interface Menu {
+    delegate: {
+      executeCommand(menu: Menu, event: any, id: number): void;
+      menuWillShow(menu: Menu): void;
+    };
+    getAcceleratorTextAt(index: number): string;
+  }
+
+  interface MenuItem {
+    getDefaultRoleAccelerator(): Accelerator | undefined;
+  }
+}
