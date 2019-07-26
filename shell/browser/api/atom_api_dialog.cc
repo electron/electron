b90a4d0aad2ce036c4e289b88a06f4412cad1830
Micha Hanselmann <deermichel@github.com>
2019-07-26 14:23:38 -0700

    move more dialog methods

--- shell/browser/api/atom_api_dialog.cc
+++ shell/browser/api/atom_api_dialog.cc
@@ -13,9 +13,9 @@
 #include "shell/browser/ui/certificate_trust.h"
 #include "shell/browser/ui/file_dialog.h"
 #include "shell/browser/ui/message_box.h"
+#include "shell/common/gin_converters/file_dialog_converter_gin_adapter.h"
 #include "shell/common/gin_converters/message_box_converter.h"
 // #include "shell/common/native_mate_converters/callback.h"
-// #include "shell/common/native_mate_converters/file_dialog_converter.h"
 // #include "shell/common/native_mate_converters/file_path_converter.h"
 // #include "shell/common/native_mate_converters/image_converter.h"
 // #include "shell/common/native_mate_converters/net_converter.h"
@@ -28,63 +28,63 @@ int ShowMessageBoxSync(const electron::MessageBoxSettings& settings) {
   return electron::ShowMessageBoxSync(settings);
 }
 
-// void ResolvePromiseObject(electron::util::Promise promise,
-//                           int result,
-//                           bool checkbox_checked) {
-//   v8::Isolate* isolate = promise.isolate();
-//   gin::Dictionary dict = gin::Dictionary::CreateEmpty(isolate);
+void ResolvePromiseObject(electron::util::Promise promise,
+                          int result,
+                          bool checkbox_checked) {
+  v8::Isolate* isolate = promise.isolate();
+  gin::Dictionary dict = gin::Dictionary::CreateEmpty(isolate);
 
-//   dict.Set("response", result);
-//   dict.Set("checkboxChecked", checkbox_checked);
+  dict.Set("response", result);
+  dict.Set("checkboxChecked", checkbox_checked);
 
-//   promise.Resolve(gin::ConvertToV8(isolate, dict));
-// }
+  promise.Resolve(gin::ConvertToV8(isolate, dict));
+}
 
-// v8::Local<v8::Promise> ShowMessageBox(
-//     const electron::MessageBoxSettings& settings,
-//     gin::Arguments* args) {
-//   v8::Isolate* isolate = args->isolate();
-//   electron::util::Promise promise(isolate);
-//   v8::Local<v8::Promise> handle = promise.GetHandle();
+v8::Local<v8::Promise> ShowMessageBox(
+    const electron::MessageBoxSettings& settings,
+    gin::Arguments* args) {
+  v8::Isolate* isolate = args->isolate();
+  electron::util::Promise promise(isolate);
+  v8::Local<v8::Promise> handle = promise.GetHandle();
 
-//   electron::ShowMessageBox(
-//       settings, base::BindOnce(&ResolvePromiseObject, std::move(promise)));
+  electron::ShowMessageBox(
+      settings, base::BindOnce(&ResolvePromiseObject, std::move(promise)));
 
-//   return handle;
-// }
+  return handle;
+}
 
-// void ShowOpenDialogSync(const file_dialog::DialogSettings& settings,
-//                         gin::Arguments* args) {
-//   std::vector<base::FilePath> paths;
-//   if (file_dialog::ShowOpenDialogSync(settings, &paths))
-//     args->Return(paths);
-// }
+void ShowOpenDialogSync(const file_dialog::DialogSettings& settings,
+                        gin::Arguments* args) {
+  std::vector<base::FilePath> paths;
+  if (file_dialog::ShowOpenDialogSync(settings, &paths))
+    args->Return(paths);
+}
 
-// v8::Local<v8::Promise> ShowOpenDialog(
-//     const file_dialog::DialogSettings& settings,
-//     gin::Arguments* args) {
-//   electron::util::Promise promise(args->isolate());
-//   v8::Local<v8::Promise> handle = promise.GetHandle();
-//   file_dialog::ShowOpenDialog(settings, std::move(promise));
-//   return handle;
-// }
+v8::Local<v8::Promise> ShowOpenDialog(
+    const file_dialog::DialogSettings& settings,
+    gin::Arguments* args) {
+  electron::util::Promise promise(args->isolate());
+  v8::Local<v8::Promise> handle = promise.GetHandle();
+  file_dialog::ShowOpenDialog(settings, std::move(promise));
+  return handle;
+}
 
-// void ShowSaveDialogSync(const file_dialog::DialogSettings& settings,
-//                         gin::Arguments* args) {
-//   base::FilePath path;
-//   if (file_dialog::ShowSaveDialogSync(settings, &path))
-//     args->Return(path);
-// }
+void ShowSaveDialogSync(const file_dialog::DialogSettings& settings,
+                        gin::Arguments* args) {
+  base::FilePath path;
+  if (file_dialog::ShowSaveDialogSync(settings, &path))
+    args->Return(path);
+}
 
-// v8::Local<v8::Promise> ShowSaveDialog(
-//     const file_dialog::DialogSettings& settings,
-//     gin::Arguments* args) {
-//   electron::util::Promise promise(args->isolate());
-//   v8::Local<v8::Promise> handle = promise.GetHandle();
+v8::Local<v8::Promise> ShowSaveDialog(
+    const file_dialog::DialogSettings& settings,
+    gin::Arguments* args) {
+  electron::util::Promise promise(args->isolate());
+  v8::Local<v8::Promise> handle = promise.GetHandle();
 
-//   file_dialog::ShowSaveDialog(settings, std::move(promise));
-//   return handle;
-// }
+  file_dialog::ShowSaveDialog(settings, std::move(promise));
+  return handle;
+}
 
 void Initialize(v8::Local<v8::Object> exports,
                 v8::Local<v8::Value> unused,
@@ -98,12 +98,36 @@ void Initialize(v8::Local<v8::Object> exports,
   dict.Set("showMessageBoxSync",
            showMessageBoxSyncT->GetFunction(context).ToLocalChecked());
 
-  // dict.SetMethod("showMessageBox", &ShowMessageBox);
-  // dict.SetMethod("showErrorBox", &electron::ShowErrorBox);
-  // dict.SetMethod("showOpenDialogSync", &ShowOpenDialogSync);
-  // dict.SetMethod("showOpenDialog", &ShowOpenDialog);
-  // dict.SetMethod("showSaveDialogSync", &ShowSaveDialogSync);
-  // dict.SetMethod("showSaveDialog", &ShowSaveDialog);
+  auto showMessageBoxT = gin::CreateFunctionTemplate(
+      isolate, base::BindRepeating(&ShowMessageBox));
+  dict.Set("showMessageBox",
+           showMessageBoxT->GetFunction(context).ToLocalChecked());
+
+  auto showErrorBoxT = gin::CreateFunctionTemplate(
+      isolate, base::BindRepeating(&electron::ShowErrorBox));
+  dict.Set("showErrorBox",
+           showErrorBoxT->GetFunction(context).ToLocalChecked());
+
+  auto showOpenDialogSyncT = gin::CreateFunctionTemplate(
+      isolate, base::BindRepeating(&ShowOpenDialogSync));
+  dict.Set("showOpenDialogSync",
+           showOpenDialogSyncT->GetFunction(context).ToLocalChecked());
+
+  auto showOpenDialogT = gin::CreateFunctionTemplate(
+      isolate, base::BindRepeating(&ShowOpenDialog));
+  dict.Set("showOpenDialog",
+           showOpenDialogT->GetFunction(context).ToLocalChecked());
+
+  auto showSaveDialogSyncT = gin::CreateFunctionTemplate(
+      isolate, base::BindRepeating(&ShowSaveDialogSync));
+  dict.Set("showSaveDialogSync",
+           showSaveDialogSyncT->GetFunction(context).ToLocalChecked());
+
+  auto showSaveDialogT = gin::CreateFunctionTemplate(
+      isolate, base::BindRepeating(&ShowSaveDialog));
+  dict.Set("showSaveDialog",
+           showSaveDialogT->GetFunction(context).ToLocalChecked());
+
 #if defined(OS_MACOSX) || defined(OS_WIN)
   // dict.SetMethod("showCertificateTrustDialog",
   //                &certificate_trust::ShowCertificateTrust);
---
+++ shell/common/gin_converters/file_dialog_converter_gin_adapter.h
@@ -0,0 +1,46 @@
+// Copyright (c) 2019 GitHub, Inc.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#ifndef SHELL_COMMON_GIN_CONVERTERS_FILE_DIALOG_CONVERTER_GIN_ADAPTER_H_
+#define SHELL_COMMON_GIN_CONVERTERS_FILE_DIALOG_CONVERTER_GIN_ADAPTER_H_
+
+#include "gin/converter.h"
+#include "shell/common/native_mate_converters/file_dialog_converter.h"
+
+// TODO(deermichel): replace adapter with real implementation after removing
+// mate
+// -- this adapter forwards all conversions to the existing mate converter --
+// (other direction might be preferred, but this is safer for now :D)
+
+namespace gin {
+
+template <>
+struct Converter<file_dialog::Filter> {
+  static v8::Local<v8::Value> ToV8(v8::Isolate* isolate,
+                                   const file_dialog::Filter& in) {
+    return mate::ConvertToV8(isolate, in);
+  }
+  static bool FromV8(v8::Isolate* isolate,
+                     v8::Local<v8::Value> val,
+                     file_dialog::Filter* out) {
+    return mate::ConvertFromV8(isolate, val, out);
+  }
+};
+
+template <>
+struct Converter<file_dialog::DialogSettings> {
+  static v8::Local<v8::Value> ToV8(v8::Isolate* isolate,
+                                   const file_dialog::DialogSettings& in) {
+    return mate::ConvertToV8(isolate, in);
+  }
+  static bool FromV8(v8::Isolate* isolate,
+                     v8::Local<v8::Value> val,
+                     file_dialog::DialogSettings* out) {
+    return mate::ConvertFromV8(isolate, val, out);
+  }
+};
+
+}  // namespace gin
+
+#endif  // SHELL_COMMON_GIN_CONVERTERS_FILE_DIALOG_CONVERTER_GIN_ADAPTER_H_
