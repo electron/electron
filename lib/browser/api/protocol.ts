f7091583b57ee2dbfb0424bb0ec0f57b28de018d
pi1024e <doremylover123@gmail.com>
2019-07-31 12:11:38 -0400

    Fixed

--- lib/browser/api/protocol.ts
+++ lib/browser/api/protocol.ts
@@ -7,10 +7,8 @@ const protocol = process.electronBinding('protocol')
 Object.setPrototypeOf(protocol, new Proxy({}, {
   get(_target, property) {
     if (app.isReady()) {
-
       const protocol = session.defaultSession!.protocol
       if (Object.getPrototypeOf(protocol).hasOwnProperty(property)) {
-
         // Returning a native function directly would throw error.
         return (...args: any[]) => (protocol[property as keyof Electron.Protocol] as Function)(...args)
       }
