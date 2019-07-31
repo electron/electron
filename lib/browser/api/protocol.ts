4efcc625b0cb5c917d96261d68e3f90aa53071ff
pi1024e <doremylover123@gmail.com>
2019-07-31 09:41:58 -0400

    One more change

--- lib/browser/api/protocol.ts
+++ lib/browser/api/protocol.ts
@@ -5,23 +5,25 @@ const protocol = process.electronBinding('protocol')
 
 // Fallback protocol APIs of default session.
 Object.setPrototypeOf(protocol, new Proxy({}, {
-  get (_target, property) {
-    if (!app.isReady()) return
+  get(_target, property) {
+    if (app.isReady()) {
 
-    const protocol = session.defaultSession!.protocol
-    if (!Object.getPrototypeOf(protocol).hasOwnProperty(property)) return
+      const protocol = session.defaultSession!.protocol
+      if (Object.getPrototypeOf(protocol).hasOwnProperty(property)) {
 
-    // Returning a native function directly would throw error.
-    return (...args: any[]) => (protocol[property as keyof Electron.Protocol] as Function)(...args)
+        // Returning a native function directly would throw error.
+        return (...args: any[]) => (protocol[property as keyof Electron.Protocol] as Function)(...args)
+      }
+    }
   },
 
-  ownKeys () {
-    if (!app.isReady()) return []
+  ownKeys() {
+    if (app.isReady()) return Object.getOwnPropertyNames(Object.getPrototypeOf(session.defaultSession!.protocol))
 
-    return Object.getOwnPropertyNames(Object.getPrototypeOf(session.defaultSession!.protocol))
+    return []
   },
 
-  getOwnPropertyDescriptor () {
+  getOwnPropertyDescriptor() {
     return { configurable: true, enumerable: true }
   }
 }))
