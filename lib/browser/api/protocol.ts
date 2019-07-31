806f222cd75afd4ff351abb31f67b181b0471f11
pi1024e <doremylover123@gmail.com>
2019-07-31 14:41:36 -0400

    Revert "One more change"
    This reverts commit 4efcc625b0cb5c917d96261d68e3f90aa53071ff.

--- lib/browser/api/protocol.ts
+++ lib/browser/api/protocol.ts
@@ -5,25 +5,23 @@ const protocol = process.electronBinding('protocol')
 
 // Fallback protocol APIs of default session.
 Object.setPrototypeOf(protocol, new Proxy({}, {
-  get(_target, property) {
-    if (app.isReady()) {
+  get (_target, property) {
+    if (!app.isReady()) return
 
-      const protocol = session.defaultSession!.protocol
-      if (Object.getPrototypeOf(protocol).hasOwnProperty(property)) {
+    const protocol = session.defaultSession!.protocol
+    if (!Object.getPrototypeOf(protocol).hasOwnProperty(property)) return
 
-        // Returning a native function directly would throw error.
-        return (...args: any[]) => (protocol[property as keyof Electron.Protocol] as Function)(...args)
-      }
-    }
+    // Returning a native function directly would throw error.
+    return (...args: any[]) => (protocol[property as keyof Electron.Protocol] as Function)(...args)
   },
 
-  ownKeys() {
-    if (app.isReady()) return Object.getOwnPropertyNames(Object.getPrototypeOf(session.defaultSession!.protocol))
+  ownKeys () {
+    if (!app.isReady()) return []
 
-    return []
+    return Object.getOwnPropertyNames(Object.getPrototypeOf(session.defaultSession!.protocol))
   },
 
-  getOwnPropertyDescriptor() {
+  getOwnPropertyDescriptor () {
     return { configurable: true, enumerable: true }
   }
 }))
