fc94fb4480c2db2fa7e2eac59c2076661c17cc32
Shelley Vohr <shelley.vohr@gmail.com>
2019-08-04 17:46:23 -0700

    fix: destroy tray on current tick (#18196) (#19619)
    This code was originally added in #6448 to handle an edge case crash in 10.9, and we no longer support 10.9 and therefore no longer need to account for this case.
    
    It addressed the crash, but also created a race condition whereby when a new tray is created the old tray's destroy wouldn't have been fully completed and therefore a new one would be spawned. This fixes that by destroying the tray on the current tick once more.

--- spec/api-tray-spec.js
+++ spec/api-tray-spec.js
@@ -1,4 +1,5 @@
 const { remote } = require('electron')
+const assert = require('assert')
 const { Menu, Tray, nativeImage } = remote
 
 describe('tray module', () => {
@@ -8,12 +9,12 @@ describe('tray module', () => {
     tray = new Tray(nativeImage.createEmpty())
   })
 
-  afterEach(() => {
-    tray.destroy()
-    tray = null
-  })
-
   describe('tray.setContextMenu', () => {
+    afterEach(() => {
+      tray.destroy()
+      tray = null
+    })
+
     it('accepts menu instance', () => {
       tray.setContextMenu(new Menu())
     })
@@ -23,7 +24,22 @@ describe('tray module', () => {
     })
   })
 
+  describe('tray.destroy()', () => {
+    it('destroys a tray', () => {
+      assert.strictEqual(tray.isDestroyed(), false)
+      tray.destroy()
+
+      assert.strictEqual(tray.isDestroyed(), true)
+      tray = null
+    })
+  })
+
   describe('tray.popUpContextMenu', () => {
+    afterEach(() => {
+      tray.destroy()
+      tray = null
+    })
+
     before(function () {
       if (process.platform !== 'win32') {
         this.skip()
@@ -43,22 +59,31 @@ describe('tray module', () => {
   describe('tray.setImage', () => {
     it('accepts empty image', () => {
       tray.setImage(nativeImage.createEmpty())
+
+      tray.destroy()
+      tray = null
     })
   })
 
   describe('tray.setPressedImage', () => {
     it('accepts empty image', () => {
       tray.setPressedImage(nativeImage.createEmpty())
+
+      tray.destroy()
+      tray = null
     })
   })
 
   describe('tray.setTitle', () => {
-    it('accepts non-empty string', () => {
-      tray.setTitle('Hello World!')
+    before(function () {
+      if (process.platform !== 'darwin') this.skip()
     })
 
-    it('accepts empty string', () => {
-      tray.setTitle('')
+    it('accepts non-empty string', () => {
+      tray.setTitle('Hello World!')
+
+      tray.destroy()
+      tray = null
     })
   })
 })
