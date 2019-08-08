29e75d321a813e6275aa037fa0b587625405d03a
Micha Hanselmann <deermichel@github.com>
2019-08-08 11:05:20 -0700

    Don't evict frames for hidden/occluded windows on Windows
    https://chromium-review.googlesource.com/c/chromium/src/+/1680969

--- shell/browser/osr/osr_render_widget_host_view.cc
+++ shell/browser/osr/osr_render_widget_host_view.cc
@@ -276,7 +276,9 @@ OffScreenRenderWidgetHostView::~OffScreenRenderWidgetHostView() {
   // Marking the DelegatedFrameHost as removed from the window hierarchy is
   // necessary to remove all connections to its old ui::Compositor.
   if (is_showing_)
-    delegated_frame_host_->WasHidden();
+    // TODO(deermichel): correct or kOther?
+    delegated_frame_host_->WasHidden(
+        content::DelegatedFrameHost::HiddenCause::kOccluded);
   delegated_frame_host_->DetachFromCompositor();
 
   delegated_frame_host_.reset(NULL);
@@ -397,7 +399,9 @@ void OffScreenRenderWidgetHostView::Hide() {
   if (render_widget_host_)
     render_widget_host_->WasHidden();
 
-  GetDelegatedFrameHost()->WasHidden();
+  // TODO(deermichel): correct or kOther?
+  GetDelegatedFrameHost()->WasHidden(
+      content::DelegatedFrameHost::HiddenCause::kOccluded);
   GetDelegatedFrameHost()->DetachFromCompositor();
 
   is_showing_ = false;
