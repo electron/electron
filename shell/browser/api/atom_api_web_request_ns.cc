9ff4b8a7a1619e776736e700ea966a4c67b46f92
Cheng Zhao <zcbenz@gmail.com>
2019-08-11 13:24:51 +0900

    feat: migrate webRequest module to NetworkService (Part 4) (#19679)
    * chore: use gin in WebRequest
    
    * Add stubs for APIs

--- shell/browser/api/atom_api_web_request_ns.cc
+++ shell/browser/api/atom_api_web_request_ns.cc
@@ -4,33 +4,105 @@
 
 #include "shell/browser/api/atom_api_web_request_ns.h"
 
+#include <set>
+#include <string>
+
+#include "extensions/common/url_pattern.h"
+#include "gin/converter.h"
+#include "gin/dictionary.h"
+#include "gin/object_template_builder.h"
 #include "shell/browser/atom_browser_context.h"
+#include "shell/common/gin_converters/callback_converter_gin_adapter.h"
+#include "shell/common/gin_converters/std_converter.h"
+#include "shell/common/gin_converters/value_converter_gin_adapter.h"
+
+namespace gin {
+
+template <>
+struct Converter<URLPattern> {
+  static bool FromV8(v8::Isolate* isolate,
+                     v8::Local<v8::Value> val,
+                     URLPattern* out) {
+    std::string pattern;
+    if (!ConvertFromV8(isolate, val, &pattern))
+      return false;
+    *out = URLPattern(URLPattern::SCHEME_ALL);
+    return out->Parse(pattern) == URLPattern::ParseResult::kSuccess;
+  }
+};
+
+}  // namespace gin
 
 namespace electron {
 
 namespace api {
 
+gin::WrapperInfo WebRequestNS::kWrapperInfo = {gin::kEmbedderNativeGin};
+
 WebRequestNS::WebRequestNS(v8::Isolate* isolate,
-                           AtomBrowserContext* browser_context) {
-  Init(isolate);
-  AttachAsUserData(browser_context);
-}
+                           AtomBrowserContext* browser_context) {}
 
 WebRequestNS::~WebRequestNS() = default;
 
-// static
-mate::Handle<WebRequestNS> WebRequestNS::Create(
-    v8::Isolate* isolate,
-    AtomBrowserContext* browser_context) {
-  return mate::CreateHandle(isolate,
-                            new WebRequestNS(isolate, browser_context));
+gin::ObjectTemplateBuilder WebRequestNS::GetObjectTemplateBuilder(
+    v8::Isolate* isolate) {
+  return gin::Wrappable<WebRequestNS>::GetObjectTemplateBuilder(isolate)
+      .SetMethod("onBeforeRequest",
+                 &WebRequestNS::SetResponseListener<kOnBeforeRequest>)
+      .SetMethod("onBeforeSendHeaders",
+                 &WebRequestNS::SetResponseListener<kOnBeforeSendHeaders>)
+      .SetMethod("onHeadersReceived",
+                 &WebRequestNS::SetResponseListener<kOnHeadersReceived>)
+      .SetMethod("onSendHeaders",
+                 &WebRequestNS::SetSimpleListener<kOnSendHeaders>)
+      .SetMethod("onBeforeRedirect",
+                 &WebRequestNS::SetSimpleListener<kOnBeforeRedirect>)
+      .SetMethod("onResponseStarted",
+                 &WebRequestNS::SetSimpleListener<kOnResponseStarted>)
+      .SetMethod("onErrorOccurred",
+                 &WebRequestNS::SetSimpleListener<kOnErrorOccurred>)
+      .SetMethod("onCompleted", &WebRequestNS::SetSimpleListener<kOnCompleted>);
+}
+
+const char* WebRequestNS::GetTypeName() {
+  return "WebRequest";
+}
+
+template <WebRequestNS::SimpleEvent event>
+void WebRequestNS::SetSimpleListener(gin::Arguments* args) {
+  SetListener<SimpleListener, SimpleEvent>(event, args);
+}
+
+template <WebRequestNS::ResponseEvent event>
+void WebRequestNS::SetResponseListener(gin::Arguments* args) {
+  SetListener<ResponseListener, ResponseEvent>(event, args);
+}
+
+template <typename Listener, typename Event>
+void WebRequestNS::SetListener(Event event, gin::Arguments* args) {
+  // { urls }.
+  std::set<URLPattern> patterns;
+  gin::Dictionary dict(args->isolate());
+  args->GetNext(&dict) && dict.Get("urls", &patterns);
+
+  // Function or null.
+  v8::Local<v8::Value> value;
+  Listener listener;
+  if (!args->GetNext(&listener) &&
+      !(args->GetNext(&value) && value->IsNull())) {
+    args->ThrowTypeError("Must pass null or a Function");
+    return;
+  }
+
+  // TODO(zcbenz): Actually set the listeners.
+  args->ThrowTypeError("This API is not implemented yet");
 }
 
 // static
-void WebRequestNS::BuildPrototype(v8::Isolate* isolate,
-                                  v8::Local<v8::FunctionTemplate> prototype) {
-  prototype->SetClassName(mate::StringToV8(isolate, "WebRequest"));
-  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate());
+gin::Handle<WebRequestNS> WebRequestNS::Create(
+    v8::Isolate* isolate,
+    AtomBrowserContext* browser_context) {
+  return gin::CreateHandle(isolate, new WebRequestNS(isolate, browser_context));
 }
 
 }  // namespace api
--- shell/browser/api/atom_api_web_request_ns.h
+++ shell/browser/api/atom_api_web_request_ns.h
@@ -5,9 +5,12 @@
 #ifndef SHELL_BROWSER_API_ATOM_API_WEB_REQUEST_NS_H_
 #define SHELL_BROWSER_API_ATOM_API_WEB_REQUEST_NS_H_
 
+#include "base/values.h"
+#include "gin/arguments.h"
+#include "gin/handle.h"
+#include "gin/wrappable.h"
 #include "native_mate/dictionary.h"
 #include "native_mate/handle.h"
-#include "shell/browser/api/trackable_object.h"
 
 namespace electron {
 
@@ -15,17 +18,46 @@ class AtomBrowserContext;
 
 namespace api {
 
-class WebRequestNS : public mate::TrackableObject<WebRequestNS> {
+class WebRequestNS : public gin::Wrappable<WebRequestNS> {
  public:
-  static mate::Handle<WebRequestNS> Create(v8::Isolate* isolate,
-                                           AtomBrowserContext* browser_context);
+  static gin::WrapperInfo kWrapperInfo;
 
-  static void BuildPrototype(v8::Isolate* isolate,
-                             v8::Local<v8::FunctionTemplate> prototype);
+  static gin::Handle<WebRequestNS> Create(v8::Isolate* isolate,
+                                          AtomBrowserContext* browser_context);
+
+  // gin::Wrappable:
+  gin::ObjectTemplateBuilder GetObjectTemplateBuilder(
+      v8::Isolate* isolate) override;
+  const char* GetTypeName() override;
 
  private:
   WebRequestNS(v8::Isolate* isolate, AtomBrowserContext* browser_context);
   ~WebRequestNS() override;
+
+  enum SimpleEvent {
+    kOnSendHeaders,
+    kOnBeforeRedirect,
+    kOnResponseStarted,
+    kOnCompleted,
+    kOnErrorOccurred,
+  };
+  enum ResponseEvent {
+    kOnBeforeRequest,
+    kOnBeforeSendHeaders,
+    kOnHeadersReceived,
+  };
+
+  using SimpleListener = base::RepeatingCallback<void(const base::Value&)>;
+  using ResponseCallback = base::OnceCallback<void(const base::Value&)>;
+  using ResponseListener =
+      base::RepeatingCallback<void(const base::Value&, ResponseCallback)>;
+
+  template <SimpleEvent event>
+  void SetSimpleListener(gin::Arguments* args);
+  template <ResponseEvent event>
+  void SetResponseListener(gin::Arguments* args);
+  template <typename Listener, typename Event>
+  void SetListener(Event event, gin::Arguments* args);
 };
 
 }  // namespace api
