4d4060547eb8fe7e27ef3d5194a8e98e9ebd629a
emmanuel.kimmerlin@thomsonreuters.com <emmanuel.kimmerlin@thomsonreuters.com>
2019-08-14 17:24:42 +0200

    Fix Mac tests

--- spec-main/api-app-user-paths-spec.ts
+++ spec-main/api-app-user-paths-spec.ts
@@ -2,8 +2,7 @@ import * as chai from 'chai'
 import * as chaiAsPromised from 'chai-as-promised'
 import * as cp from 'child_process'
 import * as path from 'path'
-// import * as os from 'os'
-import { app } from 'electron'
+import * as os from 'os'
 import { emittedOnce } from './events-helpers';
 
 const { expect } = chai
@@ -28,27 +27,24 @@ describe('app user paths module', () => {
       expect(output.userData).to.equal(path.join(output.appData, appName))
     })
 
-    // it('customize appData', async () => {
-    //   const tmpdir = app.getPath('temp')
-    //   const appdata = path.join(tmpdir, 'myappdata')
-    //   const output = await runTestApp('app-user-paths', `-custom-appdata=${appdata}`)
-    //   expect(output.appData).to.equal(appdata)
-    //   expect(output.userCache).to.equal(path.join(output.appCache, defaultAppName))
-    //   expect(output.userData).to.equal(path.join(output.appData, defaultAppName))
-    // })
+    it('customize appData', async () => {
+      const appdata = path.join(os.tmpdir(), 'myappdata')
+      const output = await runTestApp('app-user-paths', `-custom-appdata=${appdata}`)
+      expect(output.appData).to.equal(appdata)
+      expect(output.userCache).to.equal(path.join(output.appData, defaultAppName))
+      expect(output.userData).to.equal(path.join(output.appCache, defaultAppName))
+    })
   })
 
 })
-
-console.log('sdafklsflnk')
 
 
 async function runTestApp (name: string, ...args: any[]) {
   const appPath = path.join(fixturesPath, name)
   const electronPath = process.execPath
-
   const appProcess = cp.spawn(electronPath, [appPath, ...args])
 
+  let output = ''
   appProcess.stdout.on('data', (data) => { output += data })
 
   await emittedOnce(appProcess.stdout, 'end')
