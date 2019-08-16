e162e0034febca722eb89c8b02f7f98b1fa0d131
John Kleinschmidt <jkleinsc@github.com>
2019-08-16 12:24:02 -0400

    Change test back to original and see if it breaks

--- spec/fixtures/api/leak-exit-webcontentsview.js
+++ spec/fixtures/api/leak-exit-webcontentsview.js
@@ -1,13 +1,7 @@
 const { WebContentsView, app, webContents } = require('electron')
-console.log('In leak-exit-webcontentsview.js')
 app.on('ready', function () {
-  console.log('In leak-exit-webcontentsview.js ready')
   const web = webContents.create({})
-  console.log('In leak-exit-webcontentsview.js done webContents create')
   new WebContentsView(web)  // eslint-disable-line
-  console.log('In leak-exit-webcontentsview.js done new WebContentsView(web)')
-  process.nextTick(() => {
-    console.log('In leak-exit-webcontentsview.js about to call quit')
-    app.quit()
-  })
+
+  process.nextTick(() => app.quit())
 })
