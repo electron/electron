8939b26899c89ac6cb18a2a35acf49b5fbb31168
Carlos Valdez <carlosvr_24@outlook.com>
2019-08-20 14:51:37 -0700

    change up accelerator

--- docs/fiddles/accelerator/main.js
+++ docs/fiddles/accelerator/main.js
@@ -11,18 +11,18 @@ app.on('ready', () => {
   // Register a 'CommandOrControl+Y' shortcut listener.
   globalShortcut.register('CommandOrControl+Y', () => {
     // Do stuff when Y and either Command/Control is pressed.
-    console.log('creating new window...');
-    mainWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        nodeIntegration: true
-      }
-    });
-
-    // and load the index.html of the app.
-    mainWindow.loadFile('index.html');
+    console.log('shortcut...');
   });
   
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html');
 })
 
