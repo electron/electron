f6c523db13b5d015d3fdf241d3ee882847fa5979
Heilig Benedek <benecene@gmail.com>
2019-08-26 03:03:57 +0200

    fix: adjust window size in NCCALCSIZE instead of adding insets (#19883)

--- shell/browser/ui/views/root_view.cc
+++ shell/browser/ui/views/root_view.cc
@@ -176,18 +176,14 @@ void RootView::Layout() {
     return;
 
   const auto menu_bar_bounds =
-      menu_bar_visible_
-          ? gfx::Rect(insets_.left(), insets_.top(),
-                      size().width() - insets_.width(), kMenuBarHeight)
-          : gfx::Rect();
+      menu_bar_visible_ ? gfx::Rect(0, 0, size().width(), kMenuBarHeight)
+                        : gfx::Rect();
   if (menu_bar_)
     menu_bar_->SetBoundsRect(menu_bar_bounds);
 
   window_->content_view()->SetBoundsRect(
-      gfx::Rect(insets_.left(),
-                menu_bar_visible_ ? menu_bar_bounds.bottom() : insets_.top(),
-                size().width() - insets_.width(),
-                size().height() - menu_bar_bounds.height() - insets_.height()));
+      gfx::Rect(0, menu_bar_visible_ ? menu_bar_bounds.bottom() : 0,
+                size().width(), size().height() - menu_bar_bounds.height()));
 }
 
 gfx::Size RootView::GetMinimumSize() const {
@@ -222,13 +218,6 @@ void RootView::UnregisterAcceleratorsWithFocusManager() {
   views::FocusManager* focus_manager = GetFocusManager();
   accelerator_table_.clear();
   focus_manager->UnregisterAccelerators(this);
-}
-
-void RootView::SetInsets(const gfx::Insets& insets) {
-  if (insets != insets_) {
-    insets_ = insets;
-    Layout();
-  }
 }
 
 }  // namespace electron
