82d09c4908f59284e1ef0840441e28a06846679c
Shelley Vohr <shelley.vohr@gmail.com>
2019-08-27 12:12:51 -0700

    remove shelljs

--- script/gn-check.js
+++ script/gn-check.js
@@ -1,12 +1,22 @@
-const sh = require('shelljs')
 const cp = require('child_process')
+const path = require('path')
 
 const { getOutDir } = require('./lib/utils')
 
+const SOURCE_ROOT = path.normalize(path.dirname(__dirname))
+const DEPOT_TOOLS = path.resolve(SOURCE_ROOT, '..', 'third_party', 'depot_tools')
 const OUT_DIR = getOutDir()
+
 if (!OUT_DIR) {
   throw new Error(`No viable out dir: one of Debug, Testing, or Release must exist.`)
 }
+
+const env = Object.assign({
+  CHROMIUM_BUILDTOOLS_PATH: path.resolve(SOURCE_ROOT, '..', 'buildtools'),
+  DEPOT_TOOLS_WIN_TOOLCHAIN: '0'
+}, process.env)
+// Users may not have depot_tools in PATH.
+env.PATH = `${env.PATH}${path.delimiter}${DEPOT_TOOLS}`
 
 const gnCheckDirs = [
   '//electron:electron_lib',
@@ -18,7 +28,7 @@ const gnCheckDirs = [
 for (const dir of gnCheckDirs) {
   const args = ['check', `../out/${OUT_DIR}`, dir]
   const result = cp.spawnSync('gn', args, { stdio: 'inherit' })
-  if (result.status !== 0) sh.exit(result.status)
+  if (result.status !== 0) process.exit(result.status)
 }
 
-sh.exit(0)
+process.exit(0)
