ede7eeff5e616ee9bef21ea73e3a38b3d41e465f
Shelley Vohr <shelley.vohr@gmail.com>
2019-09-16 10:50:54 -0700

    build: enable native googletests

---
+++ shell/browser/ui/accelerator_util_unittests.cc
@@ -0,0 +1,15 @@
+// Copyright (c) 2019 GitHub, Inc.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#include "shell/browser/ui/accelerator_util.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace accelerator_util {
+
+TEST(AcceleratorUtilTest, StringToAccelerator) {
+  EXPECT_FALSE(false);
+}
+
+}  // namespace accelerator_util
