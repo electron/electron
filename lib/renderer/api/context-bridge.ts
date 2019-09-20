50c3dfdafb9972338f797fbfb50186b3f5e7335a
Samuel Attard <sattard@slack-corp.com>
2019-09-20 14:17:43 -0700

    chore: ensure that contextBridge can only be used when contextIsolation is enabled

--- lib/renderer/api/context-bridge.ts
+++ lib/renderer/api/context-bridge.ts
@@ -1,12 +1,23 @@
+const { hasSwitch } = process.electronBinding('command_line')
 const binding = process.electronBinding('context_bridge')
 
 const reverseBindings: Record<string, any> = {}
 
 binding._setReverseBindingStore(reverseBindings)
 
+const contextIsolationEnabled = hasSwitch('context-isolation')
+
+const checkContextIsolationEnabled = () => {
+  if (!contextIsolationEnabled) throw new Error('contextBridge API can only be used when contextIsolation is enabled')
+}
+
 const contextBridge = {
-  bindAPIInMainWorld: (key: string, api: Record<string, any>, options: { allowReverseBinding: boolean }) => binding.exposeAPIInMainWorld(key, api, options),
+  bindAPIInMainWorld: (key: string, api: Record<string, any>, options: { allowReverseBinding: boolean }) => {
+    checkContextIsolationEnabled()
+    return binding.exposeAPIInMainWorld(key, api, options)
+  },
   getReverseBinding: (key: string) => {
+    checkContextIsolationEnabled()
     return reverseBindings[key] || null
   }
 }
