17a8f953f7d25220ea12b759ad1591d1a5c288c8
Shelley Vohr <shelley.vohr@gmail.com>
2019-09-19 18:15:36 -0700

    chore: enable native test runs via spec-runner

--- script/spec-runner.js
+++ script/spec-runner.js
@@ -12,7 +12,7 @@ const pass = '✓'.green
 const fail = '✗'.red
 
 const args = require('minimist')(process.argv, {
-  string: ['runners'],
+  string: ['runners', 'target'],
   unknown: arg => unknownFlags.push(arg)
 })
 
@@ -34,7 +34,8 @@ const NPX_CMD = process.platform === 'win32' ? 'npx.cmd' : 'npx'
 
 const runners = new Map([
   ['main', { description: 'Main process specs', run: runMainProcessElectronTests }],
-  ['remote', { description: 'Remote based specs', run: runRemoteBasedElectronTests }]
+  ['remote', { description: 'Remote based specs', run: runRemoteBasedElectronTests }],
+  ['native', { description: 'Native specs', run: runNativeElectronTests }]
 ])
 
 const specHashPath = path.resolve(__dirname, '../spec/.hash')
@@ -48,7 +49,7 @@ if (args.runners) {
   }
   console.log('Only running:', runnersToRun)
 } else {
-  console.log(`Triggering both ${[...runners.keys()].join(' and ')} runners`)
+  console.log(`Triggering ${[...runners.keys()].join(' and ')} runners`)
 }
 
 async function main () {
@@ -139,6 +140,79 @@ async function runRemoteBasedElectronTests () {
     process.exit(1)
   }
   console.log(`${pass} Electron remote process tests passed.`)
+}
+
+async function runNativeElectronTests () {
+  const TEST_TARGETS = require('./native-test-targets.json')
+  const outDir = `out/${utils.getOutDir(false)}`
+
+  // If native tests are being run, only one arg would be relevant
+  if (args.target && !TEST_TARGETS.includes(args.target)) {
+    console.log(`${fail} ${args.target} must be a subset of [${[TEST_TARGETS].join(', ')}]`)
+    process.exit(1)
+  }
+
+  // If a specific target was passed, only build and run that target
+  if (args.target) {
+    console.info('\nRunning native test for target:', args.target)
+
+    // Build single test target
+    const build = childProcess.spawnSync('ninja', ['-C', outDir, args.target], {
+      cwd: path.resolve(__dirname, '../..'),
+      stdio: 'inherit'
+    })
+
+    // Exit if test target failed to build
+    if (build.status !== 0) {
+      console.log(`${fail} ${args.target} failed to build.`)
+      process.exit(1)
+    }
+
+    // Run single test target
+    const run = childProcess.spawnSync(`./${outDir}/${args.target}`, {
+      cwd: path.resolve(__dirname, '../..'),
+      stdio: 'inherit'
+    })
+
+    if (run.status !== 0) {
+      console.log(`${fail} ${args.target} test failed.`)
+      process.exit(1)
+    }
+  } else {
+    // Build all test targets
+    for (const target of TEST_TARGETS) {
+      const build = childProcess.spawnSync('ninja', ['-C', outDir, target], {
+        cwd: path.resolve(__dirname, '../..'),
+        stdio: 'inherit'
+      })
+
+      // Exit if test target failed to build
+      if (build.status !== 0) {
+        console.log(`${fail} ${target} failed to build.`)
+        process.exit(1)
+      }
+    }
+
+    // Run test targets
+    const failures = []
+    for (const target of TEST_TARGETS) {
+      const testRun = childProcess.spawnSync(`./${outDir}/${target}`, {
+        cwd: path.resolve(__dirname, '../..'),
+        stdio: 'inherit'
+      })
+
+      // Collect failures and log at end
+      if (testRun.status !== 0) failures.push(target)
+    }
+
+    // Exit if any failures
+    if (failures.length > 0) {
+      console.log(`${fail} Electron native tests failed for the following targets: `, failures)
+      process.exit(1)
+    }
+  }
+
+  console.log(`${pass} Electron native tests passed.`)
 }
 
 async function runMainProcessElectronTests () {
