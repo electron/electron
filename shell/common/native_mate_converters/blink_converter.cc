5383d56e3764cbc12e47dc73198d2fcebfb90b1a
Jeremy Apthorp <nornagon@nornagon.net>
2019-09-25 12:59:34 -0400

    fix lint more

--- shell/common/native_mate_converters/blink_converter.cc
+++ shell/common/native_mate_converters/blink_converter.cc
@@ -534,7 +534,7 @@ bool Converter<network::mojom::ReferrerPolicy>::FromV8(
 namespace {
 class V8Serializer : public v8::ValueSerializer::Delegate {
  public:
-  V8Serializer(v8::Isolate* isolate)
+  explicit V8Serializer(v8::Isolate* isolate)
       : isolate_(isolate), serializer_(isolate, this) {}
   bool Serialize(v8::Local<v8::Value> value, blink::CloneableMessage* out) {
     serializer_.WriteHeader();
