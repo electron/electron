80af35e0cd391f1a154c58a3552eae73be708312
Shelley Vohr <shelley.vohr@gmail.com>
2019-10-03 22:21:30 +0200

    chore: enable native unittesting (#20293)

---
+++ shell/browser/ui/run_all_unittests.cc
@@ -0,0 +1,15 @@
+// Copyright (c) 2019 GitHub, Inc.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/bind.h"
+#include "base/test/launcher/unit_test_launcher.h"
+#include "base/test/test_suite.h"
+#include "build/build_config.h"
+
+int main(int argc, char** argv) {
+  base::TestSuite test_suite(argc, argv);
+  return base::LaunchUnitTests(
+      argc, argv,
+      base::BindOnce(&base::TestSuite::Run, base::Unretained(&test_suite)));
+}
