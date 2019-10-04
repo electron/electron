fc668b4bf7b31c7005c1c344887ae6f740b25065
Jeremy Apthorp <jeremya@chromium.org>
2019-10-04 14:44:58 -0700

    fix include

--- shell/renderer/electron_api_service_impl.h
+++ shell/renderer/electron_api_service_impl.h
@@ -10,6 +10,7 @@
 #include "base/memory/weak_ptr.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_frame_observer.h"
+#include "electron/buildflags/buildflags.h"
 #include "electron/shell/common/api/api.mojom.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
 
