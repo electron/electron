0aec679acb1c20a2084ecb34e7605fcfe2f422b8
Amarnath  Karthi <97amarnathk@HAL9000.local>
2019-10-12 16:52:18 +0530

    Code review changes

--- docs/fiddles/system/system-app-user-information/app-information/renderer.js
+++ docs/fiddles/system/system-app-user-information/app-information/renderer.js
@@ -1,6 +1,7 @@
 const {ipcRenderer} = require('electron')
 
 const appInfoBtn = document.getElementById('app-info')
+const links = document.querySelectorAll('a[href]')
 
 appInfoBtn.addEventListener('click', () => {
   ipcRenderer.send('get-app-path')
@@ -9,4 +10,14 @@ appInfoBtn.addEventListener('click', () => {
 ipcRenderer.on('got-app-path', (event, path) => {
   const message = `This app is located at: ${path}`
   document.getElementById('got-app-info').innerHTML = message
+})
+
+Array.prototype.forEach.call(links, (link) => {
+  const url = link.getAttribute('href')
+  if (url.indexOf('http') === 0) {
+    link.addEventListener('click', (e) => {
+      e.preventDefault()
+      shell.openExternal(url)
+    })
+  }
 })
