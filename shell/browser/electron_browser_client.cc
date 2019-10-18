0b4d6cd3e74853c1a3579d4931c6b60305df43c4
deepak1556 <hop2deep@gmail.com>
2019-10-17 18:18:10 -0700

    fix: loading dedicated/shared worker over custom protocols
    Backports https://chromium-review.googlesource.com/c/chromium/src/+/1798250
    that distinguishes loading the main script resource of dedicated/shared
    worker, this allows us to register a custom URLLoaderFactory.

--- shell/browser/electron_browser_client.cc
+++ shell/browser/electron_browser_client.cc
@@ -1263,6 +1263,15 @@ void ElectronBrowserClient::RegisterNonNetworkNavigationURLLoaderFactories(
       URLLoaderFactoryType::kNavigation, factories);
 }
 
+void ElectronBrowserClient::
+    RegisterNonNetworkWorkerMainResourceURLLoaderFactories(
+        content::BrowserContext* browser_context,
+        NonNetworkURLLoaderFactoryMap* factories) {
+  auto* protocol_registry =
+      ProtocolRegistry::FromBrowserContext(browser_context);
+  protocol_registry->RegisterURLLoaderFactories(factories);
+}
+
 #if BUILDFLAG(ENABLE_ELECTRON_EXTENSIONS)
 namespace {
 
