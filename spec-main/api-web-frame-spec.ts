3055fa5dd8b2dfcebe4005b64b0c670ec38f4629
OJ Kwon <kwon.ohjoong@gmail.com>
2019-10-18 17:50:10 -0700

    test(webframe): update spellcheck test cases

--- spec-main/api-web-frame-spec.ts
+++ spec-main/api-web-frame-spec.ts
@@ -35,6 +35,36 @@ describe('webFrame module', () => {
     }
     const [words, callbackDefined] = await spellCheckerFeedback
     expect(words.sort()).to.deep.equal(['spleling', 'test', `you're`, 'you', 're'].sort())
-    expect(callbackDefined).to.be.true()
+    expect(callbackDefined).to.be.true
+  })
+
+  it('calls a spellcheck provider with multiple languages', async () => {
+    const w = new BrowserWindow({
+      show: false,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+    await w.loadFile(path.join(fixtures, 'pages', 'webframe-spell-check-multi-lang.html'))
+    w.focus()
+    await w.webContents.executeJavaScript('document.querySelector("input").focus()', true)
+
+    const spellCheckerFeedback =
+      new Promise<[string[], boolean]>(resolve => {
+        ipcMain.on('spec-spell-check', (e, words, callbackDefined) => {
+          if (words.length === 8) {
+            // The API calls the provider after every completed word.
+            // The promise is resolved only after this event is received with all words.
+            resolve([words.map((word: string) => word.normalize()), callbackDefined])
+          }
+        })
+      })
+    const inputText = `spleling test you're 틀란 햔글 입럭`
+    for (const keyCode of inputText) {
+      w.webContents.sendInputEvent({ type: 'char', keyCode })
+    }
+    const [words, callbackDefined] = await spellCheckerFeedback
+    expect(words.sort()).to.deep.equal(['spleling', 'test', `you're`, 'you', 're', '틀란', '햔글', '입럭'].sort())
+    expect(callbackDefined).to.be.true
   })
 })
