4ea61c64bd686b854fd290cd068719da059903f4
deepak1556 <hop2deep@gmail.com>
2019-10-21 18:51:05 -0700

    Add CookieAccessSemantics to cookie change notifications
    https://chromium-review.googlesource.com/c/chromium/src/+/1854649

--- shell/browser/cookie_change_notifier.h
+++ shell/browser/cookie_change_notifier.h
@@ -10,12 +10,12 @@
 #include "base/callback_list.h"
 #include "base/macros.h"
 #include "mojo/public/cpp/bindings/receiver.h"
+#include "net/cookies/cookie_change_dispatcher.h"
 #include "services/network/public/mojom/cookie_manager.mojom.h"
 
 namespace electron {
 
 class AtomBrowserContext;
-struct CookieDetails;
 
 // Sends cookie-change notifications on the UI thread.
 class CookieChangeNotifier : public network::mojom::CookieChangeListener {
@@ -24,20 +24,21 @@ class CookieChangeNotifier : public network::mojom::CookieChangeListener {
   ~CookieChangeNotifier() override;
 
   // Register callbacks that needs to notified on any cookie store changes.
-  std::unique_ptr<base::CallbackList<void(const CookieDetails*)>::Subscription>
+  std::unique_ptr<base::CallbackList<
+      void(const net::CookieChangeInfo& change)>::Subscription>
   RegisterCookieChangeCallback(
-      const base::Callback<void(const CookieDetails*)>& cb);
+      const base::Callback<void(const net::CookieChangeInfo& change)>& cb);
 
  private:
   void StartListening();
   void OnConnectionError();
 
   // network::mojom::CookieChangeListener implementation.
-  void OnCookieChange(const net::CanonicalCookie& cookie,
-                      network::mojom::CookieChangeCause cause) override;
+  void OnCookieChange(const net::CookieChangeInfo& change) override;
 
   AtomBrowserContext* browser_context_;
-  base::CallbackList<void(const CookieDetails*)> cookie_change_sub_list_;
+  base::CallbackList<void(const net::CookieChangeInfo& change)>
+      cookie_change_sub_list_;
 
   mojo::Receiver<network::mojom::CookieChangeListener> receiver_;
 
