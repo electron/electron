9de71700229ada7f2ab9b38f37713d30317c7cec
John Kleinschmidt <jkleinsc@github.com>
2019-10-25 16:54:31 -0400

    build: fix doc only change when there isn't a PR
    Fixes issue where CI was mistakenly marking a PR as a doc only change because the CI was kicked off before the PR was created.

--- script/doc-only-change.js
+++ script/doc-only-change.js
@@ -1,4 +1,5 @@
 const args = require('minimist')(process.argv.slice(2))
+const { GitProcess } = require('dugite')
 const octokit = require('@octokit/rest')()
 const path = require('path')
 
@@ -19,9 +20,12 @@ async function checkIfDocOnlyChange () {
           })
           if (prsForBranch.data.length === 1) {
             pullRequestNumber = prsForBranch.data[0].number
+          } else if (prsForBranch.data.length === 0 && args.commit) {
+            // There are no pull requests for this branch, set pullRequestNumber to -1 to use commit instead
+            pullRequestNumber = -1
           } else {
             // If there is more than one PR on a branch, just assume that this is more than a doc change
-            process.exit(0)
+            process.exit(1)
           }
         } else if (args.prURL) {
           // CircleCI doesn't provide the PR number for branch builds, but it does provide the PR URL
@@ -29,13 +33,23 @@ async function checkIfDocOnlyChange () {
           pullRequestNumber = pullRequestParts[pullRequestParts.length - 1]
         }
       }
-      const filesChanged = await octokit.pulls.listFiles({
-        owner: 'electron', repo: 'electron', pull_number: pullRequestNumber
-      })
 
-      const nonDocChange = filesChanged.data.find((fileInfo) => {
-        const fileDirs = fileInfo.filename.split('/')
-        if (fileDirs[0] !== 'docs') {
+      let filesChanged
+      if (pullRequestNumber === -1 && args.commit) {
+        // There are no pull requests for this branch, but we do have a commit that can be used to get files
+        const result = await GitProcess.exec(['diff-tree', '--no-commit-id', '--name-only', '-r', args.commit])
+        if (result.exitCode !== 0) {
+          console.log('Failed to find changed files', GitProcess.parseError(result.stderr))
+          process.exit(-1)
+        }
+        filesChanged = result.stdout.split(/\r\n|\r|\n/g)
+      } else {
+        filesChanged = await findFilesByPRNumber(pullRequestNumber)
+      }
+
+      const nonDocChange = filesChanged.find((fileName) => {
+        const fileDirs = fileName.split('/')
+        if (fileDirs[0] !== 'docs' && fileDirs[0] !== '') {
           return true
         }
       })
@@ -50,9 +64,18 @@ async function checkIfDocOnlyChange () {
     }
   } else {
     console.error(`Check if only the docs were changed for a commit.
-    Usage: doc-only-change.js --prNumber=PR_NUMBER || --prBranch=PR_BRANCH || --prURL=PR_URL`)
+    Usage: doc-only-change.js --prNumber=PR_NUMBER || --prBranch=PR_BRANCH || --prURL=PR_URL || --commit=COMMIT_SHA`)
     process.exit(-1)
   }
+}
+
+async function findFilesByPRNumber (prNumber) {
+  const filesChanged = await octokit.pulls.listFiles({
+    owner: 'electron', repo: 'electron', pull_number: prNumber
+  })
+  return filesChanged.data.map((fileInfo) => {
+    return fileInfo.filename
+  })
 }
 
 checkIfDocOnlyChange()
