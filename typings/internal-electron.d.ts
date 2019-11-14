2b84b1ea43dc0e233c5af333e463607dba9bb2e5
Milan Burda <milan.burda@gmail.com>
2019-11-14 11:36:02 +0100

    fix: NativeImage serialization of <webview>.capturePage() result (#20825) (#21104)

--- typings/internal-electron.d.ts
+++ typings/internal-electron.d.ts
@@ -157,6 +157,7 @@ declare namespace ElectronInternal {
     // Created in web-view-impl
     public getWebContents(): Electron.WebContents;
     public getWebContentsId(): number;
+    public capturePage(rect?: Electron.Rectangle): Promise<Electron.NativeImage>;
   }
 }
 
