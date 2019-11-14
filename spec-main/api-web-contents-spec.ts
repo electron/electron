c8810584c588efff06488bfeb76547113cfa3627
Jeremy Apthorp <jeremya@chromium.org>
2019-11-13 17:53:39 -0800

    remove remote usage from focus test

--- spec-main/api-web-contents-spec.ts
+++ spec-main/api-web-contents-spec.ts
@@ -695,15 +695,19 @@ describe('webContents module', () => {
   describe('focus()', () => {
     describe('when the web contents is hidden', () => {
       afterEach(closeAllWindows)
-      it('does not blur the focused window', (done) => {
+      it('does not blur the focused window', async () => {
         const w = new BrowserWindow({ show: false, webPreferences: { nodeIntegration: true } })
-        ipcMain.once('answer', (event, parentFocused, childFocused) => {
-          expect(parentFocused).to.be.true()
-          expect(childFocused).to.be.false()
-          done()
-        })
         w.show()
-        w.loadFile(path.join(fixturesPath, 'pages', 'focus-web-contents.html'))
+        await w.loadURL('about:blank')
+        w.focus()
+        const child = new BrowserWindow({ show: false })
+        child.loadURL('about:blank')
+        child.webContents.focus()
+        const currentFocused = w.isFocused()
+        const childFocused = child.isFocused()
+        child.close()
+        expect(currentFocused).to.be.true()
+        expect(childFocused).to.be.false()
       })
     })
   })
