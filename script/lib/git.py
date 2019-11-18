c79809ddb92c575ad3523eb6e1f6dd2895c254da
Charles Kerr <ckerr@github.com>
2019-11-18 12:39:59 -0600

    build: missing include in windows release builds  (#21132)
    * fix: add missing `#include <algorithm>` as needed
    
    Manual backport of #21045
    
    * fix: add missing `#include <algorithm>` as needed
    
    Manual backport of #21045
    
    * chore: add patch to include missing `#include <memory>`
    
    * chore: add another `#include <memory>` needed
    
    * chore: regenerate patches w/correct breakpad root
    
    * chore: regenerate breakpad include failure patches
    
    * refactor: use --keep-cr in the git am patch script
    
    We need something like this to patch files that have crlf endings. See
    https://stackoverflow.com/questions/6289001/git-am-format-patch-control-format-of-line-endings
    
    * chore: regenerate node patches
    
    The udpated crlf support in git-{import,export}-patches caused a new
    warning when applying patches from `electron/patches/nodes`, so refresh
    the patches.
    
    * chore: no need to regenerate node patches
    
    * chore: silence whitespace warnings
    
    * chore: fix FTBFS from stl features used but not included
    
    * fixup! refactor: use --keep-cr in the git am patch script

--- script/lib/git.py
+++ script/lib/git.py
@@ -42,7 +42,7 @@ def get_repo_root(path):
 
 def am(repo, patch_data, threeway=False, directory=None, exclude=None,
     committer_name=None, committer_email=None):
-  args = []
+  args = ['--ignore-space-change']
   if threeway:
     args += ['--3way']
   if directory is not None:
