20b64709d5ff04196feca9d886faff1b7c1a4e33
John Kleinschmidt <jkleinsc@github.com>
2019-11-19 19:07:38 -0500

    Revert "update for python3"
    This reverts commit 349ccafa435832cf22fe7ef11b9144ac6d12d1f0.

--- script/lib/config.py
+++ script/lib/config.py
@@ -47,7 +47,7 @@ def get_env_var(name):
     # TODO Remove ATOM_SHELL_* fallback values
     value = os.environ.get('ATOM_SHELL_' + name, '')
     if value:
-      print("Warning: Use $ELECTRON_" + name + " instead of $ATOM_SHELL_" + name)
+      print 'Warning: Use $ELECTRON_' + name + ' instead of $ATOM_SHELL_' + name
   return value
 
 
@@ -63,7 +63,7 @@ def s3_config():
 
 
 def enable_verbose_mode():
-  print("Running in verbose mode")
+  print 'Running in verbose mode'
   global verbose_mode
   verbose_mode = True
 
