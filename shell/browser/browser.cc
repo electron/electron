826515dca3c1259ccc5586b4969de7f6819bb90a
emmanuel.kimmerlin@thomsonreuters.com <emmanuel.kimmerlin@thomsonreuters.com>
2019-11-22 11:21:19 +0100

    lint

--- shell/browser/browser.cc
+++ shell/browser/browser.cc
@@ -123,8 +123,9 @@ std::string Browser::GetName() const {
 void Browser::SetName(const std::string& name) {
   OverrideApplicationName(name);
   if (!is_ready()) {
-    // DIR_USER_DATA, DIR_USER_CACHE and DIR_APP_LOGS depend on application name,
-    // we have to force re-computation
+    // DIR_USER_DATA, DIR_USER_CACHE and DIR_APP_LOGS depend
+    // on application name.
+    // We have to force re-computation
     base::PathService::InvalidateCache();
   }
 }
