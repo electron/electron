2e28cb201ccdd9941b574a3c6d02cd1c93a1b023
Jeremy Apthorp <jeremya@chromium.org>
2019-12-05 11:52:50 -0800

    remove dependency on a.html from debugger spec

--- spec-main/api-debugger-spec.ts
+++ spec-main/api-debugger-spec.ts
@@ -7,7 +7,7 @@ import { closeAllWindows } from './window-helpers'
 import { emittedOnce } from './events-helpers'
 
 describe('debugger module', () => {
-  const fixtures = path.resolve(__dirname, '..', 'spec', 'fixtures')
+  const fixtures = path.resolve(__dirname, 'fixtures')
   let w: BrowserWindow
 
   beforeEach(() => {
@@ -32,7 +32,7 @@ describe('debugger module', () => {
         expect(w.webContents.debugger.isAttached()).to.be.true()
         done()
       })
-      w.webContents.loadFile(path.join(fixtures, 'pages', 'a.html'))
+      w.webContents.loadURL('about:blank')
     })
 
     it('fails when protocol version is not supported', done => {
@@ -132,9 +132,9 @@ describe('debugger module', () => {
 
     it('fires message event', done => {
       const url = process.platform !== 'win32'
-        ? `file://${path.join(fixtures, 'pages', 'a.html')}`
-        : `file:///${path.join(fixtures, 'pages', 'a.html').replace(/\\/g, '/')}`
-      w.webContents.loadFile(path.join(fixtures, 'pages', 'a.html'))
+        ? `file://${path.join(fixtures, 'blank.html')}`
+        : `file:///${path.join(fixtures, 'blank.html').replace(/\\/g, '/')}`
+      w.webContents.loadURL(url)
 
       try {
         w.webContents.debugger.attach()
@@ -153,6 +153,7 @@ describe('debugger module', () => {
         }
       })
       w.webContents.debugger.sendCommand('Console.enable')
+      w.webContents.executeJavaScript('console.log("a")')
     })
 
     it('returns error message when command fails', async () => {
