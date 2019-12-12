22c29adcf7d4d2cb6cf2c700375d12a40f8a41ec
Jeremy Apthorp <jeremya@chromium.org>
2019-12-12 13:29:03 -0800

    iwyu

--- shell/browser/net/proxying_url_loader_factory.cc
+++ shell/browser/net/proxying_url_loader_factory.cc
@@ -7,6 +7,7 @@
 #include <utility>
 
 #include "base/command_line.h"
+#include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "content/public/browser/browser_context.h"
 #include "extensions/browser/extension_navigation_ui_data.h"
