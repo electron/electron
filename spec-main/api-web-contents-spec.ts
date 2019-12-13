7ab528567e176eb12159d8a7fbbfd68d0389bc0b
Andy Locascio <andy@slack-corp.com>
2019-12-12 21:22:14 -0800

    test: ensure window doesn't leak into other test

--- spec-main/api-web-contents-spec.ts
+++ spec-main/api-web-contents-spec.ts
@@ -1699,5 +1699,6 @@ describe('webContents module', () => {
     await w.webContents.executeJavaScript(`window.open('about:blank')`)
     await new Promise((resolve) => { process.nextTick(resolve) })
     expect(wasCalled).to.equal(false)
+    await closeAllWindows()
   })
 })
