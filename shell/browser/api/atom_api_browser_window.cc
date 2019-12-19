6dc78849753f0d728f00eb1cbd792a319593bcd7
Andy Locascio <andy@slack-corp.com>
2019-12-19 11:14:20 -0800

    fix: prefer occluded rather than unloading layout info

--- shell/browser/api/atom_api_browser_window.cc
+++ shell/browser/api/atom_api_browser_window.cc
@@ -439,7 +439,7 @@ void BrowserWindow::OnWindowShow() {
 }
 
 void BrowserWindow::OnWindowHide() {
-  web_contents()->WasHidden();
+  web_contents()->WasOccluded();
   TopLevelWindow::OnWindowHide();
 }
 
