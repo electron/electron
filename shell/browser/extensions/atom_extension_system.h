cf40b54fd18523b1188b27563ef3ccd5f1cc194c
Jeremy Apthorp <jeremya@chromium.org>
2020-01-21 11:33:41 -0800

    account for chrome upgrade

--- shell/browser/extensions/atom_extension_system.h
+++ shell/browser/extensions/atom_extension_system.h
@@ -68,7 +68,7 @@ class AtomExtensionSystem : public ExtensionSystem {
   AppSorting* app_sorting() override;
   void RegisterExtensionWithRequestContexts(
       const Extension* extension,
-      const base::Closure& callback) override;
+      base::OnceClosure callback) override;
   void UnregisterExtensionWithRequestContexts(
       const std::string& extension_id,
       const UnloadedExtensionReason reason) override;
