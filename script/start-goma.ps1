a2b6ce13fa11313767e2388b9557be45162cd0b8
John Kleinschmidt <jkleinsc@github.com>
2020-01-23 09:47:38 -0800

    ci: use goma for windows and linux builds (#21868)
    * ci: use goma for windows and linux builds
    
    (cherry picked from commit dc2fcff01c0fe59bb2144f93cf2d89d92670771b)

---
+++ script/start-goma.ps1
@@ -0,0 +1,11 @@
+﻿param([string]$gomaDir=$PWD)
+$cmdPath = Join-Path -Path $gomaDir -ChildPath "goma_ctl.py" 
+Start-Process -FilePath cmd -ArgumentList "/C", "python", "$cmdPath", "ensure_start"
+$env:GLOG_logtostderr = "true"
+$timedOut = $false; $waitTime = 0; $waitIncrement = 5; $timeout=120;
+Do { sleep $waitIncrement; $timedOut = (($waitTime+=$waitIncrement) -gt $timeout); iex "$gomaDir\gomacc.exe port 2" > $null; } Until(($LASTEXITCODE -eq 0) -or $timedOut) 
+if ($timedOut) { 
+    write-error 'Timed out waiting for goma to start'; exit 1; 
+} else {
+    Write-Output "Successfully started goma!"
+}
