6798825a8f1c49cb4e40dfb57fa5d42fac8cb0b2
Charles Kerr <charles@charleskerr.com>
2020-01-28 09:32:01 -0600

    refactor: use app.whenReady() for simpler logic

--- lib/browser/api/power-monitor.ts
+++ lib/browser/api/power-monitor.ts
@@ -13,12 +13,13 @@ const powerMonitor = createLazyInstance(createPowerMonitor, PowerMonitor, true)
 if (process.platform === 'linux') {
   /* In order to delay system shutdown when e.preventDefault() is invoked
    * on a powerMonitor 'shutdown' event, we need an org.freedesktop.login1
-   * delay lock. For more details see the "Taking Delay Locks" section of
-   * https://www.freedesktop.org/wiki/Software/systemd/inhibit/
+   * shutdown delay lock. For more details see the "Taking Delay Locks"
+   * section of https://www.freedesktop.org/wiki/Software/systemd/inhibit/
    *
    * So here we watch for 'shutdown' listeners to be added or removed and
    * set or unset our shutdown delay lock accordingly. */
-  const onceReady = (): void => {
+  const { app } = require('electron')
+  app.whenReady().then(() => {
     powerMonitor.on('newListener', (event: string) => {
       // whenever the listener count is incremented to one...
       if (event === 'shutdown' && powerMonitor.listenerCount('shutdown') === 0) {
@@ -31,14 +32,7 @@ if (process.platform === 'linux') {
         powerMonitor.unblockShutdown()
       }
     })
-  }
-
-  const { app } = require('electron')
-  if (app.isReady()) {
-    onceReady()
-  } else {
-    app.once('ready', onceReady)
-  }
+  })
 }
 
 module.exports = powerMonitor
