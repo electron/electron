14152c1e85501d8506f6869094d1e8a6822d518c
John Kleinschmidt <jkleinsc@github.com>
2020-02-07 16:24:05 -0500

    Update script/strip-binaries.py
    Co-Authored-By: Jeremy Apthorp <jeremya@chromium.org>

--- script/strip-binaries.py
+++ script/strip-binaries.py
@@ -22,7 +22,7 @@ def strip_binary(binary_path, target_cpu):
     strip = 'mips64el-redhat-linux-strip'
   else:
     strip = 'strip'
-  execute([strip, '-p', binary_path])
+  execute([strip, '--preserve-dates', binary_path])
 
 def main():
   args = parse_args()
