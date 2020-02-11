da6660863e4cdee66262ef359812661d2aa84d26
Erick Zhao <ezhao@slack-corp.com>
2020-02-10 16:41:05 -0800

    tests

--- spec-main/spellchecker-spec.ts
+++ spec-main/spellchecker-spec.ts
@@ -1,4 +1,4 @@
-import { BrowserWindow } from 'electron'
+import { BrowserWindow, Session, session } from 'electron'
 
 import { expect } from 'chai'
 import * as path from 'path'
@@ -52,5 +52,70 @@ describe('spellchecker', () => {
     const contextMenuParams: Electron.ContextMenuParams = (await contextMenuPromise)[1]
     expect(contextMenuParams.misspelledWord).to.eq('Beautifulllll')
     expect(contextMenuParams.dictionarySuggestions).to.have.length.of.at.least(1)
+  })
+
+  describe('custom dictionary word list API', () => {
+    let ses: Session
+
+    beforeEach(async () => {
+      ses = session.fromPartition(`persist:customdictionary-test-${Date.now()}`)
+    })
+
+    afterEach(() => {
+      if (ses) {
+        ses.clearStorageData({})
+        ses.destroy()
+      }
+    })
+
+    describe('ses.listWordsFromSpellCheckerDictionary', () => {
+      it('should successfully list words in custom dictionary', async () => {
+        const words = ['foo', 'bar', 'baz']
+        const results = words.map(word => ses.addWordToSpellCheckerDictionary(word))
+        expect(results).to.eql([true, true, true])
+
+        const wordList = await ses.listWordsInSpellCheckerDictionary()
+        expect(wordList).to.have.deep.members(words)
+      })
+
+      it('should return an empty array if no words are added', async () => {
+        const wordList = await ses.listWordsInSpellCheckerDictionary()
+        expect(wordList).to.have.length(0)
+      })
+    })
+
+    describe('ses.addWordToSpellCheckerDictionary', () => {
+      it('should successfully add word to custom dictionary', async () => {
+        const result = ses.addWordToSpellCheckerDictionary('foobar')
+        expect(result).to.equal(true)
+        const wordList = await ses.listWordsInSpellCheckerDictionary()
+        expect(wordList).to.eql(['foobar'])
+      })
+
+      it('should fail for an empty string', async () => {
+        const result = ses.addWordToSpellCheckerDictionary('')
+        expect(result).to.equal(false)
+        const wordList = await ses.listWordsInSpellCheckerDictionary
+        expect(wordList).to.have.length(0)
+      })
+    })
+
+    describe('ses.removeWordFromSpellCheckerDictionary', () => {
+      it('should successfully remove words to custom dictionary', async () => {
+        const result1 = ses.addWordToSpellCheckerDictionary('foobar')
+        expect(result1).to.equal(true)
+        const wordList1 = await ses.listWordsInSpellCheckerDictionary()
+        expect(wordList1).to.eql(['foobar'])
+        const result2 = ses.removeWordFromSpellCheckerDictionary('foobar')
+        expect(result2).to.equal(true)
+        const wordList2 = await ses.listWordsInSpellCheckerDictionary()
+        expect(wordList2).to.have.length(0)
+      })
+
+      it('should fail for words not in custom dictionary', () => {
+        const result2 = ses.removeWordFromSpellCheckerDictionary('foobar')
+        expect(result2).to.equal(false)
+      })
+    })
   })
 })
