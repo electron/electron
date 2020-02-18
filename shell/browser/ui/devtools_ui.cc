3375e7525c83f331c5991376c1cae44e0294ccf2
Samuel Attard <samuel.r.attard@gmail.com>
2020-02-17 23:52:35 -0800

    fix: load devtools://remote assets so that lighthouse works

--- shell/browser/ui/devtools_ui.cc
+++ shell/browser/ui/devtools_ui.cc
@@ -4,6 +4,7 @@
 
 #include "shell/browser/ui/devtools_ui.h"
 
+#include <list>
 #include <memory>
 #include <string>
 #include <utility>
@@ -11,10 +12,14 @@
 #include "base/memory/ref_counted_memory.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
+#include "chrome/browser/devtools/devtools_ui_bindings.h"
+#include "chrome/browser/devtools/url_constants.h"
 #include "content/public/browser/devtools_frontend_host.h"
+#include "content/public/browser/storage_partition.h"
 #include "content/public/browser/url_data_source.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
+#include "services/network/public/cpp/simple_url_loader.h"
 
 namespace electron {
 
@@ -22,6 +27,7 @@ namespace {
 
 const char kChromeUIDevToolsHost[] = "devtools";
 const char kChromeUIDevToolsBundledPath[] = "bundled";
+const char kChromeUIDevToolsRemotePath[] = "remote";
 
 std::string PathWithoutParams(const std::string& path) {
   return GURL(std::string("devtools://devtools/") + path).path().substr(1);
@@ -53,10 +59,35 @@ std::string GetMimeTypeForPath(const std::string& path) {
   return "text/html";
 }
 
+scoped_refptr<base::RefCountedMemory> CreateNotFoundResponse() {
+  const char kHttpNotFound[] = "HTTP/1.1 404 Not Found\n\n";
+  return base::MakeRefCounted<base::RefCountedStaticMemory>(
+      kHttpNotFound, strlen(kHttpNotFound));
+}
+
 class BundledDataSource : public content::URLDataSource {
  public:
-  BundledDataSource() = default;
+  BundledDataSource(
+      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory)
+      : url_loader_factory_(std::move(url_loader_factory)) {}
   ~BundledDataSource() override = default;
+
+  struct PendingRequest {
+    PendingRequest() = default;
+
+    PendingRequest(PendingRequest&& other) = default;
+
+    ~PendingRequest() {
+      if (callback)
+        std::move(callback).Run(CreateNotFoundResponse());
+    }
+    PendingRequest& operator=(PendingRequest&& other) = default;
+
+    GotDataCallback callback;
+    std::unique_ptr<network::SimpleURLLoader> loader;
+
+    DISALLOW_COPY_AND_ASSIGN(PendingRequest);
+  };
 
   // content::URLDataSource implementation.
   std::string GetSource() override { return kChromeUIDevToolsHost; }
@@ -68,10 +99,27 @@ class BundledDataSource : public content::URLDataSource {
     // Serve request from local bundle.
     std::string bundled_path_prefix(kChromeUIDevToolsBundledPath);
     bundled_path_prefix += "/";
+    LOG(ERROR) << "Got Request: " << url.spec();
     if (base::StartsWith(path, bundled_path_prefix,
                          base::CompareCase::INSENSITIVE_ASCII)) {
       StartBundledDataRequest(path.substr(bundled_path_prefix.length()),
                               std::move(callback));
+      return;
+    }
+
+    std::string remote_path_prefix(kChromeUIDevToolsRemotePath);
+    remote_path_prefix += "/";
+    if (base::StartsWith(path, remote_path_prefix,
+                         base::CompareCase::INSENSITIVE_ASCII)) {
+      GURL url(kRemoteFrontendBase + path.substr(remote_path_prefix.length()));
+
+      CHECK_EQ(url.host(), kRemoteFrontendDomain);
+      if (url.is_valid() && DevToolsUIBindings::IsValidRemoteFrontendURL(url)) {
+        StartRemoteDataRequest(url, std::move(callback));
+      } else {
+        DLOG(ERROR) << "Refusing to load invalid remote front-end URL";
+        std::move(callback).Run(CreateNotFoundResponse());
+      }
       return;
     }
 
@@ -102,7 +150,68 @@ class BundledDataSource : public content::URLDataSource {
     std::move(callback).Run(bytes);
   }
 
+  void StartRemoteDataRequest(
+      const GURL& url,
+      content::URLDataSource::GotDataCallback callback) {
+    CHECK(url.is_valid());
+    net::NetworkTrafficAnnotationTag traffic_annotation =
+        net::DefineNetworkTrafficAnnotation("devtools_hard_coded_data_source",
+                                            R"(
+        semantics {
+          sender: "Developer Tools Remote Data Request From Google"
+          description:
+            "This service fetches Chromium DevTools front-end files from the "
+            "cloud for the remote debugging scenario."
+          trigger:
+            "When user attaches to mobile phone for debugging."
+          data: "None"
+          destination: GOOGLE_OWNED_SERVICE
+        }
+        policy {
+          cookies_allowed: YES
+          cookies_store: "user"
+          setting: "This feature cannot be disabled by settings."
+          chrome_policy {
+            DeveloperToolsAvailability {
+              policy_options {mode: MANDATORY}
+              DeveloperToolsAvailability: 2
+            }
+          }
+        })");
+
+    StartNetworkRequest(url, traffic_annotation, std::move(callback));
+  }
+
+  void StartNetworkRequest(
+      const GURL& url,
+      const net::NetworkTrafficAnnotationTag& traffic_annotation,
+      GotDataCallback callback) {
+    auto request = std::make_unique<network::ResourceRequest>();
+    request->url = url;
+
+    auto request_iter = pending_requests_.emplace(pending_requests_.begin());
+    request_iter->callback = std::move(callback);
+    request_iter->loader = network::SimpleURLLoader::Create(std::move(request),
+                                                            traffic_annotation);
+    request_iter->loader->DownloadToStringOfUnboundedSizeUntilCrashAndDie(
+        url_loader_factory_.get(),
+        base::BindOnce(&BundledDataSource::OnLoadComplete,
+                       base::Unretained(this), request_iter));
+  }
+
+  void OnLoadComplete(std::list<PendingRequest>::iterator request_iter,
+                      std::unique_ptr<std::string> response_body) {
+    std::move(request_iter->callback)
+        .Run(response_body
+                 ? base::RefCountedString::TakeString(response_body.get())
+                 : CreateNotFoundResponse());
+    pending_requests_.erase(request_iter);
+  }
+
  private:
+  scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory_;
+  std::list<PendingRequest> pending_requests_;
+
   DISALLOW_COPY_AND_ASSIGN(BundledDataSource);
 };
 
@@ -111,9 +220,12 @@ class BundledDataSource : public content::URLDataSource {
 DevToolsUI::DevToolsUI(content::BrowserContext* browser_context,
                        content::WebUI* web_ui)
     : WebUIController(web_ui) {
+  auto factory = content::BrowserContext::GetDefaultStoragePartition(
+                     web_ui->GetWebContents()->GetBrowserContext())
+                     ->GetURLLoaderFactoryForBrowserProcess();
   web_ui->SetBindings(0);
-  content::URLDataSource::Add(browser_context,
-                              std::make_unique<BundledDataSource>());
+  content::URLDataSource::Add(
+      browser_context, std::make_unique<BundledDataSource>(std::move(factory)));
 }
 
 }  // namespace electron
