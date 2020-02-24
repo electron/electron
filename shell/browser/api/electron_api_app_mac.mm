c4054cf6118179545d2fe85487247b5f1f505e83
emmanuel.kimmerlin@thomsonreuters.com <emmanuel.kimmerlin@thomsonreuters.com>
2020-02-24 10:16:32 +0100

    Restore Mac missing function

---
+++ shell/browser/api/electron_api_app_mac.mm
@@ -0,0 +1,37 @@
+// Copyright (c) 2019 GitHub, Inc.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+#include "base/path_service.h"
+#include "shell/browser/api/electron_api_app.h"
+#include "shell/browser/electron_paths.h"
+
+#import <Cocoa/Cocoa.h>
+
+namespace electron {
+
+namespace api {
+
+void App::SetActivationPolicy(gin_helper::ErrorThrower thrower,
+                              const std::string& policy) {
+  NSApplicationActivationPolicy activation_policy;
+  if (policy == "accessory") {
+    activation_policy = NSApplicationActivationPolicyAccessory;
+  } else if (policy == "prohibited") {
+    activation_policy = NSApplicationActivationPolicyProhibited;
+  } else if (policy == "regular") {
+    activation_policy = NSApplicationActivationPolicyRegular;
+  } else {
+    thrower.ThrowError("Invalid activation policy: must be one of 'regular', "
+                       "'accessory', or 'prohibited'");
+    return;
+  }
+
+  [NSApp setActivationPolicy:activation_policy];
+}
+
+}  // namespace api
+
+}  // namespace electron
