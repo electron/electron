f78c988c95f79d12c61b01d420a21090af5807f3
Cheng Zhao <zcbenz@gmail.com>
2020-03-02 15:52:54 +0900

    fix: also match the behavior on Linux

--- shell/browser/api/electron_api_web_contents.cc
+++ shell/browser/api/electron_api_web_contents.cc
@@ -2071,9 +2071,9 @@ void WebContents::CopyImageAt(int x, int y) {
 }
 
 void WebContents::Focus() {
-  // Focusing on WebContents does not automatically focus the window on macOS,
-  // do it manually to match the behavior on Windows.
-#if defined(OS_MACOSX)
+  // Focusing on WebContents does not automatically focus the window on macOS
+  // and Linux, do it manually to match the behavior on Windows.
+#if defined(OS_MACOSX) || defined(OS_LINUX)
   if (owner_window())
     owner_window()->Focus(true);
 #endif
