95fbd05a040696f1109f39812e27ab78312deb38
Jeremy Apthorp <jeremya@chromium.org>
2020-03-04 15:05:51 -0800

    you get a scope. and you get a scope

--- shell/browser/login_handler.cc
+++ shell/browser/login_handler.cc
@@ -51,6 +51,7 @@ void LoginHandler::EmitEvent(
     scoped_refptr<net::HttpResponseHeaders> response_headers,
     bool first_auth_attempt) {
   v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::HandleScope scope(isolate);
 
   auto api_web_contents = api::WebContents::From(isolate, web_contents());
   if (api_web_contents.IsEmpty()) {
