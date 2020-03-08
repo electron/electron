6584d8e0bafba9cd7c8a7c0c154f89bbb74cc091
Tristan Partin <tristan@partin.io>
2020-03-08 16:30:19 -0500

    fix: remove unused gi18n.h include
    Not sure why this is

--- shell/browser/ui/file_dialog_gtk.cc
+++ shell/browser/ui/file_dialog_gtk.cc
@@ -2,7 +2,6 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
-#include <glib/gi18n.h>  // _() macro
 #include <gmodule.h>
 
 #include <memory>
