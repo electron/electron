1b353d1ed3dee226400560404ced3ee78c5c3964
Shelley Vohr <shelley.vohr@gmail.com>
2020-03-13 17:16:08 +0000

    chore: revert deprecated WebContents properties (#22640)
    * chore: revert deprecated WebContents properties
    
    * Fix failing zoomFactor test

--- spec-main/api-browser-window-spec.ts
+++ spec-main/api-browser-window-spec.ts
@@ -3958,19 +3958,23 @@ describe('BrowserWindow module', () => {
       })
     })
 
-    // TODO(codebytere): remove in Electron v8.0.0
-    describe('window.webContents.getFrameRate()', () => {
-      it('has default frame rate', (done) => {
+    describe('frameRate APIs', () => {
+      it('has default frame rate (functions)', (done) => {
         w.webContents.once('paint', function () {
           expect(w.webContents.getFrameRate()).to.equal(60)
           done()
         })
         w.loadFile(path.join(fixtures, 'api', 'offscreen-rendering.html'))
       })
-    })
 
-    // TODO(codebytere): remove in Electron v8.0.0
-    describe('window.webContents.setFrameRate(frameRate)', () => {
+      it('has default frame rate', (done) => {
+        w.webContents.once('paint', function () {
+          expect(w.webContents.frameRate).to.equal(60)
+          done()
+        })
+        w.loadFile(path.join(fixtures, 'api', 'offscreen-rendering.html'))
+      })
+
       it('sets custom frame rate', (done) => {
         w.webContents.on('dom-ready', () => {
           w.webContents.setFrameRate(30)
@@ -3978,16 +3982,6 @@ describe('BrowserWindow module', () => {
             expect(w.webContents.getFrameRate()).to.equal(30)
             done()
           })
-        })
-        w.loadFile(path.join(fixtures, 'api', 'offscreen-rendering.html'))
-      })
-    })
-
-    describe('window.webContents.FrameRate', () => {
-      it('has default frame rate', (done) => {
-        w.webContents.once('paint', function () {
-          expect(w.webContents.frameRate).to.equal(60)
-          done()
         })
         w.loadFile(path.join(fixtures, 'api', 'offscreen-rendering.html'))
       })
