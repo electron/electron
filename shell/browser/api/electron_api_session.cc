d3634c04fe2b936a804a5a46abff4a169f35c722
Jeremy Apthorp <nornagon@nornagon.net>
2020-03-24 14:35:10 -0700

    refactor: ginify Cookies

--- shell/browser/api/electron_api_session.cc
+++ shell/browser/api/electron_api_session.cc
@@ -305,7 +305,6 @@ Session::~Session() {
   // TODO(zcbenz): Now since URLRequestContextGetter is gone, is this still
   // needed?
   // Refs https://github.com/electron/electron/pull/12305.
-  DestroyGlobalHandle(isolate(), cookies_);
   DestroyGlobalHandle(isolate(), protocol_);
   g_sessions.erase(weak_map_id());
 }
@@ -1053,9 +1052,6 @@ void Initialize(v8::Local<v8::Object> exports,
   dict.Set(
       "Session",
       Session::GetConstructor(isolate)->GetFunction(context).ToLocalChecked());
-  dict.Set(
-      "Cookies",
-      Cookies::GetConstructor(isolate)->GetFunction(context).ToLocalChecked());
   dict.Set(
       "Protocol",
       Protocol::GetConstructor(isolate)->GetFunction(context).ToLocalChecked());
