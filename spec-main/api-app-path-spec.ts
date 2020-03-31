6a7fc0855d08caae53ddd561087a15ba25ac2526
emmanuel.kimmerlin@thomsonreuters.com <emmanuel.kimmerlin@thomsonreuters.com>
2020-03-31 14:16:45 +0200

    tslint hell

--- spec-main/api-app-path-spec.ts
+++ spec-main/api-app-path-spec.ts
@@ -150,22 +150,22 @@ describe('app path module', () => {
   describe('getAppPath', () => {
     it('works for directories with package.json', async () => {
       const { appPath } = await runTestApp('app-path');
-      expect(appPath).to.equal(path.resolve(fixturesPath, 'api/app-path'));
+      expect(appPath).to.equal(path.resolve(fixturesPath, 'app-path'));
     });
 
     it('works for directories with index.js', async () => {
       const { appPath } = await runTestApp('app-path/lib');
-      expect(appPath).to.equal(path.resolve(fixturesPath, 'api/app-path/lib'));
+      expect(appPath).to.equal(path.resolve(fixturesPath, 'app-path/lib'));
     });
 
     it('works for files without extension', async () => {
       const { appPath } = await runTestApp('app-path/lib/index');
-      expect(appPath).to.equal(path.resolve(fixturesPath, 'api/app-path/lib'));
+      expect(appPath).to.equal(path.resolve(fixturesPath, 'app-path/lib'));
     });
 
     it('works for files', async () => {
       const { appPath } = await runTestApp('app-path/lib/index.js');
-      expect(appPath).to.equal(path.resolve(fixturesPath, 'api/app-path/lib'));
+      expect(appPath).to.equal(path.resolve(fixturesPath, 'app-path/lib'));
     });
   });
 
