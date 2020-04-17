b6f450db04f5edc4cacc9c9f223b350f0bb5b509
Charles Kerr <ckerr@github.com>
2020-04-17 07:50:06 -0500

    fix: use Node's microtasks policy in node_main.cc
    Fixes #21515.

--- shell/app/node_main.cc
+++ shell/app/node_main.cc
@@ -78,6 +78,7 @@ int NodeMain(int argc, char* argv[]) {
     JavascriptEnvironment gin_env(loop);
 
     v8::Isolate* isolate = gin_env.isolate();
+    isolate->SetMicrotasksPolicy(v8::MicrotasksPolicy::kExplicit);
 
     node::IsolateData* isolate_data =
         node::CreateIsolateData(isolate, loop, gin_env.platform());
