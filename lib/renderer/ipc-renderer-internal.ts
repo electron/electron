0b3bf1e5566efe64c31d5b6aa07c0a8eb1c81836
Samuel Attard <sattard@slack-corp.com>
2020-04-22 17:36:55 -0700

    fix: do not mutate ipc instances across contexts (#23239)

--- lib/renderer/ipc-renderer-internal.ts
+++ lib/renderer/ipc-renderer-internal.ts
@@ -5,18 +5,20 @@ const v8Util = process.electronBinding('v8_util');
 export const ipcRendererInternal: Electron.IpcRendererInternal = v8Util.getHiddenValue(global, 'ipc-internal');
 const internal = true;
 
-ipcRendererInternal.send = function (channel, ...args) {
-  return binding.ipc.send(internal, channel, args);
-};
+if (!ipcRendererInternal.send) {
+  ipcRendererInternal.send = function (channel, ...args) {
+    return binding.ipc.send(internal, channel, args);
+  };
 
-ipcRendererInternal.sendSync = function (channel, ...args) {
-  return binding.ipc.sendSync(internal, channel, args)[0];
-};
+  ipcRendererInternal.sendSync = function (channel, ...args) {
+    return binding.ipc.sendSync(internal, channel, args)[0];
+  };
 
-ipcRendererInternal.sendTo = function (webContentsId, channel, ...args) {
-  return binding.ipc.sendTo(internal, false, webContentsId, channel, args);
-};
+  ipcRendererInternal.sendTo = function (webContentsId, channel, ...args) {
+    return binding.ipc.sendTo(internal, false, webContentsId, channel, args);
+  };
 
-ipcRendererInternal.sendToAll = function (webContentsId, channel, ...args) {
-  return binding.ipc.sendTo(internal, true, webContentsId, channel, args);
-};
+  ipcRendererInternal.sendToAll = function (webContentsId, channel, ...args) {
+    return binding.ipc.sendTo(internal, true, webContentsId, channel, args);
+  };
+}
