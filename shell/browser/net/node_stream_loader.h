58f3770c195263a8be1c0f2bcccb2b87f7aba2bf
Samuel Attard <samuel.r.attard@gmail.com>
2020-05-04 17:25:06 -0700

    refactor: FollowRedirect takes in cors exempt headers now
    Refs: https://chromium-review.googlesource.com/c/chromium/src/+/2129787

--- shell/browser/net/node_stream_loader.h
+++ shell/browser/net/node_stream_loader.h
@@ -51,9 +51,11 @@ class NodeStreamLoader : public network::mojom::URLLoader {
   void On(const char* event, EventCallback callback);
 
   // URLLoader:
-  void FollowRedirect(const std::vector<std::string>& removed_headers,
-                      const net::HttpRequestHeaders& modified_headers,
-                      const base::Optional<GURL>& new_url) override {}
+  void FollowRedirect(
+      const std::vector<std::string>& removed_headers,
+      const net::HttpRequestHeaders& modified_headers,
+      const net::HttpRequestHeaders& modified_cors_exempt_headers,
+      const base::Optional<GURL>& new_url) override {}
   void SetPriority(net::RequestPriority priority,
                    int32_t intra_priority_value) override {}
   void PauseReadingBodyFromNet() override {}
