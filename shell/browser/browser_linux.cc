1f0a7d8fddbc1b784beca3d1890f66cc4ab9673c
Shelley Vohr <shelley.vohr@gmail.com>
2020-05-14 08:56:03 -0700

    gin_helper -> mate

--- shell/browser/browser_linux.cc
+++ shell/browser/browser_linux.cc
@@ -84,7 +84,7 @@ bool SetDefaultWebClient(const std::string& protocol) {
   return ran_ok && exit_code == EXIT_SUCCESS;
 }
 
-void Browser::Focus(gin_helper::Arguments* args) {
+void Browser::Focus(mate::Arguments* args) {
   // Focus on the first visible window.
   for (auto* const window : WindowList::GetWindows()) {
     if (window->IsVisible()) {
