07e28436006dd0074feb7e1e591550e2385afb54
Lukas Weber <luweber@microsoft.com>
2020-06-01 10:05:05 +0200

    feat: Add 'did-becom-active' event on Mac

--- shell/browser/browser.cc
+++ shell/browser/browser.cc
@@ -262,6 +262,11 @@ void Browser::NewWindowForTab() {
   for (BrowserObserver& observer : observers_)
     observer.OnNewWindowForTab();
 }
+
+void Browser::DidBecomeActive() {
+  for (BrowserObserver& observer : observers_)
+    observer.OnDidBecomeActive();
+}
 #endif
 
 }  // namespace electron
