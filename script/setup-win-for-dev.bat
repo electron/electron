2e2e9a370259bd5f9da1fdf4ee8979bae317824c
Andrea Brancaleoni <abc@pompel.me>
2020-06-11 16:05:03 +0200

    ci: use call instead of cmd /C (#23985)
    Since we want to import the environment change from RefreshEnv.cmd
    we also want to share the same execution context of the
    caller.

--- script/setup-win-for-dev.bat
+++ script/setup-win-for-dev.bat
@@ -71,11 +71,11 @@ SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 REM Install nodejs python git and yarn needed dependencies
 choco install -y nodejs python2 git yarn windows-sdk-10-version-1903-windbg
-cmd /c C:\ProgramData\chocolatey\bin\RefreshEnv.cmd
+call C:\ProgramData\chocolatey\bin\RefreshEnv.cmd
 SET PATH=C:\Python27\;C:\Python27\Scripts;%PATH%
 
 pip install pywin32
-cmd /c C:\ProgramData\chocolatey\bin\RefreshEnv.cmd
+call C:\ProgramData\chocolatey\bin\RefreshEnv.cmd
 pip2 install pywin32
 
 REM Setup Depot Tools
