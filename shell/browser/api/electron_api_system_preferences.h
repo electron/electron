a22384c9ff55a31104b86ddd7ac65227a6c84e1c
Milan Burda <milan.burda@gmail.com>
2020-07-07 18:03:55 +0200

    feat: implement systemPreferences.getMediaAccessStatus() on Windows (#24313)

--- shell/browser/api/electron_api_system_preferences.h
+++ shell/browser/api/electron_api_system_preferences.h
@@ -51,6 +51,10 @@ class SystemPreferences : public mate::EventEmitter<SystemPreferences>
   std::string GetAccentColor();
   std::string GetColor(gin_helper::ErrorThrower thrower,
                        const std::string& color);
+  // TODO(codebytere): Write tests for these methods once we
+  // are running tests on a Mojave machine
+  std::string GetMediaAccessStatus(const std::string& media_type,
+                                   mate::Arguments* args);
 #endif
 #if defined(OS_WIN)
   bool IsAeroGlassEnabled();
@@ -103,10 +107,6 @@ class SystemPreferences : public mate::EventEmitter<SystemPreferences>
 
   static bool IsTrustedAccessibilityClient(bool prompt);
 
-  // TODO(codebytere): Write tests for these methods once we
-  // are running tests on a Mojave machine
-  std::string GetMediaAccessStatus(const std::string& media_type,
-                                   mate::Arguments* args);
   v8::Local<v8::Promise> AskForMediaAccess(v8::Isolate* isolate,
                                            const std::string& media_type);
 
