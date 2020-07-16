53ba5c870f645d22bddcf931dc59c4c9a2647900
Jeremy Rose <nornagon@nornagon.net>
2020-07-16 11:20:47 -0700

    refactor: stop using isolate() in WebContents

--- shell/browser/api/electron_api_web_contents.h
+++ shell/browser/api/electron_api_web_contents.h
@@ -232,7 +232,7 @@ class WebContents : public gin_helper::TrackableObject<WebContents>,
   bool IsCurrentlyAudible();
   void SetEmbedder(const WebContents* embedder);
   void SetDevToolsWebContents(const WebContents* devtools);
-  v8::Local<v8::Value> GetNativeView() const;
+  v8::Local<v8::Value> GetNativeView(v8::Isolate* isolate) const;
   void IncrementCapturerCount(gin_helper::Arguments* args);
   void DecrementCapturerCount(gin_helper::Arguments* args);
   bool IsBeingCaptured();
@@ -359,7 +359,7 @@ class WebContents : public gin_helper::TrackableObject<WebContents>,
   v8::Local<v8::Value> GetLastWebPreferences(v8::Isolate* isolate) const;
 
   // Returns the owner window.
-  v8::Local<v8::Value> GetOwnerBrowserWindow() const;
+  v8::Local<v8::Value> GetOwnerBrowserWindow(v8::Isolate* isolate) const;
 
   // Grants the child process the capability to access URLs with the origin of
   // the specified URL.
@@ -368,7 +368,8 @@ class WebContents : public gin_helper::TrackableObject<WebContents>,
   // Notifies the web page that there is user interaction.
   void NotifyUserActivation();
 
-  v8::Local<v8::Promise> TakeHeapSnapshot(const base::FilePath& file_path);
+  v8::Local<v8::Promise> TakeHeapSnapshot(v8::Isolate* isolate,
+                                          const base::FilePath& file_path);
 
   // Properties.
   int32_t ID() const;
