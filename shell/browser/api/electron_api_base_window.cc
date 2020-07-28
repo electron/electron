0699bcb375cdea5bcc8e3d1884fffef2d338b714
Jeremy Rose <jeremya@chromium.org>
2020-07-28 10:02:01 -0700

    Update shell/browser/api/electron_api_base_window.cc

--- shell/browser/api/electron_api_base_window.cc
+++ shell/browser/api/electron_api_base_window.cc
@@ -301,7 +301,7 @@ void BaseWindow::OnWindowMessage(UINT message, WPARAM w_param, LPARAM l_param) {
     v8::Locker locker(isolate);
     v8::HandleScope scope(isolate);
     messages_callback_map_[message].Run(
-        ToBuffer(isolate(), static_cast<void*>(&w_param), sizeof(WPARAM)),
+        ToBuffer(isolate, static_cast<void*>(&w_param), sizeof(WPARAM)),
         ToBuffer(isolate(), static_cast<void*>(&l_param), sizeof(LPARAM)));
   }
 }
