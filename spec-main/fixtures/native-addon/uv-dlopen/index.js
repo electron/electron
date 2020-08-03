2c6bfbee5e9c2433a913054bae083f107a11996a
Cheng Zhao <zcbenz@gmail.com>
2020-08-03 16:28:19 +0900

    use force_load for mac

--- spec-main/fixtures/native-addon/uv-dlopen/index.js
+++ spec-main/fixtures/native-addon/uv-dlopen/index.js
@@ -3,8 +3,9 @@ const testLoadLibrary = require('./build/Release/test_module');
 const lib = (() => {
   switch (process.platform) {
     case 'linux':
-    case 'darwin':
       return `${__dirname}/build/Release/foo.so`;
+    case 'darwin':
+      return `${__dirname}/build/Release/foo.dylib`;
     case 'win32':
       return `${__dirname}/build/Release/libfoo.dll`;
     default:
