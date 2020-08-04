01a2e23194f86df1d0dcc4a3ccb3ea37e8f8c98b
Jeremy Rose <jeremya@chromium.org>
2020-08-04 11:48:04 -0700

    refactor: mmap asar files (#24470)

--- typings/internal-ambient.d.ts
+++ typings/internal-ambient.d.ts
@@ -68,7 +68,8 @@ declare namespace NodeJS {
     readdir(path: string): string[] | false;
     realpath(path: string): string | false;
     copyFileOut(path: string): string | false;
-    getFd(): number | -1;
+    read(offset: number, size: number): Promise<ArrayBuffer>;
+    readSync(offset: number, size: number): ArrayBuffer;
   }
 
   interface AsarBinding {
