945defd47ea2ec626c16c6954d37bde98f43a0d3
Samuel Attard <sattard@slack-corp.com>
2020-08-25 03:22:13 -0700

    refactor: wire will-navigate up to a navigation throttle instead of OpenURL (#25109)
    * refactor: wire will-navigate up to a navigation throttle instead of OpenURL (#25065)
    
    * refactor: wire will-navigate up to a navigation throttle instead of OpenURL
    
    * spec: add test for x-site _top navigation
    
    * chore: old code be old

--- shell/browser/api/electron_api_web_contents.cc
+++ shell/browser/api/electron_api_web_contents.cc
@@ -741,12 +741,6 @@ content::WebContents* WebContents::OpenURLFromTab(
     return nullptr;
   }
 
-  // Give user a chance to cancel navigation.
-  if (Emit("will-navigate", params.url))
-    return nullptr;
-
-  // Don't load the URL if the web contents was marked as destroyed from a
-  // will-navigate event listener
   if (IsDestroyed())
     return nullptr;
 
