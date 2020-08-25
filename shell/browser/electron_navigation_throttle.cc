18613925610ba319da7f497b6deed85ad712c59b
Samuel Attard <sattard@slack-corp.com>
2020-08-25 03:22:24 -0700

    refactor: wire will-navigate up to a navigation throttle instead of OpenURL (#25108)
    * refactor: wire will-navigate up to a navigation throttle instead of OpenURL (#25065)
    
    * refactor: wire will-navigate up to a navigation throttle instead of OpenURL
    
    * spec: add test for x-site _top navigation
    
    * chore: old code be old

--- shell/browser/electron_navigation_throttle.cc
+++ shell/browser/electron_navigation_throttle.cc
@@ -20,6 +20,30 @@ const char* ElectronNavigationThrottle::GetNameForLogging() {
 }
 
 content::NavigationThrottle::ThrottleCheckResult
+ElectronNavigationThrottle::WillStartRequest() {
+  auto* handle = navigation_handle();
+  auto* contents = handle->GetWebContents();
+  if (!contents) {
+    NOTREACHED();
+    return PROCEED;
+  }
+
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::HandleScope scope(isolate);
+  auto api_contents = electron::api::WebContents::From(isolate, contents);
+  if (api_contents.IsEmpty()) {
+    // No need to emit any event if the WebContents is not available in JS.
+    return PROCEED;
+  }
+
+  if (handle->IsRendererInitiated() && handle->IsInMainFrame() &&
+      api_contents->EmitNavigationEvent("will-navigate", handle)) {
+    return CANCEL;
+  }
+  return PROCEED;
+}
+
+content::NavigationThrottle::ThrottleCheckResult
 ElectronNavigationThrottle::WillRedirectRequest() {
   auto* handle = navigation_handle();
   auto* contents = handle->GetWebContents();
