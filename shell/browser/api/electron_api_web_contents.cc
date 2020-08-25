944b9ae454cd7e8feaa0045daf02d1a2edc05d62
Jeremy Rose <nornagon@nornagon.net>
2020-08-25 10:06:57 -0700

    2345900: Move content::RecordContentToVisibleTimeRequest struct to mojo
    https://chromium-review.googlesource.com/c/chromium/src/+/2345900
    (cherry picked from commit fb3eb6da4f8b2e68d64e2749f614bbe92a0c99f6)

--- shell/browser/api/electron_api_web_contents.cc
+++ shell/browser/api/electron_api_web_contents.cc
@@ -1501,7 +1501,7 @@ void WebContents::SetBackgroundThrottling(bool allowed) {
   web_contents()->GetRenderViewHost()->SetSchedulerThrottling(allowed);
 
   if (rwh_impl->is_hidden()) {
-    rwh_impl->WasShown(base::nullopt);
+    rwh_impl->WasShown({});
   }
 }
 
