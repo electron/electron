59d9280ffd78d93f2cd4a1bc4071dadca3cd5cda
Shelley Vohr <shelley.vohr@gmail.com>
2020-09-01 19:33:01 -0700

    Allow preventing InitializeInspector in env

--- shell/common/node_bindings.cc
+++ shell/common/node_bindings.cc
@@ -392,11 +392,12 @@ node::Environment* NodeBindings::CreateEnvironment(
       node::CreateIsolateData(context->GetIsolate(), uv_loop_, platform);
 
   node::Environment* env;
-  uint64_t flags = node::EnvironmentFlags::kDefaultFlags | node::EnvironmentFlags::kNoInitializeInspector;
+  uint64_t flags = node::EnvironmentFlags::kDefaultFlags |
+                   node::EnvironmentFlags::kNoInitializeInspector;
   if (browser_env_ != BrowserEnvironment::BROWSER) {
     v8::TryCatch try_catch(context->GetIsolate());
-    node::Environment* env = node::CreateEnvironment(
-        isolate_data_, context, args, exec_args, (node::EnvironmentFlags::Flags) flags);
+    env = node::CreateEnvironment(isolate_data_, context, args, exec_args,
+                              (node::EnvironmentFlags::Flags)flags);
     DCHECK(env);
 
     // This will only be caught when something has gone terrible wrong as all
@@ -406,8 +407,8 @@ node::Environment* NodeBindings::CreateEnvironment(
                  << process_type;
     }
   } else {
-    node::Environment* env = node::CreateEnvironment(
-        isolate_data_, context, args, exec_args, (node::EnvironmentFlags::Flags) flags);
+    env = node::CreateEnvironment(isolate_data_, context, args, exec_args,
+                              (node::EnvironmentFlags::Flags)flags);
     DCHECK(env);
   }
 
