7a66a59b1794e9fa5849216937b3f4847db55ef3
Eryk Rakowski <sentialx@gmail.com>
2020-09-06 20:49:48 +0200

    Update electron_extensions_api_client.cc

--- shell/browser/extensions/electron_extensions_api_client.cc
+++ shell/browser/extensions/electron_extensions_api_client.cc
@@ -65,11 +65,6 @@ ElectronExtensionsAPIClient::CreateManagementAPIDelegate() const {
   return new ElectronManagementAPIDelegate;
 }
 
-ManagementAPIDelegate*
-ElectronExtensionsAPIClient::CreateManagementAPIDelegate() const {
-  return new ElectronManagementAPIDelegate;
-}
-
 std::unique_ptr<MimeHandlerViewGuestDelegate>
 ElectronExtensionsAPIClient::CreateMimeHandlerViewGuestDelegate(
     MimeHandlerViewGuest* guest) const {
