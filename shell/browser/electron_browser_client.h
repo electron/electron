284abad17d9897d3bbffe4f47481daf467d35f41
John Kleinschmidt <jkleinsc@github.com>
2020-09-08 12:16:59 -0400

    Make FileURLLoaderFactory always owned by its |receivers_|.
    https://chromium-review.googlesource.com/c/chromium/src/+/2337411

--- shell/browser/electron_browser_client.h
+++ shell/browser/electron_browser_client.h
@@ -178,13 +178,16 @@ class ElectronBrowserClient : public content::ContentBrowserClient,
   void RegisterNonNetworkNavigationURLLoaderFactories(
       int frame_tree_node_id,
       base::UkmSourceId ukm_source_id,
+      NonNetworkURLLoaderFactoryDeprecatedMap* uniquely_owned_factories,
       NonNetworkURLLoaderFactoryMap* factories) override;
   void RegisterNonNetworkWorkerMainResourceURLLoaderFactories(
       content::BrowserContext* browser_context,
-      NonNetworkURLLoaderFactoryMap* factories) override;
+      NonNetworkURLLoaderFactoryDeprecatedMap* uniquely_owned_factories)
+      override;
   void RegisterNonNetworkSubresourceURLLoaderFactories(
       int render_process_id,
       int render_frame_id,
+      NonNetworkURLLoaderFactoryDeprecatedMap* uniquely_owned_factories,
       NonNetworkURLLoaderFactoryMap* factories) override;
   void CreateWebSocket(
       content::RenderFrameHost* frame,
