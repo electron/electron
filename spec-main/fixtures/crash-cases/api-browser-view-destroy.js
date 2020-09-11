4b498f87dba55d4f48c54e73ec26dc75e200aa7e
Michaela Laurencin <mlaurencin@microsoft.com>
2020-09-11 13:58:30 -0700

    Relocate crash test to proper location

---
+++ spec-main/fixtures/crash-cases/api-browser-view-destroy.js
@@ -0,0 +1,31 @@
+const {app, BrowserWindow, BrowserView} = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    },
+  })
+  const view = new BrowserView();
+  mainWindow.addBrowserView(view);
+  view.webContents.destroy()
+  //setTimeout(() => view.setBounds({ x: 0, y: 0, width: 300, height: 300 }), 10000) //Asynchronous Test
+  view.setBounds({ x: 0, y: 0, width: 300, height: 300 }) //Synchronous Test
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
