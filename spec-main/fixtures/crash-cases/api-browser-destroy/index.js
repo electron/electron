0e4a1f71e108adb3f43a67b9895743ea75bbef07
Michaela Laurencin <mlaurencin@microsoft.com>
2020-09-15 16:32:21 -0700

    Fix linting errors

--- spec-main/fixtures/crash-cases/api-browser-destroy/index.js
+++ spec-main/fixtures/crash-cases/api-browser-destroy/index.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow, BrowserView} = require('electron');
+const { app, BrowserWindow, BrowserView } = require('electron');
 const { expect } = require('chai');
 
 function createWindow () {
@@ -8,18 +8,18 @@ function createWindow () {
     height: 600,
     webPreferences: {
       nodeIntegration: true
-    },
-  })
+    }
+  });
   const view = new BrowserView();
   mainWindow.addBrowserView(view);
-  view.webContents.destroy()
-  view.setBounds({ x: 0, y: 0, width: 0, height: 0 })
-  const bounds = view.getBounds()
-  expect(bounds).to.deep.equal({ x: 0, y: 0, width: 0, height: 0 })
-  view.setBackgroundColor(1)
+  view.webContents.destroy();
+  view.setBounds({ x: 0, y: 0, width: 0, height: 0 });
+  const bounds = view.getBounds();
+  expect(bounds).to.deep.equal({ x: 0, y: 0, width: 0, height: 0 });
+  view.setBackgroundColor(1);
 }
 
 app.on('ready', () => {
   createWindow();
   setTimeout(() => app.quit());
-})
+});
