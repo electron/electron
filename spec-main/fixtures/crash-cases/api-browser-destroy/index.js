505bb8ac36d62926c3ddd0495c0f31b7196e554d
Michaela Laurencin <mlaurencin@microsoft.com>
2020-09-15 18:22:08 -0700

    Fix styling of test file

--- spec-main/fixtures/crash-cases/api-browser-destroy/index.js
+++ spec-main/fixtures/crash-cases/api-browser-destroy/index.js
@@ -8,7 +8,7 @@ function createWindow () {
     height: 600,
     webPreferences: {
       nodeIntegration: true
-    },
+    }
   });
   const view = new BrowserView();
   mainWindow.addBrowserView(view);
