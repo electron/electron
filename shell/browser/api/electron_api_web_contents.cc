ae97b87b529c81749f77e135fdddf93e4fcaac28
Samuel Attard <samuel.r.attard@gmail.com>
2020-09-18 09:14:03 -0700

    chore: fix lint

--- shell/browser/api/electron_api_web_contents.cc
+++ shell/browser/api/electron_api_web_contents.cc
@@ -22,8 +22,8 @@
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/ssl/security_state_tab_helper.h"
-#include "content/browser/renderer_host/frame_tree_node.h"             // nogncheck
-#include "content/browser/renderer_host/render_frame_host_manager.h"   // nogncheck
+#include "content/browser/renderer_host/frame_tree_node.h"  // nogncheck
+#include "content/browser/renderer_host/render_frame_host_manager.h"  // nogncheck
 #include "content/browser/renderer_host/render_widget_host_impl.h"  // nogncheck
 #include "content/browser/renderer_host/render_widget_host_view_base.h"  // nogncheck
 #include "content/common/widget_messages.h"
--- shell/browser/ui/x/x_window_utils.cc
+++ shell/browser/ui/x/x_window_utils.cc
@@ -20,9 +20,10 @@
 namespace electron {
 
 void SetWMSpecState(x11::Window window, bool enabled, x11::Atom state) {
-  ui::SendClientMessage(
-      window, ui::GetX11RootWindow(), gfx::GetAtom("_NET_WM_STATE"),
-      {enabled ? 1 : 0, static_cast<uint32_t>(state), static_cast<uint32_t>(x11::Window::None), 1, 0});
+  ui::SendClientMessage(window, ui::GetX11RootWindow(),
+                        gfx::GetAtom("_NET_WM_STATE"),
+                        {enabled ? 1 : 0, static_cast<uint32_t>(state),
+                         static_cast<uint32_t>(x11::Window::None), 1, 0});
 }
 
 void SetWindowType(x11::Window window, const std::string& type) {
