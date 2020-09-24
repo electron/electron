6b2dbfd74d0758822d8d93b096b19f80ba7afd23
Shelley Vohr <shelley.vohr@gmail.com>
2020-09-24 10:14:53 -0700

    fix: prevent destroyed view references from causing crashes

--- spec-main/fixtures/crash-cases/api-browser-destroy/index.js
+++ spec-main/fixtures/crash-cases/api-browser-destroy/index.js
@@ -15,13 +15,10 @@ function createWindow () {
   mainWindow.addBrowserView(view);
   view.webContents.destroy();
 
-  setTimeout(() => {
-    assert.strictEqual(view.webContents.isDestroyed, true, 'webContents has not been destroyed yet');
-    view.setBounds({ x: 0, y: 0, width: 0, height: 0 });
-    const bounds = view.getBounds();
-    expect(bounds).to.deep.equal({ x: 0, y: 0, width: 0, height: 0 });
-    view.setBackgroundColor('#56cc5b10');
-  }, 15000);
+  view.setBounds({ x: 0, y: 0, width: 0, height: 0 });
+  const bounds = view.getBounds();
+  expect(bounds).to.deep.equal({ x: 0, y: 0, width: 0, height: 0 });
+  view.setBackgroundColor('#56cc5b10');
 }
 
 app.on('ready', () => {
