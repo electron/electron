f1568cd3e476c5fc35e8f9be7dc3824be3945de2
Shelley Vohr <shelley.vohr@gmail.com>
2020-09-30 09:47:58 -0700

    [XProto] Remove usage of Xlib Visuals
    https://chromium-review.googlesource.com/c/chromium/src/+/2429933

--- shell/browser/ui/x/x_window_utils.cc
+++ shell/browser/ui/x/x_window_utils.cc
@@ -4,9 +4,6 @@
 
 #include "shell/browser/ui/x/x_window_utils.h"
 
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-
 #include <memory>
 
 #include "base/environment.h"
@@ -86,9 +83,8 @@ void MoveWindowAbove(x11::Window window, x11::Window other_window) {
 }
 
 bool IsWindowValid(x11::Window window) {
-  XWindowAttributes attrs;
-  return XGetWindowAttributes(gfx::GetXDisplay(), static_cast<uint32_t>(window),
-                              &attrs);
+  auto* conn = x11::Connection::Get();
+  return conn->GetWindowAttributes({window}).Sync();
 }
 
 }  // namespace electron
