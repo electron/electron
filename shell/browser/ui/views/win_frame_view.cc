da5da8ad5b813ab86f7f3b0eadff2f9fffa8e41c
Cheng Zhao <zcbenz@gmail.com>
2020-10-14 12:15:59 +0100

    fix: maximized frameless window bleeding to other monitors

--- shell/browser/ui/views/win_frame_view.cc
+++ shell/browser/ui/views/win_frame_view.cc
@@ -6,28 +6,10 @@
 
 #include "base/win/windows_version.h"
 #include "shell/browser/native_window_views.h"
-#include "ui/display/win/screen_win.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/win/hwnd_util.h"
 
 namespace electron {
-
-namespace {
-
-gfx::Insets GetGlassInsets() {
-  int frame_height =
-      display::win::ScreenWin::GetSystemMetricsInDIP(SM_CYSIZEFRAME) +
-      display::win::ScreenWin::GetSystemMetricsInDIP(SM_CXPADDEDBORDER);
-
-  int frame_size =
-      base::win::GetVersion() < base::win::Version::WIN8
-          ? display::win::ScreenWin::GetSystemMetricsInDIP(SM_CXSIZEFRAME)
-          : 0;
-
-  return gfx::Insets(frame_height, frame_size, frame_size, frame_size);
-}
-
-}  // namespace
 
 const char WinFrameView::kViewClassName[] = "WinFrameView";
 
@@ -40,17 +22,6 @@ gfx::Rect WinFrameView::GetWindowBoundsForClientBounds(
   return views::GetWindowBoundsForClientBounds(
       static_cast<views::View*>(const_cast<WinFrameView*>(this)),
       client_bounds);
-}
-
-gfx::Rect WinFrameView::GetBoundsForClientView() const {
-  if (window_->IsMaximized() && !window_->has_frame()) {
-    gfx::Insets insets = GetGlassInsets();
-    gfx::Rect result(width(), height());
-    result.Inset(insets);
-    return result;
-  } else {
-    return bounds();
-  }
 }
 
 int WinFrameView::NonClientHitTest(const gfx::Point& point) {
