d4112d38de267f9eb0b62f1af9619da251ab31a0
David Sanders <dsanders11@ucsbalum.com>
2020-10-26 11:48:12 -0700

    chore: prefer empty() check for readability (#26157)

--- shell/renderer/api/electron_api_context_bridge.cc
+++ shell/renderer/api/electron_api_context_bridge.cc
@@ -564,7 +564,7 @@ void OverrideGlobalValueFromIsolatedWorld(
     const std::vector<std::string>& key_path,
     v8::Local<v8::Object> value,
     bool support_dynamic_properties) {
-  if (key_path.size() == 0)
+  if (key_path.empty())
     return;
 
   auto* render_frame = GetRenderFrame(value);
@@ -596,7 +596,7 @@ bool OverrideGlobalPropertyFromIsolatedWorld(
     v8::Local<v8::Object> getter,
     v8::Local<v8::Value> setter,
     gin_helper::Arguments* args) {
-  if (key_path.size() == 0)
+  if (key_path.empty())
     return false;
 
   auto* render_frame = GetRenderFrame(getter);
