4f20c36b2db5e25144591aab01d0e1a7458a0694
Shelley Vohr <shelley.vohr@gmail.com>
2020-10-27 11:42:12 -0700

    Remove usage of deprecated form of ExtensionFunction::OneArgument()
    https://chromium-review.googlesource.com/c/chromium/src/+/2473502

--- shell/browser/extensions/api/resources_private/resources_private_api.cc
+++ shell/browser/extensions/api/resources_private/resources_private_api.cc
@@ -107,7 +107,8 @@ ExtensionFunction::ResponseAction ResourcesPrivateGetStringsFunction::Run() {
   const std::string& app_locale = g_browser_process->GetApplicationLocale();
   webui::SetLoadTimeDataDefaults(app_locale, dict.get());
 
-  return RespondNow(OneArgument(std::move(dict)));
+  return RespondNow(
+      OneArgument(base::Value::FromUniquePtrValue(std::move(dict))));
 }
 
 }  // namespace extensions
