6b759f0da651a0fa82e6b7ace023ff97bdd124c3
Cheng Zhao <zcbenz@gmail.com>
2020-11-05 21:39:53 +0900

    fix: make draggable region work when devtools is open

--- shell/browser/ui/cocoa/electron_inspectable_web_contents_view.mm
+++ shell/browser/ui/cocoa/electron_inspectable_web_contents_view.mm
@@ -11,6 +11,18 @@
 #include "shell/browser/ui/inspectable_web_contents_view_mac.h"
 #include "ui/gfx/mac/scoped_cocoa_disable_screen_updates.h"
 
+@implementation ControlRegionView
+
+- (BOOL)mouseDownCanMoveWindow {
+  return NO;
+}
+
+- (NSView*)hitTest:(NSPoint)aPoint {
+  return nil;
+}
+
+@end
+
 @implementation ElectronInspectableWebContentsView
 
 - (instancetype)initWithInspectableWebContentsViewMac:
@@ -48,6 +60,9 @@
     [self addSubview:contentsView];
   }
 
+  // This will float above devtools to exclude it from dragging.
+  devtools_mask_.reset([[ControlRegionView alloc] initWithFrame:NSZeroRect]);
+
   // See https://code.google.com/p/chromium/issues/detail?id=348490.
   [self setWantsLayer:YES];
 
@@ -84,6 +99,12 @@
   devtools_visible_ = visible;
   if (devtools_docked_) {
     if (visible) {
+      // The devToolsView is placed under the contentsView, so it has to be
+      // draggable to make draggable region of contentsView work.
+      [devToolsView setMouseDownCanMoveWindow:YES];
+      // This view will exclude the actual devtools part from dragging.
+      [self addSubview:devtools_mask_.get()];
+
       // Place the devToolsView under contentsView, notice that we didn't set
       // sizes for them until the setContentsResizingStrategy message.
       [self addSubview:devToolsView positioned:NSWindowBelow relativeTo:nil];
@@ -94,6 +115,7 @@
     } else {
       gfx::ScopedCocoaDisableScreenUpdates disabler;
       [devToolsView removeFromSuperview];
+      [devtools_mask_ removeFromSuperview];
       [self adjustSubviews];
     }
   } else {
@@ -182,7 +204,7 @@
   NSView* devToolsView = [[self subviews] objectAtIndex:0];
   NSView* contentsView = [[self subviews] objectAtIndex:1];
 
-  DCHECK_EQ(2u, [[self subviews] count]);
+  DCHECK_EQ(3u, [[self subviews] count]);
 
   gfx::Rect new_devtools_bounds;
   gfx::Rect new_contents_bounds;
@@ -191,6 +213,26 @@
       &new_devtools_bounds, &new_contents_bounds);
   [devToolsView setFrame:[self flipRectToNSRect:new_devtools_bounds]];
   [contentsView setFrame:[self flipRectToNSRect:new_contents_bounds]];
+
+  // Move mask to the devtools area to exclude it from dragging.
+  NSRect cf = contentsView.frame;
+  NSRect sb = [self bounds];
+  NSRect devtools_frame;
+  if (cf.size.height < sb.size.height) {  // bottom docked
+    devtools_frame.origin.x = 0;
+    devtools_frame.origin.y = 0;
+    devtools_frame.size.width = sb.size.width;
+    devtools_frame.size.height = sb.size.height - cf.size.height;
+  } else {                // left or right docked
+    if (cf.origin.x > 0)  // left docked
+      devtools_frame.origin.x = 0;
+    else  // right docked.
+      devtools_frame.origin.x = cf.size.width;
+    devtools_frame.origin.y = 0;
+    devtools_frame.size.width = sb.size.width - cf.size.width;
+    devtools_frame.size.height = sb.size.height;
+  }
+  [devtools_mask_ setFrame:devtools_frame];
 }
 
 - (void)setTitle:(NSString*)title {
