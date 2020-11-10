ffd4a2037874dadd7564b067ce751788bc36e040
Shelley Vohr <shelley.vohr@gmail.com>
2020-11-10 09:17:58 -0800

    fix: renderer crash on setImmediate

---
+++ spec-main/fixtures/crash-cases/setimmediate-renderer-crash/index.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+app.whenReady().then(() => {
+  const win = new BrowserWindow({
+    show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.resolve(__dirname, 'preload.js')
+    }
+  });
+
+  win.loadURL('about:blank');
+
+  win.webContents.on('render-process-gone', () => {
+    process.exit(1);
+  });
+
+  win.webContents.on('did-finish-load', () => {
+    setTimeout(() => app.quit());
+  });
+});
