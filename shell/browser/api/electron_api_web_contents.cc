655435fd12c8aafdf8fd322d27008f37c7f718a4
Jeremy Rose <nornagon@nornagon.net>
2020-11-25 14:06:05 -0800

    fix: uaf in WebContents::DidStopLoading

--- shell/browser/api/electron_api_web_contents.cc
+++ shell/browser/api/electron_api_web_contents.cc
@@ -1447,12 +1447,12 @@ void WebContents::DidStartLoading() {
 }
 
 void WebContents::DidStopLoading() {
-  Emit("did-stop-loading");
-
   auto* web_preferences = WebContentsPreferences::From(web_contents());
   if (web_preferences &&
       web_preferences->IsEnabled(options::kEnablePreferredSizeMode))
     web_contents()->GetRenderViewHost()->EnablePreferredSizeMode();
+
+  Emit("did-stop-loading");
 }
 
 bool WebContents::EmitNavigationEvent(
