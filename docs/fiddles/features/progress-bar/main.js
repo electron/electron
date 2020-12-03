de12232df542e66db1f353036f85bb1475e807be
Erick Zhao <erick@hotmail.ca>
2020-12-02 22:54:49 -0800

    docs: added fiddle support for code samples (#26766)
    Co-authored-by: Antonio <bandantonio@users.noreply.github.com>

---
+++ docs/fiddles/features/progress-bar/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+  win.setProgressBar(0.5)
+}
+
+
+app.whenReady().then(createWindow)
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
