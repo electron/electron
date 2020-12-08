c9b813a1f9ae9b6211c5bf338812997113d1c465
Milan Burda <milan.burda@gmail.com>
2020-12-08 05:39:33 +0100

    refactor: convert more C++ enums to C++11 enum classes (#26850)

--- shell/browser/api/electron_api_system_preferences.h
+++ shell/browser/api/electron_api_system_preferences.h
@@ -26,7 +26,7 @@ namespace electron {
 namespace api {
 
 #if defined(OS_MAC)
-enum NotificationCenterKind {
+enum class NotificationCenterKind {
   kNSDistributedNotificationCenter = 0,
   kNSNotificationCenter,
   kNSWorkspaceNotificationCenter,
