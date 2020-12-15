a3bc2f03c04d74b5835cbd22eda1ac125bc68e34
Samuel Attard <samuel.r.attard@gmail.com>
2020-12-15 15:33:09 -0800

    fix: refactor X11 event handling
    Refs: https://chromium-review.googlesource.com/c/chromium/src/+/2577887
    Refs: https://chromium-review.googlesource.com/c/chromium/src/+/2585750

--- shell/browser/ui/x/window_state_watcher.h
+++ shell/browser/ui/x/window_state_watcher.h
@@ -19,18 +19,14 @@ class WindowStateWatcher : public ui::XEventObserver {
 
  protected:
   // ui::XEventObserver:
-  void WillProcessXEvent(x11::Event* x11_event) override;
-  void DidProcessXEvent(x11::Event* x11_event) override;
+  void OnEvent(const x11::Event& x11_event) override;
 
  private:
-  bool IsWindowStateEvent(x11::Event* x11_event) const;
+  bool IsWindowStateEvent(const x11::Event& x11_event) const;
 
   NativeWindowViews* window_;
   gfx::AcceleratedWidget widget_;
   const x11::Atom window_state_atom_;
-
-  bool was_minimized_ = false;
-  bool was_maximized_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(WindowStateWatcher);
 };
