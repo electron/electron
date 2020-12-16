21eefe9970c210fba55b296d71675c651f327b44
Samuel Attard <samuel.r.attard@gmail.com>
2020-12-16 14:58:57 -0800

    chore: DidProcessXEvent has been removed

--- shell/browser/ui/x/window_state_watcher.cc
+++ shell/browser/ui/x/window_state_watcher.cc
@@ -3,6 +3,9 @@
 // found in the LICENSE file.
 
 #include "shell/browser/ui/x/window_state_watcher.h"
+
+#include <vector>
+
 #include "ui/base/x/x11_util.h"
 #include "ui/gfx/x/x11_atom_cache.h"
 
@@ -25,13 +28,21 @@ void WindowStateWatcher::OnEvent(const x11::Event& x11_event) {
     bool was_maximized_ = window_->IsMaximized();
 
     std::vector<x11::Atom> wm_states;
-    
-    if (ui::GetAtomArrayProperty(static_cast<x11::Window>(window_->GetAcceleratedWidget()),
-                             "_NET_WM_STATE", &wm_states)) {
-      auto props = base::flat_set<x11::Atom>(std::begin(wm_states), std::end(wm_states));
-      bool is_minimized = props.find(gfx::GetAtom("_NET_WM_STATE_HIDDEN")) != props.end();
-      bool is_maximized = props.find(gfx::GetAtom("_NET_WM_STATE_MAXIMIZED_VERT")) != props.end() && props.find(gfx::GetAtom("_NET_WM_STATE_MAXIMIZED_HORZ")) != props.end();
-      bool is_fullscreen = props.find(gfx::GetAtom("_NET_WM_STATE_FULLSCREEN")) != props.end();
+
+    if (ui::GetAtomArrayProperty(
+            static_cast<x11::Window>(window_->GetAcceleratedWidget()),
+            "_NET_WM_STATE", &wm_states)) {
+      auto props =
+          base::flat_set<x11::Atom>(std::begin(wm_states), std::end(wm_states));
+      bool is_minimized =
+          props.find(gfx::GetAtom("_NET_WM_STATE_HIDDEN")) != props.end();
+      bool is_maximized =
+          props.find(gfx::GetAtom("_NET_WM_STATE_MAXIMIZED_VERT")) !=
+              props.end() &&
+          props.find(gfx::GetAtom("_NET_WM_STATE_MAXIMIZED_HORZ")) !=
+              props.end();
+      bool is_fullscreen =
+          props.find(gfx::GetAtom("_NET_WM_STATE_FULLSCREEN")) != props.end();
 
       if (is_minimized != was_minimized_) {
         if (is_minimized)
@@ -47,8 +58,8 @@ void WindowStateWatcher::OnEvent(const x11::Event& x11_event) {
         // If this is neither a "maximize" or "minimize" event, then we think it
         // is a "fullscreen" event.
         // The "IsFullscreen()" becomes true immediately before "OnEvent"
-        // is called, so we can not handle this like "maximize" and "minimize" by
-        // watching whether they have changed.
+        // is called, so we can not handle this like "maximize" and "minimize"
+        // by watching whether they have changed.
         if (is_fullscreen)
           window_->NotifyWindowEnterFullScreen();
         else
@@ -57,12 +68,6 @@ void WindowStateWatcher::OnEvent(const x11::Event& x11_event) {
     }
   }
 }
-
-// void WindowStateWatcher::DidProcessXEvent(x11::Event* x11_event) {
-//   if (IsWindowStateEvent(x11_event)) {
-    
-//   }
-// }
 
 bool WindowStateWatcher::IsWindowStateEvent(const x11::Event& x11_event) const {
   auto* property = x11_event.As<x11::PropertyNotifyEvent>();
