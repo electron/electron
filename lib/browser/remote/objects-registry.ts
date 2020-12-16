4143e75a7007a62f7f9c42f997c218d8772d0ad1
Milan Burda <milan.burda@gmail.com>
2020-12-16 02:19:35 +0100

    refactor: store WeakMaps in CallbacksRegistry / ObjectsRegistry

--- lib/browser/remote/objects-registry.ts
+++ lib/browser/remote/objects-registry.ts
@@ -4,8 +4,6 @@ const getOwnerKey = (webContents: WebContents, contextId: string) => {
   return `${webContents.id}-${contextId}`;
 };
 
-const electronIds = new WeakMap<Object, number>();
-
 class ObjectsRegistry {
   private nextId: number = 0
 
@@ -16,6 +14,8 @@ class ObjectsRegistry {
   // Stores the IDs + refCounts of objects referenced by WebContents.
   // (ownerKey) => { id: refCount }
   private owners: Record<string, Map<number, number>> = {}
+
+  private electronIds = new WeakMap<Object, number>();
 
   // Register a new object and return its assigned ID. If the object is already
   // registered then the already assigned ID would be returned.
@@ -81,14 +81,14 @@ class ObjectsRegistry {
 
   // Private: Saves the object into storage and assigns an ID for it.
   saveToStorage (object: any) {
-    let id = electronIds.get(object);
+    let id = this.electronIds.get(object);
     if (!id) {
       id = ++this.nextId;
       this.storage[id] = {
         count: 0,
         object: object
       };
-      electronIds.set(object, id);
+      this.electronIds.set(object, id);
     }
     return id;
   }
@@ -101,7 +101,7 @@ class ObjectsRegistry {
     }
     pointer.count -= 1;
     if (pointer.count === 0) {
-      electronIds.delete(pointer.object);
+      this.electronIds.delete(pointer.object);
       delete this.storage[id];
     }
   }
