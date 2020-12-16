4bd3097388cb3b2f35ec69e1a83115cb410e5d01
Milan Burda <milan.burda@gmail.com>
2020-12-16 01:58:58 +0100

    Update lib/common/property-utils.ts
    Co-authored-by: Samuel Attard <sam@electronjs.org>

--- lib/common/property-utils.ts
+++ lib/common/property-utils.ts
@@ -1,4 +1,4 @@
-export function defineProperty(target: Object, propertyName: string, getter: () => any, setter?: (value: any) => void) {
+export function defineProperty<T extends object, K extends keyof T>(target: T, propertyName: K, getter: () => T[K], setter?: (value: T[K]) => void) {
   Object.defineProperty(target, propertyName, {
     get: () => getter.call(target),
     set: setter ? (value) => setter.call(target, value) : undefined
