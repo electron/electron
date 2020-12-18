4c1c674d58f08d5d290fca76cf0d61caccff6d3c
Tristan Partin <tristan@partin.io>
2020-12-18 10:10:38 -0600

    Remove unused header

--- shell/browser/ui/file_dialog_gtk.cc
+++ shell/browser/ui/file_dialog_gtk.cc
@@ -15,7 +15,6 @@
 #include "shell/browser/ui/gtk_util.h"
 #include "shell/browser/unresponsive_suppressor.h"
 #include "shell/common/gin_converters/file_path_converter.h"
-#include "ui/aura/window.h"
 #include "ui/base/glib/glib_signal.h"
 #include "ui/gtk/gtk_util.h"
 
