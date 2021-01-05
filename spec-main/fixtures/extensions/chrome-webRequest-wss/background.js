700c410ec3b5451bd6971556c5e6ccef216c51c0
Eryk Rakowski <sentialx@gmail.com>
2021-01-05 03:40:36 +0100

    fix(extensions): implement missing web_request hooks (#27097)
    * fix(extensions): implement missing web_request hooks (#22655)
    
    Co-authored-by: Jeremy Apthorp <nornagon@nornagon.net>
    Co-authored-by: samuelmaddock <samuel.maddock@gmail.com>
    
    * fix: remove ukm_source_id
    
    Co-authored-by: Jeremy Apthorp <nornagon@nornagon.net>
    Co-authored-by: samuelmaddock <samuel.maddock@gmail.com>

---
+++ spec-main/fixtures/extensions/chrome-webRequest-wss/background.js
@@ -0,0 +1,12 @@
+/* global chrome */
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  (details) => {
+    if (details.requestHeaders) {
+      details.requestHeaders.foo = 'bar';
+    }
+    return { cancel: false, requestHeaders: details.requestHeaders };
+  },
+  { urls: ['*://127.0.0.1:*'] },
+  ['blocking']
+);
