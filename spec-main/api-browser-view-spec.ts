c0b48658d8abf83d7bd6d49ba0bbd92257ca4984
Shelley Vohr <shelley.vohr@gmail.com>
2021-01-07 11:43:46 -0800

    fix: handle BrowserView reparenting (#27219)

--- spec-main/api-browser-view-spec.ts
+++ spec-main/api-browser-view-spec.ts
@@ -158,6 +158,27 @@ describe('BrowserView module', () => {
         w.addBrowserView(view1);
       }).to.not.throw();
     });
+
+    it('can handle BrowserView reparenting', async () => {
+      view = new BrowserView();
+
+      w.addBrowserView(view);
+      view.webContents.loadURL('about:blank');
+      await emittedOnce(view.webContents, 'did-finish-load');
+
+      const w2 = new BrowserWindow({ show: false });
+      w2.addBrowserView(view);
+
+      w.close();
+
+      view.webContents.loadURL(`file://${fixtures}/pages/blank.html`);
+      await emittedOnce(view.webContents, 'did-finish-load');
+
+      // Clean up - the afterEach hook assumes the webContents on w is still alive.
+      w = new BrowserWindow({ show: false });
+      w2.close();
+      w2.destroy();
+    });
   });
 
   describe('BrowserWindow.removeBrowserView()', () => {
