159a80d2d2c80800fbedc6d77a999282e84eb943
mlaurencin <mlaurencin@electronjs.org>
2021-01-26 11:18:40 -0800

    modify frameName object property test

--- spec-main/chromium-spec.ts
+++ spec-main/chromium-spec.ts
@@ -748,8 +748,10 @@ describe('chromium features', () => {
       const w = new BrowserWindow({ show: false });
       w.loadURL('about:blank');
       w.webContents.executeJavaScript('{ b = window.open(\'\', \'__proto__\'); null }');
-      const [, window] = await emittedOnce(app, 'browser-window-created');
-      expect(window.getTitle()).to.equal('__proto__');
+      const [, , frameName, , test] = await emittedOnce(w.webContents, 'new-window');
+
+      expect(frameName).to.equal('__proto__');
+      console.log(test);
     });
 
     it('denies custom open when nativeWindowOpen: true', async () => {
