5e7d127eb17e05e36a118213404fb1535e75f4fd
Cheng Zhao <zcbenz@gmail.com>
2021-02-16 10:53:25 +0900

    test: enable did-change-theme-color test for WOA

--- spec-main/api-web-contents-spec.ts
+++ spec-main/api-web-contents-spec.ts
@@ -1383,8 +1383,7 @@ describe('webContents module', () => {
     }
   });
 
-  // TODO (jkleinsc) - reenable this test on WOA once https://github.com/electron/electron/issues/26045 is resolved
-  ifdescribe(process.platform !== 'win32' || process.arch !== 'arm64')('did-change-theme-color event', () => {
+  describe('did-change-theme-color event', () => {
     afterEach(closeAllWindows);
     it('is triggered with correct theme color', (done) => {
       const w = new BrowserWindow({ show: true });
