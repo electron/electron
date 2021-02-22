2063c446c4155e30ba65c7d80eaf2647d5fc901e
Tony <TonyWuu@users.noreply.github.com>
2021-02-22 10:27:09 -0800

    Update lib/browser/api/base-window.ts
    Co-authored-by: John Kleinschmidt <jkleinsc@github.com>

--- lib/browser/api/base-window.ts
+++ lib/browser/api/base-window.ts
@@ -43,7 +43,7 @@ Object.defineProperty(BaseWindow.prototype, 'kiosk', {
 });
 
 Object.defineProperty(BaseWindow.prototype, 'documentEdited', {
-  get: function () { return this.isFullScreen(); },
+  get: function () { return this.isDocumentEdited(); },
   set: function (edited) { this.setDocumentEdited(edited); }
 });
 
