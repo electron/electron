2e091d401e1636bb91e80c9eaa09d82d15fda66e
Jeremy Rose <jeremya@chromium.org>
2021-03-03 16:18:07 -0800

    docs: update fiddles/quick-start to set contextIsolation: false (#27970)

--- docs/fiddles/quick-start/main.js
+++ docs/fiddles/quick-start/main.js
@@ -5,7 +5,8 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false
     }
   })
 
