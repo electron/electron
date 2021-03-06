5960fa00edb47212f4de06c90bd29aaa33c0e4e9
Samuel Attard <samuel.r.attard@gmail.com>
2021-03-05 16:00:59 -0800

    refacotr: grit::ResourceMap replaced with webui::ResourcePath
    Refs: https://chromium-review.googlesource.com/c/chromium/src/+/2685601
    (cherry picked from commit 59669e99cbfe98458dc314a5ebd55dccd5a7c6e8)

--- shell/browser/extensions/electron_component_extension_resource_manager.h
+++ shell/browser/extensions/electron_component_extension_resource_manager.h
@@ -13,6 +13,7 @@
 #include "base/files/file_path.h"
 #include "base/macros.h"
 #include "extensions/browser/component_extension_resource_manager.h"
+#include "ui/base/webui/resource_path.h"
 
 struct GritResourceMap;
 
@@ -32,7 +33,7 @@ class ElectronComponentExtensionResourceManager
       const std::string& extension_id) const override;
 
  private:
-  void AddComponentResourceEntries(const GritResourceMap* entries, size_t size);
+  void AddComponentResourceEntries(const webui::ResourcePath* entries, size_t size);
 
   // A map from a resource path to the resource ID.  Used by
   // IsComponentExtensionResource.
