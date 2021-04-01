145d399c48e1b91aea5cb0c29d35ee2ed4bbedea
Charles Kerr <charles@charleskerr.com>
2021-03-31 19:51:39 -0500

    refactor: remove routing_id from CreateLoaderAndStart
    Refs: https://chromium-review.googlesource.com/c/chromium/src/+/2762858
    
    NB: One logic branch in ProxyingURLLoaderFactory::CreateLoaderAndStart
    calls std::make_unique<InProgressRequest>, which needs a routing_id.
    This PR uses the member field `routing_id_` since there's no longer one
    being passed into CreateLoaderAndStart.
    
    (cherry picked from commit 70759ad3424ba94559dc2a88f68b4d76d22bd137)

--- shell/browser/electron_browser_client.cc
+++ shell/browser/electron_browser_client.cc
@@ -1334,7 +1334,6 @@ class FileURLLoaderFactory : public network::SelfDeletingURLLoaderFactory {
   // network::mojom::URLLoaderFactory:
   void CreateLoaderAndStart(
       mojo::PendingReceiver<network::mojom::URLLoader> loader,
-      int32_t routing_id,
       int32_t request_id,
       uint32_t options,
       const network::ResourceRequest& request,
