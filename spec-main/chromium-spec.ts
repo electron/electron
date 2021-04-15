95d45033adb0a1f8d380f05eb2eff675453d6541
Samuel Attard <samuel.r.attard@gmail.com>
2021-04-15 14:36:20 -0700

    chore: clean up some spec things

--- spec-main/chromium-spec.ts
+++ spec-main/chromium-spec.ts
@@ -1454,7 +1454,13 @@ describe('font fallback', () => {
     const fonts = await getRenderedFonts(html);
     expect(fonts).to.be.an('array');
     expect(fonts).to.have.length(1);
-    if (process.platform === 'win32') { expect(fonts[0].familyName).to.equal('Arial'); } else if (process.platform === 'darwin') { expect(fonts[0].familyName).to.equal('Helvetica'); } else if (process.platform === 'linux') { expect(fonts[0].familyName).to.equal('DejaVu Sans'); } // I think this depends on the distro? We don't specify a default.
+    if (process.platform === 'win32') {
+      expect(fonts[0].familyName).to.equal('Arial');
+    } else if (process.platform === 'darwin') {
+      expect(fonts[0].familyName).to.equal('Helvetica');
+    } else if (process.platform === 'linux') {
+      expect(fonts[0].familyName).to.equal('DejaVu Sans');
+    } // I think this depends on the distro? We don't specify a default.
   });
 
   ifit(process.platform !== 'linux')('should fall back to Japanese font for sans-serif Japanese script', async function () {
