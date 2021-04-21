02eb79e2127966732e892f23d8c8bb255106719b
Jeremy Rose <jeremya@chromium.org>
2021-04-21 10:37:15 -0700

    Update spec-main/api-shell-spec.ts

--- spec-main/api-shell-spec.ts
+++ spec-main/api-shell-spec.ts
@@ -80,7 +80,7 @@ describe('shell module', () => {
     it('works in the renderer process', async () => {
       const w = new BrowserWindow({ show: false, webPreferences: { nodeIntegration: true, contextIsolation: false } });
       w.loadURL('about:blank');
-      await expect(w.webContents.executeJavaScript('require(\'electron\').shell.trashItem(\'does-not-exist\')')).to.be.rejectedWith(/does-not-exist|Failed to move item/);
+      await expect(w.webContents.executeJavaScript('require(\'electron\').shell.trashItem(\'does-not-exist\')')).to.be.rejectedWith(/does-not-exist|Failed to move item|Failed to create FileOperation/);
     });
   });
 });
