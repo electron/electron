e5c1064e2d2b72ce81850492986b29035e138747
Shelley Vohr <shelley.vohr@gmail.com>
2021-05-10 22:51:25 +0200

    fix: illegal access errors with nodeIntegrationInSubFrames

--- shell/renderer/electron_sandboxed_renderer_client.cc
+++ shell/renderer/electron_sandboxed_renderer_client.cc
@@ -198,8 +198,13 @@ void ElectronSandboxedRendererClient::DidCreateScriptContext(
   bool is_main_frame = render_frame->IsMainFrame();
   bool is_devtools =
       IsDevTools(render_frame) || IsDevToolsExtension(render_frame);
+
+  // See comment in ElectronRendererClient::DidCreateScriptContext.
+  GURL url = render_frame->GetWebFrame()->GetDocument().Url();
   bool allow_node_in_sub_frames =
-      render_frame->GetBlinkPreferences().node_integration_in_sub_frames;
+      render_frame->GetBlinkPreferences().node_integration_in_sub_frames &&
+      !url.IsAboutBlank();
+
   bool should_load_preload =
       (is_main_frame || is_devtools || allow_node_in_sub_frames) &&
       !IsWebViewFrame(context, render_frame);
