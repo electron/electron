c13a523a792bb714bb049257d06c4cd0e6573de7
Jeremy Rose <jeremya@chromium.org>
2021-05-12 12:03:18 -0700

    chore: remove no-op EnableWebComponentsV0 feature (#29107)

--- shell/browser/feature_list.cc
+++ shell/browser/feature_list.cc
@@ -38,12 +38,6 @@ void InitializeFeatureList() {
       std::string(",") +
       net::features::kCookiesWithoutSameSiteMustBeSecure.name;
 
-  // https://www.polymer-project.org/blog/2018-10-02-webcomponents-v0-deprecations
-  // https://chromium-review.googlesource.com/c/chromium/src/+/1869562
-  // Any website which uses older WebComponents will fail in without this
-  // enabled, since Electron does not support origin trials.
-  enable_features += std::string(",") + "WebComponentsV0Enabled";
-
 #if !BUILDFLAG(ENABLE_PICTURE_IN_PICTURE)
   disable_features += std::string(",") + media::kPictureInPicture.name;
 #endif
