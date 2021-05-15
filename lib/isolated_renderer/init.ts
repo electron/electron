c68c65f383f7b210c2ad93715dd70dbb9dcb4798
Milan Burda <milan.burda@gmail.com>
2021-05-15 09:42:07 +0200

    refactor: implement <webview> using contextBridge (#29037)
    * refactor: implement <webview> using contextBridge
    
    * chore: address PR feedback
    
    * chore: address PR feedback
    
    * fix: check for HTMLIFrameElement instance in attachGuest

--- lib/isolated_renderer/init.ts
+++ lib/isolated_renderer/init.ts
@@ -1,15 +1,9 @@
-/* global nodeProcess, isolatedWorld */
+/* global isolatedApi */
 
 import type * as webViewElementModule from '@electron/internal/renderer/web-view/web-view-element';
 
-process._linkedBinding = nodeProcess._linkedBinding;
-
-const v8Util = process._linkedBinding('electron_common_v8_util');
-
-const webViewImpl = v8Util.getHiddenValue(isolatedWorld, 'web-view-impl');
-
-if (webViewImpl) {
+if (isolatedApi.guestViewInternal) {
   // Must setup the WebView element in main world.
   const { setupWebView } = require('@electron/internal/renderer/web-view/web-view-element') as typeof webViewElementModule;
-  setupWebView(webViewImpl as any);
+  setupWebView(isolatedApi);
 }
