b5d57cb8cd5f24a735452e0698aaf4854145d846
Tony Ferrell <anf@microsoft.com>
2021-05-17 16:20:44 -0700

    Remove preload and ;'s

--- docs/fiddles/features/macos-dock-menu/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge } = require('electron');
-
-contextBridge.exposeInMainWorld('versions',
-{
-    'chrome': process.versions.chrome,
-    'electron': process.versions.electron,
-    'node': process.versions.node,
-});
