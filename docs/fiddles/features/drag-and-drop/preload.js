801d2575a426e3df7807dace6ab51298426f3dea
Tony Ferrell <anf@microsoft.com>
2021-05-17 17:05:06 -0700

    Working

--- docs/fiddles/features/drag-and-drop/preload.js
+++ docs/fiddles/features/drag-and-drop/preload.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 contextBridge.exposeInMainWorld('electron', {
   startDrag: (fileName) => {
+    // Create a new file to copy - you can copy something 
     fs.writeFileSync(fileName, '# Test drag and drop');
     ipcRenderer.send('ondragstart', process.cwd() + `/${fileName}`)
   },
