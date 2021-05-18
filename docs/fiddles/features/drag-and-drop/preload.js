b184d4c84bad9c38e7fde9153b11d921d56d8bf7
Tony Ferrell <anf@microsoft.com>
2021-05-17 20:58:04 -0700

    Use more path.join instead of interpolation

--- docs/fiddles/features/drag-and-drop/preload.js
+++ docs/fiddles/features/drag-and-drop/preload.js
@@ -1,11 +1,12 @@
 const { contextBridge, ipcRenderer } = require('electron')
-const fs = require('fs').promises;
+const fs = require('fs').promises
+const path = require('path')
 
 contextBridge.exposeInMainWorld('electron', {
   startDrag: async (fileName) => {
     // Create a new file to copy - you can also copy existing files.
     await fs.writeFile(fileName, '# Test drag and drop')
 
-    ipcRenderer.send('ondragstart', process.cwd() + `/${fileName}`)
+    ipcRenderer.send('ondragstart', path.join(process.cwd(), fileName))
   }
 })
