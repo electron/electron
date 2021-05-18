9b3404f4c086a7fa51f5f962f5b97d1f6586a2dc
Jeremy Foster <jeremy.foster@live.com>
2021-05-18 10:54:36 -0700

    improve progress bar fiddle

--- docs/fiddles/features/progress-bar/main.js
+++ docs/fiddles/features/progress-bar/main.js
@@ -10,7 +10,18 @@ function createWindow () {
   })
 
   win.loadFile('index.html')
-  win.setProgressBar(0.5)
+  
+  const INCREMENT = 0.02;
+  const INTERVAL_DELAY = 100; //ms
+  let c = 0;
+  let interval = setInterval(() => {
+    win.setProgressBar(c);
+    if(c > 1) {
+      win.setProgressBar(-1);
+      clearTimeout(interval);
+    }
+    c += INCREMENT;
+  }, INTERVAL_DELAY);
 }
 
 
