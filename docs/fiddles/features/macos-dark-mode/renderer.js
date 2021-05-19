5656493676763b37c0f94e7afc09b0cdd3053ba8
Ethan Arrowood <ethan@arrowood.dev>
2021-05-18 19:02:12 -0600

    docs: update macos-dark-mode fiddle and guide content (#29198)
    * update macos dark mode docs for Electron v12
    
    * pr review fixes
    
    * more pr review fixes
    
    * reorg paragraphs for better flow
    
    * Update docs/tutorial/dark-mode.md
    
    Co-authored-by: Erick Zhao <erick@hotmail.ca>
    
    * pr fixes
    
    Co-authored-by: Erick Zhao <erick@hotmail.ca>

--- docs/fiddles/features/macos-dark-mode/renderer.js
+++ docs/fiddles/features/macos-dark-mode/renderer.js
@@ -1,11 +1,9 @@
-const { ipcRenderer } = require('electron')
-
 document.getElementById('toggle-dark-mode').addEventListener('click', async () => {
-  const isDarkMode = await ipcRenderer.invoke('dark-mode:toggle')
+  const isDarkMode = await window.darkMode.toggle()
   document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
 })
 
 document.getElementById('reset-to-system').addEventListener('click', async () => {
-  await ipcRenderer.invoke('dark-mode:system')
+  await window.darkMode.system()
   document.getElementById('theme-source').innerHTML = 'System'
 })
