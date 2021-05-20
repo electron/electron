ba4e80eb6b115e38780a427e2d3760e7164ba3f6
Jeremy Foster <jeremy.foster@live.com>
2021-05-20 11:26:57 -0700

    format code and update readme

--- docs/fiddles/features/notifications/main/main.js
+++ docs/fiddles/features/notifications/main/main.js
@@ -9,12 +9,11 @@ function createWindow () {
   win.loadFile('index.html')
 }
 
+const NOTIFICATION_TITLE = 'Basic Notification'
+const NOTIFICATION_BODY = 'Notification from the Main process'
+
 function showNotification () {
-  const notification = {
-    title: 'Basic Notification',
-    body: 'Notification from the Main process'
-  }
-  new Notification(notification).show()
+  new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY }).show()
 }
 
 app.whenReady().then(createWindow).then(showNotification)
