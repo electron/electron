61b21342959cd8b72db3088280db947d8f087825
Jeremy Foster <jeremy.foster@live.com>
2021-05-21 08:39:18 -0700

    add logic to show reset state briefly

--- docs/fiddles/features/progress-bar/main.js
+++ docs/fiddles/features/progress-bar/main.js
@@ -18,10 +18,13 @@ function createWindow () {
     // update progress bar to next value
     // values between 0 and 1 will show progress, >1 will show indeterminate or stick at 100%
     win.setProgressBar(c)
-    
+
     // increment or reset progress bar
-    if (c < 2) c += INCREMENT
-    else c = 0
+    if (c < 2) {
+      c += INCREMENT
+    } else {
+      c = (-INCREMENT * 5) // reset to a bit less than 0 to show reset state
+    }
   }, INTERVAL_DELAY)
 }
 
