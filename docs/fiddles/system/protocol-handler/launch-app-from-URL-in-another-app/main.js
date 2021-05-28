287a5c83992ee4fa21ce62d14f80acb76f809bc6
George Xu <gxu@slack-corp.com>
2021-05-28 16:50:22 -0700

    docs: add guide for launch-app-from-URL-in-other-app

--- docs/fiddles/system/protocol-handler/launch-app-from-URL-in-another-app/main.js
+++ docs/fiddles/system/protocol-handler/launch-app-from-URL-in-another-app/main.js
@@ -2,6 +2,8 @@
 const { app, BrowserWindow, ipcMain, shell } = require('electron')
 const path = require('path')
 
+let mainWindow;
+
 if (process.defaultApp) {
   if (process.argv.length >= 2) {
     app.setAsDefaultProtocolClient('electron-fiddle', process.execPath, [path.resolve(process.argv[1])])
@@ -10,9 +12,32 @@ if (process.defaultApp) {
     app.setAsDefaultProtocolClient('electron-fiddle')
 }
 
+const gotTheLock = app.requestSingleInstanceLock()
+
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+
+  // Create mainWindow, load the rest of the app, etc...
+  app.whenReady().then(() => {
+    createWindow()
+  })
+  
+  app.on('open-url', (event, url) => {
+      dialog.showErrorBox('Welcome Back', `You arrived from: ${url}`)
+  })
+}
+
 function createWindow () {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -28,17 +53,6 @@ function createWindow () {
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
-})
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  mainWindow = createWindow()
-})
-
-app.on('open-url', (event, url) => {
-    dialog.showErrorBox('Welcome Back', `You arrived from: ${url}`)
 })
 
 // Handle window controls via IPC
