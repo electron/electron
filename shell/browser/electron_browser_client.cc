e69b021f2cc83992a8f61859e71f57a1e2bc3125
Charles Kerr <charles@charleskerr.com>
2021-06-11 10:34:23 -0500

    chore: fix code shear in ElectronBrowserClient
    Refs: https://chromium-review.googlesource.com/c/chromium/src/+/2416811
    
    The signature for HandleExternalProtocol() got tweaked upstream.

--- shell/browser/electron_browser_client.cc
+++ shell/browser/electron_browser_client.cc
@@ -940,7 +940,7 @@ void HandleExternalProtocolInUI(
 
 bool ElectronBrowserClient::HandleExternalProtocol(
     const GURL& url,
-    content::WebContents::OnceGetter web_contents_getter,
+    content::WebContents::Getter web_contents_getter,
     int child_id,
     int frame_tree_node_id,
     content::NavigationUIData* navigation_data,
