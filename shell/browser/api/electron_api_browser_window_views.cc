5967a76c3b3d34859408828331a9f974d6224f83
Shelley Vohr <shelley.vohr@gmail.com>
2021-06-14 22:46:50 +0200

    fix: draggable regions with devtools open

--- shell/browser/api/electron_api_browser_window_views.cc
+++ shell/browser/api/electron_api_browser_window_views.cc
@@ -5,6 +5,7 @@
 #include "shell/browser/api/electron_api_browser_window.h"
 
 #include "shell/browser/native_window_views.h"
+#include "ui/aura/window.h"
 
 namespace electron {
 
@@ -14,8 +15,20 @@ void BrowserWindow::UpdateDraggableRegions(
     const std::vector<mojom::DraggableRegionPtr>& regions) {
   if (window_->has_frame())
     return;
+
+  if (&draggable_regions_ != &regions) {
+    auto const offset =
+        web_contents()->GetNativeView()->GetBoundsInRootWindow();
+    auto snapped_regions = mojo::Clone(regions);
+    for (auto& snapped_region : snapped_regions) {
+      snapped_region->bounds.Offset(offset.x(), offset.y());
+    }
+
+    draggable_regions_ = mojo::Clone(snapped_regions);
+  }
+
   static_cast<NativeWindowViews*>(window_.get())
-      ->UpdateDraggableRegions(regions);
+      ->UpdateDraggableRegions(draggable_regions_);
 }
 
 }  // namespace api
