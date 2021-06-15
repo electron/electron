3f563c984d37288e411261060e46e295d1130370
Milan Burda <milan.burda@gmail.com>
2021-06-15 12:02:03 +0200

    fix: don't warn about enableRemoteModule when it's disabled at build time

--- lib/renderer/security-warnings.ts
+++ lib/renderer/security-warnings.ts
@@ -298,7 +298,9 @@ const logSecurityWarnings = function (
   warnAboutEnableBlinkFeatures(webPreferences);
   warnAboutInsecureCSP();
   warnAboutAllowedPopups();
-  warnAboutRemoteModuleWithRemoteContent(webPreferences);
+  if (BUILDFLAG(ENABLE_REMOTE_MODULE)) {
+    warnAboutRemoteModuleWithRemoteContent(webPreferences);
+  }
 };
 
 const getWebPreferences = async function () {
