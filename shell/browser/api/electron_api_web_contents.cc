f4d5b1605088c0c63746d1ccf2635eb53459fa39
samuelmaddock <samuel.maddock@gmail.com>
2021-06-16 02:49:32 -0400

    refactor: call methods directly from electron:WebContents
    Writing static method boilerplate isn't fun

--- shell/browser/api/electron_api_web_contents.cc
+++ shell/browser/api/electron_api_web_contents.cc
@@ -1400,6 +1400,10 @@ void WebContents::HandleNewRenderFrame(
 void WebContents::RenderFrameCreated(
     content::RenderFrameHost* render_frame_host) {
   HandleNewRenderFrame(render_frame_host);
+
+  auto* web_frame = WebFrameMain::From(render_frame_host);
+  if (web_frame)
+    web_frame->Connect();
 }
 
 void WebContents::RenderViewDeleted(content::RenderViewHost* render_view_host) {
@@ -1646,7 +1650,9 @@ void WebContents::RenderFrameDeleted(
     content::RenderFrameHost* render_frame_host) {
   // A WebFrameMain can outlive its RenderFrameHost so we need to mark it as
   // disposed to prevent access to it.
-  WebFrameMain::RenderFrameDeleted(render_frame_host);
+  auto* web_frame = WebFrameMain::From(render_frame_host);
+  if (web_frame)
+    web_frame->MarkRenderFrameDisposed();
 }
 
 void WebContents::DidStartNavigation(
