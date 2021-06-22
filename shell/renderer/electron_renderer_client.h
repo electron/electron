1fa88e9874fdc5555a917658533f5e17088afec5
deepak1556 <hop2deep@gmail.com>
2021-06-22 03:56:18 -0700

    chore: remove revert_remove_contentrendererclient_shouldfork.patch

--- shell/renderer/electron_renderer_client.h
+++ shell/renderer/electron_renderer_client.h
@@ -36,10 +36,6 @@ class ElectronRendererClient : public RendererClientBase {
   void RenderFrameCreated(content::RenderFrame*) override;
   void RunScriptsAtDocumentStart(content::RenderFrame* render_frame) override;
   void RunScriptsAtDocumentEnd(content::RenderFrame* render_frame) override;
-  bool ShouldFork(blink::WebLocalFrame* frame,
-                  const GURL& url,
-                  const std::string& http_method,
-                  bool is_server_redirect) override;
   void WorkerScriptReadyForEvaluationOnWorkerThread(
       v8::Local<v8::Context> context) override;
   void WillDestroyWorkerContextOnWorkerThread(
