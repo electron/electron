d0dc8cfe59eae55a5fe79576c8018dd01dab95b8
George Xu <gxu@slack-corp.com>
2021-07-02 14:35:20 -0700

    chore: fix function signatures

--- shell/browser/api/electron_api_safe_storage.cc
+++ shell/browser/api/electron_api_safe_storage.cc
@@ -9,89 +9,6 @@
 #include "shell/common/node_includes.h"
 #include "shell/common/platform_util.h"
 
-#include <memory>
-#include <string>
-#include <utility>
-#include <vector>
-
-#include "base/callback_helpers.h"
-#include "base/command_line.h"
-#include "base/environment.h"
-#include "base/files/file_path.h"
-#include "base/files/file_util.h"
-#include "base/path_service.h"
-#include "base/system/sys_info.h"
-#include "chrome/browser/browser_process.h"
-#include "chrome/browser/icon_manager.h"
-#include "chrome/common/chrome_paths.h"
-#include "content/browser/gpu/compositor_util.h"        // nogncheck
-#include "content/browser/gpu/gpu_data_manager_impl.h"  // nogncheck
-#include "content/public/browser/browser_accessibility_state.h"
-#include "content/public/browser/browser_child_process_host.h"
-#include "content/public/browser/child_process_data.h"
-#include "content/public/browser/client_certificate_delegate.h"
-#include "content/public/browser/gpu_data_manager.h"
-#include "content/public/browser/render_frame_host.h"
-#include "content/public/common/content_switches.h"
-#include "media/audio/audio_manager.h"
-#include "net/ssl/client_cert_identity.h"
-#include "net/ssl/ssl_cert_request_info.h"
-#include "sandbox/policy/switches.h"
-#include "shell/browser/api/electron_api_app.h"
-#include "shell/browser/api/electron_api_menu.h"
-#include "shell/browser/api/electron_api_session.h"
-#include "shell/browser/api/electron_api_web_contents.h"
-#include "shell/browser/api/gpuinfo_manager.h"
-#include "shell/browser/electron_browser_context.h"
-#include "shell/browser/electron_browser_main_parts.h"
-#include "shell/browser/javascript_environment.h"
-#include "shell/browser/login_handler.h"
-#include "shell/browser/relauncher.h"
-#include "shell/common/application_info.h"
-#include "shell/common/electron_command_line.h"
-#include "shell/common/electron_paths.h"
-#include "shell/common/gin_converters/base_converter.h"
-#include "shell/common/gin_converters/callback_converter.h"
-#include "shell/common/gin_converters/file_path_converter.h"
-#include "shell/common/gin_converters/gurl_converter.h"
-#include "shell/common/gin_converters/image_converter.h"
-#include "shell/common/gin_converters/net_converter.h"
-#include "shell/common/gin_converters/value_converter.h"
-#include "shell/common/gin_helper/dictionary.h"
-#include "shell/common/gin_helper/object_template_builder.h"
-#include "shell/common/node_includes.h"
-#include "shell/common/options_switches.h"
-#include "shell/common/platform_util.h"
-#include "third_party/abseil-cpp/absl/types/optional.h"
-#include "ui/gfx/image/image.h"
-
-#include "base/callback_list.h"
-#include "gin/handle.h"
-#include "net/cookies/canonical_cookie.h"
-#include "net/cookies/cookie_change_dispatcher.h"
-#include "shell/browser/event_emitter_mixin.h"
-#include "shell/common/gin_helper/promise.h"
-#include "shell/common/gin_helper/trackable_object.h"
-
-#include "base/task/cancelable_task_tracker.h"
-#include "chrome/browser/icon_manager.h"
-#include "chrome/browser/process_singleton.h"
-#include "content/public/browser/browser_child_process_observer.h"
-#include "content/public/browser/gpu_data_manager_observer.h"
-#include "content/public/browser/render_process_host.h"
-#include "gin/handle.h"
-#include "net/base/completion_once_callback.h"
-#include "net/base/completion_repeating_callback.h"
-#include "net/ssl/client_cert_identity.h"
-#include "shell/browser/api/process_metric.h"
-#include "shell/browser/browser.h"
-#include "shell/browser/browser_observer.h"
-#include "shell/browser/electron_browser_client.h"
-#include "shell/browser/event_emitter_mixin.h"
-#include "shell/common/gin_helper/dictionary.h"
-#include "shell/common/gin_helper/error_thrower.h"
-#include "shell/common/gin_helper/promise.h"
-
 namespace electron {
 
 namespace api {
@@ -104,16 +21,31 @@ electron::api::SafeStorage::~SafeStorage() = default;
 bool SafeStorage::IsEncryptionAvailable() {
   return OSCrypt::IsEncryptionAvailable();
 }
-// TODO: are all the pointers and dereferences right...
-// should i be passing in *plaintext, &ciphertext
-bool SafeStorage::EncryptString(std::string plaintext, std::string ciphertext) {
-  std::string* ciphertext_pointer = &ciphertext;
-  return OSCrypt::EncryptString(plaintext, ciphertext_pointer);
+
+std::string SafeStorage::EncryptString(v8::Isolate* isolate,
+                                       const std::string& plaintext) {
+  std::string ciphertext;
+  bool encrypted = OSCrypt::EncryptString(plaintext, &ciphertext);
+
+  if (!encrypted) {
+    gin_helper::ErrorThrower(isolate).ThrowError(
+        "Error while decrypting the key.");
+  }
+
+  return ciphertext;
 }
 
-bool SafeStorage::DecryptString(std::string ciphertext, std::string plaintext) {
-  std::string* plaintext_pointer = &plaintext;
-  return OSCrypt::DecryptString(ciphertext, plaintext_pointer);
+std::string SafeStorage::DecryptString(v8::Isolate* isolate,
+                                       const std::string& ciphertext) {
+  std::string plaintext;
+  bool encrypted = OSCrypt::DecryptString(ciphertext, &plaintext);
+
+  if (!encrypted) {
+    gin_helper::ErrorThrower(isolate).ThrowError(
+        "Error while decrypting the key.");
+  }
+
+  return plaintext;
 }
 
 gin::ObjectTemplateBuilder SafeStorage::GetObjectTemplateBuilder(
