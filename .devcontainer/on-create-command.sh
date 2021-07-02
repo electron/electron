4c60d0af6a1958052341751d48348316cc4df953
Codespaces <codespaces@github.com>
2021-07-02 06:40:40 +0000

    Codespaces

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-npm i -g @electron/build-tools
+set -eo pipefail
+
+buildtools=$HOME/.electron_build_tools
+rm -rf $buildtools
+git clone https://github.com/electron/build-tools.git $buildtools
+pushd $buildtools
+npx yarn
+popd
+
+echo export PATH=\"\$PATH:$buildtools/src\" >> ~/.bashrc
+
+/workspaces/electron/.git
+/workspaces/src/electron/.git
+/workspaces/src/chrome/.git
+/workspaces/src/.git
