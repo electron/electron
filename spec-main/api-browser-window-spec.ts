ebbaaf505eab09d4f9705c8f4596cf433d358e08
deepak1556 <hop2deep@gmail.com>
2021-07-11 21:54:43 -0700

    chore: fix flaky fullscreen inheritance test

--- spec-main/api-browser-window-spec.ts
+++ spec-main/api-browser-window-spec.ts
@@ -4211,8 +4211,10 @@ describe('BrowserWindow module', () => {
         await enterFullScreen;
         expect(w.isFullScreen()).to.be.true('isFullScreen');
         await delay();
-        const w2 = new BrowserWindow();
-        await delay();
+        const w2 = new BrowserWindow({ show: false });
+        const enterFullScreen2 = emittedOnce(w2, 'enter-full-screen');
+        w2.show();
+        await enterFullScreen2;
         expect(w2.isFullScreen()).to.be.true('isFullScreen');
       });
     });
