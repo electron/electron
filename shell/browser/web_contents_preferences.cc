2b082d9c37a943ae13a6fe717ccd470c226a38b3
Jeremy Rose <nornagon@nornagon.net>
2021-07-14 16:17:33 -0700

    disable when preload or nodeIntegrationInWorker

--- shell/browser/web_contents_preferences.cc
+++ shell/browser/web_contents_preferences.cc
@@ -161,8 +161,12 @@ WebContentsPreferences::WebContentsPreferences(
   SetDefaultBoolIfUndefined(options::kNodeIntegrationInWorker, false);
   SetDefaultBoolIfUndefined(options::kDisableHtmlFullscreenWindowResize, false);
   SetDefaultBoolIfUndefined(options::kWebviewTag, false);
-  bool sandbox_enabled_by_default = !IsEnabled(options::kNodeIntegration);
-  SetDefaultBoolIfUndefined(options::kSandbox, sandbox_enabled_by_default);
+  base::FilePath preload_path;
+  bool has_preload = GetPreloadPath(&preload_path);
+  bool sandbox_disabled_by_default =
+      IsEnabled(options::kNodeIntegration) ||
+      IsEnabled(options::kNodeIntegrationInWorker) || has_preload;
+  SetDefaultBoolIfUndefined(options::kSandbox, !sandbox_disabled_by_default);
   SetDefaultBoolIfUndefined(options::kNativeWindowOpen, true);
   SetDefaultBoolIfUndefined(options::kContextIsolation, true);
   SetDefaultBoolIfUndefined(options::kJavaScript, true);
