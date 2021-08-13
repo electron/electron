electron/electron

commit 439e83de6ced559a59da44d1a01fec305f84014b

refactor: remove all usages of the legacy request module (#30492)
* Replaces request with got
* Replaces nugget with got streams
* Replaces request in docs with got
* Upgrades dugite to drop requests dependency

script/release/ci-release-build.js
@@ -1,7 +1,7 @@
 if (!process.env.CI) require('dotenv-safe').load();
 
 const assert = require('assert');
-const request = require('request');
+const got = require('got');
 
 const BUILD_APPVEYOR_URL = 'https://ci.appveyor.com/api/builds';
 const CIRCLECI_PIPELINE_URL = 'https://circleci.com/api/v2/project/gh/electron/electron/pipeline';
@@ -35,31 +35,18 @@ const vstsArmJobs = [
 
 let jobRequestedCount = 0;
 
-async function makeRequest (requestOptions, parseResponse) {
-  return new Promise((resolve, reject) => {
-    request(requestOptions, (err, res, body) => {
-      if (!err && res.statusCode >= 200 && res.statusCode < 300) {
-        if (parseResponse) {
-          const build = JSON.parse(body);
-          resolve(build);
-        } else {
-          resolve(body);
-        }
-      } else {
-        console.error('Error occurred while requesting:', requestOptions.url);
-        if (parseResponse) {
-          try {
-            console.log('Error: ', `(status ${res.statusCode})`, err || JSON.parse(res.body));
-          } catch (err) {
-            console.log('Error: ', `(status ${res.statusCode})`, res.body);
-          }
-        } else {
-          console.log('Error: ', `(status ${res.statusCode})`, err || res.body);
-        }
-        reject(err);
-      }
-    });
+async function makeRequest ({ auth, url, headers, body, method }) {
+  const response = await got(url, {
+    headers,
+    body,
+    method,
+    auth: auth ? `${auth.username}:${auth.password}` : undefined
   });
+  if (response.statusCode !== 200) {
+    console.error('Error: ', `(status ${response.statusCode})`, response.body);
+    throw new Error(`Unexpected status code ${response.statusCode} from ${url}`);
+  }
+  return JSON.parse(response.body);
 }
 
 async function circleCIcall (targetBranch, workflowName, options) {

script/download-circleci-artifacts.js
@@ -1,52 +1,23 @@
 const args = require('minimist')(process.argv.slice(2));
-const nugget = require('nugget');
-const request = require('request');
+const fs = require('fs');
+const got = require('got');
+const stream = require('stream');
+const { promisify } = require('util');
 
-async function makeRequest (requestOptions, parseResponse) {
-  return new Promise((resolve, reject) => {
-    request(requestOptions, (err, res, body) => {
-      if (!err && res.statusCode >= 200 && res.statusCode < 300) {
-        if (parseResponse) {
-          const build = JSON.parse(body);
-          resolve(build);
-        } else {
-          resolve(body);
-        }
-      } else {
-        if (args.verbose) {
-          console.error('Error occurred while requesting:', requestOptions.url);
-          if (parseResponse) {
-            try {
-              console.log('Error: ', `(status ${res.statusCode})`, err || JSON.parse(res.body), requestOptions);
-            } catch (err) {
-              console.log('Error: ', `(status ${res.statusCode})`, err || res.body, requestOptions);
-            }
-          } else {
-            console.log('Error: ', `(status ${res.statusCode})`, err || res.body, requestOptions);
-          }
-        }
-        reject(err);
-      }
-    });
-  });
-}
+const pipeline = promisify(stream.pipeline);
 
 async function downloadArtifact (name, buildNum, dest) {
   const circleArtifactUrl = `https://circleci.com/api/v1.1/project/github/electron/electron/${args.buildNum}/artifacts?circle-token=${process.env.CIRCLE_TOKEN}`;
-  const artifacts = await makeRequest({
-    method: 'GET',
-    url: circleArtifactUrl,
+  const responsePromise = got(circleArtifactUrl, {
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json'
     }
-  }, true).catch(err => {
-    if (args.verbose) {
-      console.log('Error calling CircleCI:', err);
-    } else {
-      console.error('Error calling CircleCI to get artifact details');
-    }
   });
+  const [response, artifacts] = await Promise.all([responsePromise, responsePromise.json()]);
+  if (response.statusCode !== 200) {
+    console.error('Could not fetch circleci artifact list, got status code:', response.statusCode);
+  }
   const artifactToDownload = artifacts.find(artifact => {
     return (artifact.path === name);
   });
@@ -86,19 +57,10 @@ async function downloadWithRetry (url, directory) {
 }
 
 function downloadFile (url, directory) {
-  return new Promise((resolve, reject) => {
-    const nuggetOpts = {
-      dir: directory,
-      quiet: args.verbose
-    };
-    nugget(url, nuggetOpts, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
+  return pipeline(
+    got.stream(url),
+    fs.createWriteStream(directory)
+  );
 }
 
 if (!args.name || !args.buildNum || !args.dest) {

script/release/get-asset.js
@@ -1,0 +1,43 @@
+const { Octokit } = require('@octokit/rest');
+const got = require('got');
+
+const octokit = new Octokit({
+  userAgent: 'electron-asset-fetcher',
+  auth: process.env.ELECTRON_GITHUB_TOKEN
+});
+
+async function getAssetContents (repo, assetId) {
+  const requestOptions = octokit.repos.getReleaseAsset.endpoint({
+    owner: 'electron',
+    repo,
+    asset_id: assetId,
+    headers: {
+      Accept: 'application/octet-stream'
+    }
+  });
+
+  const { url, headers } = requestOptions;
+  headers.authorization = `token ${process.env.ELECTRON_GITHUB_TOKEN}`;
+
+  const response = await got(url, {
+    followRedirect: false,
+    method: 'HEAD',
+    headers
+  });
+  if (!response.headers.location) {
+    console.error(response.headers, response.body);
+    throw new Error(`cannot find asset[${assetId}], asset download did not redirect`);
+  }
+
+  const fileResponse = await got(response.headers.location);
+  if (fileResponse.status !== 200) {
+    console.error(fileResponse.headers, fileResponse.body);
+    throw new Error(`cannot download asset[${assetId}] from ${response.headers.location}, got status: ${fileResponse.status}`);
+  }
+
+  return fileResponse.body;
+}
+
+module.exports = {
+  getAssetContents
+};

script/release/publish-to-npm.js
@@ -2,12 +2,14 @@ const temp = require('temp');
 const fs = require('fs');
 const path = require('path');
 const childProcess = require('child_process');
-const { getCurrentBranch, ELECTRON_DIR } = require('../lib/utils');
-const request = require('request');
+const got = require('got');
 const semver = require('semver');
+
+const { getCurrentBranch, ELECTRON_DIR } = require('../lib/utils');
 const rootPackageJson = require('../../package.json');
 
 const { Octokit } = require('@octokit/rest');
+const { getAssetContents } = require('./get-asset');
 const octokit = new Octokit({
   userAgent: 'electron-npm-publisher',
   auth: process.env.ELECTRON_GITHUB_TOKEN
@@ -91,22 +93,13 @@ new Promise((resolve, reject) => {
     if (!tsdAsset) {
       throw new Error(`cannot find electron.d.ts from v${rootPackageJson.version} release assets`);
     }
-    return new Promise((resolve, reject) => {
-      request.get({
-        url: tsdAsset.url,
-        headers: {
-          accept: 'application/octet-stream',
-          'user-agent': 'electron-npm-publisher'
-        }
-      }, (err, response, body) => {
-        if (err || response.statusCode !== 200) {
-          reject(err || new Error('Cannot download electron.d.ts'));
-        } else {
-          fs.writeFileSync(path.join(tempDir, 'electron.d.ts'), body);
-          resolve(release);
-        }
-      });
-    });
+
+    const typingsContent = await getAssetContents(
+      rootPackageJson.version.indexOf('nightly') > 0 ? 'nightlies' : 'electron',
+      tsdAsset.id
+    );
+
+    fs.writeFileSync(path.join(tempDir, 'electron.d.ts'), typingsContent);
   })
   .then(async (release) => {
     const currentBranch = await getCurrentBranch();

script/release/release.js
@@ -384,7 +384,7 @@ async function verifyDraftGitHubReleaseAssets (release) {
   console.log('Fetching authenticated GitHub artifact URLs to verify shasums');
 
   const remoteFilesToHash = await Promise.all(release.assets.map(async asset => {
-    const requestOptions = await octokit.repos.getReleaseAsset.endpoint({
+    const requestOptions = octokit.repos.getReleaseAsset.endpoint({
       owner: 'electron',
       repo: targetRepo,
       asset_id: asset.id,

spec/ts-smoke/electron/main.ts
@@ -1104,7 +1104,7 @@ shell.writeShortcutLink('/home/user/Desktop/shortcut.lnk', 'update', shell.readS
 
 session.defaultSession.on('will-download', (event, item, webContents) => {
   event.preventDefault()
-  require('request')(item.getURL(), (data: any) => {
+  require('got')(item.getURL()).then((data: any) => {
     require('fs').writeFileSync('/somewhere', data)
   })
 })

commit 4eef3ea476de32fbd6b8fc45162a482729a3989e

build: fix release CI jobs start script
This broke in #30492, we weren't handled 20X status codes and weren't authing to appveyor correctly.

script/release/ci-release-build.js
@@ -37,12 +37,15 @@ let jobRequestedCount = 0;
 
 async function makeRequest ({ auth, url, headers, body, method }) {
   const response = await got(url, {
-    headers,
+    headers: {
+      Authorization: auth && auth.bearer ? `Bearer ${auth.bearer}` : undefined,
+      ...(headers || {})
+    },
     body,
     method,
-    auth: auth ? `${auth.username}:${auth.password}` : undefined
+    auth: auth && auth.password ? `${auth.username}:${auth.password}` : undefined
   });
-  if (response.statusCode !== 200) {
+  if (response.statusCode < 200 || response.statusCode >= 300) {
     console.error('Error: ', `(status ${response.statusCode})`, response.body);
     throw new Error(`Unexpected status code ${response.statusCode} from ${url}`);
   }