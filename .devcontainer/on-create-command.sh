fc5a49d18521ef8c7ceeacfab7c235a289405cb9
Samuel Attard <sam@electronjs.org>
2021-08-14 10:08:18 +0000

    build: use auth.notgoma codespace token auth to auto-auth goma

--- .devcontainer/on-create-command.sh
+++ .devcontainer/on-create-command.sh
@@ -30,27 +30,39 @@ solutions = [
 
 mkdir -p $buildtools/configs
 
-echo "
-{
-    \"root\": \"/workspaces/gclient\",
-    \"goma\": \"cache-only\",
-    \"gen\": {
-        \"args\": [
-            \"import(\\\"//electron/build/args/testing.gn\\\")\",
-            \"import(\\\"/home/builduser/.electron_build_tools/third_party/goma.gn\\\")\"
-        ],
-        \"out\": \"Testing\"
-    },
-    \"env\": {
-        \"CHROMIUM_BUILDTOOLS_PATH\": \"/workspaces/gclient/src/buildtools\",
-        \"GIT_CACHE_PATH\": \"/workspaces/gclient/.git-cache\"
-    },
-    \"remotes\": {
-        \"electron\": {
-            \"origin\": \"https://github.com/electron/electron.git\"
+write_config() {
+    echo "
+    {
+        \"root\": \"/workspaces/gclient\",
+        \"goma\": \"$1\",
+        \"gen\": {
+            \"args\": [
+                \"import(\\\"//electron/build/args/testing.gn\\\")\",
+                \"import(\\\"/home/builduser/.electron_build_tools/third_party/goma.gn\\\")\"
+            ],
+            \"out\": \"Testing\"
+        },
+        \"env\": {
+            \"CHROMIUM_BUILDTOOLS_PATH\": \"/workspaces/gclient/src/buildtools\",
+            \"GIT_CACHE_PATH\": \"/workspaces/gclient/.git-cache\"
+        },
+        \"remotes\": {
+            \"electron\": {
+                \"origin\": \"https://github.com/electron/electron.git\"
+            }
         }
     }
+    " > $buildtools/configs/evm.testing.json
 }
-" > $buildtools/configs/evm.testing.json
+
+# Start out as cache only
+write_config cache-only
 
 e use testing
+
+# Attempt to auth to the goma service via codespaces tokens
+# if it works we can use the goma cluster
+export NOTGOMA_CODESPACES_TOKEN=$GITHUB_TOKEN
+if e d goma_auth login; then
+    write_config cluster
+fi
