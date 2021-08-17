de9a51fd96472c1b5e7bd31e282bec89c13c0d3f
deepak1556 <hop2deep@gmail.com>
2021-08-17 12:18:19 -0700

    Disable kDesktopCaptureMacV2
    https://chromium-review.googlesource.com/c/chromium/src/+/3069272

--- shell/browser/native_window_mac.mm
+++ shell/browser/native_window_mac.mm
@@ -23,7 +23,6 @@
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/desktop_media_id.h"
-#include "content/public/common/content_features.h"
 #include "shell/browser/javascript_environment.h"
 #include "shell/browser/native_browser_view_mac.h"
 #include "shell/browser/ui/cocoa/electron_native_widget_mac.h"
@@ -234,6 +233,11 @@ void ViewDidMoveToSuperview(NSView* self, SEL _cmd) {
   }
   [self setFrame:[[self superview] bounds]];
 }
+
+// Refs https://github.com/electron/electron/pull/30507
+// TODO: Enable this flag to match upstream
+const base::Feature kWindowCaptureMacV2{"WindowCaptureMacV2",
+                                        base::FEATURE_DISABLED_BY_DEFAULT};
 
 }  // namespace
 
@@ -1236,7 +1240,7 @@ content::DesktopMediaID NativeWindowMac::GetDesktopMediaID() const {
       content::DesktopMediaID::TYPE_WINDOW, GetAcceleratedWidget());
   // c.f.
   // https://source.chromium.org/chromium/chromium/src/+/master:chrome/browser/media/webrtc/native_desktop_media_list.cc;l=372?q=kWindowCaptureMacV2&ss=chromium
-  if (base::FeatureList::IsEnabled(features::kWindowCaptureMacV2)) {
+  if (base::FeatureList::IsEnabled(kWindowCaptureMacV2)) {
     if (remote_cocoa::ScopedCGWindowID::Get(desktop_media_id.id)) {
       desktop_media_id.window_id = desktop_media_id.id;
     }
