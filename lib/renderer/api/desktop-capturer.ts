ba9ff3778f7c4a6afc17d6679755ff8661aa2a02
Jeremy Rose <nornagon@nornagon.net>
2021-08-27 13:49:17 -0700

    fix import

--- lib/renderer/api/desktop-capturer.ts
+++ lib/renderer/api/desktop-capturer.ts
@@ -1,6 +1,6 @@
 import { ipcRendererInternal } from '@electron/internal/renderer/ipc-renderer-internal';
 import { deserialize } from '@electron/internal/common/type-utils';
-import { deprecate } from '@electron/internal/common/api/deprecate';
+import deprecate from '@electron/internal/common/api/deprecate';
 import { IPC_MESSAGES } from '@electron/internal/common/ipc-messages';
 
 const { hasSwitch } = process._linkedBinding('electron_common_command_line');
