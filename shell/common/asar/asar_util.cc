3a7ed6ca65e01962ac76ffbd682379b1885221c1
Samuel Attard <samuel.r.attard@gmail.com>
2021-09-03 02:59:53 -0700

    chore: fix missing log include on non-darwin

--- shell/common/asar/asar_util.cc
+++ shell/common/asar/asar_util.cc
@@ -11,6 +11,7 @@
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/lazy_instance.h"
+#include "base/logging.h"
 #include "base/no_destructor.h"
 #include "base/stl_util.h"
 #include "base/synchronization/lock.h"
