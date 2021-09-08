43301582001c7cec343cac851159b28e8901491d
Shelley Vohr <shelley.vohr@gmail.com>
2021-09-08 23:36:59 +0200

    chore: remove upstreamed patch

--- shell/browser/file_select_helper.cc
+++ shell/browser/file_select_helper.cc
@@ -1,5 +1,5 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
+// Copyright (c) 2021 Microsoft. All rights reserved.
+// Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
 #include "shell/browser/file_select_helper.h"
