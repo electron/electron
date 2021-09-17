6fdf350bea8ec5452b66043cd81eff6917802068
Shelley Vohr <shelley.vohr@gmail.com>
2021-09-17 15:54:15 +0200

    fix: disabling and enabling resizability on macOS (#30999)

--- shell/browser/native_window_mac.h
+++ shell/browser/native_window_mac.h
@@ -191,6 +191,7 @@ class NativeWindowMac : public NativeWindow,
  protected:
   // views::WidgetDelegate:
   views::View* GetContentsView() override;
+  bool CanMaximize() const override;
 
   // ui::NativeThemeObserver:
   void OnNativeThemeUpdated(ui::NativeTheme* observed_theme) override;
