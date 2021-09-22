533abe4aca4b030e11acfd2e642fbbe25ea47ccd
Charles Kerr <charles@charleskerr.com>
2021-09-22 14:23:33 -0500

    chore: update Allocator construction
    Xref: https://chromium-review.googlesource.com/c/chromium/src/+/3135195
    
    Change RefCount::kAllowed/Disallowed semantics into BackupRefPtr::kEnabled/Disabled

--- shell/browser/javascript_environment.cc
+++ shell/browser/javascript_environment.cc
@@ -76,7 +76,7 @@ class ArrayBufferAllocator : public v8::ArrayBuffer::Allocator {
                       base::PartitionOptions::ThreadCache::kDisabled,
                       base::PartitionOptions::Quarantine::kAllowed,
                       base::PartitionOptions::Cookie::kAllowed,
-                      base::PartitionOptions::RefCount::kDisallowed});
+                      base::PartitionOptions::BackupRefPtr::kDisabled});
   }
 
   // Allocate() methods return null to signal allocation failure to V8, which
