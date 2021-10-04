86cfa560fb52d9be04419ea5ed1b94f0d0d394d5
deepak1556 <hop2deep@gmail.com>
2021-10-04 21:31:31 +0900

    chore: fix windows build

--- shell/browser/api/electron_api_event.cc
+++ shell/browser/api/electron_api_event.cc
@@ -9,17 +9,12 @@
 
 namespace {
 
-v8::Local<v8::Object> CreateWithSender(v8::Isolate* isolate,
-                                       v8::Local<v8::Object> sender) {
-  return gin_helper::internal::CreateEvent(isolate, sender);
-}
-
 void Initialize(v8::Local<v8::Object> exports,
                 v8::Local<v8::Value> unused,
                 v8::Local<v8::Context> context,
                 void* priv) {
   gin_helper::Dictionary dict(context->GetIsolate(), exports);
-  dict.SetMethod("createWithSender", &CreateWithSender);
+  dict.SetMethod("createWithSender", &gin_helper::internal::CreateCustomEvent);
   dict.SetMethod("createEmpty", &gin_helper::Event::Create);
 }
 
