caea89df05298c19116a42f1b38bab73ad32141c
t57ser <seve@live.at>
2021-10-12 09:59:24 +0200

    fix; check if webContents is destroyed

--- lib/browser/guest-window-manager.ts
+++ lib/browser/guest-window-manager.ts
@@ -65,8 +65,8 @@ export function openGuestWindow ({ event, embedder, guest, referrer, disposition
   // https://html.spec.whatwg.org/multipage/window-object.html#apis-for-creating-and-navigating-browsing-contexts-by-name
   const existingWindow = getGuestWindowByFrameName(frameName);
   if (existingWindow) {
-    if (existingWindow.isDestroyed()) {
-      // The window is destroyed for some reason, unregister the frame name
+    if (existingWindow.webContents.isDestroyed()) {
+      // The webContents is destroyed for some reason, unregister the frame name
       unregisterFrameName(frameName);
     } else {
       existingWindow.loadURL(url);
