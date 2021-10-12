5f599c94220049151491a21e928ae181a3a0f6e3
Shelley Vohr <shelley.vohr@gmail.com>
2021-10-12 16:48:31 +0200

    3186491: Add 'devtools.sync_preferences' preference
    https://chromium-review.googlesource.com/c/chromium/src/+/3186491

--- shell/browser/ui/inspectable_web_contents.cc
+++ shell/browser/ui/inspectable_web_contents.cc
@@ -343,10 +343,10 @@ void InspectableWebContents::RegisterPrefs(PrefRegistrySimple* registry) {
                                    RectToDictionary(gfx::Rect(0, 0, 800, 600)));
   registry->RegisterDoublePref(kDevToolsZoomPref, 0.);
   registry->RegisterDictionaryPref(kDevToolsPreferences);
-  registry->RegisterBooleanPref(kDevToolsSyncPreferences,
-                              kSyncDevToolsPreferencesDefault);
   registry->RegisterDictionaryPref(kDevToolsSyncedPreferencesSyncEnabled);
   registry->RegisterDictionaryPref(kDevToolsSyncedPreferencesSyncDisabled);
+  registry->RegisterBooleanPref(kDevToolsSyncPreferences,
+                                kSyncDevToolsPreferencesDefault);
 }
 
 InspectableWebContents::InspectableWebContents(
