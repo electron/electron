603a736737ebab53aaf5001fd134cfad76bede39
VerteDinde <khammond@slack-corp.com>
2021-11-15 12:46:18 -0800

    fix: include sandbox policy features

--- shell/browser/net/system_network_context_manager.h
+++ shell/browser/net/system_network_context_manager.h
@@ -8,6 +8,7 @@
 #include "base/memory/ref_counted.h"
 #include "chrome/browser/net/proxy_config_monitor.h"
 #include "mojo/public/cpp/bindings/remote.h"
+#include "sandbox/policy/features.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "services/network/public/mojom/network_service.mojom.h"
