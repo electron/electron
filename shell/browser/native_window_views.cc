d4abb6fb66d2075d055cf78767c8a74d621b514b
Micha Hanselmann <micha.hanselmann@gmail.com>
2021-11-15 10:51:27 +0100

    fix: setContentProtection affects BrowserWindow frame (#31768)

--- shell/browser/native_window_views.cc
+++ shell/browser/native_window_views.cc
@@ -1108,6 +1108,8 @@ void NativeWindowViews::SetIgnoreMouseEvents(bool ignore, bool forward) {
 void NativeWindowViews::SetContentProtection(bool enable) {
 #if defined(OS_WIN)
   HWND hwnd = GetAcceleratedWidget();
+  DWORD affinity = enable ? WDA_EXCLUDEFROMCAPTURE : WDA_NONE;
+  ::SetWindowDisplayAffinity(hwnd, affinity);
   if (!layered_) {
     // Workaround to prevent black window on screen capture after hiding and
     // showing the BrowserWindow.
@@ -1116,8 +1118,6 @@ void NativeWindowViews::SetContentProtection(bool enable) {
     ::SetWindowLong(hwnd, GWL_EXSTYLE, ex_style);
     layered_ = true;
   }
-  DWORD affinity = enable ? WDA_EXCLUDEFROMCAPTURE : WDA_NONE;
-  ::SetWindowDisplayAffinity(hwnd, affinity);
 #endif
 }
 
