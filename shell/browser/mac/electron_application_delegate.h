7ff15038d64bd0ca3621a7f741b2b067b6b8a351
Milan Burda <milan.burda@gmail.com>
2021-11-22 01:36:32 +0100

    fix: lint Objective-C header files (#31923)

--- shell/browser/mac/electron_application_delegate.h
+++ shell/browser/mac/electron_application_delegate.h
@@ -2,6 +2,9 @@
 // Use of this source code is governed by the MIT license that can be
 // found in the LICENSE file.
 
+#ifndef SHELL_BROWSER_MAC_ELECTRON_APPLICATION_DELEGATE_H_
+#define SHELL_BROWSER_MAC_ELECTRON_APPLICATION_DELEGATE_H_
+
 #import <Cocoa/Cocoa.h>
 
 #import "shell/browser/ui/cocoa/electron_menu_controller.h"
@@ -15,3 +18,5 @@
 - (void)setApplicationDockMenu:(electron::ElectronMenuModel*)model;
 
 @end
+
+#endif  // SHELL_BROWSER_MAC_ELECTRON_APPLICATION_DELEGATE_H_
