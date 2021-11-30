ee9853a4c07c1d975e3418f9e2644eb0e5b649c8
Erick Zhao <erick@hotmail.ca>
2021-11-29 16:03:12 -0800

    docs: add IPC doc

---
+++ docs/fiddles/ipc/pattern-2/renderer.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('btn')
+const filePathElement = document.getElementById('filePath')
+
+btn.addEventListener('click', async () => {
+  const filePath = await window.electronAPI.openFile()
+  filePathElement.innerText = filePath
+})
