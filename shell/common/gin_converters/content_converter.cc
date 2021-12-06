0f4e4137db4e115ece08ab9e5498cbc659dbb3b4
VerteDinde <khammond@slack-corp.com>
2021-12-06 15:33:24 -0800

    3293841: Remove File Handling permissions code
    Ref: https://chromium-review.googlesource.com/c/chromium/src/+/3293841

--- shell/common/gin_converters/content_converter.cc
+++ shell/common/gin_converters/content_converter.cc
@@ -154,8 +154,6 @@ v8::Local<v8::Value> Converter<content::PermissionType>::ToV8(
       return StringToV8(isolate, "clipboard-read");
     case content::PermissionType::CLIPBOARD_SANITIZED_WRITE:
       return StringToV8(isolate, "clipboard-sanitized-write");
-    case content::PermissionType::FILE_HANDLING:
-      return StringToV8(isolate, "file-handling");
     case content::PermissionType::FONT_ACCESS:
       return StringToV8(isolate, "font-access");
     case content::PermissionType::IDLE_DETECTION:
