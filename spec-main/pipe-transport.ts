594e1fccb6976ca941114107b8def2e84f62a606
Jimmy Wärting <jimmy@warting.se>
2022-01-25 14:14:50 +0100

    refactor: use private class field instead

--- spec-main/pipe-transport.ts
+++ spec-main/pipe-transport.ts
@@ -1,27 +1,27 @@
 // A small pipe transport for talking to Electron over CDP.
 export class PipeTransport {
-  private _pipeWrite: NodeJS.WritableStream | null;
-  private _pendingMessage = '';
+  #pipeWrite: NodeJS.WritableStream | null;
+  #pendingMessage = '';
 
   onmessage?: (message: string) => void;
 
   constructor (pipeWrite: NodeJS.WritableStream, pipeRead: NodeJS.ReadableStream) {
-    this._pipeWrite = pipeWrite;
-    pipeRead.on('data', buffer => this._dispatch(buffer));
+    this.#pipeWrite = pipeWrite;
+    pipeRead.on('data', buffer => this.#dispatch(buffer));
   }
 
   send (message: Object) {
-    this._pipeWrite!.write(JSON.stringify(message));
-    this._pipeWrite!.write('\0');
+    this.#pipeWrite!.write(JSON.stringify(message));
+    this.#pipeWrite!.write('\0');
   }
 
-  _dispatch (buffer: Buffer) {
+  #dispatch (buffer: Buffer) {
     let end = buffer.indexOf('\0');
     if (end === -1) {
-      this._pendingMessage += buffer.toString();
+      this.#pendingMessage += buffer.toString();
       return;
     }
-    const message = this._pendingMessage + buffer.toString(undefined, 0, end);
+    const message = this.#pendingMessage + buffer.toString(undefined, 0, end);
     if (this.onmessage) { this.onmessage.call(null, JSON.parse(message)); }
 
     let start = end + 1;
@@ -32,6 +32,6 @@ export class PipeTransport {
       start = end + 1;
       end = buffer.indexOf('\0', start);
     }
-    this._pendingMessage = buffer.toString(undefined, start);
+    this.#pendingMessage = buffer.toString(undefined, start);
   }
 }
