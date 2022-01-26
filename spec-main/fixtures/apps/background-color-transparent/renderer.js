3152d99b19bf544161eaf3b45f39af4f35e6c80d
Shelley Vohr <shelley.vohr@gmail.com>
2022-01-26 16:54:16 +0100

    chore: address feedback from review

--- spec-main/fixtures/apps/background-color-transparent/renderer.js
+++ spec-main/fixtures/apps/background-color-transparent/renderer.js
@@ -2,8 +2,8 @@ const { ipcRenderer } = require('electron');
 
 window.setTimeout(async (_) => {
   document.body.style.background = 'transparent';
-}, 3000);
 
-window.setTimeout(async (_) => {
-  ipcRenderer.send('set-transparent');
-}, 5000);
+  window.setTimeout(async (_) => {
+    ipcRenderer.send('set-transparent');
+  }, 2000);
+}, 3000);
