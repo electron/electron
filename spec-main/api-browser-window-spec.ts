aa570153c53ae2a9ee31093185d7d3d95c2ea97b
Shelley Vohr <shelley.vohr@gmail.com>
2022-01-27 19:12:55 +0100

    chore: fixup tests

--- spec-main/api-browser-window-spec.ts
+++ spec-main/api-browser-window-spec.ts
@@ -1118,7 +1118,7 @@ describe('BrowserWindow module', () => {
           await unmaximize;
           expectBoundsEqual(w.getNormalBounds(), bounds);
         });
-        it('can check transparent window maximization', async () => {
+        it('correctly checks transparent window maximization state', async () => {
           w.destroy();
           w = new BrowserWindow({
             show: false,
@@ -1127,12 +1127,12 @@ describe('BrowserWindow module', () => {
             transparent: true
           });
 
-          const maximize = emittedOnce(w, 'resize');
+          const maximize = emittedOnce(w, 'maximize');
           w.show();
           w.maximize();
           await maximize;
           expect(w.isMaximized()).to.equal(true);
-          const unmaximize = emittedOnce(w, 'resize');
+          const unmaximize = emittedOnce(w, 'unmaximize');
           w.unmaximize();
           await unmaximize;
           expect(w.isMaximized()).to.equal(false);
@@ -3224,9 +3224,7 @@ describe('BrowserWindow module', () => {
       await maximize;
     });
 
-    // Transparent window max/unmax uses different logic and does not go through
-    // NativeWindowViews::HandleSizeEvent, so we need to test it separately.
-    ifit(process.platform === 'win32')('emits an event when a transparent window is maximized', async () => {
+    it('emits an event when a transparent window is maximized', async () => {
       const w = new BrowserWindow({
         show: false,
         frame: false,
@@ -3257,9 +3255,7 @@ describe('BrowserWindow module', () => {
       await unmaximize;
     });
 
-    // Transparent window max/unmax uses different logic and does not go through
-    // NativeWindowViews::HandleSizeEvent, so we need to test it separately.
-    ifit(process.platform === 'win32')('emits an event when a transparent window is unmaximized', async () => {
+    it('emits an event when a transparent window is unmaximized', async () => {
       const w = new BrowserWindow({
         show: false,
         frame: false,
