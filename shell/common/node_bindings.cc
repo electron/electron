bcd88f84f6a287c5b457755dbdf863163c012c5f
Shelley Vohr <shelley.vohr@gmail.com>
2022-02-10 12:17:56 +0100

    src: add flags for controlling process behavior
    https://github.com/nodejs/node/pull/40339

--- shell/common/node_bindings.cc
+++ shell/common/node_bindings.cc
@@ -365,7 +365,6 @@ bool NodeBindings::IsInitialized() {
 void NodeBindings::Initialize() {
   TRACE_EVENT0("electron", "NodeBindings::Initialize");
   // Open node's error reporting system for browser process.
-  node::g_upstream_node_mode = false;
 
 #if BUILDFLAG(IS_LINUX)
   // Get real command line in renderer process forked by zygote.
@@ -381,14 +380,17 @@ void NodeBindings::Initialize() {
 
   auto env = base::Environment::Create();
   SetNodeOptions(env.get());
-  node::Environment::should_read_node_options_from_env_ =
-      fuses::IsNodeOptionsEnabled();
 
   std::vector<std::string> argv = {"electron"};
   std::vector<std::string> exec_argv;
   std::vector<std::string> errors;
+  uint64_t process_flags = node::ProcessFlags::kEnableStdioInheritance;
+  if (!fuses::IsNodeOptionsEnabled())
+    process_flags |= node::ProcessFlags::kDisableNodeOptionsEnv;
 
-  int exit_code = node::InitializeNodeWithArgs(&argv, &exec_argv, &errors);
+  int exit_code = node::InitializeNodeWithArgs(
+      &argv, &exec_argv, &errors,
+      static_cast<node::ProcessFlags::Flags>(process_flags));
 
   for (const std::string& error : errors)
     fprintf(stderr, "%s: %s\n", argv[0].c_str(), error.c_str());
