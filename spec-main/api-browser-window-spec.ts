fba3708cd3d6850b38dc199d6c7762b7d7ba3bbb
samuelmaddock <samuel.maddock@gmail.com>
2022-02-14 16:13:14 -0500

    test: disable linux capturing

--- spec-main/api-browser-window-spec.ts
+++ spec-main/api-browser-window-spec.ts
@@ -4941,7 +4941,8 @@ describe('BrowserWindow module', () => {
       expect(w.getBounds()).to.deep.equal(newBounds);
     });
 
-    it('should not display a visible background', async () => {
+    // Linux doesn't return any capture sources.
+    ifit(process.platform !== 'linux')('should not display a visible background', async () => {
       const display = screen.getPrimaryDisplay();
 
       const backgroundWindow = new BrowserWindow({
@@ -4982,7 +4983,8 @@ describe('BrowserWindow module', () => {
   describe('"backgroundColor" option', () => {
     afterEach(closeAllWindows);
 
-    it('should display the set color', async () => {
+    // Linux doesn't return any capture sources.
+    ifit(process.platform !== 'linux')('should display the set color', async () => {
       const display = screen.getPrimaryDisplay();
 
       const w = new BrowserWindow({
