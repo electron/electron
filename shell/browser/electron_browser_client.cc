9281df54cbe6b2c10c86f3af74aed2f273e40b68
VerteDinde <vertedinde@electronjs.org>
2022-03-10 19:49:04 -0800

    3506590: Reland "Support ChromeOS external protocol dialog for Fenced Frame navigations"
    Ref: https://chromium-review.googlesource.com/c/chromium/src/+/3506590

--- shell/browser/electron_browser_client.cc
+++ shell/browser/electron_browser_client.cc
@@ -1044,6 +1044,7 @@ bool ElectronBrowserClient::HandleExternalProtocol(
     int frame_tree_node_id,
     content::NavigationUIData* navigation_data,
     bool is_primary_main_frame,
+    bool is_in_fenced_frame_tree,
     network::mojom::WebSandboxFlags sandbox_flags,
     ui::PageTransition page_transition,
     bool has_user_gesture,
