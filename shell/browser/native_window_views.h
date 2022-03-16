bcedc8aa07ea78ba8393bc87ce690cfc63491f50
Shelley Vohr <shelley.vohr@gmail.com>
2022-03-16 10:45:44 +0100

    fix: ensure bounds changes apply when moving windows

--- shell/browser/native_window_views.h
+++ shell/browser/native_window_views.h
@@ -8,6 +8,7 @@
 #include "shell/browser/native_window.h"
 
 #include <memory>
+#include <queue>
 #include <set>
 #include <string>
 #include <vector>
@@ -302,6 +303,8 @@ class NativeWindowViews : public NativeWindow,
 
   // Whether the window is currently being moved.
   bool is_moving_ = false;
+
+  absl::optional<gfx::Rect> pending_bounds_change_;
 
   // The color to use as the theme and symbol colors respectively for Window
   // Controls Overlay if enabled on Windows.
