5c80f29da677e1d439592d1cdebd4b20da642720
Micha Hanselmann <micha.hanselmann@gmail.com>
2022-03-31 16:30:30 +0200

    update test

---
+++ spec/fixtures/api/singleton-userdata/main.js
@@ -0,0 +1,12 @@
+const { app } = require('electron');
+const fs = require('fs');
+const path = require('path');
+
+// non-existing user data folder should not break requestSingleInstanceLock()
+// ref: https://github.com/electron/electron/issues/33547
+const userDataFolder = path.join(app.getPath('home'), 'singleton-userdata');
+fs.rmSync(userDataFolder, { force: true, recursive: true });
+app.setPath('userData', userDataFolder);
+
+const gotTheLock = app.requestSingleInstanceLock();
+app.exit(gotTheLock ? 0 : 1);
