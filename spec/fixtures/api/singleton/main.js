701a76d3dd26ed5382abb1060ff11e79266528cf
Micha Hanselmann <micha.hanselmann@gmail.com>
2022-03-31 15:05:03 +0200

    test: use custom userData folder for requestSingleInstanceLock()

--- spec/fixtures/api/singleton/main.js
+++ spec/fixtures/api/singleton/main.js
@@ -1,8 +1,16 @@
 const { app } = require('electron');
+const fs = require('fs');
+const path = require('path');
 
 app.whenReady().then(() => {
   console.log('started'); // ping parent
 });
+
+// non-existing user data folder should not break requestSingleInstanceLock()
+// ref: https://github.com/electron/electron/issues/33547
+const userDataFolder = path.join(app.getPath('home'), 'electron-userData');
+fs.rmSync(userDataFolder, { force: true, recursive: true });
+app.setPath('userData', userDataFolder);
 
 const gotTheLock = app.requestSingleInstanceLock();
 
