aa94ed2b297d81bf3ea01f5cee4de5c2f0be3392
Charles Kerr <charles@charleskerr.com>
2022-03-31 13:09:03 -0500

    test: re-enable power monitor tests on arm64 linux
    disabled in e89b1af during a Chromium roll.

--- spec-main/api-power-monitor-spec.ts
+++ spec-main/api-power-monitor-spec.ts
@@ -14,8 +14,7 @@ import { promisify } from 'util';
 describe('powerMonitor', () => {
   let logindMock: any, dbusMockPowerMonitor: any, getCalls: any, emitSignal: any, reset: any;
 
-  // TODO(deepak1556): Enable on arm64 after upgrade, it crashes at the moment.
-  ifdescribe(process.platform === 'linux' && process.arch !== 'arm64' && process.env.DBUS_SYSTEM_BUS_ADDRESS != null)('when powerMonitor module is loaded with dbus mock', () => {
+  ifdescribe(process.platform === 'linux' && process.env.DBUS_SYSTEM_BUS_ADDRESS != null)('when powerMonitor module is loaded with dbus mock', () => {
     before(async () => {
       const systemBus = dbus.systemBus();
       const loginService = systemBus.getService('org.freedesktop.login1');
