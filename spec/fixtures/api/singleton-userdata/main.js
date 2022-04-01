b2e7aeaa38408f175abeadefdad3eca8088e598b
Micha Hanselmann <micha.hanselmann@gmail.com>
2022-04-01 10:13:56 +0200

    prefix test folder path

--- spec/fixtures/api/singleton-userdata/main.js
+++ spec/fixtures/api/singleton-userdata/main.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // non-existing user data folder should not break requestSingleInstanceLock()
 // ref: https://github.com/electron/electron/issues/33547
-const userDataFolder = path.join(app.getPath('home'), 'singleton-userdata');
+const userDataFolder = path.join(app.getPath('home'), 'electron-test-singleton-userdata');
 fs.rmSync(userDataFolder, { force: true, recursive: true });
 app.setPath('userData', userDataFolder);
 
