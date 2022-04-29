6887d3d9a73a58476ca274c05e024293d38f8314
Keeley Hammond <khammond@slack-corp.com>
2022-04-29 12:24:33 -0700

    build: revert migrate urllib to python3
    This reverts commit a29136950e87a64e69c1e2e8965f7d45acc1a2dc.

--- script/release/uploaders/upload-index-json.py
+++ script/release/uploaders/upload-index-json.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 import json
 import os
 import sys
-from urllib.request import Request, urlopen
+import urllib2
 
 sys.path.append(
   os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + "/../.."))
@@ -28,12 +28,12 @@ def is_json(myjson):
 
 def get_content(retry_count = 5):
   try:
-    request = Request(
+    request = urllib2.Request(
       BASE_URL + version,
       headers={"Authorization" : authToken}
     )
 
-    proposed_content = urlopen(
+    proposed_content = urllib2.urlopen(
       request
     ).read()
 
