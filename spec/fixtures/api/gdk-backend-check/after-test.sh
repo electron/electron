85fb016fb0ae8809228085e9c6390fefe9dfcf51
Bala Avulapati <bavulapati@gmail.com>
2022-04-29 15:27:57 +0530

    Make sure the applications directory exists

--- spec/fixtures/api/gdk-backend-check/after-test.sh
+++ spec/fixtures/api/gdk-backend-check/after-test.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 set -o errexit
+set -o nounset
 
 rm -f /tmp/groot-says
 rm -f /tmp/groot
-rm -f ~/.local/share/applications/groot.desktop 
+rm -f ~/.local/share/applications/groot.desktop
 
 update-desktop-database ~/.local/share/applications
 
-rm -f ~/.local/share/mime/packages/groot.xml 
+rm -f ~/.local/share/mime/packages/groot.xml
 
 update-mime-database ~/.local/share/mime
--- spec/fixtures/api/gdk-backend-check/before-test.sh
+++ spec/fixtures/api/gdk-backend-check/before-test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -o errexit
+set -o nounset
 
 cleanup () {
   exec "./after-test.sh"
@@ -10,12 +11,18 @@ trap cleanup HUP INT
 
 # Install the application Groot
 cp ./groot /tmp/
+ 
+# Make sure the applications directory exists.
+mkdir -p ~/.local/share/applications
 
 # Register Groot as desktop application
-cp ./groot.desktop ~/.local/share/applications 
+cp ./groot.desktop ~/.local/share/applications
 
 # Update database of desktop entries
 update-desktop-database ~/.local/share/applications
+
+# Make sure the packages directory exists.
+mkdir -p ~/.local/share/mime/packages
 
 # Associate 'application/x-groot' mime type with '*.groot' files
 cp ./groot.xml ~/.local/share/mime/packages/
