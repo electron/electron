5dd27fc8be366a49d7b68a035b983aeef70ae66d
Milan Burda <milan.burda@gmail.com>
2022-05-03 05:00:29 +0200

    test: fix nativeModulesEnabled in spec/webview-spec.js

--- spec/webview-spec.js
+++ spec/webview-spec.js
@@ -7,7 +7,7 @@ const { emittedOnce, waitForEvent } = require('./events-helpers');
 const { ifdescribe, ifit, delay } = require('./spec-helpers');
 
 const features = process._linkedBinding('electron_common_features');
-const nativeModulesEnabled = process.env.ELECTRON_SKIP_NATIVE_MODULE_TESTS;
+const nativeModulesEnabled = !process.env.ELECTRON_SKIP_NATIVE_MODULE_TESTS;
 
 /* Most of the APIs here don't use standard callbacks */
 /* eslint-disable standard/no-callback-literal */
