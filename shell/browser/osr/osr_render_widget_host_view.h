681bffdc33c036525f5116e0dd7a8730cad6b18c
Shelley Vohr <shelley.vohr@gmail.com>
2022-05-04 14:42:23 +0200

    fix: offscreen rendering crash on input select

--- shell/browser/osr/osr_render_widget_host_view.h
+++ shell/browser/osr/osr_render_widget_host_view.h
@@ -121,7 +121,7 @@ class OffScreenRenderWidgetHostView : public content::RenderWidgetHostViewBase,
 
   void ResetFallbackToFirstNavigationSurface() override;
   void InitAsPopup(content::RenderWidgetHostView* parent_host_view,
-                   const gfx::Rect& pos,
+                   const gfx::Rect& bounds,
                    const gfx::Rect& anchor_rect) override;
   void UpdateCursor(const content::WebCursor&) override;
   void SetIsLoading(bool is_loading) override;
