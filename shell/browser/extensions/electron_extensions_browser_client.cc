3c72ed49ac8195a9ba5512d3403a581874b4625f
Shelley Vohr <shelley.vohr@gmail.com>
2022-05-18 20:14:28 +0200

    3612725: [Extensions] ListValue->Value::List changes in extensions event code.
    https://chromium-review.googlesource.com/c/chromium/src/+/3612725

--- shell/browser/extensions/electron_extensions_browser_client.cc
+++ shell/browser/extensions/electron_extensions_browser_client.cc
@@ -297,7 +297,7 @@ ElectronExtensionsBrowserClient::GetComponentExtensionResourceManager() {
 void ElectronExtensionsBrowserClient::BroadcastEventToRenderers(
     extensions::events::HistogramValue histogram_value,
     const std::string& event_name,
-    std::unique_ptr<base::ListValue> args,
+    base::Value::List args,
     bool dispatch_to_off_the_record_profiles) {
   if (!BrowserThread::CurrentlyOn(BrowserThread::UI)) {
     content::GetUIThreadTaskRunner({})->PostTask(
@@ -309,8 +309,11 @@ void ElectronExtensionsBrowserClient::BroadcastEventToRenderers(
     return;
   }
 
-  auto event = std::make_unique<extensions::Event>(
-      histogram_value, event_name, std::move(*args).TakeListDeprecated());
+  std::vector<base::Value> event_args(args.size());
+  std::transform(args.begin(), args.end(), event_args.begin(),
+                 [](const base::Value& arg) { return arg.Clone(); });
+  auto event = std::make_unique<extensions::Event>(histogram_value, event_name,
+                                       std::move(event_args));
   auto& context_map = ElectronBrowserContext::browser_context_map();
   for (auto const& entry : context_map) {
     if (entry.second) {
