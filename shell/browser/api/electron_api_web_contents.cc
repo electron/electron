7bc4b919dcb978426b2cb7114ced85801ec3f776
Shelley Vohr <shelley.vohr@gmail.com>
2022-05-25 06:38:38 +0200

    fix: esc not working on Windows during fullscreen (#34317)
    * fix: esc not working on Windows during fullscreen
    
    * chore: fix lint

--- shell/browser/api/electron_api_web_contents.cc
+++ shell/browser/api/electron_api_web_contents.cc
@@ -1308,8 +1308,6 @@ void WebContents::EnterFullscreenModeForTab(
       base::BindRepeating(&WebContents::OnEnterFullscreenModeForTab,
                           base::Unretained(this), requesting_frame, options);
   permission_helper->RequestFullscreenPermission(callback);
-  exclusive_access_manager_->fullscreen_controller()->EnterFullscreenModeForTab(
-      requesting_frame, options.display_id);
 }
 
 void WebContents::OnEnterFullscreenModeForTab(
@@ -1324,6 +1322,9 @@ void WebContents::OnEnterFullscreenModeForTab(
     DCHECK_EQ(fullscreen_frame_, source->GetFocusedFrame());
     return;
   }
+
+  exclusive_access_manager_->fullscreen_controller()->EnterFullscreenModeForTab(
+      requesting_frame, options.display_id);
 
   SetHtmlApiFullscreen(true);
 
