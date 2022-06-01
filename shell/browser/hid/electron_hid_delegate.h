3726ede01cec130dc2bfe04e950d36db5b366654
Shelley Vohr <shelley.vohr@gmail.com>
2022-06-01 11:32:38 +0200

    3611967: webhid: Migrate HidDelegate to use BrowserContext and Origin
    https://chromium-review.googlesource.com/c/chromium/src/+/3611967
    (cherry picked from commit 18aac8e93e03c35e1bbce77c8d7e21b10edf593c)

--- shell/browser/hid/electron_hid_delegate.h
+++ shell/browser/hid/electron_hid_delegate.h
@@ -34,22 +34,24 @@ class ElectronHidDelegate : public content::HidDelegate,
       std::vector<blink::mojom::HidDeviceFilterPtr> exclusion_filters,
       content::HidChooser::Callback callback) override;
   bool CanRequestDevicePermission(
-      content::RenderFrameHost* render_frame_host) override;
-  bool HasDevicePermission(content::RenderFrameHost* render_frame_host,
+      content::BrowserContext* browser_context,
+      const url::Origin& origin) override;
+  bool HasDevicePermission(content::BrowserContext* browser_context,
+                           const url::Origin& origin,
                            const device::mojom::HidDeviceInfo& device) override;
   void RevokeDevicePermission(
-      content::RenderFrameHost* render_frame_host,
+      content::BrowserContext* browser_context,
+      const url::Origin& origin,
       const device::mojom::HidDeviceInfo& device) override;
   device::mojom::HidManager* GetHidManager(
-      content::RenderFrameHost* render_frame_host) override;
-  void AddObserver(content::RenderFrameHost* render_frame_host,
+      content::BrowserContext* browser_context) override;
+  void AddObserver(content::BrowserContext* browser_context,
                    content::HidDelegate::Observer* observer) override;
-  void RemoveObserver(content::RenderFrameHost* render_frame_host,
-                      content::HidDelegate::Observer* observer) override;
+  void RemoveObserver(content::HidDelegate::Observer* observer) override;
   const device::mojom::HidDeviceInfo* GetDeviceInfo(
-      content::RenderFrameHost* render_frame_host,
+      content::BrowserContext* browser_context,
       const std::string& guid) override;
-  bool IsFidoAllowedForOrigin(content::RenderFrameHost* render_frame_host,
+  bool IsFidoAllowedForOrigin(content::BrowserContext* browser_context,
                               const url::Origin& origin) override;
 
   // HidChooserContext::DeviceObserver:
