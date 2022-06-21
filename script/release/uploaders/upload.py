2bbbc66eb83b42041d768ea64f5ada9ed1ffd301
Keeley Hammond <vertedinde@electronjs.org>
2022-06-20 18:35:16 -0700

    build: build & release libcxx objects on darwin (#34586)
    * build: build & release libcxx objects on darwin
    
    * Fix merge error
    
    Co-authored-by: Cheng Zhao <zcbenz@gmail.com>

--- script/release/uploaders/upload.py
+++ script/release/uploaders/upload.py
@@ -94,7 +94,13 @@ def main():
 
     dsym_snapshot_zip = os.path.join(OUT_DIR, DSYM_SNAPSHOT_NAME)
     shutil.copy2(os.path.join(OUT_DIR, 'dsym-snapshot.zip'), dsym_snapshot_zip)
-    upload_electron(release, dsym_snapshot_zip, args)    
+    upload_electron(release, dsym_snapshot_zip, args)
+
+    libcxx_objects = get_zip_name('libcxx-objects', ELECTRON_VERSION)
+    libcxx_objects_zip = os.path.join(OUT_DIR, libcxx_objects)
+    shutil.copy2(os.path.join(OUT_DIR, 'libcxx_objects.zip'),
+        libcxx_objects_zip)
+    upload_electron(release, libcxx_objects_zip, args)
   elif PLATFORM == 'win32':
     if get_target_arch() != 'ia32':
       pdb_zip = os.path.join(OUT_DIR, PDB_NAME)
@@ -105,7 +111,6 @@ def main():
     shutil.copy2(os.path.join(OUT_DIR, 'debug.zip'), debug_zip)
     upload_electron(release, debug_zip, args)
 
-    # Upload libcxx_objects.zip for linux only
     libcxx_objects = get_zip_name('libcxx-objects', ELECTRON_VERSION)
     libcxx_objects_zip = os.path.join(OUT_DIR, libcxx_objects)
     shutil.copy2(os.path.join(OUT_DIR, 'libcxx_objects.zip'),
