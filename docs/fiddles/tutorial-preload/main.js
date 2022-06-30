67957aac67607ae67af86505241182a444677de0
Keeley Hammond <vertedinde@electronjs.org>
2022-06-30 10:07:35 -0700

    docs: expand tutorial (#34604) (#34797)
    * docs: base tutorial update
    
    * more docs
    
    * zzz
    
    * remove unused images
    
    Co-authored-by: Erick Zhao <erick@hotmail.ca>

---
+++ docs/fiddles/tutorial-preload/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  win.loadFile('index.html');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
