098a53f9080778889904eec4205cb320a67a8ba7
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2022-09-16 18:21:05 -0700

    Fix mac compile

--- shell/browser/electron_browser_main_parts.h
+++ shell/browser/electron_browser_main_parts.h
@@ -130,7 +130,7 @@ class ElectronBrowserMainParts : public content::BrowserMainParts {
   void FreeAppDelegate();
   void RegisterURLHandler();
   void InitializeMainNib();
-  std::string GetSystemLocale();
+  std::string GetCurrentSystemLocale();
 #endif
 
 #if BUILDFLAG(IS_MAC)
