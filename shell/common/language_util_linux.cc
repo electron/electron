95efefa2266e70afe4d911fd96497409c90be8e8
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2022-10-19 18:09:01 -0700

    Don't add C to list

--- shell/common/language_util_linux.cc
+++ shell/common/language_util_linux.cc
@@ -13,7 +13,7 @@ namespace electron {
 std::vector<std::string> GetPreferredLanguages() {
   std::vector<std::string> preferredLanguages;
 
-  // From
+  // Based on
   // https://source.chromium.org/chromium/chromium/src/+/refs/tags/108.0.5329.0:ui/base/l10n/l10n_util.cc;l=543-554
   // GLib implements correct environment variable parsing with
   // the precedence order: LANGUAGE, LC_ALL, LC_MESSAGES and LANG.
@@ -22,7 +22,9 @@ std::vector<std::string> GetPreferredLanguages() {
   DCHECK(*languages);  // At least one entry, "C", is guaranteed.
 
   for (; *languages; ++languages) {
-    preferredLanguages.push_back(base::i18n::GetCanonicalLocale(*languages));
+    if (strcmp(*languages, "C") != 0) {
+      preferredLanguages.push_back(base::i18n::GetCanonicalLocale(*languages));
+    }
   }
 
   return preferredLanguages;
