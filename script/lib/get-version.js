a9eef81f7f7b77175135511a6f783386cccafcc6
Samuel Attard <sattard@salesforce.com>
2022-10-25 10:53:59 -0700

    build: ensure get-version runs in the electron git checkout

--- script/lib/get-version.js
+++ script/lib/get-version.js
@@ -1,5 +1,5 @@
 const { spawnSync } = require('child_process');
-const rootPackageJson = require('../../package.json');
+const path = require('path');
 
 module.exports.getElectronVersion = () => {
   // Find the nearest tag to the current HEAD
@@ -11,7 +11,9 @@ module.exports.getElectronVersion = () => {
   // The only difference in the "git describe" technique is that technically a commit can "change" it's version
   // number if a tag is created / removed retroactively.  i.e. the first time a commit is pushed it will be 1.2.3
   // and after the tag is made rebuilding the same commit will result in it being 1.2.4
-  const output = spawnSync('git', ['describe', '--tags', '--abbrev=0']);
+  const output = spawnSync('git', ['describe', '--tags', '--abbrev=0'], {
+    cwd: path.resolve(__dirname, '..', '..')
+  });
   if (output.status !== 0) {
     console.error(output.stderr);
     throw new Error('Failed to get current electron version');
