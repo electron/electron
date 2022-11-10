9671fa7c3d3c83623672544769fa0eb0c38e02e5
Michał Zarach <michal.m.zarach@gmail.com>
2022-11-10 20:17:48 +0100

    Lint file

--- lib/browser/api/in-app-purchase.ts
+++ lib/browser/api/in-app-purchase.ts
@@ -4,7 +4,7 @@ let _inAppPurchase;
 
 if (process.platform === 'darwin') {
   const { inAppPurchase } = process._linkedBinding('electron_browser_in_app_purchase');
-  const _purchase = inAppPurchase.purchaseProduct as (productID: string, quantity?: number, username?: string) => Promise<boolean>
+  const _purchase = inAppPurchase.purchaseProduct as (productID: string, quantity?: number, username?: string) => Promise<boolean>;
   inAppPurchase.purchaseProduct = (productID: string, opts?: number | { quantity?: number, username?: string }) => {
     const quantity = typeof opts === 'object' ? opts.quantity : opts;
     const username = typeof opts === 'object' ? opts.username : undefined;
