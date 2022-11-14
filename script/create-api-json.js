b782bc2e2f2d3e9589abe3005c734c93b13844eb
Samuel Attard <sam@electronjs.org>
2022-11-14 07:04:36 -0800

    build: provide moduleVersion to docs-parser directly (#36329)

---
+++ script/create-api-json.js
@@ -0,0 +1,17 @@
+const { parseDocs } = require('@electron/docs-parser');
+const fs = require('fs');
+const path = require('path');
+
+const { getElectronVersion } = require('./lib/get-version');
+
+parseDocs({
+  baseDirectory: path.resolve(__dirname, '..'),
+  packageMode: 'single',
+  useReadme: false,
+  moduleVersion: getElectronVersion()
+}).then((api) => {
+  return fs.promises.writeFile(path.resolve(__dirname, '..', 'electron-api.json'), JSON.stringify(api, null, 2));
+}).catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
