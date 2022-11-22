5d9f6d9eb58174fb8615826c5e4c3711e333e106
Samuel Attard <samuel.r.attard@gmail.com>
2022-11-22 00:24:17 +0000

    build: only run sync in prebuild environment

--- .devcontainer/update-content-command.sh
+++ .devcontainer/update-content-command.sh
@@ -3,10 +3,10 @@
 set -eo pipefail
 
 buildtools=$HOME/.electron_build_tools
-gclient_root=/workspaces/gclient
-buildtools_configs=/workspaces/buildtools-configs
 
 export PATH="$PATH:$buildtools/src"
 
-# Sync latest gclient
-e d gclient sync --with_branch_heads --with_tags
+# Sync latest gclient, but only in a prebuild
+if [ ! -z "$CODESPACES_PREBUILD_TOKEN" ]; then
+  e d gclient sync --with_branch_heads --with_tags
+fi
