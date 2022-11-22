89ef6e36580c06b97e5dfd843f97043f304770b3
Samuel Attard <samuel.r.attard@gmail.com>
2022-11-22 00:55:20 +0000

    build: run sync always

--- .devcontainer/update-content-command.sh
+++ .devcontainer/update-content-command.sh
@@ -6,12 +6,5 @@ buildtools=$HOME/.electron_build_tools
 
 export PATH="$PATH:$buildtools/src"
 
-# Sync latest gclient, but only in a prebuild
-if [ ! -z "$CODESPACES_PREBUILD_TOKEN" ]; then
-  e d gclient sync --with_branch_heads --with_tags
-fi
-
-env | while IFS= read -r line; do
-  name=${line%%=*}
-  echo "Env: $name"
-done
+# Sync latest
+e d gclient sync --with_branch_heads --with_tags
