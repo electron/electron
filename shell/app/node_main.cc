ac8b23fdc7a4da2722cb5480e44f02ee436265ab
VerteDinde <vertedinde@electronjs.org>
2022-11-28 07:05:10 -0800

    fix: expose bound methods to node_main

--- shell/app/node_main.cc
+++ shell/app/node_main.cc
@@ -13,6 +13,7 @@
 
 #include "base/base_switches.h"
 #include "base/command_line.h"
+#include "base/environment.h"
 #include "base/feature_list.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -193,6 +194,14 @@ int NodeMain(int argc, char* argv[]) {
 
       gin_helper::Dictionary process(isolate, env->process_object());
       process.SetMethod("crash", &ElectronBindings::Crash);
+#if BUILDFLAG(IS_LINUX)
+      // we don't want to pass the entire electron_bindings object here;
+      // just expose the methods we need for child processes.
+      process.SetMethod("getCrashdumpSignalFD",
+                        &ElectronBindings::GetCrashdumpSignalFD);
+      process.SetMethod("getCrashpadHandlerPID",
+                        &ElectronBindings::GetCrashpadHandlerPID);
+#endif
 
       // Setup process.crashReporter in child node processes
       gin_helper::Dictionary reporter = gin::Dictionary::CreateEmpty(isolate);
