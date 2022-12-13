425f1ffa987434d87348868696658af7412fc2bb
John Kleinschmidt <jkleinsc@electronjs.org>
2022-12-13 14:28:09 -0500

    chore: update AppVeyor image to include latest Windows updates (#36649)
    clenaup
    
    Co-authored-by: VerteDinde <vertedinde@electronjs.org>

--- script/prepare-appveyor.js
+++ script/prepare-appveyor.js
@@ -14,8 +14,8 @@ const ROLLER_BRANCH_PATTERN = /^roller\/chromium$/;
 
 const DEFAULT_BUILD_CLOUD_ID = '1598';
 const DEFAULT_BUILD_CLOUD = 'electronhq-16-core';
-const DEFAULT_BAKE_BASE_IMAGE = 'Windows_Default_Appveyor';
-const DEFAULT_BUILD_IMAGE = 'Windows_Default_Appveyor';
+const DEFAULT_BAKE_BASE_IMAGE = 'e-110.0.5451.0';
+const DEFAULT_BUILD_IMAGE = 'e-110.0.5451.0';
 
 const appveyorBakeJob = 'electron-bake-image';
 const appVeyorJobs = {
