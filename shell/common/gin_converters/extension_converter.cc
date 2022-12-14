38f7ade3363fc98f95385b8b8031881c04b32084
Shelley Vohr <shelley.vohr@gmail.com>
2022-12-14 12:23:35 +0100

    4025927: [Code Health] Migrate e/c/manifest.cc to base::Value::Dict interface
    https://chromium-review.googlesource.com/c/chromium/src/+/4025927

--- shell/common/gin_converters/extension_converter.cc
+++ shell/common/gin_converters/extension_converter.cc
@@ -22,8 +22,7 @@ v8::Local<v8::Value> Converter<const extensions::Extension*>::ToV8(
   dict.Set("path", extension->path());
   dict.Set("url", extension->url());
   dict.Set("version", extension->VersionString());
-  dict.Set("manifest",
-           *static_cast<const base::Value*>(extension->manifest()->value()));
+  dict.Set("manifest", extension->manifest()->value()->Clone());
 
   return gin::ConvertToV8(isolate, dict);
 }
