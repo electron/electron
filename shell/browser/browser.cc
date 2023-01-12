262271521cebbdcf65027b4528b43da3416dfffb
Charles Kerr <charles@charleskerr.com>
2023-01-11 19:00:15 -0600

    chore: lint

--- shell/browser/browser.cc
+++ shell/browser/browser.cc
@@ -37,7 +37,8 @@ namespace {
 void RunQuitClosure(base::OnceClosure quit) {
   // On Linux/Windows the "ready" event is emitted in "PreMainMessageLoopRun",
   // make sure we quit after message loop has run for once.
-  base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(FROM_HERE, std::move(quit));
+  base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(FROM_HERE,
+                                                              std::move(quit));
 }
 
 }  // namespace
