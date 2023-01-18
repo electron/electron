1d98b27a6643d6b6cbc4d9d426d6d5acc4b45e2a
Shelley Vohr <shelley.vohr@gmail.com>
2023-01-18 14:46:27 +0100

    chore: update node types version (#36924)

--- spec/types-spec.ts
+++ spec/types-spec.ts
@@ -5,7 +5,6 @@ describe('bundled @types/node', () => {
     expect(require('../npm/package.json').dependencies).to.have.property('@types/node');
     const range = require('../npm/package.json').dependencies['@types/node'];
     expect(range).to.match(/^\^.+/, 'should allow any type dep in a major range');
-    // TODO(codebytere): re-enable after https://github.com/DefinitelyTyped/DefinitelyTyped/pull/52594 is merged.
-    // expect(range.slice(1).split('.')[0]).to.equal(process.versions.node.split('.')[0]);
+    expect(range.slice(1).split('.')[0]).to.equal(process.versions.node.split('.')[0]);
   });
 });
