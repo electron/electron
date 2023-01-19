09ffd2ad4c3a0afecf06d17693a02fe907e9a05b
Shelley Vohr <shelley.vohr@gmail.com>
2023-01-19 12:10:56 +0100

    spec: add a datalist spec for time type

--- spec/autofill-spec.ts
+++ spec/autofill-spec.ts
@@ -9,10 +9,11 @@ const fixturesPath = path.resolve(__dirname, 'fixtures');
 describe('autofill', () => {
   afterEach(closeAllWindows);
 
-  it('can be selected via keyboard', async () => {
+  it('can be selected via keyboard for a <datalist> with text type', async () => {
     const w = new BrowserWindow({ show: true });
-    await w.loadFile(path.join(fixturesPath, 'pages', 'datalist.html'));
+    await w.loadFile(path.join(fixturesPath, 'pages', 'datalist-text.html'));
     w.webContents.sendInputEvent({ type: 'keyDown', keyCode: 'Tab' });
+
     const inputText = 'clap';
     for (const keyCode of inputText) {
       w.webContents.sendInputEvent({ type: 'char', keyCode });
@@ -24,5 +25,23 @@ describe('autofill', () => {
 
     const value = await w.webContents.executeJavaScript("document.querySelector('input').value");
     expect(value).to.equal('Eric Clapton');
+  });
+
+  it('can be selected via keyboard for a <datalist> with time type', async () => {
+    const w = new BrowserWindow({ show: true });
+    await w.loadFile(path.join(fixturesPath, 'pages', 'datalist-time.html'));
+
+    const inputText = '11P'; // 1:01 PM
+    for (const keyCode of inputText) {
+      w.webContents.sendInputEvent({ type: 'keyDown', keyCode: 'Tab' });
+      w.webContents.sendInputEvent({ type: 'keyDown', keyCode });
+      w.webContents.sendInputEvent({ type: 'char', keyCode });
+      await delay(100);
+    }
+
+    w.webContents.sendInputEvent({ type: 'keyDown', keyCode: 'Tab' });
+
+    const value = await w.webContents.executeJavaScript("document.querySelector('input').value");
+    expect(value).to.equal('13:01');
   });
 });
