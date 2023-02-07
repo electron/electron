52740f127f3b98ee6cd9499dcacbc6b92eb0071b
Samuel Attard <marshallofsound@electronjs.org>
2023-02-07 15:42:03 -0800

    refactor: remove GetFontLookupTableCacheDir
    Ref: https://chromium-review.googlesource.com/c/chromium/src/+/4191820

--- shell/browser/electron_browser_client.h
+++ shell/browser/electron_browser_client.h
@@ -71,7 +71,6 @@ class ElectronBrowserClient : public content::ContentBrowserClient,
 
   // content::ContentBrowserClient:
   std::string GetApplicationLocale() override;
-  base::FilePath GetFontLookupTableCacheDir() override;
   bool ShouldEnableStrictSiteIsolation() override;
   void BindHostReceiverForRenderer(
       content::RenderProcessHost* render_process_host,
