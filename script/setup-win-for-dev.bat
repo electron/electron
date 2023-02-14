76866dde757016f3ef29762334c6c9318161347b
VerteDinde <vertedinde@electronjs.org>
2023-02-13 21:25:51 -0800

    build: force uninstall/reinstall 18.12.1

--- script/setup-win-for-dev.bat
+++ script/setup-win-for-dev.bat
@@ -56,7 +56,8 @@ REM Install Windows SDK
 choco install windows-sdk-10-version-2104-all
 
 REM Install nodejs python git and yarn needed dependencies
-choco install -y --force nodejs@v18.12.1
+choco uninstall -y nodejs-lts
+choco install -y --force nodejs --version=18.12.1
 choco install -y python2 git yarn
 choco install python --version 3.7.9
 call C:\ProgramData\chocolatey\bin\RefreshEnv.cmd
