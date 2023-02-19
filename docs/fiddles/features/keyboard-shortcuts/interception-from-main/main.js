757515b5a35e58b97e6fadaf6ae34be78479ff95
shivam <shivampandey870@gmail.com>
2023-02-19 05:35:18 -0800

    fix: A bug fix Issue #37336

--- docs/fiddles/features/keyboard-shortcuts/interception-from-main/main.js
+++ docs/fiddles/features/keyboard-shortcuts/interception-from-main/main.js
@@ -4,9 +4,21 @@ app.whenReady().then(() => {
   const win = new BrowserWindow({ width: 800, height: 600 })
 
   win.loadFile('index.html')
+  
+  let preventNextKeyUp = false
+
   win.webContents.on('before-input-event', (event, input) => {
     if (input.control && input.key.toLowerCase() === 'i') {
       console.log('Pressed Control+I')
+      event.preventDefault()
+      preventNextKeyUp = true
+    }
+  })
+  
+
+ win.webContents.on('keyup', (event) => {
+    if (preventNextKeyUp) {
+      preventNextKeyUp = false
       event.preventDefault()
     }
   })
