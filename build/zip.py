8eeb55514560f908604e1f2f5f13ce1129b8174a
Jeremy Rose <jeremya@chromium.org>
2023-02-28 10:37:16 -0800

    move dbgcore/dbghelp to extensions_to_skip

--- build/zip.py
+++ build/zip.py
@@ -10,7 +10,13 @@ EXTENSIONS_TO_SKIP = [
   '.mojom.js',
   '.mojom-lite.js',
   '.info',
-  '.m.js'
+  '.m.js',
+
+  # These are only needed for Chromium tests we don't run. Listed in
+  # 'extensions' because the mksnapshot zip has these under a subdirectory, and
+  # the PATHS_TO_SKIP is checked with |startswith|.
+  'dbgcore.dll',
+  'dbghelp.dll',
 ]
 
 PATHS_TO_SKIP = [
@@ -35,9 +41,6 @@ PATHS_TO_SKIP = [
   'resources/inspector',
   'gen/third_party/devtools-frontend/src',
   'gen/ui/webui',
-  # These are only needed for Chromium tests we don't run.
-  'dbgcore.dll',
-  'dbghelp.dll',
 ]
 
 def skip_path(dep, dist_zip, target_cpu):
