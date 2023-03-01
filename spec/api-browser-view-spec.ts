5e25d23794a4b3b1bda35c79fcc1b2fdd787ad69
Shelley Vohr <shelley.vohr@gmail.com>
2023-03-01 11:35:06 +0100

    fix: handle closing `webContents` in `BrowserView`s (#37420)
    * fix: handle closing webContents in BrowserViews
    
    * test: add window.close() test

--- spec/api-browser-view-spec.ts
+++ spec/api-browser-view-spec.ts
@@ -345,6 +345,24 @@ describe('BrowserView module', () => {
       const [code] = await once(rc.process, 'exit');
       expect(code).to.equal(0);
     });
+
+    it('emits the destroyed event when webContents.close() is called', async () => {
+      view = new BrowserView();
+      w.setBrowserView(view);
+      await view.webContents.loadFile(path.join(fixtures, 'pages', 'a.html'));
+
+      view.webContents.close();
+      await once(view.webContents, 'destroyed');
+    });
+
+    it('emits the destroyed event when window.close() is called', async () => {
+      view = new BrowserView();
+      w.setBrowserView(view);
+      await view.webContents.loadFile(path.join(fixtures, 'pages', 'a.html'));
+
+      view.webContents.executeJavaScript('window.close()');
+      await once(view.webContents, 'destroyed');
+    });
   });
 
   describe('window.open()', () => {
