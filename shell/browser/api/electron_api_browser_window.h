e2bf1aa5fdb4b9c480535f7d65f2a89729174da1
Jeremy Rose <jeremya@chromium.org>
2023-04-10 16:45:09 -0700

    refactor: remove BrowserWindow::OnWindow{Show,Hide}

--- shell/browser/api/electron_api_browser_window.h
+++ shell/browser/api/electron_api_browser_window.h
@@ -72,8 +72,6 @@ class BrowserWindow : public BaseWindow,
   void SetBackgroundColor(const std::string& color_name) override;
   void SetBrowserView(
       absl::optional<gin::Handle<BrowserView>> browser_view) override;
-  void OnWindowShow() override;
-  void OnWindowHide() override;
 
   // BrowserWindow APIs.
   void FocusOnWebView();
