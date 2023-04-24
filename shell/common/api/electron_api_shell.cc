27575566c9309237bcef95975fe1886ca16bbff9
Milan Burda <milan.burda@gmail.com>
2023-04-24 15:25:29 +0200

    fix: defaults broken in shell.openExternal() options (#38092)
    fix: defaults broken in shell.openExternal() options (#38038)
    
    Co-authored-by: Milan Burda <miburda@microsoft.com>

--- shell/common/api/electron_api_shell.cc
+++ shell/common/api/electron_api_shell.cc
@@ -59,12 +59,10 @@ v8::Local<v8::Promise> OpenExternal(const GURL& url, gin::Arguments* args) {
   v8::Local<v8::Promise> handle = promise.GetHandle();
 
   platform_util::OpenExternalOptions options;
-  if (args->Length() >= 2) {
-    gin::Dictionary obj(nullptr);
-    if (args->GetNext(&obj)) {
-      obj.Get("activate", &options.activate);
-      obj.Get("workingDirectory", &options.working_dir);
-    }
+  gin_helper::Dictionary obj;
+  if (args->GetNext(&obj)) {
+    obj.Get("activate", &options.activate);
+    obj.Get("workingDirectory", &options.working_dir);
   }
 
   platform_util::OpenExternal(
