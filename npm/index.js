16a29e2869381eba227b2c09ef01ca1f78552bea
Ashish Kunal <ashishkunal678@gmail.com>
2023-04-24 17:13:29 +0530

    updated npm/index.js

--- npm/index.js
+++ npm/index.js
@@ -1,9 +1,35 @@
 const fs = require('fs');
 const path = require('path');
+const { Menu } = require('electron');
 
 const pathFile = path.join(__dirname, 'path.txt');
 
-function getElectronPath () {
+function getElectronPath() {
+
+  // Create the application menu.
+  const template = [
+    {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'Node.js Documentation',
+          click() { require('electron').shell.openExternal('https://nodejs.org/docs/') }
+        },
+        {
+          label: 'Chromium Documentation',
+          click() { require('electron').shell.openExternal('https://www.chromium.org/Home') }
+        },
+        {
+          label: 'Electron Documentation',
+          click() { require('electron').shell.openExternal('https://www.electronjs.org/docs') }
+        }
+      ]
+    }
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
   let executablePath;
   if (fs.existsSync(pathFile)) {
     executablePath = fs.readFileSync(pathFile, 'utf-8');
