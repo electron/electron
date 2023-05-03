a4ac36f52e8c1ceee7027edfe2b3d8641d5d79ee
David Sanders <dsanders11@ucsbalum.com>
2023-05-03 11:58:43 -0700

    fix: falsy transparent shouldn't affect webContents background (#38159)

--- spec/guest-window-manager-spec.ts
+++ spec/guest-window-manager-spec.ts
@@ -1,7 +1,9 @@
-import { BrowserWindow } from 'electron';
+import { BrowserWindow, screen } from 'electron';
 import { expect, assert } from 'chai';
+import { emittedOnce } from './events-helpers';
+import { areColorsSimilar, captureScreen, HexColors, getPixelColor } from './screen-helpers';
+import { ifit, delay } from './spec-helpers';
 import { closeAllWindows } from './window-helpers';
-const { emittedOnce } = require('./events-helpers');
 
 describe('webContents.setWindowOpenHandler', () => {
   let browserWindow: BrowserWindow;
@@ -193,5 +195,32 @@ describe('webContents.setWindowOpenHandler', () => {
     browserWindow.webContents.setWindowOpenHandler(() => ({ action: 'deny' }));
     browserWindow.webContents.executeJavaScript("window.open('https://127.0.0.1')");
     expect(await browserWindow.webContents.executeJavaScript('42')).to.equal(42);
+  });
+
+  // Linux and arm64 platforms (WOA and macOS) do not return any capture sources
+  ifit(process.platform === 'darwin' && process.arch === 'x64')('should not make child window background transparent', (done) => {
+    browserWindow.webContents.setWindowOpenHandler(() => ({ action: 'allow' }));
+
+    browserWindow.webContents.once('did-create-window', async (childWindow) => {
+      const display = screen.getPrimaryDisplay();
+      childWindow.setBounds(display.bounds);
+      await childWindow.webContents.executeJavaScript("const meta = document.createElement('meta'); meta.name = 'color-scheme'; meta.content = 'dark'; document.head.appendChild(meta); true;");
+      await delay(1000);
+      const screenCapture = await captureScreen();
+      const centerColor = getPixelColor(screenCapture, {
+        x: display.size.width / 2,
+        y: display.size.height / 2
+      });
+
+      try {
+        // color-scheme is set to dark so background should not be white
+        expect(areColorsSimilar(centerColor, HexColors.WHITE)).to.be.false();
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+
+    browserWindow.webContents.executeJavaScript("window.open('about:blank') && true");
   });
 });
