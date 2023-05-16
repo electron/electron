07a42e351e82ce00dba27581431fb41859c6de77
Charles Kerr <charles@charleskerr.com>
2023-05-16 18:02:06 -0500

    refactor: add WebViewGuestDelegate::GetGuestDelegateWeakPtr()
    Xref: https://chromium-review.googlesource.com/c/chromium/src/+/4515455
    
    This approach copied from GuestViewBase::GetGuestDelegateWeakPtr() approach in that same commit.
    
    (cherry picked from commit 3f3ab39e3a1077f71aa90319d7a81d53cfb3c55e)

--- shell/browser/web_view_guest_delegate.h
+++ shell/browser/web_view_guest_delegate.h
@@ -37,6 +37,8 @@ class WebViewGuestDelegate : public content::BrowserPluginGuestDelegate,
   content::WebContents* GetOwnerWebContents() final;
   std::unique_ptr<content::WebContents> CreateNewGuestWindow(
       const content::WebContents::CreateParams& create_params) final;
+  base::WeakPtr<content::BrowserPluginGuestDelegate> GetGuestDelegateWeakPtr()
+      final;
 
   // WebContentsZoomController::Observer:
   void OnZoomLevelChanged(content::WebContents* web_contents,
@@ -55,6 +57,8 @@ class WebViewGuestDelegate : public content::BrowserPluginGuestDelegate,
   WebContentsZoomController* embedder_zoom_controller_ = nullptr;
 
   api::WebContents* api_web_contents_ = nullptr;
+
+  base::WeakPtrFactory<WebViewGuestDelegate> weak_ptr_factory_{this};
 };
 
 }  // namespace electron
