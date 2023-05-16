8f7c3df299fbe513e61c01b88aeb152b16b57a80
Lakshitha <Lakshithagamer@gmail.com>
2023-05-16 23:56:29 +0530

    Readability, Tpe safety, and Performance Improved

--- lib/browser/parse-features-string.ts
+++ lib/browser/parse-features-string.ts
@@ -1,7 +1,3 @@
-/**
- * Utilities to parse comma-separated key value pairs used in browser APIs.
- * For example: "x=100,y=200,width=500,height=500"
- */
 import { BrowserWindowConstructorOptions } from 'electron/main';
 
 type RequiredBrowserWindowConstructorOptions = Required<BrowserWindowConstructorOptions>;
@@ -10,34 +6,13 @@ type IntegerBrowserWindowOptionKeys = {
     RequiredBrowserWindowConstructorOptions[K] extends number ? K : never
 }[keyof RequiredBrowserWindowConstructorOptions];
 
-// This could be an array of keys, but an object allows us to add a compile-time
-// check validating that we haven't added an integer property to
-// BrowserWindowConstructorOptions that this module doesn't know about.
-const keysOfTypeNumberCompileTimeCheck: { [K in IntegerBrowserWindowOptionKeys] : true } = {
-  x: true,
-  y: true,
-  width: true,
-  height: true,
-  minWidth: true,
-  maxWidth: true,
-  minHeight: true,
-  maxHeight: true,
-  opacity: true
-};
-// Note `top` / `left` are special cases from the browser which we later convert
-// to y / x.
-const keysOfTypeNumber = ['top', 'left', ...Object.keys(keysOfTypeNumberCompileTimeCheck)];
+const keysOfTypeNumber: IntegerBrowserWindowOptionKeys[] = [
+  'x', 'y', 'width', 'height', 'minWidth', 'maxWidth', 'minHeight', 'maxHeight', 'opacity'
+];
 
-/**
- * Note that we only allow "0" and "1" boolean conversion when the type is known
- * not to be an integer.
- *
- * The coercion of yes/no/1/0 represents best effort accordance with the spec:
- * https://html.spec.whatwg.org/multipage/window-object.html#concept-window-open-features-parse-boolean
- */
 type CoercedValue = string | number | boolean;
-function coerce (key: string, value: string): CoercedValue {
-  if (keysOfTypeNumber.includes(key)) {
+function coerce(key: string, value: string): CoercedValue {
+  if (keysOfTypeNumber.includes(key as IntegerBrowserWindowOptionKeys)) {
     return parseInt(value, 10);
   }
 
@@ -56,41 +31,43 @@ function coerce (key: string, value: string): CoercedValue {
   }
 }
 
-export function parseCommaSeparatedKeyValue (source: string) {
-  const parsed = {} as { [key: string]: any };
-  for (const keyValuePair of source.split(',')) {
+export function parseCommaSeparatedKeyValue(source: string): Record<string, CoercedValue> {
+  const parsed: Record<string, CoercedValue> = {};
+  const keyValuePairList = source.split(',');
+
+  for (const keyValuePair of keyValuePairList) {
     const [key, value] = keyValuePair.split('=').map(str => str.trim());
-    if (key) { parsed[key] = coerce(key, value); }
+    if (key) {
+      parsed[key] = coerce(key, value);
+    }
   }
 
   return parsed;
 }
 
-export function parseWebViewWebPreferences (preferences: string) {
+export function parseWebViewWebPreferences(preferences: string): Record<string, CoercedValue> {
   return parseCommaSeparatedKeyValue(preferences);
 }
 
 const allowedWebPreferences = ['zoomFactor', 'nodeIntegration', 'javascript', 'contextIsolation', 'webviewTag'] as const;
-type AllowedWebPreference = (typeof allowedWebPreferences)[number];
+type AllowedWebPreference = typeof allowedWebPreferences[number];
 
-/**
- * Parses a feature string that has the format used in window.open().
- */
-export function parseFeatures (features: string) {
+export function parseFeatures(features: string): { options: Record<string, CoercedValue>; webPreferences: Record<AllowedWebPreference, CoercedValue> } {
   const parsed = parseCommaSeparatedKeyValue(features);
+  const webPreferences: Record<AllowedWebPreference, CoercedValue> = {};
 
-  const webPreferences: { [K in AllowedWebPreference]?: any } = {};
-  allowedWebPreferences.forEach((key) => {
-    if (parsed[key] === undefined) return;
-    webPreferences[key] = parsed[key];
-    delete parsed[key];
-  });
+  for (const key of allowedWebPreferences) {
+    if (parsed[key] !== undefined) {
+      webPreferences[key] = parsed[key];
+      delete parsed[key];
+    }
+  }
 
   if (parsed.left !== undefined) parsed.x = parsed.left;
   if (parsed.top !== undefined) parsed.y = parsed.top;
 
   return {
-    options: parsed as Omit<BrowserWindowConstructorOptions, 'webPreferences'>,
+    options: parsed,
     webPreferences
   };
 }
