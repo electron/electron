470b1d9e9d4e49e0ca61004b234c3c4fc4a42680
Milan Burda <milan.burda@gmail.com>
2023-05-25 13:36:12 +0200

    refactor: split clipboard module implementation for browser / renderer (#38429)

--- lib/browser/api/module-list.ts
+++ lib/browser/api/module-list.ts
@@ -7,6 +7,7 @@ export const browserModuleList: ElectronInternal.ModuleEntry[] = [
   { name: 'BaseWindow', loader: () => require('./base-window') },
   { name: 'BrowserView', loader: () => require('./browser-view') },
   { name: 'BrowserWindow', loader: () => require('./browser-window') },
+  { name: 'clipboard', loader: () => require('./clipboard') },
   { name: 'contentTracing', loader: () => require('./content-tracing') },
   { name: 'crashReporter', loader: () => require('./crash-reporter') },
   { name: 'desktopCapturer', loader: () => require('./desktop-capturer') },
