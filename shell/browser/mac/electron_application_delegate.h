6c99d60837bb2aa429d7d4bd055d7edb5b6a2605
Shelley Vohr <shelley.vohr@gmail.com>
2023-06-06 21:06:18 +0200

    refactor: add ARC scaffolding for macOS

--- shell/browser/mac/electron_application_delegate.h
+++ shell/browser/mac/electron_application_delegate.h
@@ -9,10 +9,7 @@
 
 #import "shell/browser/ui/cocoa/electron_menu_controller.h"
 
-@interface ElectronApplicationDelegate : NSObject <NSApplicationDelegate> {
- @private
-  base::scoped_nsobject<ElectronMenuController> menu_controller_;
-}
+@interface ElectronApplicationDelegate : NSObject <NSApplicationDelegate>
 
 // Sets the menu that will be returned in "applicationDockMenu:".
 - (void)setApplicationDockMenu:(electron::ElectronMenuModel*)model;
