6e1e0ac98c1bfbcf683c8c106b0eb7c392c13c33
deepak1556 <hop2deep@gmail.com>
2023-06-20 23:30:31 +0900

    feat: expose safestorage backend information on linux

--- spec/fixtures/api/safe-storage/encrypt-app/main.js
+++ spec/fixtures/api/safe-storage/encrypt-app/main.js
@@ -6,6 +6,9 @@ const pathToEncryptedString = path.resolve(__dirname, '..', 'encrypted.txt');
 const writeFile = fs.writeFile;
 
 app.whenReady().then(async () => {
+  if (process.platform === 'linux') {
+    safeStorage.setUsePlainTextEncryption(true);
+  }
   const encrypted = safeStorage.encryptString('plaintext');
   await writeFile(pathToEncryptedString, encrypted);
   app.quit();
