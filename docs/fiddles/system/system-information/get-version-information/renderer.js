b0f4faf748f88f8ff6f0ec644eef0228c379f664
Milan Burda <milan.burda@gmail.com>
2023-07-11 23:55:04 +0200

    fix: open links in default browser

--- docs/fiddles/system/system-information/get-version-information/renderer.js
+++ docs/fiddles/system/system-information/get-version-information/renderer.js
@@ -1,3 +1,5 @@
+const { shell } = require('electron')
+
 const versionInfoBtn = document.getElementById('version-info')
 
 const electronVersion = process.versions.electron
@@ -6,3 +8,15 @@ versionInfoBtn.addEventListener('click', () => {
   const message = `This app is using Electron version: ${electronVersion}`
   document.getElementById('got-version-info').innerHTML = message
 })
+
+const links = document.querySelectorAll('a[href]')
+
+for (const link of links) {
+  const url = link.getAttribute('href')
+  if (url.indexOf('http') === 0) {
+    link.addEventListener('click', (e) => {
+      e.preventDefault()
+      shell.openExternal(url)
+    })
+  }
+}
