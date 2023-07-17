80de76e5129d469c01ccb9ceffa12d3a2dd86b48
deepak1556 <hop2deep@gmail.com>
2023-07-17 14:06:39 +0900

    Revert "feat: expose safestorage backend information on linux (#39107)"
    This reverts commit 459fe0e68d2c1961e68b071ebc93ea9456457454.

--- shell/browser/browser_process_impl.h
+++ shell/browser/browser_process_impl.h
@@ -23,10 +23,6 @@
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "shell/browser/net/system_network_context_manager.h"
 
-#if BUILDFLAG(IS_LINUX)
-#include "components/os_crypt/sync/key_storage_util_linux.h"
-#endif
-
 namespace printing {
 class PrintJobManager;
 }
@@ -56,11 +52,6 @@ class BrowserProcessImpl : public BrowserProcess {
 
   void SetSystemLocale(const std::string& locale);
   const std::string& GetSystemLocale() const;
-
-#if BUILDFLAG(IS_LINUX)
-  void SetLinuxStorageBackend(os_crypt::SelectedLinuxBackend selected_backend);
-  const std::string& GetLinuxStorageBackend() const;
-#endif
 
   void EndSession() override {}
   void FlushLocalStateAndReply(base::OnceClosure reply) override {}
@@ -129,9 +120,6 @@ class BrowserProcessImpl : public BrowserProcess {
   std::unique_ptr<PrefService> local_state_;
   std::string locale_;
   std::string system_locale_;
-#if BUILDFLAG(IS_LINUX)
-  std::string selected_linux_storage_backend_;
-#endif
   embedder_support::OriginTrialsSettingsStorage origin_trials_settings_storage_;
 
   std::unique_ptr<network::NetworkQualityTracker> network_quality_tracker_;
