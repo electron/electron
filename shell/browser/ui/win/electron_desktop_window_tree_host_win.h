11b070b3d6ba3f45f3a8d2f44a2a0620192c6bdb
David Sanders <dsanders11@ucsbalum.com>
2023-07-29 23:01:26 -0700

    fix: initial dark mode title bar on Windows 10

--- shell/browser/ui/win/electron_desktop_window_tree_host_win.h
+++ shell/browser/ui/win/electron_desktop_window_tree_host_win.h
@@ -8,6 +8,7 @@
 #include <windows.h>
 
 #include "shell/browser/native_window_views.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
 #include "ui/views/widget/desktop_aura/desktop_window_tree_host_win.h"
 
 namespace electron {
@@ -31,6 +32,7 @@ class ElectronDesktopWindowTreeHostWin : public views::DesktopWindowTreeHostWin,
                     WPARAM w_param,
                     LPARAM l_param,
                     LRESULT* result) override;
+  bool ShouldPaintAsActive() const override;
   bool GetDwmFrameInsetsInPixels(gfx::Insets* insets) const override;
   bool GetClientAreaInsets(gfx::Insets* insets,
                            HMONITOR monitor) const override;
@@ -41,6 +43,7 @@ class ElectronDesktopWindowTreeHostWin : public views::DesktopWindowTreeHostWin,
 
  private:
   raw_ptr<NativeWindowViews> native_window_view_;  // weak ref
+  absl::optional<bool> force_should_paint_as_active_;
 };
 
 }  // namespace electron
