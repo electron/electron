a83dfb6d32ed6be353c5c69c2bf9e995fcb17795
Milan Burda <milan.burda@gmail.com>
2023-08-09 00:03:57 +0200

    docs: fix take-screenshot fiddle to use desktopCapturer in main.js

--- docs/fiddles/media/screenshot/take-screenshot/renderer.js
+++ docs/fiddles/media/screenshot/take-screenshot/renderer.js
@@ -1,6 +1,6 @@
-const { desktopCapturer, shell, ipcRenderer } = require('electron')
+const { shell, ipcRenderer } = require('electron')
 
-const fs = require('node:fs')
+const fs = require('node:fs').promises
 const os = require('node:os')
 const path = require('node:path')
 
@@ -12,24 +12,19 @@ screenshot.addEventListener('click', async (event) => {
   const thumbSize = await determineScreenShotSize()
   const options = { types: ['screen'], thumbnailSize: thumbSize }
 
-  desktopCapturer.getSources(options, (error, sources) => {
-    if (error) return console.log(error)
+  const sources = await ipcRenderer.invoke('get-sources', options)
+  for (const source of sources) {
+    const sourceName = source.name.toLowerCase()
+    if (sourceName === 'entire screen' || sourceName === 'screen 1') {
+      const screenshotPath = path.join(os.tmpdir(), 'screenshot.png')
 
-    sources.forEach((source) => {
-      const sourceName = source.name.toLowerCase()
-      if (sourceName === 'entire screen' || sourceName === 'screen 1') {
-        const screenshotPath = path.join(os.tmpdir(), 'screenshot.png')
+      await fs.writeFile(screenshotPath, source.thumbnail.toPNG())
+      shell.openExternal(`file://${screenshotPath}`)
 
-        fs.writeFile(screenshotPath, source.thumbnail.toPNG(), (error) => {
-          if (error) return console.log(error)
-          shell.openExternal(`file://${screenshotPath}`)
-
-          const message = `Saved screenshot to: ${screenshotPath}`
-          screenshotMsg.textContent = message
-        })
-      }
-    })
-  })
+      const message = `Saved screenshot to: ${screenshotPath}`
+      screenshotMsg.textContent = message
+    }
+  }
 })
 
 async function determineScreenShotSize () {
