b42840893eeab6c1d2618d4056921b00ed758df2
Shelley Vohr <shelley.vohr@gmail.com>
2023-08-08 20:02:16 +0200

    chore: remove i18n JSON spec (#39391)

--- spec/extensions-spec.ts
+++ spec/extensions-spec.ts
@@ -702,21 +702,6 @@ describe('chrome extensions', () => {
       expect(scope).equals(extension.url);
     });
 
-    it('can run chrome extension APIs', async () => {
-      const customSession = session.fromPartition(`persist:${uuid.v4()}`);
-      const w = new BrowserWindow({ show: false, webPreferences: { session: customSession, nodeIntegration: true } });
-      await customSession.loadExtension(path.join(fixtures, 'extensions', 'mv3-service-worker'));
-
-      await w.loadURL(url);
-
-      w.webContents.executeJavaScript('window.postMessage(\'fetch-confirmation\', \'*\')');
-
-      const [, , responseString] = await emittedOnce(w.webContents, 'console-message');
-      const { message } = JSON.parse(responseString);
-
-      expect(message).to.equal('Hello from background.js');
-    });
-
     describe('chrome.i18n', () => {
       let customSession: Session;
       let w = null as unknown as BrowserWindow;
