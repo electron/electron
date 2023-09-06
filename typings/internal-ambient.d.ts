e71a56d11e5457b9dc7ff56735c97a2200a2428a
Milan Burda <milan.burda@gmail.com>
2023-09-07 00:04:25 +0200

    refactor: const Module = require('module') as NodeJS.ModuleInternal; (#38757)
    Co-authored-by: Milan Burda <miburda@microsoft.com>

--- typings/internal-ambient.d.ts
+++ typings/internal-ambient.d.ts
@@ -3,6 +3,17 @@ declare const BUILDFLAG: (flag: boolean) => boolean;
 declare const ENABLE_VIEWS_API: boolean;
 
 declare namespace NodeJS {
+  interface ModuleInternal extends NodeJS.Module {
+    new(id: string, parent?: NodeJS.Module | null): NodeJS.Module;
+    _load(request: string, parent?: NodeJS.Module | null, isMain?: boolean): any;
+    _resolveFilename(request: string, parent?: NodeJS.Module | null, isMain?: boolean, options?: { paths: string[] }): string;
+    _preloadModules(requests: string[]): void;
+    _nodeModulePaths(from: string): string[];
+    _extensions: Record<string, (module: NodeJS.Module, filename: string) => any>;
+    _cache: Record<string, NodeJS.Module>;
+    wrapper: [string, string];
+  }
+
   interface FeaturesBinding {
     isBuiltinSpellCheckerEnabled(): boolean;
     isPDFViewerEnabled(): boolean;
