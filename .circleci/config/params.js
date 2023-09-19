a71836204b2432ecd874299233d32a6221aa3fff
Samuel Attard <sam@electronjs.org>
2023-09-19 03:12:14 -0700

    build: use afs on aks instead of circle cache (#39881)
    * build: use afs on aks instead of circle cache
    
    * build: do not use aks logic on linux hosts checking out for macOS
    
    * build: fix gn-check could-be-aks
    
    * build: sigh
    
    * build: no ls mnt
    
    * build: keep build alive while debugging
    
    * build: make debuggable
    
    ---------
    
    Co-authored-by: John Kleinschmidt <jkleinsc@electronjs.org>

--- .circleci/config/params.js
+++ .circleci/config/params.js
@@ -7,5 +7,6 @@ const content = JSON.parse(fs.readFileSync(PARAMS_PATH, 'utf-8'));
 // Choose resource class for linux hosts
 const currentBranch = process.env.CIRCLE_BRANCH || '';
 content['large-linux-executor'] = /^pull\/[0-9-]+$/.test(currentBranch) ? '2xlarge' : 'electronjs/aks-linux-large';
+content['medium-linux-executor'] = /^pull\/[0-9-]+$/.test(currentBranch) ? 'medium' : 'electronjs/aks-linux-medium';
 
 fs.writeFileSync(PARAMS_PATH, JSON.stringify(content));
