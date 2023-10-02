a2563f81f59d23e986b5c80f70a6f3f3fd4d3753
Aryan Sehdev <sehdevaryan16@gmail.com>
2023-10-02 13:27:44 +0530

    updated create-api-json.js for better error handling

--- script/create-api-json.js
+++ script/create-api-json.js
@@ -4,14 +4,29 @@ const path = require('node:path');
 
 const { getElectronVersion } = require('./lib/get-version');
 
-parseDocs({
-  baseDirectory: path.resolve(__dirname, '..'),
-  packageMode: 'single',
-  useReadme: false,
-  moduleVersion: getElectronVersion()
-}).then((api) => {
-  return fs.promises.writeFile(path.resolve(__dirname, '..', 'electron-api.json'), JSON.stringify(api, null, 2));
-}).catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+async function generateElectronAPI(){
+  try{
+    const baseDirectory = path.resolve(__dirname, '..');
+    const packageMode = 'single';
+    const useReadme = false;
+    const moduleVersion = getElectronVersion();
+
+
+    const api = await parseDocs({
+      baseDirectory,
+      packageMode,
+      useReadme,
+      moduleVersion
+    });
+    
+    const ouputFilePath = path.resolve(baseDirectory, 'electron-api.json');
+    await fs.promises.writeFile(outputFilePath, JSON.stringify(api,null, 2));
+
+    console.log('Electron API documentation generated Successfully');
+  }catch(err){
+    console.error('Error generating Electron API documentaion:',err);
+    process.exit(1);
+  }
+}
+
+generateElectronAPI();
---
+++ update.md
@@ -0,0 +1,7 @@
+---
+layout: electron
+title: creating-json-api.js
+author: Aryan
+---
+with the changes, the code is better organised and it provides more informative error messages . Additionally it logs a success
+message when the documentation is generated . This should be easier to understand.
