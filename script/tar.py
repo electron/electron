5058adc264fa5b8b55bd1330e4f7a08042fa08c5
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-11-05 15:16:17 +0530

    Update tar.py

--- script/tar.py
+++ script/tar.py
@@ -1,12 +1,19 @@
+# os package imported
 import os
+
+# system package imported
 import sys
+
 import tarfile
 
 source = sys.argv[1]
+
 target = sys.argv[2]
 
 os.chdir(os.path.dirname(source))
 
 tarball = tarfile.open(name=os.path.basename(target), mode='w:gz')
+
 tarball.add(os.path.relpath(source))
+
 tarball.close()
