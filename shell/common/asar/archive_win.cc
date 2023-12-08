d6b4b4dc062afd0f7a635cf2738eadd9f838b82b
Weiyun D <weiyun.chn@gmail.com>
2023-12-08 10:33:56 -0800

    Fix Archive::RelativePath() on Windows platform.
    Co-authored-by: guohaolay <guohaolay@gmail.com>

--- shell/common/asar/archive_win.cc
+++ shell/common/asar/archive_win.cc
@@ -1,3 +1,8 @@
+// Copyright 2023 Slack Technologies, Inc.
+// Contributors: Weiyun Dai (https://github.com/WeiyunD/), Andrew Lay
+// (https://github.com/guohaolay) Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
 #include "shell/common/asar/archive.h"
 
 #include <algorithm>
@@ -5,6 +10,7 @@
 
 #include "base/logging.h"
 #include "base/strings/string_util.h"
+#include "base/strings/string_util_win.h"
 #include "shell/common/asar/asar_util.h"
 
 using namespace std;
@@ -14,39 +20,54 @@ using namespace std;
 
 namespace asar {
 
-HRSRC FindElectronAsarIntegrityResource(HMODULE handle) {
-  HRSRC resource = ::FindResource(handle, INTEGRITY_CHECK_RESOURCE_ITEM,
+absl::optional<base::FilePath> Archive::RelativePath() const {
+  // In case user has Windows long path enabled, the exe path should still no
+  // larger than the asar path.
+  size_t buf_len = max(path_.value().size(), (size_t)MAX_PATH);
+  wchar_t* buf = new (std::nothrow) wchar_t[buf_len];
+  if (!buf) {
+    LOG(WARNING) << " " << __func__
+                 << "(): Failed to alloc enough space to store exe file path";
+    return absl::nullopt;
+  }
+
+  DWORD len = GetModuleFileName(NULL, buf, buf_len);
+  wstring exe_path = wstring(buf, len);
+  delete[] buf;
+
+  if (!len) {
+    LOG(WARNING) << " " << __func__
+                 << "(): GetModuleFileName failed. Last error: "
+                 << GetLastError();
+    return absl::nullopt;
+  }
+
+  base::FilePath exe_dir = base::FilePath(exe_path).DirName();
+  base::FilePath relative_path;
+  if (!exe_dir.AppendRelativePath(path_, &relative_path)) {
+    return absl::nullopt;
+  }
+
+  return relative_path;
+}
+
+string GetIntegrityConfig() {
+  // Load integrity config from exe resource
+  HMODULE module_handle = ::GetModuleHandle(NULL);
+
+  HRSRC resource = ::FindResource(module_handle, INTEGRITY_CHECK_RESOURCE_ITEM,
                                   INTEGRITY_CHECK_RESOURCE_TYPE);
   if (!resource) {
     LOG(WARNING) << " " << __func__
                  << "(): FindResource failed. Last error: " << GetLastError();
-    return NULL;
+    return "";
   }
 
-  return resource;
-}
-
-/*
-  Since the Windows asar header integrity config is part of the exe resource not
-  a file on disk, returning a blank file path object when the config is present.
-*/
-absl::optional<base::FilePath> Archive::RelativePath() const {
-  if (FindElectronAsarIntegrityResource(::GetModuleHandle(NULL))) {
-    return base::FilePath();
-  } else {
-    return absl::nullopt;
-  }
-}
-
-absl::optional<IntegrityPayload> Archive::HeaderIntegrity() const {
-  HMODULE handle = ::GetModuleHandle(NULL);
-  HRSRC resource = FindElectronAsarIntegrityResource(handle);
-
-  HGLOBAL rcData = ::LoadResource(handle, resource);
+  HGLOBAL rcData = ::LoadResource(module_handle, resource);
   if (!rcData) {
     LOG(WARNING) << " " << __func__
                  << "(): LoadResource failed. Last error: " << GetLastError();
-    return absl::nullopt;
+    return "";
   }
 
   auto* res_data = static_cast<const char*>(::LockResource(rcData));
@@ -55,28 +76,48 @@ absl::optional<IntegrityPayload> Archive::HeaderIntegrity() const {
   if (!res_data || !res_size) {
     LOG(WARNING) << " " << __func__
                  << "(): Failed to integrity config from exe resource.";
-    return absl::nullopt;
+    return "";
   }
 
-  istringstream res_stream(string(res_data, res_size));
-  string filepath, hash_alg, hash_value;
-  if (!getline(res_stream, filepath) || !getline(res_stream, hash_alg) ||
-      !getline(res_stream, hash_value)) {
+  return string(res_data, res_size);
+}
+
+absl::optional<IntegrityPayload> Archive::HeaderIntegrity() const {
+  absl::optional<base::FilePath> relative_path = RelativePath();
+  // Callers should have already asserted this
+  CHECK(relative_path.has_value());
+
+  // Load integrity config from exe resource
+  string integrity_config = GetIntegrityConfig();
+  if (integrity_config.empty()) {
     LOG(WARNING) << " " << __func__
-                 << "(): Failed to read 3 lines from integrity config (invalid "
-                    "config format).";
+                 << "(): Failed to integrity config from exe resource.";
     return absl::nullopt;
   }
 
-  auto trimLineEndingCR = [](string& line) {
-    if (!line.empty() && line.back() == '\r') {
-      line.pop_back();
+  // Parse integrity config
+  istringstream integrity_config_stream(integrity_config);
+  string filepath, hash_alg, hash_value;
+  while (!base::EqualsCaseInsensitiveASCII(filepath,
+                                           relative_path.value().value())) {
+    if (!getline(integrity_config_stream, filepath) ||
+        !getline(integrity_config_stream, hash_alg) ||
+        !getline(integrity_config_stream, hash_value)) {
+      LOG(WARNING)
+          << " " << __func__
+          << "(): Failed to read 3 lines from integrity config (invalid "
+             "config format).";
+      return absl::nullopt;
     }
-  };
 
-  trimLineEndingCR(filepath);
-  trimLineEndingCR(hash_alg);
-  trimLineEndingCR(hash_value);
+    base::TrimString(filepath, "\r", &filepath);  // Trim trailing \r
+    base::TrimString(
+        hash_alg, "\r\t ",
+        &hash_alg);  // Trim leading tab or whitespace and trailing \r
+    base::TrimString(
+        hash_value, "\r\t ",
+        &hash_value);  // Trim leading tab or whitespace and trailing \r
+  }
 
   if (base::ToLowerASCII(hash_alg) != "sha256") {
     LOG(WARNING)
