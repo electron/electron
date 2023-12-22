0d9b295edf42dae28d672ce9f9d9dcd5ea07eee3
Charles Kerr <charles@charleskerr.com>
2023-12-22 15:55:49 -0600

    refactor: use base::flat_map for InspectableWebContents::extensions_api_

--- shell/browser/ui/inspectable_web_contents.h
+++ shell/browser/ui/inspectable_web_contents.h
@@ -6,11 +6,11 @@
 #ifndef ELECTRON_SHELL_BROWSER_UI_INSPECTABLE_WEB_CONTENTS_H_
 #define ELECTRON_SHELL_BROWSER_UI_INSPECTABLE_WEB_CONTENTS_H_
 
-#include <map>
 #include <memory>
 #include <set>
 #include <string>
 
+#include "base/containers/flat_map.h"
 #include "base/containers/span.h"
 #include "base/containers/unique_ptr_adapters.h"
 #include "base/memory/raw_ptr.h"
@@ -244,8 +244,8 @@ class InspectableWebContents
   std::set<std::unique_ptr<NetworkResourceLoader>, base::UniquePtrComparator>
       loaders_;
 
-  using ExtensionsAPIs = std::map<std::string, std::string>;
-  ExtensionsAPIs extensions_api_;
+  // origin -> script
+  base::flat_map<std::string, std::string> extensions_api_;
 
   // Contains the set of synced settings.
   // The DevTools frontend *must* call `Register` for each setting prior to
