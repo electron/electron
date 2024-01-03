a5dc9ac4738c5dec9712b2b2ba3ed8666d998e8b
Jeremy Rose <jeremya@chromium.org>
2024-01-03 11:55:49 -0800

    fix types

--- lib/browser/api/browser-window.ts
+++ lib/browser/api/browser-window.ts
@@ -36,7 +36,7 @@ BrowserWindow.prototype._init = function (this: BWT) {
     app.emit('browser-window-focus', event, this);
   });
 
-  let unresponsiveEvent: NodeJS.Timer | null = null;
+  let unresponsiveEvent: NodeJS.Timeout | null = null;
   const emitUnresponsiveEvent = () => {
     unresponsiveEvent = null;
     if (!this.isDestroyed() && this.isEnabled()) { this.emit('unresponsive'); }
