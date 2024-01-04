8c71e2adc9b47f8d9e9ad07be9e6a9b3a764a670
Devraj Mehta <devm33@github.com>
2024-01-04 16:20:37 -0500

    feat: add net module to utility process (#40017)
    * chore: initial prototype of net api from utility process
    
    * chore: update url loader to work on both browser and utility processes
    
    * chore: add net files to utility process bundle
    
    * chore: re-add app ready check but only on main process
    
    * chore: replace browser thread dcheck's with sequence checker
    
    * refactor: move url loader from browser to common
    
    * refactor: move net-client-request.ts from browser to common
    
    * docs: add utility process to net api docs
    
    * refactor: move net module app ready check to browser only
    
    * refactor: switch import from main to common after moving to common
    
    * test: add basic net module test for utility process
    
    * refactor: switch browser pid with utility pid
    
    * refactor: move electron_api_net from browser to common
    
    * chore: add fetch to utility net module
    
    * chore: add isOnline and online to utility net module
    
    * refactor: move net spec helpers into helper file
    
    * refactor: break apart net module tests
    
    Adds two additional net module test files: `api-net-session-spec.ts` for
    tests that depend on a session being available (aka depend on running on
    the main process) and `api-net-custom-protocols-spec.ts` for custom
    protocol tests. This enables running `api-net-spec.ts` in the utility
    process.
    
    * test: add utility process mocha runner to run net module tests
    
    * docs: add utility process to net module classes
    
    * refactor: update imports in lib/utility to use electron/utility
    
    * chore: check browser context before using in main process
    
    Since the browser context supplied to the SimpleURLLoaderWrapper can now
    be null for use in the UtilityProcess, adding a null check for the main
    process before use to get a more sensible error if something goes wrong.
    
    Co-authored-by: Cheng Zhao <github@zcbenz.com>
    
    * chore: remove test debugging
    
    * chore: remove unnecessary header include
    
    * docs: add utility process net module limitations
    
    * test: run net module tests in utility process individually
    
    * refactor: clean up prior utility process net tests
    
    * chore: add resolveHost to utility process net module
    
    * chore: replace resolve host dcheck with sequence checker
    
    * test: add net module tests for net.resolveHost
    
    * docs: remove utility process limitation for resolveHost
    
    ---------
    
    Co-authored-by: deepak1556 <hop2deep@gmail.com>
    Co-authored-by: Cheng Zhao <github@zcbenz.com>

--- shell/services/node/node_service.cc
+++ shell/services/node/node_service.cc
@@ -7,7 +7,11 @@
 #include <utility>
 
 #include "base/command_line.h"
+#include "base/no_destructor.h"
 #include "base/strings/utf_string_conversions.h"
+#include "services/network/public/cpp/wrapper_shared_url_loader_factory.h"
+#include "services/network/public/mojom/host_resolver.mojom.h"
+#include "services/network/public/mojom/network_context.mojom.h"
 #include "shell/browser/javascript_environment.h"
 #include "shell/common/api/electron_bindings.h"
 #include "shell/common/gin_converters/file_path_converter.h"
@@ -17,6 +21,34 @@
 #include "shell/services/node/parent_port.h"
 
 namespace electron {
+
+URLLoaderBundle::URLLoaderBundle() = default;
+
+URLLoaderBundle::~URLLoaderBundle() = default;
+
+URLLoaderBundle* URLLoaderBundle::GetInstance() {
+  static base::NoDestructor<URLLoaderBundle> instance;
+  return instance.get();
+}
+
+void URLLoaderBundle::SetURLLoaderFactory(
+    mojo::PendingRemote<network::mojom::URLLoaderFactory> pending_factory,
+    mojo::Remote<network::mojom::HostResolver> host_resolver) {
+  factory_ = network::SharedURLLoaderFactory::Create(
+      std::make_unique<network::WrapperPendingSharedURLLoaderFactory>(
+          std::move(pending_factory)));
+  host_resolver_ = std::move(host_resolver);
+}
+
+scoped_refptr<network::SharedURLLoaderFactory>
+URLLoaderBundle::GetSharedURLLoaderFactory() {
+  return factory_;
+}
+
+network::mojom::HostResolver* URLLoaderBundle::GetHostResolver() {
+  DCHECK(host_resolver_);
+  return host_resolver_.get();
+}
 
 NodeService::NodeService(
     mojo::PendingReceiver<node::mojom::NodeService> receiver)
@@ -41,6 +73,10 @@ void NodeService::Initialize(node::mojom::NodeServiceParamsPtr params) {
     return;
 
   ParentPort::GetInstance()->Initialize(std::move(params->port));
+
+  URLLoaderBundle::GetInstance()->SetURLLoaderFactory(
+      std::move(params->url_loader_factory),
+      mojo::Remote(std::move(params->host_resolver)));
 
   js_env_ = std::make_unique<JavascriptEnvironment>(node_bindings_->uv_loop());
 
