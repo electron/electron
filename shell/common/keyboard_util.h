a4561d2dd44af8cc32865490c6ee162403e2b789
Charles Kerr <charles@charleskerr.com>
2024-01-05 18:02:40 -0600

    chore: migrate from base::StringPiece to std::string_view in keyboard_util.cc

--- shell/common/keyboard_util.h
+++ shell/common/keyboard_util.h
@@ -5,9 +5,15 @@
 #ifndef ELECTRON_SHELL_COMMON_KEYBOARD_UTIL_H_
 #define ELECTRON_SHELL_COMMON_KEYBOARD_UTIL_H_
 
+<<<<<<< HEAD
 #include <optional>
 
 #include "base/strings/string_piece.h"
+=======
+#include <string_view>
+
+#include "third_party/abseil-cpp/absl/types/optional.h"
+>>>>>>> 47d2f8d56e (chore: migrate from base::StringPiece to std::string_view in keyboard_util.cc)
 #include "ui/events/keycodes/keyboard_codes.h"
 
 namespace electron {
@@ -15,7 +21,7 @@ namespace electron {
 // Return key code of the |str|, if the original key is a shifted character,
 // for example + and /, set it in |shifted_char|.
 // pressed.
-ui::KeyboardCode KeyboardCodeFromStr(base::StringPiece str,
+ui::KeyboardCode KeyboardCodeFromStr(std::string_view str,
                                      std::optional<char16_t>* shifted_char);
 
 }  // namespace electron
