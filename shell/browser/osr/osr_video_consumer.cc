89a8da49b82f086c6589b2e70d27f6dde84023a6
Charles Kerr <charles@charleskerr.com>
2024-01-29 20:43:28 -0600

    refactor: inline simple getters (#41125)

--- shell/browser/osr/osr_video_consumer.cc
+++ shell/browser/osr/osr_video_consumer.cc
@@ -42,7 +42,7 @@ OffScreenVideoConsumer::OffScreenVideoConsumer(
   video_capturer_->SetFormat(media::PIXEL_FORMAT_ARGB);
 
   SizeChanged(view_->SizeInPixels());
-  SetFrameRate(view_->GetFrameRate());
+  SetFrameRate(view_->frame_rate());
 }
 
 OffScreenVideoConsumer::~OffScreenVideoConsumer() = default;
