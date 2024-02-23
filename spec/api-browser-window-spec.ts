84abfba9d6422a6b0bdf8f4bb3729564a17a2648
John Kleinschmidt <jkleinsc@electronjs.org>
2024-02-23 16:41:22 -0500

    Remove Setup.Install.CumulativeDiskUsage2 and supporting code
    https://chromium-review.googlesource.com/c/chromium/src/+/5314269

--- spec/api-browser-window-spec.ts
+++ spec/api-browser-window-spec.ts
@@ -3806,7 +3806,6 @@ describe('BrowserWindow module', () => {
         expect(test.processMemoryInfo).to.be.an('object');
         expect(test.systemVersion).to.be.a('string');
         expect(test.cpuUsage).to.be.an('object');
-        expect(test.ioCounters).to.be.an('object');
         expect(test.uptime).to.be.a('number');
         expect(test.arch).to.equal(process.arch);
         expect(test.platform).to.equal(process.platform);
