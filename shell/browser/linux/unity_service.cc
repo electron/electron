07a68c2bf8b53ea1c639b5c12fa725e5ae81fab0
JakobDev <jakobdev@gmx.de>
2024-04-19 15:58:32 +0200

    fix: don't check for Desktop Environment in unity_service.cc (#41211)
    Don't check for Desktop Environment in unity_service.cc

--- shell/browser/linux/unity_service.cc
+++ shell/browser/linux/unity_service.cc
@@ -51,20 +51,9 @@ unity_launcher_entry_set_progress_visible_func entry_set_progress_visible =
     nullptr;
 
 void EnsureLibUnityLoaded() {
-  using base::nix::GetDesktopEnvironment;
-
   if (attempted_load)
     return;
   attempted_load = true;
-
-  auto env = base::Environment::Create();
-  base::nix::DesktopEnvironment desktop_env = GetDesktopEnvironment(env.get());
-
-  // The "icon-tasks" KDE task manager also honors Unity Launcher API.
-  if (desktop_env != base::nix::DESKTOP_ENVIRONMENT_UNITY &&
-      desktop_env != base::nix::DESKTOP_ENVIRONMENT_KDE4 &&
-      desktop_env != base::nix::DESKTOP_ENVIRONMENT_KDE5)
-    return;
 
   // Ubuntu still hasn't given us a nice libunity.so symlink.
   void* unity_lib = dlopen("libunity.so.4", RTLD_LAZY);
