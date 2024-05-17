ee4ff6ae13ef4efeaaf568c291cb2b6e58776258
George Xu <george.xu@slack-corp.com>
2024-05-17 12:45:47 -0700

    use @

--- lib/utility/api/module-list.ts
+++ lib/utility/api/module-list.ts
@@ -1,5 +1,5 @@
 // Utility side modules, please sort alphabetically.
 export const utilityNodeModuleList: ElectronInternal.ModuleEntry[] = [
   { name: 'net', loader: () => require('./net') },
-  { name: 'systemPreferences', loader: () => require('../../browser/api/system-preferences') }
+  { name: 'systemPreferences', loader: () => require('@electron/internal/browser/api/system-preferences') }
 ];
