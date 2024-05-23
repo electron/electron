ba78225502e73389af7e13365d647329605376d0
clavin <clavin@electronjs.org>
2024-05-23 14:52:32 -0600

    5539888: [api] Remove several APIs deprecated in version 12.6 https://chromium-review.googlesource.com/c/v8/v8/+/5539888
    This commit essentially only removes the `only_terminate_in_safe_scope` isolate creation parameter. This undoes some work that was originally done in #35766.
    
    (cherry picked from commit ceb6182b199e2471c64700203bf42b73052a38c6)

--- shell/browser/javascript_environment.cc
+++ shell/browser/javascript_environment.cc
@@ -75,38 +75,15 @@ struct base::trace_event::TraceValue::Helper<
 
 namespace electron {
 
-namespace {
-
-gin::IsolateHolder CreateIsolateHolder(v8::Isolate* isolate) {
-  std::unique_ptr<v8::Isolate::CreateParams> create_params =
-      gin::IsolateHolder::getDefaultIsolateParams();
-  // Align behavior with V8 Isolate default for Node.js.
-  // This is necessary for important aspects of Node.js
-  // including heap and cpu profilers to function properly.
-  //
-  // Additional note:
-  // We do not want to invoke a termination exception at exit when
-  // we're running with only_terminate_in_safe_scope set to false. Heap and
-  // coverage profilers run after environment exit and if there is a pending
-  // exception at this stage then they will fail to generate the appropriate
-  // profiles. Node.js does not call node::Stop(), which calls
-  // isolate->TerminateExecution(), and therefore does not have this issue
-  // when also running with only_terminate_in_safe_scope set to false.
-  create_params->only_terminate_in_safe_scope = false;
-
-  return gin::IsolateHolder(
-      base::SingleThreadTaskRunner::GetCurrentDefault(),
-      gin::IsolateHolder::kSingleThread,
-      gin::IsolateHolder::IsolateType::kUtility, std::move(create_params),
-      gin::IsolateHolder::IsolateCreationMode::kNormal, nullptr, isolate);
-}
-
-}  // namespace
-
 JavascriptEnvironment::JavascriptEnvironment(uv_loop_t* event_loop,
                                              bool setup_wasm_streaming)
-    : isolate_holder_{CreateIsolateHolder(
-          Initialize(event_loop, setup_wasm_streaming))},
+    : isolate_holder_(base::SingleThreadTaskRunner::GetCurrentDefault(),
+                      gin::IsolateHolder::kSingleThread,
+                      gin::IsolateHolder::IsolateType::kUtility,
+                      gin::IsolateHolder::getDefaultIsolateParams(),
+                      gin::IsolateHolder::IsolateCreationMode::kNormal,
+                      nullptr,
+                      Initialize(event_loop, setup_wasm_streaming)),
       isolate_{isolate_holder_.isolate()},
       locker_{isolate_} {
   isolate_->Enter();
