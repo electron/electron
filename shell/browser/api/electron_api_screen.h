8d66214614f2c140a5770571caf2375d5bb73501
clavin <clavin@electronjs.org>
2024-05-23 14:17:13 -0600

    5502081: Migrate OnDisplayRemoved to OnDisplaysRemoved https://chromium-review.googlesource.com/c/chromium/src/+/5502081
    (cherry picked from commit ccf9a5137efdf2ca18d9ee9851388338c26d036e)

--- shell/browser/api/electron_api_screen.h
+++ shell/browser/api/electron_api_screen.h
@@ -57,7 +57,7 @@ class Screen : public gin::Wrappable<Screen>,
 
   // display::DisplayObserver:
   void OnDisplayAdded(const display::Display& new_display) override;
-  void OnDisplayRemoved(const display::Display& old_display) override;
+  void OnDisplaysRemoved(const display::Displays& removed_displays) override;
   void OnDisplayMetricsChanged(const display::Display& display,
                                uint32_t changed_metrics) override;
 
