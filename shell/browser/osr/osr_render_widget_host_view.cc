40123290f64f7c5eeb69df949d680a4f488f5f30
Charles Kerr <charles@charleskerr.com>
2024-06-02 11:50:52 -0500

    Pull RWHIER and RWT to //content/common/input.
    Xref: https://chromium-review.googlesource.com/c/chromium/src/+/5397681

--- shell/browser/osr/osr_render_widget_host_view.cc
+++ shell/browser/osr/osr_render_widget_host_view.cc
@@ -21,9 +21,9 @@
 #include "components/viz/common/frame_sinks/copy_output_request.h"
 #include "components/viz/common/frame_sinks/delay_based_time_source.h"
 #include "components/viz/common/quads/compositor_render_pass.h"
-#include "content/browser/renderer_host/cursor_manager.h"  // nogncheck
 #include "content/browser/renderer_host/render_widget_host_delegate.h"  // nogncheck
 #include "content/browser/renderer_host/render_widget_host_owner_delegate.h"  // nogncheck
+#include "content/common/input/cursor_manager.h"
 #include "content/common/input/synthetic_gesture.h"  // nogncheck
 #include "content/common/input/synthetic_gesture_target.h"
 #include "content/public/browser/browser_task_traits.h"
