e6c3b8a5ab037a3d8b7a8fe7d88c7496df51cf47
Shelley Vohr <shelley.vohr@gmail.com>
2024-06-11 21:47:58 -0500

    Use artifacts for build artifact persisting

--- script/actions/move-artifacts.sh
+++ script/actions/move-artifacts.sh
@@ -2,10 +2,14 @@
 
 set -eo pipefail
 
-if [ -z "$MAS_BUILD" ]; then
-  BUILD_TYPE="darwin"
-else
-  BUILD_TYPE="mas"
+if [ "`uname`" == "Darwin" ]; then
+  if [ -z "$MAS_BUILD" ]; then
+    BUILD_TYPE="darwin"
+  else
+    BUILD_TYPE="mas"
+  fi
+elif [ "`uname`" == "Linux" ]; then
+  BUILD_TYPE="linux"
 fi
 
 echo Creating generated_artifacts_${BUILD_TYPE}_${TARGET_ARCH}...
@@ -20,6 +24,7 @@ mv_if_exist() {
     echo Skipping $1 - It is not present on disk
   fi
 }
+
 cp_if_exist() {
   if [ -f "$1" ] || [ -d "$1" ]; then
     echo Storing $1
@@ -29,6 +34,40 @@ cp_if_exist() {
   fi
 }
 
+tar_src_dirs_if_exist() {
+  mkdir build_artifacts
+
+  for dir in \
+    src/out/Default/gen/node_headers \
+    src/out/Default/overlapped-checker \
+    src/out/Default/ffmpeg \
+    src/out/Default/hunspell_dictionaries \
+    src/electron \
+    src/third_party/electron_node \
+    src/third_party/nan \
+    src/cross-arch-snapshots \
+    src/third_party/llvm-build \
+    src/build/linux \
+    src/buildtools/mac \
+    src/buildtools/third_party/libc++ \
+    src/buildtools/third_party/libc++abi \
+    src/third_party/libc++ \
+    src/third_party/libc++abi \
+    src/out/Default/obj/buildtools/third_party \
+    src/v8/tools/builtins-pgo
+  do
+    if [ -d "$dir" ]; then
+      mkdir -p build_artifacts/$dir
+      cp -r $dir build_artifacts/$dir
+    fi      
+  done
+
+  tar -czf build_artifacts.tar.gz build_artifacts
+
+  mv_if_exist build_artifacts.tar.gz
+}
+
+# Generated Artifacts
 mv_if_exist src/out/Default/dist.zip
 mv_if_exist src/out/Default/gen/node_headers.tar.gz
 mv_if_exist src/out/Default/symbols.zip
@@ -39,3 +78,6 @@ mv_if_exist src/out/Default/hunspell_dictionaries.zip
 mv_if_exist src/cross-arch-snapshots
 cp_if_exist src/out/electron_ninja_log
 cp_if_exist src/out/Default/.ninja_log
+
+tar_src_dirs_if_exist
+
