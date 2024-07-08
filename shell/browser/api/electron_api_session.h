e89da2bad2f4c449903de1edc7b8655320a6dadc
Charles Kerr <charles@charleskerr.com>
2024-07-08 05:55:48 -0500

    fix: dangling raw_ptr<Session> in UserDataLink (#42786)
    * fix: dangling raw_ptr<Session> in UserDataLink
    
    * chore: make linter happy

--- shell/browser/api/electron_api_session.h
+++ shell/browser/api/electron_api_session.h
@@ -10,6 +10,7 @@
 #include <vector>
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/values.h"
 #include "content/public/browser/download_manager.h"
 #include "electron/buildflags/buildflags.h"
@@ -218,6 +219,8 @@ class Session : public gin::Wrappable<Session>,
   base::UnguessableToken network_emulation_token_;
 
   const raw_ref<ElectronBrowserContext> browser_context_;
+
+  base::WeakPtrFactory<Session> weak_factory_{this};
 };
 
 }  // namespace api
