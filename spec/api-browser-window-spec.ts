d4cc07fa7c557eed5dd401eea8800306b3447c02
reito <cnschwarzer@qq.com>
2024-07-18 18:44:32 +0800

    fix: osr image compare

--- spec/api-browser-window-spec.ts
+++ spec/api-browser-window-spec.ts
@@ -6469,6 +6469,8 @@ describe('BrowserWindow module', () => {
   describe('offscreen rendering image', () => {
     afterEach(closeAllWindows);
 
+    const imagePath = path.join(fixtures, 'assets', 'osr.png');
+    const targetImage = nativeImage.createFromPath(imagePath);
     const nativeModulesEnabled = !process.env.ELECTRON_SKIP_NATIVE_MODULE_TESTS;
     ifit(nativeModulesEnabled && ['win32'].includes(process.platform))('use shared texture, hardware acceleration enabled', (done) => {
       const { ExtractPixels, InitializeGpu } = require('@electron-ci/osr-gpu');
@@ -6490,23 +6492,22 @@ describe('BrowserWindow module', () => {
         },
         transparent: true,
         frame: false,
-        width: 100,
-        height: 100
+        width: 128,
+        height: 128
       });
 
       w.webContents.once('paint', async (e, dirtyRect) => {
         try {
           expect(e.texture).to.be.not.null();
           const pixels = ExtractPixels(e.texture!.textureInfo);
-          const target = await w.webContents.capturePage();
           const img = nativeImage.createFromBitmap(pixels, { width: dirtyRect.width, height: dirtyRect.height, scaleFactor: 1 });
-          expect(img.toBitmap().equals(target.toBitmap())).to.equal(true);
+          expect(img.toBitmap().equals(targetImage.toBitmap())).to.equal(true);
           done();
         } catch (e) {
           done(e);
         }
       });
-      w.loadFile(path.join(fixtures, 'api', 'offscreen-rendering-image.html'));
+      w.loadFile(imagePath);
     });
 
     it('use shared memory, hardware acceleration enabled', (done) => {
@@ -6518,20 +6519,19 @@ describe('BrowserWindow module', () => {
         },
         transparent: true,
         frame: false,
-        width: 100,
-        height: 100
+        width: 128,
+        height: 128
       });
 
       w.webContents.once('paint', async (e, dirtyRect, image) => {
         try {
-          const target = await w.webContents.capturePage();
-          expect(image.toBitmap().equals(target.toBitmap())).to.equal(true);
+          expect(image.toBitmap().equals(targetImage.toBitmap())).to.equal(true);
           done();
         } catch (e) {
           done(e);
         }
       });
-      w.loadFile(path.join(fixtures, 'api', 'offscreen-rendering-image.html'));
+      w.loadFile(imagePath);
     });
   });
 
