c9d876c193452c3a06977f281f52d1d484ea4b99
Charles Kerr <charles@charleskerr.com>
2024-07-21 09:34:51 -0500

    refactor: remove WindowListObserver::OnWindowAdded()
    refactor: remove WindowListObserver::OnWindowRemove()
    
    these have never been used

--- shell/browser/window_list_observer.h
+++ shell/browser/window_list_observer.h
@@ -13,12 +13,6 @@ class NativeWindow;
 
 class WindowListObserver : public base::CheckedObserver {
  public:
-  // Called immediately after a window is added to the list.
-  virtual void OnWindowAdded(NativeWindow* window) {}
-
-  // Called immediately after a window is removed from the list.
-  virtual void OnWindowRemoved(NativeWindow* window) {}
-
   // Called when a window close is cancelled by beforeunload handler.
   virtual void OnWindowCloseCancelled(NativeWindow* window) {}
 
