0d3922cfeb4c4828967fbadd8b7d414ab1971f4a
Charles Kerr <charles@charleskerr.com>
2024-07-30 12:09:11 -0500

    chore: remove unused KeyWeakMap JS bindings
    The last three pieces of code that used it were removed in:
    
    - Oct 2020 (8df4faa8 #25711)
    - Jun 2020 (e1e73fa5 #24115)
    - Jun 2020 (c0182bca #24116).

--- shell/common/api/electron_api_v8_util.cc
+++ shell/common/api/electron_api_v8_util.cc
@@ -7,7 +7,6 @@
 
 #include "base/run_loop.h"
 #include "electron/buildflags/buildflags.h"
-#include "shell/common/api/electron_api_key_weak_map.h"
 #include "shell/common/gin_converters/content_converter.h"
 #include "shell/common/gin_converters/gurl_converter.h"
 #include "shell/common/gin_converters/std_converter.h"
