360fd4e7753b00fefeedb0b1f2b2ca088804ead6
Keeley Hammond <khammond@slack-corp.com>
2024-08-21 18:44:25 -0700

    feat: partially implement IsDisplayMediaSystemPickerAvailable

--- shell/browser/feature_list_mac.mm
+++ shell/browser/feature_list_mac.mm
@@ -28,6 +28,25 @@ std::string EnablePlatformSpecificFeatures() {
            "ThumbnailCapturerMac:capture_mode/sc_screenshot_manager";
 #endif
   }
+  if (@available(macOS 15.0, *)) {
+    // These flags aren't exported so reference them by name directly, they are
+    // used to ensure that screen and window capture exclusive use
+    // ScreenCaptureKit APIs to avoid warning dialogs on macOS 15.0 and higher.
+    // kScreenCaptureKitPickerScreen,
+    // chrome/browser/media/webrtc/thumbnail_capturer_mac.mm
+    // kScreenCaptureKitStreamPickerSonoma,
+    // chrome/browser/media/webrtc/thumbnail_capturer_mac.mm
+    // kThumbnailCapturerMac,
+    // chrome/browser/media/webrtc/thumbnail_capturer_mac.mm
+    // kUseSCContentSharingPicker,
+    // chrome/browser/media/webrtc/thumbnail_capturer_mac.mm
+#if DCHECK_IS_ON()
+    return "ScreenCaptureKitPickerScreen,ScreenCaptureKitStreamPickerSonoma";
+#else
+    return "ScreenCaptureKitPickerScreen,ScreenCaptureKitStreamPickerSonoma,UseSCContentSharingPicker,"
+           "ThumbnailCapturerMac:capture_mode/sc_screenshot_manager";
+#endif
+  }
   return "";
 }
 
