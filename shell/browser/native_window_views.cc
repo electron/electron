e086cbe2fa9088c3021393bb4ee7321567a924b0
Shelley Vohr <shelley.vohr@gmail.com>
2024-08-22 15:10:08 +0200

    fix: ensure bounds stability in OnWidgetBoundsChanged

--- shell/browser/native_window_views.cc
+++ shell/browser/native_window_views.cc
@@ -1637,10 +1637,21 @@ void NativeWindowViews::OnWidgetBoundsChanged(views::Widget* changed_widget,
   if (changed_widget != widget())
     return;
 
-  // Note: We intentionally use `GetBounds()` instead of `bounds` to properly
-  // handle minimized windows on Windows.
+  // |GetWindowBoundsInScreen| has a ~1 pixel margin of error, so if we check
+  // existing bounds directly against the new bounds without accounting for that
+  // we'll have constant false positives when the window is moving but the user
+  // hasn't changed its size at all.
+  auto areWithinOnePixel = [](gfx::Size old_size, gfx::Size new_size) -> bool {
+    bool within_range_width =
+        std::abs(old_size.width() - new_size.width()) <= 1;
+    bool within_range_height =
+        std::abs(old_size.height() - new_size.height()) <= 1;
+    return within_range_width && within_range_height;
+  };
+
+  // We use |GetBounds| to account for minimized windows on Windows.
   const auto new_bounds = GetBounds();
-  if (widget_size_ != new_bounds.size()) {
+  if (!areWithinOnePixel(widget_size_, new_bounds.size())) {
     NotifyWindowResize();
     widget_size_ = new_bounds.size();
   }
