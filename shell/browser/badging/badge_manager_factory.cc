0805fbd85d4a06c3b36209d7211af3509c92002b
Charles Kerr <charles@charleskerr.com>
2024-09-11 14:09:30 -0500

    refactor: update BadgeManagerFactory

--- shell/browser/badging/badge_manager_factory.cc
+++ shell/browser/badging/badge_manager_factory.cc
@@ -31,9 +31,10 @@ BadgeManagerFactory::BadgeManagerFactory()
 
 BadgeManagerFactory::~BadgeManagerFactory() = default;
 
-KeyedService* BadgeManagerFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+BadgeManagerFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
-  return new BadgeManager();
+  return std::make_unique<BadgeManager>();
 }
 
 }  // namespace badging
