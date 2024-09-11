1d9c0e0950cd0a547b08380500c0ac68b341e7a2
Charles Kerr <charles@charleskerr.com>
2024-09-11 08:22:07 -0500

    chore: remove unused includes base/notreached.h

--- shell/browser/net/asar/asar_file_validator.cc
+++ shell/browser/net/asar/asar_file_validator.cc
@@ -10,7 +10,6 @@
 #include <vector>
 
 #include "base/logging.h"
-#include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "crypto/sha2.h"
