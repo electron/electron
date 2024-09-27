e4dbb6b250b713ca0b8200e377bab81434ac3e4f
alice <alice@makenotion.com>
2024-09-26 22:09:00 -0700

    5878695: Add IsolationInfo for embedders handling external protocols | https://chromium-review.googlesource.com/c/chromium/src/+/5878695

--- shell/browser/electron_browser_client.h
+++ shell/browser/electron_browser_client.h
@@ -276,6 +276,7 @@ class ElectronBrowserClient : public content::ContentBrowserClient,
       bool has_user_gesture,
       const std::optional<url::Origin>& initiating_origin,
       content::RenderFrameHost* initiator_document,
+      const net::IsolationInfo& isolation_info,
       mojo::PendingRemote<network::mojom::URLLoaderFactory>* out_factory)
       override;
   std::unique_ptr<content::LoginDelegate> CreateLoginDelegate(
