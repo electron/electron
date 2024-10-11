8201623d9298155c6c3f8174b257d13fe5784edb
John Kleinschmidt <jkleinsc@electronjs.org>
2024-10-11 10:00:22 -0400

    test: fix visibility-state-spec.ts flaky test (#44037)
    * test: refactor visibility-state-spec
    
    * ci: shard tests
    
    * test: update split-tests for use on Windows
    
    * test: run visibility-state-spec.ts first

--- script/split-tests.js
+++ script/split-tests.js
@@ -1,6 +1,9 @@
 const glob = require('glob');
 
 const fs = require('node:fs');
+const path = require('node:path');
+
+const VISIBILITY_SPEC = 'spec\\visibility-state-spec.ts';
 
 const currentShard = parseInt(process.argv[2], 10);
 const shardCount = parseInt(process.argv[3], 10);
@@ -25,9 +28,16 @@ specFiles.sort((a, b) => {
 
 let shard = 0;
 for (const specFile of specFiles) {
-  buckets[shard].push(specFile);
+  buckets[shard].push(path.normalize(specFile));
   shard++;
   if (shard === shardCount) shard = 0;
 }
 
+const visiblitySpecIdx = buckets[currentShard - 1];
+if (visiblitySpecIdx > -1) {
+  // If visibility-state-spec is in the list, move it to the first position
+  // so that it gets executed first to avoid other specs interferring with it.
+  buckets[currentShard - 1].splice(visiblitySpecIdx, 1);
+  buckets[currentShard - 1].unshift(VISIBILITY_SPEC);
+}
 console.log(buckets[currentShard - 1].join(' '));
