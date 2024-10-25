ecbcdf6e6482462fa130a281a1949b199a210977
Charles Kerr <charles@charleskerr.com>
2024-10-25 09:53:01 -0500

    chore: remove unused shell/common/electron_constants.cc

--- shell/common/electron_constants.cc
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2015 GitHub, Inc.
-// Use of this source code is governed by the MIT license that can be
-// found in the LICENSE file.
-
-#include "shell/common/electron_constants.h"
-
-namespace electron {}  // namespace electron
