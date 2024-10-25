3bd133b5998bc917a716e4564b60dbe1fd43b1f1
Samuel Maddock <smaddock@slack-corp.com>
2024-10-25 15:33:43 -0400

    fix: crash on netlog connection error

--- shell/browser/api/electron_api_net_log.cc
+++ shell/browser/api/electron_api_net_log.cc
@@ -166,6 +166,7 @@ void NetLog::StartNetLogAfterCreateFile(net::NetLogCaptureMode capture_mode,
     std::move(*pending_start_promise_)
         .RejectWithErrorMessage(
             base::File::ErrorToString(output_file.error_details()));
+    pending_start_promise_.reset();
     net_log_exporter_.reset();
     return;
   }
@@ -178,13 +179,17 @@ void NetLog::StartNetLogAfterCreateFile(net::NetLogCaptureMode capture_mode,
 void NetLog::NetLogStarted(int32_t error) {
   DCHECK(pending_start_promise_);
   ResolvePromiseWithNetError(std::move(*pending_start_promise_), error);
+  pending_start_promise_.reset();
 }
 
 void NetLog::OnConnectionError() {
   net_log_exporter_.reset();
   if (pending_start_promise_) {
+    DCHECK(pending_start_promise_->isolate() !=
+           nullptr);  // std::move(*promise) called without resetting
     std::move(*pending_start_promise_)
         .RejectWithErrorMessage("Failed to start net log exporter");
+    pending_start_promise_.reset();
   }
 }
 
