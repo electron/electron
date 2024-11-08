84cb23f7931a0a3e862ae81a3cae164304645e8b
Savely Krasovsky <savely@krasovs.ky>
2024-11-08 22:47:44 +0100

    feat: add win.preventShutdown function on Windows

--- shell/browser/api/electron_api_base_window.cc
+++ shell/browser/api/electron_api_base_window.cc
@@ -8,6 +8,9 @@
 #include <string>
 #include <utility>
 #include <vector>
+#if BUILDFLAG(IS_WIN)
+#include <winuser.h>
+#endif
 
 #include "base/containers/contains.h"
 #include "base/task/single_thread_task_runner.h"
@@ -1050,6 +1053,24 @@ void BaseWindow::SetAppDetails(const gin_helper::Dictionary& options) {
                                   relaunch_command, relaunch_display_name,
                                   window_->GetAcceleratedWidget());
 }
+
+bool BaseWindow::PreventShutdown(const std::string& reason) {
+  LONG res;
+  if (reason.length() == 0)
+    res = ShutdownBlockReasonDestroy(window_->GetAcceleratedWidget());
+  else {
+    std::wstring wreason = base::UTF8ToWide(reason);
+    res = ShutdownBlockReasonCreate(window_->GetAcceleratedWidget(),
+                                    wreason.c_str());
+  }
+
+  if (res != 0)
+    return true;
+
+  DWORD shutdown_block_error = ::GetLastError();
+  base::debug::Alias(&shutdown_block_error);
+  return false;
+}
 #endif
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
@@ -1301,6 +1322,7 @@ void BaseWindow::BuildPrototype(v8::Isolate* isolate,
       .SetMethod("setThumbnailClip", &BaseWindow::SetThumbnailClip)
       .SetMethod("setThumbnailToolTip", &BaseWindow::SetThumbnailToolTip)
       .SetMethod("setAppDetails", &BaseWindow::SetAppDetails)
+      .SetMethod("preventShutdown", &BaseWindow::PreventShutdown)
 #endif
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
       .SetMethod("setTitleBarOverlay", &BaseWindow::SetTitleBarOverlay)
