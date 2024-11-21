ad2a95a851c91a0a71e48ce866284adc65479391
Felix Rieseberg <fr@makenotion.com>
2024-11-21 13:52:09 -0800

    Add tests

---
+++ spec/fixtures/api/shared-dictionary/main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow, session } = require('electron');
+
+const path = require('node:path');
+
+app.setPath('userData', path.join(__dirname, 'user-data-dir'));
+
+// Grab the command to run from process.argv
+const command = process.argv[2];
+app.whenReady().then(async () => {
+  const bw = new BrowserWindow({ show: false });
+  await bw.loadURL('https://compression-dictionary-transport-threejs-demo.glitch.me/demo.html?r=151');
+
+  try {
+    let result;
+    const isolationKey = {
+      frameOrigin: 'https://compression-dictionary-transport-threejs-demo.glitch.me',
+      topFrameSite: 'https://compression-dictionary-transport-threejs-demo.glitch.me'
+    };
+
+    if (command === 'getSharedDictionaryInfo') {
+      result = await session.defaultSession.getSharedDictionaryInfo(isolationKey);
+    } else if (command === 'getSharedDictionaryUsageInfo') {
+      result = await session.defaultSession.getSharedDictionaryUsageInfo();
+    } else if (command === 'clearSharedDictionaryCache') {
+      await session.defaultSession.clearSharedDictionaryCache();
+      result = await session.defaultSession.getSharedDictionaryUsageInfo();
+    } else if (command === 'clearSharedDictionaryCacheForIsolationKey') {
+      await session.defaultSession.clearSharedDictionaryCacheForIsolationKey(isolationKey);
+      result = await session.defaultSession.getSharedDictionaryUsageInfo();
+    }
+
+    console.log(JSON.stringify(result));
+  } catch (e) {
+    console.log('error', e);
+  } finally {
+    app.quit();
+  }
+});
