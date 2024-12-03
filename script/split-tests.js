31c88f3a6835c4c41b257d96b6a254a6d2b46ccc
John Kleinschmidt <jkleinsc@electronjs.org>
2024-12-03 15:02:30 -0500

    Revert "chore: sort files alphabetically"
    This reverts commit d9e217ffb1522076e150fce9e43a31bf56716acb.

--- script/split-tests.js
+++ script/split-tests.js
@@ -1,5 +1,6 @@
 const glob = require('glob');
 
+const fs = require('node:fs');
 const path = require('node:path');
 
 const currentShard = parseInt(process.argv[2], 10);
@@ -13,7 +14,15 @@ for (let i = 0; i < shardCount; i++) {
   buckets.push([]);
 }
 
-specFiles.sort();
+const testsInSpecFile = Object.create(null);
+for (const specFile of specFiles) {
+  const testContent = fs.readFileSync(specFile, 'utf8');
+  testsInSpecFile[specFile] = testContent.split('it(').length;
+}
+
+specFiles.sort((a, b) => {
+  return testsInSpecFile[b] - testsInSpecFile[a];
+});
 
 let shard = 0;
 for (const specFile of specFiles) {
