0a1c9386e7319eb7c44c6030fc3a23c59788ab87
James Brown <randomvoidmail@foxmail.com>
2025-01-16 22:24:16 +0800

    Update node_bindings.cc
    Add more allowed NODE_OPTIONS

--- shell/common/node_bindings.cc
+++ shell/common/node_bindings.cc
@@ -329,6 +329,9 @@ bool IsAllowedOption(const std::string_view option) {
       "--throw-deprecation",
       "--trace-deprecation",
       "--trace-warnings",
+      "--enable-source-maps",
+      "--async-stack-traces",
+      "--experimental-specifier-resolution=node",
   });
 
   if (debug_options.contains(option))
@@ -353,6 +356,9 @@ void SetNodeOptions(base::Environment* env) {
   static constexpr auto pkg_opts = base::MakeFixedFlatSet<std::string_view>({
       "--http-parser",
       "--max-http-header-size",
+      "--enable-source-maps",
+      "--async-stack-traces",
+      "--experimental-specifier-resolution=node",
   });
 
   if (env->HasVar("NODE_EXTRA_CA_CERTS")) {
@@ -368,8 +374,12 @@ void SetNodeOptions(base::Environment* env) {
       env->GetVar("NODE_OPTIONS", &options);
       std::vector<std::string> parts = base::SplitString(
           options, " ", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
-
+      
       bool is_packaged_app = electron::api::App::IsPackaged();
+
+      if (env->HasVar("ELECTRON_LESS_RESTRICTIVE_NODE_OPTIONS_IN_PACKAGED_APP") {
+        is_packaged_app = false;
+      }
 
       for (const auto& part : parts) {
         // Strip off values passed to individual NODE_OPTIONs
