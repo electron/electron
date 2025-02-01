5ec0471c26474e488b62c19f3043cd542a888799
Felix Rieseberg <fr@makenotion.com>
2025-02-01 10:37:11 -0800

    feat: Working navigationHistory.restore with just title/url

--- typings/internal-electron.d.ts
+++ typings/internal-electron.d.ts
@@ -115,6 +115,7 @@ declare namespace Electron {
     _goToIndex(index: number): void;
     _removeNavigationEntryAtIndex(index: number): boolean;
     _getHistory(): Electron.NavigationEntry[];
+    _restoreHistory(index: number, entries: Electron.NavigationEntry[]): void
     _clearHistory():void
     canGoToIndex(index: number): boolean;
     destroy(): void;
