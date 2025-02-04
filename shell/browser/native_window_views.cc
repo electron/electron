8794a25a76cc046752a248fea85c3da659ba7bb6
Shelley Vohr <shelley.vohr@gmail.com>
2025-02-04 21:03:32 +0100

    Fix rounded corners after restore

--- shell/browser/native_window_views.cc
+++ shell/browser/native_window_views.cc
@@ -721,7 +721,11 @@ void NativeWindowViews::Restore() {
   if (IsMaximized() && IsTranslucent()) {
     SetBounds(restore_bounds_, false);
     NotifyWindowRestore();
-    UpdateThickFrame();
+    if (transparent()) {
+      UpdateThickFrame();
+    } else {
+      SetRoundedCorners(true);
+    }
     return;
   }
 #endif
