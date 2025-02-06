5436ddcc8db265ef1ffe506e7ef2f1715c1ea0f7
clavin <clavin@electronjs.org>
2025-02-05 17:16:51 -0700

    Add tests

---
+++ spec/api-corner-smoothing-spec.ts
@@ -0,0 +1,98 @@
+import { nativeImage } from 'electron/common';
+import { BrowserWindow } from 'electron/main';
+
+import { AssertionError, expect } from 'chai';
+
+import path = require('node:path');
+
+import { createArtifact } from './lib/artifacts';
+import { closeAllWindows } from './lib/window-helpers';
+
+const FIXTURE_PATH = path.resolve(
+  __dirname,
+  'fixtures',
+  'api',
+  'corner-smoothing'
+);
+
+/** Recipe for tests. */
+async function test (
+  pagePath: string,
+  expectedImgPath: string,
+  artifactName: string,
+  cornerSmoothingAvailable: boolean = true
+): Promise<void> {
+  const w = new BrowserWindow({
+    show: false,
+    width: 800,
+    height: 600,
+    useContentSize: true,
+    webPreferences: {
+      cornerSmoothingCSS: cornerSmoothingAvailable
+    }
+  });
+  await w.loadFile(pagePath);
+  w.show();
+
+  // Wait for a render frame to prepare the page.
+  await w.webContents.executeJavaScript(
+    'new Promise((resolve) => { requestAnimationFrame(() => resolve()); })'
+  );
+
+  const actualImg = await w.webContents.capturePage();
+  expect(actualImg.isEmpty()).to.be.false('Failed to capture page image');
+
+  const expectedImg = nativeImage.createFromPath(expectedImgPath);
+  expect(expectedImg.isEmpty()).to.be.false(
+    'Failed to read expected reference image'
+  );
+
+  // Compare the actual page image to the expected reference image, creating an
+  // artifact if they do not match.
+  const matches = actualImg.toBitmap().equals(expectedImg.toBitmap());
+  if (!matches) {
+    const artifactFileName = `corner-rounding-expected-${artifactName}.png`;
+    await createArtifact(artifactFileName, actualImg.toPNG());
+
+    throw new AssertionError(
+      `Actual image did not match expected reference image. Actual: "${artifactFileName}" in artifacts, Expected: "${path.relative(
+        path.resolve(__dirname, '..'),
+        expectedImgPath
+      )}" in source`
+    );
+  }
+}
+
+describe('-electron-corner-smoothing', () => {
+  afterEach(async () => {
+    await closeAllWindows();
+  });
+
+  describe('shape', () => {
+    for (const available of [true, false]) {
+      it(`matches the reference with web preference = ${available}`, async () => {
+        await test(
+          path.join(FIXTURE_PATH, 'shape', 'test.html'),
+          path.join(FIXTURE_PATH, 'shape', `expected-${available}.png`),
+          `shape-${available}`,
+          available
+        );
+      });
+    }
+  });
+
+  describe('system-ui keyword', () => {
+    const { platform } = process;
+    it(`matches the reference for platform = ${platform}`, async () => {
+      await test(
+        path.join(FIXTURE_PATH, 'system-ui-keyword', 'test.html'),
+        path.join(
+          FIXTURE_PATH,
+          'system-ui-keyword',
+          `expected-${platform}.png`
+        ),
+        `system-ui-${platform}`
+      );
+    });
+  });
+});
