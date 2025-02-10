e055ce7c39c80e38321769df78b7323b9a00e52b
Milan Burda <milan.burda@gmail.com>
2025-02-10 21:17:07 +0100

    chore: deprecate `systemPreferences.isAeroGlassEnabled()` (#45434)

--- spec/api-system-preferences-spec.ts
+++ spec/api-system-preferences-spec.ts
@@ -2,6 +2,7 @@ import { systemPreferences } from 'electron/main';
 
 import { expect } from 'chai';
 
+import { expectDeprecationMessages } from './lib/deprecate-helpers';
 import { ifdescribe } from './lib/spec-helpers';
 
 describe('systemPreferences module', () => {
@@ -56,6 +57,13 @@ describe('systemPreferences module', () => {
           systemPreferences.registerDefaults(badDefault as any);
         }).to.throw('Error processing argument at index 0, conversion failure from ');
       }
+    });
+  });
+
+  ifdescribe(process.platform === 'win32')('systemPreferences.isAeroGlassEnabled()', () => {
+    it('always returns true', () => {
+      expect(systemPreferences.isAeroGlassEnabled()).to.equal(true);
+      expectDeprecationMessages(() => systemPreferences.isAeroGlassEnabled(), '\'systemPreferences.isAeroGlassEnabled\' is deprecated and will be removed.');
     });
   });
 
