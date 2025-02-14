a1e4550c9e5b2b220530a9238795286bd028dbfb
Charles Kerr <charles@charleskerr.com>
2025-02-14 03:18:42 -0600

    chore: disable flaky content tracing tests on Linux (#45612)

--- spec/api-content-tracing-spec.ts
+++ spec/api-content-tracing-spec.ts
@@ -6,7 +6,7 @@ import * as fs from 'node:fs';
 import * as path from 'node:path';
 import { setTimeout } from 'node:timers/promises';
 
-import { ifdescribe, ifit } from './lib/spec-helpers';
+import { ifdescribe } from './lib/spec-helpers';
 
 // FIXME: The tests are skipped on linux arm/arm64
 ifdescribe(!(['arm', 'arm64'].includes(process.arch)) || (process.platform !== 'linux'))('contentTracing', () => {
@@ -91,7 +91,7 @@ ifdescribe(!(['arm', 'arm64'].includes(process.arch)) || (process.platform !== '
     });
   });
 
-  describe('stopRecording', function () {
+  ifdescribe(process.platform !== 'linux')('stopRecording', function () {
     if (process.platform === 'win32' && process.arch === 'arm64') {
       // WOA needs more time
       this.timeout(10e3);
@@ -112,8 +112,7 @@ ifdescribe(!(['arm', 'arm64'].includes(process.arch)) || (process.platform !== '
       expect(fs.statSync(path).isFile()).to.be.true('output exists');
     });
 
-    // FIXME(ckerr): this test regularly flakes
-    ifit(process.platform !== 'linux')('calls its callback with a result file path', async () => {
+    it('calls its callback with a result file path', async () => {
       const resultFilePath = await record(/* options */ {}, outputFilePath);
       expect(resultFilePath).to.be.a('string').and.be.equal(outputFilePath);
     });
