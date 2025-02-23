2d90dce83c501b75fef4c2c859d0881b96a59f92
Charles Kerr <charles@charleskerr.com>
2025-02-23 17:36:34 -0600

    refactor: remove unused isolate arg from GlobalShortcut constructor
    has not been used since f1a0d5e811 (#22755)

--- shell/browser/api/electron_api_global_shortcut.cc
+++ shell/browser/api/electron_api_global_shortcut.cc
@@ -50,7 +50,7 @@ namespace electron::api {
 
 gin::WrapperInfo GlobalShortcut::kWrapperInfo = {gin::kEmbedderNativeGin};
 
-GlobalShortcut::GlobalShortcut(v8::Isolate* isolate) {}
+GlobalShortcut::GlobalShortcut() {}
 
 GlobalShortcut::~GlobalShortcut() {
   UnregisterAll();
@@ -216,7 +216,7 @@ void GlobalShortcut::UnregisterAll() {
 
 // static
 gin::Handle<GlobalShortcut> GlobalShortcut::Create(v8::Isolate* isolate) {
-  return gin::CreateHandle(isolate, new GlobalShortcut(isolate));
+  return gin::CreateHandle(isolate, new GlobalShortcut());
 }
 
 // static
