40b92d1ef9b63a3531a71577c22e38d8f1bae7b3
Nilay Arya <nilay2014@gmail.com>
2025-02-27 12:59:23 +0530

    fixed some linting issues

--- shell/browser/mac/geo_utils.h
+++ shell/browser/mac/geo_utils.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 GitHub, Inc.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
 #ifndef ELECTRON_SHELL_BROWSER_MAC_GEO_UTILS_H_
 #define ELECTRON_SHELL_BROWSER_MAC_GEO_UTILS_H_
 
@@ -5,6 +9,6 @@ namespace electron {
 
 void InitializeGeolocation();
 
-}
+}  // namespace electron
 
-#endif
+#endif  // ELECTRON_SHELL_BROWSER_MAC_GEO_UTILS_H_
--- shell/browser/mac/geo_utils.mm
+++ shell/browser/mac/geo_utils.mm
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 GitHub, Inc.
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
+
 #include "shell/browser/mac/geo_utils.h"
 
 #include "services/device/public/cpp/geolocation/geolocation_system_permission_manager.h"
