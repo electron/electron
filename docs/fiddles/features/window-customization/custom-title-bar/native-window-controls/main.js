96c2022483338b458782840be1e3a5b71b4c3f44
co63oc <co63oc@users.noreply.github.com>
2025-02-27 21:31:21 +0800

    chore: Fix typos (#45825)

--- docs/fiddles/features/window-customization/custom-title-bar/native-window-controls/main.js
+++ docs/fiddles/features/window-customization/custom-title-bar/native-window-controls/main.js
@@ -4,7 +4,7 @@ function createWindow () {
   const win = new BrowserWindow({
     // remove the default titlebar
     titleBarStyle: 'hidden',
-    // expose window controlls in Windows/Linux
+    // expose window controls in Windows/Linux
     ...(process.platform !== 'darwin' ? { titleBarOverlay: true } : {})
   })
   win.loadURL('https://example.com')
