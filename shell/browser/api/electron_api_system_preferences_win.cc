abe40aeb4d53bad5747c2968b40828b1c55f3eb4
deepak1556 <hop2deep@gmail.com>
2025-03-03 14:41:11 +0900

    Don't assume STL iterators are pointers
    Refs https://issues.chromium.org/issues/328308661

--- shell/browser/api/electron_api_system_preferences_win.cc
+++ shell/browser/api/electron_api_system_preferences_win.cc
@@ -129,7 +129,7 @@ std::string SystemPreferences::GetColor(gin_helper::ErrorThrower thrower,
       {"window-text", COLOR_WINDOWTEXT},
   });
 
-  if (const auto* iter = Lookup.find(color); iter != Lookup.end())
+  if (auto iter = Lookup.find(color); iter != Lookup.end())
     return ToRGBAHex(color_utils::GetSysSkColor(iter->second));
 
   thrower.ThrowError("Unknown color: " + color);
