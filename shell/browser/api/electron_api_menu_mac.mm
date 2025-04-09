5e8f158bcb85dcfd05e8197f0b19d9f97b7b0da4
Keeley Hammond <khammond@slack-corp.com>
2025-04-09 13:49:20 -0700

    fix: fallback to old MacOS context menu behavior if no frame is present
    Co-authored-by: georgexu99 <georgexu99@electronjs.org>
    Co-authored-by: erickzhao <erickzhao@electronjs.org>
    Co-authored-by: clavin <calvin@electronjs.org>

--- shell/browser/api/electron_api_menu_mac.mm
+++ shell/browser/api/electron_api_menu_mac.mm
@@ -165,18 +165,30 @@ void MenuMac::PopupOnUI(const base::WeakPtr<NativeWindow>& native_window,
       RenderWidgetHostViewCocoa* cocoa_view = rwhvm->GetInProcessNSView();
       view = cocoa_view;
     }
-  }
+    NSEvent* dummy_event = [NSEvent mouseEventWithType:NSEventTypeRightMouseDown
+                                              location:position
+                                         modifierFlags:0
+                                             timestamp:0
+                                          windowNumber:nswindow.windowNumber
+                                               context:nil
+                                           eventNumber:0
+                                            clickCount:1
+                                              pressure:0];
+    ui::ShowContextMenu(menu, dummy_event, view, true);
+  } else {
+    // Make sure events can be pumped while the menu is up.
+    base::CurrentThread::ScopedAllowApplicationTasksInNativeNestedLoop allow;
 
-  NSEvent* dummy_event = [NSEvent mouseEventWithType:NSEventTypeRightMouseDown
-                                            location:position
-                                       modifierFlags:0
-                                           timestamp:0
-                                        windowNumber:nswindow.windowNumber
-                                             context:nil
-                                         eventNumber:0
-                                          clickCount:1
-                                            pressure:0];
-  ui::ShowContextMenu(menu, dummy_event, view, true);
+    // One of the events that could be pumped is |window.close()|.
+    // User-initiated event-tracking loops protect against this by
+    // setting flags in -[CrApplication sendEvent:], but since
+    // web-content menus are initiated by IPC message the setup has to
+    // be done manually.
+    base::mac::ScopedSendingEvent sendingEventScoper;
+
+    // Don't emit unresponsive event when showing menu.
+    [menu popUpMenuPositioningItem:item atLocation:position inView:view];
+  }
 }
 
 void MenuMac::ClosePopupAt(int32_t window_id) {
