58d48eb5641a130a8d8901a26928a0d2b029c20a
Will Anderson <andersonw@dropbox.com>
2025-04-24 08:49:21 -0700

    test: Fill out Window embedding tests to better reflect how someone might use an ImageView

--- spec/api-image-view-spec.ts
+++ spec/api-image-view-spec.ts
@@ -1,6 +1,8 @@
 import { nativeImage } from 'electron/common';
 import { BaseWindow, BrowserWindow, ImageView } from 'electron/main';
 
+import { expect } from 'chai';
+
 import * as path from 'node:path';
 
 import { closeAllWindows } from './lib/window-helpers';
@@ -36,14 +38,49 @@ describe('ImageView', () => {
   });
 
   it('can be embedded in a BaseWindow', () => {
-    const w = new BaseWindow();
+    const w = new BaseWindow({ show: false });
     const view = new ImageView();
+    const image = nativeImage.createFromPath(path.join(__dirname, 'fixtures', 'assets', 'capybara.png'));
+    view.setImage(image);
     w.setContentView(view);
+    w.setContentSize(image.getSize().width, image.getSize().height);
+    view.setBounds({
+      x: 0,
+      y: 0,
+      width: image.getSize().width,
+      height: image.getSize().height
+    });
   });
 
   it('can be embedded in a BrowserWindow', () => {
-    const w = new BrowserWindow();
+    const w = new BrowserWindow({ show: false });
+    const image = nativeImage.createFromPath(path.join(__dirname, 'fixtures', 'assets', 'logo.png'));
     const view = new ImageView();
-    w.setContentView(view);
+    view.setImage(image);
+    w.contentView.addChildView(view);
+    w.setContentSize(image.getSize().width, image.getSize().height);
+    view.setBounds({
+      x: 0,
+      y: 0,
+      width: image.getSize().width,
+      height: image.getSize().height
+    });
+
+    expect(w.contentView.children).to.include(view);
+  });
+
+  it('can be removed from a BrowserWindow', async () => {
+    const w = new BrowserWindow({ show: false });
+    const image = nativeImage.createFromPath(path.join(__dirname, 'fixtures', 'assets', 'logo.png'));
+    const view = new ImageView();
+    view.setImage(image);
+
+    w.contentView.addChildView(view);
+    expect(w.contentView.children).to.include(view);
+
+    await w.loadFile(path.join(__dirname, 'fixtures', 'api', 'blank.html'));
+
+    w.contentView.removeChildView(view);
+    expect(w.contentView.children).to.not.include(view);
   });
 });
