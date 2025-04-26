b71300270e4ca0b0bb3aacea0d9ba95d69621f24
Charles Kerr <charles@charleskerr.com>
2025-04-26 12:13:08 -0500

    fix: nested it() calls in visibleOnAllWorkspaces specs

--- spec/api-browser-window-spec.ts
+++ spec/api-browser-window-spec.ts
@@ -5416,7 +5416,7 @@ describe('BrowserWindow module', () => {
     });
 
     ifdescribe(process.platform !== 'win32')('visibleOnAllWorkspaces state', () => {
-      it('with properties', () => {
+      describe('with properties', () => {
         it('can be changed', () => {
           const w = new BrowserWindow({ show: false });
           expect(w.visibleOnAllWorkspaces).to.be.false();
