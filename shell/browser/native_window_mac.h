63114e2b8ead2c20fadfd1c8ea6f32e7607e6044
Keeley Hammond <vertedinde@electronjs.org>
2025-05-01 13:49:55 -0700

    fix: revert macOS content protection logic refactor (#46886)
    Revert "refactor: use upstream content protection logic on macOS (#46813)"
    
    This reverts commit 34adb976b632157379de34cc1a71bdf6cc089714.

--- shell/browser/native_window_mac.h
+++ shell/browser/native_window_mac.h
@@ -108,6 +108,8 @@ class NativeWindowMac : public NativeWindow,
   void SetIgnoreMouseEvents(bool ignore, bool forward) override;
   bool IsHiddenInMissionControl() const override;
   void SetHiddenInMissionControl(bool hidden) override;
+  void SetContentProtection(bool enable) override;
+  bool IsContentProtected() const override;
   void SetFocusable(bool focusable) override;
   bool IsFocusable() const override;
   void SetParentWindow(NativeWindow* parent) override;
