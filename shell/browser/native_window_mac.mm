102be15e3684cad0c2c6061cf4ebecd1ca82d06a
Michaela Laurencin <35157522+mlaurencin@users.noreply.github.com>
2025-05-09 12:03:45 -0400

    feat: enable innerWidth and innerHeight for window open (#46749)
    * feat: enable innerWidth and innerHeight for window open
    
    * update comment for added special innerWidth and innerHeight
    
    * update 100 min spec requirement handling
    
    * update testing to include getContentSize
    
    * update macOS min requirement handling
    
    * adjust refactored consts
    
    * update const values from nativewindowviews

--- shell/browser/native_window_mac.mm
+++ shell/browser/native_window_mac.mm
@@ -291,6 +291,22 @@ NativeWindowMac::NativeWindowMac(const gin_helper::Dictionary& options,
   // Resize to content bounds.
   bool use_content_size = false;
   options.Get(options::kUseContentSize, &use_content_size);
+  // NOTE(@mlaurencin) Spec requirements can be found here:
+  // https://developer.mozilla.org/en-US/docs/Web/API/Window/open#width
+  constexpr int kMinSizeReqdBySpec = 100;
+  int inner_width = 0;
+  int inner_height = 0;
+
+  options.Get(options::kinnerWidth, &inner_width);
+  options.Get(options::kinnerHeight, &inner_height);
+  if (inner_width || inner_height) {
+    use_content_size = true;
+    if (inner_width)
+      width = std::max(kMinSizeReqdBySpec, inner_width);
+    if (inner_height)
+      height = std::max(kMinSizeReqdBySpec, inner_height);
+  }
+
   if (!has_frame() || use_content_size)
     SetContentSize(gfx::Size(width, height));
 
