8eba283395de20100a4088575c5a0291b61c9ded
David Sanders <dsanders11@ucsbalum.com>
2025-05-15 14:20:57 -0700

    6544950: Revert "FSA: Only normalize the hardcoded rules once during initialization"
    Refs https://chromium-review.googlesource.com/c/chromium/src/+/6544950

--- shell/browser/file_system_access/file_system_access_permission_context.h
+++ shell/browser/file_system_access/file_system_access_permission_context.h
@@ -12,14 +12,13 @@
 #include <string>
 #include <vector>
 
-#include "base/callback_list.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/clock.h"
 #include "base/time/default_clock.h"
 #include "base/values.h"
-#include "chrome/browser/file_system_access/chrome_file_system_access_permission_context.h"  // nogncheck
 #include "components/keyed_service/core/keyed_service.h"
+#include "content/public/browser/file_system_access_permission_context.h"
 
 class GURL;
 
@@ -136,14 +135,6 @@ class FileSystemAccessPermissionContext
 
   void PermissionGrantDestroyed(PermissionGrantImpl* grant);
 
-  void CheckShouldBlockAccessToPathAndReply(
-      base::FilePath path,
-      HandleType handle_type,
-      std::vector<ChromeFileSystemAccessPermissionContext::BlockPathRule>
-          extra_rules,
-      base::OnceCallback<void(bool)> callback,
-      ChromeFileSystemAccessPermissionContext::BlockPathRules block_path_rules);
-
   void CheckPathAgainstBlocklist(const content::PathInfo& path,
                                  HandleType handle_type,
                                  base::OnceCallback<void(bool)> callback);
@@ -168,11 +159,6 @@ class FileSystemAccessPermissionContext
                                    const base::FilePath& path,
                                    GrantType grant_type) const;
 
-  void ResetBlockPaths();
-  void UpdateBlockPaths(
-      std::unique_ptr<ChromeFileSystemAccessPermissionContext::BlockPathRules>
-          block_path_rules);
-
   base::WeakPtr<FileSystemAccessPermissionContext> GetWeakPtr();
 
   const raw_ptr<content::BrowserContext, DanglingUntriaged> browser_context_;
@@ -189,14 +175,6 @@ class FileSystemAccessPermissionContext
 
   std::map<base::FilePath, base::OnceCallback<void(SensitiveEntryResult)>>
       callback_map_;
-
-  std::unique_ptr<ChromeFileSystemAccessPermissionContext::BlockPathRules>
-      block_path_rules_;
-  bool is_block_path_rules_init_complete_ = false;
-  std::vector<base::CallbackListSubscription> block_rules_check_subscription_;
-  base::OnceCallbackList<void(
-      ChromeFileSystemAccessPermissionContext::BlockPathRules)>
-      block_rules_check_callbacks_;
 
   base::WeakPtrFactory<FileSystemAccessPermissionContext> weak_factory_{this};
 };
