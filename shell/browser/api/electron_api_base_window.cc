5eef357196c1a73e6fd5c7aa577aa2dd598c07e7
Cezary Kulakowski <cezary@openfin.co>
2025-05-19 16:27:18 +0200

    fix: fixed white flash on call to BrowserWindow.show

--- shell/browser/api/electron_api_base_window.cc
+++ shell/browser/api/electron_api_base_window.cc
@@ -386,9 +386,6 @@ void BaseWindow::Show() {
 }
 
 void BaseWindow::ShowInactive() {
-  // This method doesn't make sense for modal window.
-  if (IsModal())
-    return;
   window_->ShowInactive();
 }
 
