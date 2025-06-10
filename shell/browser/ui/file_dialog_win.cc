88e1574df06ee88581446c9356f503d85b0a124d
Charles Kerr <charles@charleskerr.com>
2025-06-09 19:47:43 -0500

    refactor: have ShowSaveDialogSync() return a std::optional<base::FilePath>

--- shell/browser/ui/file_dialog_win.cc
+++ shell/browser/ui/file_dialog_win.cc
@@ -219,11 +219,12 @@ void ShowOpenDialog(const DialogSettings& settings,
                      base::BindOnce(done, std::move(promise)));
 }
 
-bool ShowSaveDialogSync(const DialogSettings& settings, base::FilePath* path) {
+std::optional<base::FilePath> ShowSaveDialogSync(
+    const DialogSettings& settings) {
   ATL::CComPtr<IFileSaveDialog> file_save_dialog;
   HRESULT hr = file_save_dialog.CoCreateInstance(CLSID_FileSaveDialog);
   if (FAILED(hr))
-    return false;
+    return {};
 
   DWORD options = FOS_FORCEFILESYSTEM | FOS_PATHMUSTEXIST | FOS_OVERWRITEPROMPT;
   if (settings.properties & SAVE_DIALOG_SHOW_HIDDEN_FILES)
@@ -236,22 +237,21 @@ bool ShowSaveDialogSync(const DialogSettings& settings, base::FilePath* path) {
   hr = ShowFileDialog(file_save_dialog, settings);
 
   if (FAILED(hr))
-    return false;
+    return {};
 
   CComPtr<IShellItem> pItem;
   hr = file_save_dialog->GetResult(&pItem);
   if (FAILED(hr))
-    return false;
+    return {};
 
   PWSTR result_path = nullptr;
   hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &result_path);
   if (!SUCCEEDED(hr))
-    return false;
+    return {};
 
-  *path = base::FilePath(result_path);
+  auto path = base::FilePath{result_path};
   CoTaskMemFree(result_path);
-
-  return true;
+  return path;
 }
 
 void ShowSaveDialog(const DialogSettings& settings,
