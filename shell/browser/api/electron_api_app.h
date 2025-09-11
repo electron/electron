a509b4b6eda990ff0bdf65c563a9ff1146801453
Charles Kerr <charles@charleskerr.com>
2025-09-11 15:33:08 -0500

    refactor: narrow App:SetJumpList() arg from gin::Arguments* to v8::Isolate*

--- shell/browser/api/electron_api_app.h
+++ shell/browser/api/electron_api_app.h
@@ -258,7 +258,7 @@ class App final : public gin::Wrappable<App>,
   v8::Local<v8::Value> GetJumpListSettings();
 
   // Set or remove a custom Jump List for the application.
-  JumpListResult SetJumpList(v8::Local<v8::Value> val, gin::Arguments* args);
+  JumpListResult SetJumpList(v8::Isolate* isolate, v8::Local<v8::Value> val);
 #endif  // BUILDFLAG(IS_WIN)
 
   std::unique_ptr<ProcessSingleton> process_singleton_;
