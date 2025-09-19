067b3d2b8a4011b26c342c53688b070ca931edaa
Charles Kerr <charles@charleskerr.com>
2025-09-19 10:39:05 -0500

    refactor: move GetClipboadBuffer() into anonymous namespace

--- shell/common/api/electron_api_clipboard.h
+++ shell/common/api/electron_api_clipboard.h
@@ -66,9 +66,6 @@ class Clipboard {
                           gin::Arguments* args);
 
   static void WriteFilesForTesting(const std::vector<base::FilePath>& files);
-
- private:
-  static ui::ClipboardBuffer GetClipboardBuffer(gin_helper::Arguments* args);
 };
 
 }  // namespace electron::api
