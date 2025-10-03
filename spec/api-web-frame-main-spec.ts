ef085f88a1af53b6408a7af695cc60b8681398cf
Samuel Maddock <smaddock@slack-corp.com>
2025-10-03 11:26:40 -0700

    chore: try non-proprietary video format

--- spec/api-web-frame-main-spec.ts
+++ spec/api-web-frame-main-spec.ts
@@ -535,7 +535,7 @@ describe('webFrameMain module', () => {
 
   describe('webFrameMain.copyVideoFrameAt', () => {
     const insertVideoInFrame = async (frame: WebFrameMain) => {
-      const videoPath = path.join(fixtures, 'cat-spin.mp4');
+      const videoPath = path.join(fixtures, 'cat-spin.webm');
       await frame.executeJavaScript(`
         const video = document.createElement('video');
         video.src = '${videoPath}';
