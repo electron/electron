b6f7f94471f8d1fae71054a914e3d8bda9782eca
David Sanders <dsanders11@ucsbalum.com>
2025-10-13 22:21:13 -0700

    7003989: Add wrapper struct for CopyFromSurface output
    Refs https://chromium-review.googlesource.com/c/chromium/src/+/7003989

--- shell/browser/osr/osr_render_widget_host_view.h
+++ shell/browser/osr/osr_render_widget_host_view.h
@@ -147,7 +147,8 @@ class OffScreenRenderWidgetHostView
   void CopyFromSurface(
       const gfx::Rect& src_rect,
       const gfx::Size& output_size,
-      base::OnceCallback<void(const SkBitmap&)> callback) override;
+      base::OnceCallback<void(const viz::CopyOutputBitmapWithMetadata&)>
+          callback) override;
   display::ScreenInfo GetScreenInfo() const override;
   void TransformPointToRootSurface(gfx::PointF* point) override {}
   gfx::Rect GetBoundsInRootWindow() override;
