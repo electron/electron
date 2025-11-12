3754d4e625353ea497791fd7e8f4c8fccfb80ed9
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2025-11-12 17:05:26 +1100

    fix: return type

--- shell/browser/api/electron_api_view.h
+++ shell/browser/api/electron_api_view.h
@@ -70,7 +70,7 @@ class View : public gin_helper::EventEmitter<View>,
   void OnChildViewRemoved(views::View* observed_view,
                           views::View* child) override;
 
-  void GetLayer();
+  ui::Layer* GetLayer();
   void ApplyBorderRadius();
   void ReorderChildView(gin_helper::Handle<View> child, size_t index);
 
