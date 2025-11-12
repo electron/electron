b4152290c5e671fa9c2d5d9d5279ae6474c5a7df
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2025-11-12 19:37:26 +1100

    spec: add test for symbol options

--- spec/api-native-image-spec.ts
+++ spec/api-native-image-spec.ts
@@ -353,6 +353,15 @@ describe('nativeImage module', () => {
       expect(image.isEmpty()).to.be.false();
     });
 
+    ifit(process.platform === 'darwin')('returns a valid named symbol with options on darwin', function () {
+      const image = nativeImage.createFromNamedImage('atom', undefined, {
+        weight: 'ultralight',
+        scale: 'small',
+        pointSize: 24
+      });
+      expect(image.isEmpty()).to.be.false();
+    });
+
     ifit(process.platform === 'darwin')('returns allows an HSL shift for a valid image on darwin', function () {
       const image = nativeImage.createFromNamedImage('NSActionTemplate', [0.5, 0.2, 0.8]);
       expect(image.isEmpty()).to.be.false();
