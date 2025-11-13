32ae696ee04490e6076ebd90605bc28a960d405a
Nilay Arya <84241885+nilayarya@users.noreply.github.com>
2025-11-13 10:39:03 -0500

    feat: add --disable-geolocation command-line flag for macOS (#45934)
    * feat(macos): add --disable-geolocation-mac command-line flag
    
    * internally deny geolocation requests if flag set
    
    e
    
    * wrap PermissionRequestHandler instead
    
    * wrap custom handler and deny regardless of response
    
    * Update docs/api/command-line-switches.md
    
    Co-authored-by: Will Anderson <will@itsananderson.com>
    
    * resolving conflicts during rebase
    
    * tests added
    
    * tests added: minor changes
    
    * move IsGeolocationDisabledViaCommandLine inside ElectronPermissionManager as a static member
    
    * test: inject fixturesPath via --boot-eval
    
    * Update shell/browser/electron_permission_manager.cc
    
    Co-authored-by: Robo <hop2deep@gmail.com>
    
    * chore: Fixup after merge
    
    * fixup after merge
    
    ---------
    
    Co-authored-by: Will Anderson <will@itsananderson.com>
    Co-authored-by: Robo <hop2deep@gmail.com>
    Co-authored-by: John Kleinschmidt <jkleinsc@electronjs.org>

--- shell/browser/api/electron_api_session.cc
+++ shell/browser/api/electron_api_session.cc
@@ -886,6 +886,24 @@ void Session::SetPermissionRequestHandler(v8::Local<v8::Value> val,
          blink::PermissionType permission_type,
          ElectronPermissionManager::StatusCallback callback,
          const base::Value& details) {
+#if (BUILDFLAG(IS_MAC))
+        if (permission_type == blink::PermissionType::GEOLOCATION) {
+          if (ElectronPermissionManager::
+                  IsGeolocationDisabledViaCommandLine()) {
+            auto original_callback = std::move(callback);
+            callback = base::BindOnce(
+                [](ElectronPermissionManager::StatusCallback callback,
+                   content::PermissionResult /*ignored_result*/) {
+                  // Always deny regardless of what
+                  // content::PermissionResult is passed here
+                  std::move(callback).Run(content::PermissionResult(
+                      blink::mojom::PermissionStatus::DENIED,
+                      content::PermissionStatusSource::UNSPECIFIED));
+                },
+                std::move(original_callback));
+          }
+        }
+#endif
         handler->Run(web_contents, permission_type, std::move(callback),
                      details);
       },
