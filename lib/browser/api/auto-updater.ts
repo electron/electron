cb9775a1e59ac2b1ebb9a5b48697b9f6fa2268b0
Jan Hannemann <jan.hannemann@outlook.com>
2026-01-20 13:28:49 -0800

    fix: review feedback, add comment

--- lib/browser/api/auto-updater.ts
+++ lib/browser/api/auto-updater.ts
@@ -1,4 +1,5 @@
 if (process.platform === 'win32') {
+  // windowsStore indicates whether the app is running as a packaged app (MSIX), even outside of the store
   if (process.windowsStore) {
     module.exports = require('./auto-updater/auto-updater-msix');
   } else {
