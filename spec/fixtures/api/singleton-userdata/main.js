30152894b39d822535edd7c44699d841a6b30d95
Noah Gregory <noahmgregory@gmail.com>
2026-01-30 18:49:45 -0500

    refactor: reintroduce `userDataFolder` declaration in singleton test

--- spec/fixtures/api/singleton-userdata/main.js
+++ spec/fixtures/api/singleton-userdata/main.js
@@ -3,13 +3,14 @@ const { app } = require('electron');
 const fs = require('node:fs');
 const path = require('node:path');
 
-app.setPath('userData', path.join(app.getPath('temp'), 'electron-test-singleton-userdata'));
+const userDataFolder = path.join(app.getPath('temp'), 'electron-test-singleton-userdata')
+app.setPath('userData', userDataFolder);
 
 // non-existent user data folder should not break requestSingleInstanceLock()
 // ref: https://github.com/electron/electron/issues/33547
-fs.rmSync(app.getPath('userData'), { recursive: true, force: true });
+fs.rmSync(userDataFolder, { recursive: true, force: true });
 
 const gotTheLock = app.requestSingleInstanceLock();
 app.exit(gotTheLock ? 0 : 1);
 
-fs.rmSync(app.getPath('userData'), { recursive: true, force: true });
+fs.rmSync(userDataFolder, { recursive: true, force: true });
