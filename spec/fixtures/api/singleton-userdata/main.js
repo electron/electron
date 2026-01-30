90a6ae62aa62e2e6d8f5687ba07ede0114c1f635
Noah Gregory <noahmgregory@gmail.com>
2026-01-30 15:11:45 -0500

    fix: create directory for singleton test in `temp` instead of `home`

--- spec/fixtures/api/singleton-userdata/main.js
+++ spec/fixtures/api/singleton-userdata/main.js
@@ -5,7 +5,7 @@ const path = require('node:path');
 
 // non-existent user data folder should not break requestSingleInstanceLock()
 // ref: https://github.com/electron/electron/issues/33547
-const userDataFolder = path.join(app.getPath('home'), 'electron-test-singleton-userdata');
+const userDataFolder = path.join(app.getPath('temp'), 'electron-test-singleton-userdata');
 fs.rmSync(userDataFolder, { force: true, recursive: true });
 app.setPath('userData', userDataFolder);
 
