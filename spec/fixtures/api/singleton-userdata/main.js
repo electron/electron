d97846ec673e63d7be48fef000f1a17ff8b59a35
Noah Gregory <noahmgregory@gmail.com>
2026-01-30 15:20:51 -0500

    refactor: avoid extraneous declarations in singleton test

--- spec/fixtures/api/singleton-userdata/main.js
+++ spec/fixtures/api/singleton-userdata/main.js
@@ -3,23 +3,13 @@ const { app } = require('electron');
 const fs = require('node:fs');
 const path = require('node:path');
 
-const userDataFolder = path.join(app.getPath('temp'), 'electron-test-singleton-userdata');
-
-function removeUserDataFolder() {
-  try {
-    fs.rmSync(userDataFolder, { recursive: true, force: true });
-  } catch (e) {
-    // ignore
-  }
-}
+app.setPath('userData', path.join(app.getPath('temp'), 'electron-test-singleton-userdata'));
 
 // non-existent user data folder should not break requestSingleInstanceLock()
 // ref: https://github.com/electron/electron/issues/33547
-removeUserDataFolder();
-
-app.setPath('userData', userDataFolder);
+fs.rmSync(app.getPath('userData'), { recursive: true, force: true });
 
 const gotTheLock = app.requestSingleInstanceLock();
 app.exit(gotTheLock ? 0 : 1);
 
-removeUserDataFolder();
+fs.rmSync(app.getPath('userData'), { recursive: true, force: true });
