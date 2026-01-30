10668a631df79e85af97d0f9bf7e169c7838901b
Noah Gregory <noahmgregory@gmail.com>
2026-01-30 15:13:31 -0500

    fix: remove directory for singleton test at test end

--- spec/fixtures/api/singleton-userdata/main.js
+++ spec/fixtures/api/singleton-userdata/main.js
@@ -3,11 +3,23 @@ const { app } = require('electron');
 const fs = require('node:fs');
 const path = require('node:path');
 
+const userDataFolder = path.join(app.getPath('temp'), 'electron-test-singleton-userdata');
+
+function removeUserDataFolder() {
+  try {
+    fs.rmSync(userDataFolder, { recursive: true, force: true });
+  } catch (e) {
+    // ignore
+  }
+}
+
 // non-existent user data folder should not break requestSingleInstanceLock()
 // ref: https://github.com/electron/electron/issues/33547
-const userDataFolder = path.join(app.getPath('temp'), 'electron-test-singleton-userdata');
-fs.rmSync(userDataFolder, { force: true, recursive: true });
+removeUserDataFolder();
+
 app.setPath('userData', userDataFolder);
 
 const gotTheLock = app.requestSingleInstanceLock();
 app.exit(gotTheLock ? 0 : 1);
+
+removeUserDataFolder();
