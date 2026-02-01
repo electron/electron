83eaa3f0459b65280ecd99a5eb269deed3555954
Noah Gregory <noahmgregory@gmail.com>
2026-02-01 14:39:23 -0500

    refactor: set the user data path after pre-test cleanup in singleton test

--- spec/fixtures/api/singleton-userdata/main.js
+++ spec/fixtures/api/singleton-userdata/main.js
@@ -4,12 +4,15 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const userDataFolder = path.join(app.getPath('temp'), 'electron-test-singleton-userdata');
-app.setPath('userData', userDataFolder);
 
 // non-existent user data folder should not break requestSingleInstanceLock()
 // ref: https://github.com/electron/electron/issues/33547
 fs.rmSync(userDataFolder, { recursive: true, force: true });
+
+// set the user data path after clearing out old state and right before we use it
+app.setPath('userData', userDataFolder);
 const gotTheLock = app.requestSingleInstanceLock();
+
 fs.rmSync(userDataFolder, { recursive: true, force: true });
 
 app.exit(gotTheLock ? 0 : 1);
