6718f2b5e3bef1f33815209de13085813996a362
reito <reito@chromium.org>
2026-02-14 03:41:38 +0800

    feat: add nv12 osr output format.

--- shell/browser/osr/osr_video_consumer.cc
+++ shell/browser/osr/osr_video_consumer.cc
@@ -24,6 +24,8 @@ media::VideoPixelFormat GetTargetPixelFormatFromOption(
     return media::PIXEL_FORMAT_ARGB;
   } else if (pixel_format_option == "rgbaf16") {
     return media::PIXEL_FORMAT_RGBAF16;
+  }else if (pixel_format_option == "nv12") {
+    return media::PIXEL_FORMAT_NV12;
   }
 
   // Use ARGB as default.
