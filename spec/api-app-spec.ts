89483e7e89f3b95db4ed85171024e9de2ac9c529
Zach Waugh <zwaugh@gmail.com>
2026-03-02 11:47:37 -0500

    feat: add macOS-only api to determine if app is currently active (#49622)
    * feat: add macOS-only api to determine if app is currently active
    
    You can `focus()` the app and get events for `did-become-active`, but there's currently not a way to directly check if your app is the active (foreground) application.
    
    * test: add unit test for app.isActive api
    
    * fix: ensure we hide app after showing in test
    
    If the app is still active, it may affect other tests like dock.bounce
    that behave differently depending on whether the app is active
    
    * docs: simplify isActive api description

--- spec/api-app-spec.ts
+++ spec/api-app-spec.ts
@@ -1773,6 +1773,31 @@ describe('app module', () => {
     });
   });
 
+  ifdescribe(process.platform === 'darwin')('app isActive API', () => {
+    describe('app.isActive', () => {
+      afterEach(closeAllWindows);
+
+      it('returns true when the app becomes active', async () => {
+        expect(app.isActive()).to.equal(false);
+
+        const w = new BrowserWindow({
+          width: 200,
+          height: 200,
+          show: false
+        });
+
+        w.show();
+
+        await expect(
+          waitUntil(() => app.isActive())
+        ).to.eventually.be.fulfilled();
+
+        w.close();
+        app.hide();
+      });
+    });
+  });
+
   ifdescribe(process.platform === 'darwin')('app hide and show API', () => {
     describe('app.isHidden', () => {
       it('returns true when the app is hidden', async () => {
