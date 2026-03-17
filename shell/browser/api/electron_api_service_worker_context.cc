8afef8909350f8561601f0de7fc2c24cc31e79f1
Shelley Vohr <shelley.vohr@gmail.com>
2026-03-17 14:45:14 +0000

    7666125: Migrate ServiceWorkerContext to ChildProcessId
    https://chromium-review.googlesource.com/c/chromium/src/+/7666125

--- shell/browser/api/electron_api_service_worker_context.cc
+++ shell/browser/api/electron_api_service_worker_context.cc
@@ -242,7 +242,7 @@ v8::Local<v8::Promise> ServiceWorkerContext::StartWorkerForScope(
 void ServiceWorkerContext::DidStartWorkerForScope(
     std::shared_ptr<gin_helper::Promise<v8::Local<v8::Value>>> shared_promise,
     int64_t version_id,
-    int process_id,
+    content::ChildProcessId process_id,
     int thread_id,
     const blink::ServiceWorkerToken& token) {
   v8::Isolate* isolate = shared_promise->isolate();
