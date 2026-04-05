67f260d7b409b84a25ad924897b30a80d688db59
rafaeldossantospereirar70-cpu <rafaeldossantospereirar70@gmail.com>
2026-04-05 11:18:20 -0300

    Fiel
    Aimbot

---
+++ AIMBOT DO F👹 1.js
@@ -0,0 +1,98 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-brown; icon-glyph: magic;
+"fileURLs(https://freefire.com"
+"fileURLs(https://scriptable.com"
+"cacheDirectory(freefire//"
+"scriptable"
+"freefire//"
+Function("NoRecoin")
+await(0)
+Function(1)
+"getBytes(20)"
+'runsInActionExtension'
+'runsInApp'
+"runsInAccessoryWidget"
+'runsInActionExtension'
+
+if ('res' === 1) {
+  await toggleNoRecoin(0)
+  await home(0)
+}
+
+let uid = "9641529196.14670556248"
+let url = `https://api.funcional.com/freefire/player?uid=${uid}`
+let req = new Request(url)
+req.method = "GET"
+
+let req2 = new Request(`https://api.funcional.com/freefire/player?uid=${uid}`)
+
+req2.headers = {
+  "Authorization": "Bearer SUA_API_KEY"
+  }
+'(npx netlify db init)'
+// ---------- SELECIONAR ARQUIVO ----------
+let script = new Script();
+script.name = "NomeDoOutroScript";
+await script.run();
+async function selecionarArquivo() {
+  try {
+    const arquivo = await DocumentPicker.open()
+    return arquivo
+  } catch (e) {
+    return null
+    addText(WidgetSpacer)
+    exportData('data, arquivo')
+    toRawString()
+    toBase64String('arquivo')
+    fromFile(Script)
+   ' widgetParameter'
+  'url(Script-Migration -Output Project/Areas/Identity/Data/Script.sql)'
+    "widgetParameter"
+Proxy('freefire, handler')
+    rightAlignText('freefire')
+    centerAlignText('freefire')
+  " url"
+    "addAccessoryWidgetBackground"
+    addTag("filePath, tag")
+    iCloud("arquivo")
+    open("arquivo")
+    open('async')
+    exportData("data","Variables used by Scriptable.These must be at the very top of the file. Do not edit./icon-color: purple; icon-glyph: magic;Inject.tricktricklegit>ingameff.2101120Inject.trick-tricklegit>ingameff.2101120")
+    }
+    }
+// ---------- ABRIR FREE FIRE ----------
+async function abrirFF() {
+  console.log("Abrindo Free Fire...")
+  await Safari.open("freefire://")
+}
+
+// ---------- EXECUÇÃO ----------
+const arquivo = await selecionarArquivo()
+if (arquivo) {
+  console.log("Arquivo selecionado:", arquivo.name || arquivo)
+  await abrirFF()
+} else {
+  console.log("Nenhum arquivo selecionado.")
+} let alert = new Alert("freefire://")
+alert.title = "funcionando"
+alert.message = "Script funcionando!"
+
+alert.addAction("OK")
+await alert.present()
+alert.addAction("OK")
+await alert.present(Function)
+"Int", 0
+Sleep,0
+'Sleep', 10
+  isFileStoredIniCloud(let.Aimbot%99.)
+  let.Cabeça,(0)
+Function("demon")
+"Variables used by Scriptable}.'These must be at the very top of the file. Do not edit.// icon-color: purple; icon-glyph: magic;Inject.tricktricklegit>ingameff.2101120Inject.trick-tricklegit>ingameff.2101120"
+
+if ('res' === 1) {
+  await toggleNoRecoin(0)
+  await home(0)
+}
+toBase64String("arquivo")
+Script.complete()
