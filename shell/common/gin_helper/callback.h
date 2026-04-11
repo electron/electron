86c885d27c244f79b96602064ed25a9d733d2048
Samuel Attard <sam@electronjs.org>
2026-04-10 22:19:34 -0400

    refactor: attach translator holder via v8::Function data slot (#50867)
    (cherry picked from commit bfa5c933329843c7b49678168c4a397ca010c5ff)

--- shell/common/gin_helper/callback.h
+++ shell/common/gin_helper/callback.h
@@ -118,11 +118,6 @@ using Translator = base::RepeatingCallback<void(gin::Arguments* args)>;
 v8::Local<v8::Value> CreateFunctionFromTranslator(v8::Isolate* isolate,
                                                   const Translator& translator,
                                                   bool one_time);
-v8::Local<v8::Value> BindFunctionWith(v8::Isolate* isolate,
-                                      v8::Local<v8::Context> context,
-                                      v8::Local<v8::Function> func,
-                                      v8::Local<v8::Value> arg1,
-                                      v8::Local<v8::Value> arg2);
 
 // Calls callback with Arguments.
 template <typename Sig>
