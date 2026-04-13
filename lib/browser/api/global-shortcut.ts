610c452b9a4392a07b037a641244ffed8912b69e
deepak1556 <hop2deep@gmail.com>
2026-04-13 15:27:25 +0900

    chore: fix lint

--- lib/browser/api/global-shortcut.ts
+++ lib/browser/api/global-shortcut.ts
@@ -8,29 +8,32 @@ const createGlobalShortcutIfNeeded = () => {
   }
 };
 
-export default new Proxy({}, {
-  get: (_target, property: keyof Electron.GlobalShortcut) => {
-    createGlobalShortcutIfNeeded();
-    const value = globalShortcut[property];
-    if (typeof value === 'function') {
-      return value.bind(globalShortcut);
+export default new Proxy(
+  {},
+  {
+    get: (_target, property: keyof Electron.GlobalShortcut) => {
+      createGlobalShortcutIfNeeded();
+      const value = globalShortcut[property];
+      if (typeof value === 'function') {
+        return value.bind(globalShortcut);
+      }
+      return value;
+    },
+    set: (_target, property: string, value: unknown) => {
+      createGlobalShortcutIfNeeded();
+      return Reflect.set(globalShortcut, property, value);
+    },
+    ownKeys: () => {
+      createGlobalShortcutIfNeeded();
+      return Reflect.ownKeys(globalShortcut);
+    },
+    has: (_target, property: string) => {
+      createGlobalShortcutIfNeeded();
+      return property in globalShortcut;
+    },
+    getOwnPropertyDescriptor: (_target, property: string) => {
+      createGlobalShortcutIfNeeded();
+      return Reflect.getOwnPropertyDescriptor(globalShortcut, property);
     }
-    return value;
-  },
-  set: (_target, property: string, value: unknown) => {
-    createGlobalShortcutIfNeeded();
-    return Reflect.set(globalShortcut, property, value);
-  },
-  ownKeys: () => {
-    createGlobalShortcutIfNeeded();
-    return Reflect.ownKeys(globalShortcut);
-  },
-  has: (_target, property: string) => {
-    createGlobalShortcutIfNeeded();
-    return property in globalShortcut;
-  },
-  getOwnPropertyDescriptor: (_target, property: string) => {
-    createGlobalShortcutIfNeeded();
-    return Reflect.getOwnPropertyDescriptor(globalShortcut, property);
   }
-});
+);
