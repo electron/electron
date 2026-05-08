c6800ea590ff71605507dbd5ce85d36c6202cc03
Mitchell Cohen <mitch.cohen@me.com>
2026-05-08 19:09:10 -0400

    fix: improve resize band positioning and scaling for frameless windows on Windows (#51560)
    fix: improve resize band positioning and scaling for frameless windows on Windows (#51503)
    
    fix internal resize and scaling for frameless windows

--- shell/browser/ui/views/win_frame_view.h
+++ shell/browser/ui/views/win_frame_view.h
@@ -39,6 +39,7 @@ class WinFrameView : public FramelessView {
   gfx::Size GetMaximumSize() const override;
 
   // views::FramelessView:
+  int ResizingBorderHitTest(const gfx::Point& point) override;
   gfx::Insets RestoredFrameBorderInsets() const override;
 
   WinCaptionButtonContainer* caption_button_container() {
