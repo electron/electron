bc843cc397d43c6c32040265af79f8940a6f3892
Felix Rieseberg <f@anthropic.com>
2026-06-30 10:29:38 -0700

    chore: fix formatting

--- spec/api-web-contents-spec.ts
+++ spec/api-web-contents-spec.ts
@@ -1567,7 +1567,9 @@ describe('webContents module', () => {
         await openCustomDevTools(w, devtools);
 
         let emittedContextMenu = false;
-        devtools.webContents.once('context-menu', () => { emittedContextMenu = true; });
+        devtools.webContents.once('context-menu', () => {
+          emittedContextMenu = true;
+        });
 
         const roundTripped = await devToolsMenuRequestRoundTrips(devtools.webContents);
         expect(roundTripped).to.be.true();
