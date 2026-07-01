8d25cb3f46716d2604715e588fe850babb8398ec
Fedor Indutny <79877362+indutny-signal@users.noreply.github.com>
2026-07-01 12:49:20 -0700

    fix: running under `NODE_OPTIONS='--import=tsx'` (#52046)
    fix: running under `NODE_OPTIONS='--import=tsx'` (#51947)
    
    Newer versions of `tsx` use Node.js customization hooks for transpiling
    .ts files on the fly. Here we add `electron:` scheme to the validation
    list so that it doesn't throw an error.

---
+++ spec/fixtures/baz.txt
@@ -0,0 +1,1 @@
+Lorem Ipsum
--- spec/node-spec.ts
+++ spec/node-spec.ts
@@ -7,6 +7,7 @@ import { once } from 'node:events';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import { EventEmitter } from 'node:stream';
+import { pathToFileURL } from 'node:url';
 import * as util from 'node:util';
 
 import {
@@ -818,6 +819,21 @@ describe('node feature', () => {
         ...process.env,
         ELECTRON_FORCE_IS_PACKAGED: 'true',
         NODE_OPTIONS: `--require=${path.join(fixtures, 'module', 'fail.js')}`
+      };
+      // App should exit with code 0.
+      const child = childProcess.spawn(process.execPath, [appPath], { env });
+      const [code] = await once(child, 'exit');
+      expect(code).to.equal(0);
+    });
+
+    it('does not throw --import hook in non-packaged apps', async () => {
+      const appPath = path.join(fixtures, 'module', 'noop.js');
+      // Since this is the import path - we should always use backward slashes
+      // regardless of the `path.sep` value.
+      const hookUrl = pathToFileURL(path.join(fixtures, 'module', 'hook.mjs'));
+      const env = {
+        ...process.env,
+        NODE_OPTIONS: `--import=${hookUrl}`
       };
       // App should exit with code 0.
       const child = childProcess.spawn(process.execPath, [appPath], { env });
