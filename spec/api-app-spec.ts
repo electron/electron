4a040286c3156a7a9a874b62caeef09dd2fd12a0
Keeley Hammond <vertedinde@electronjs.org>
2026-08-18 00:32:41 -0700

    fix: don't crash on non-UTF8 serviceName in app.setLoginItemSettings (#52944)
    fix: don't crash on non-UTF8 serviceName in app.setLoginItemSettings (#52773)
    
    * test: add coverage for non-UTF8 serviceName in app.setLoginItemSettings
    
    * fix: don't crash on non-UTF8 serviceName in app.setLoginItemSettings
    
    (cherry picked from commit 4a9168d07880ab32850e4891c0fa958afd17c11b)
    
    Co-authored-by: David Sanders <dsanders11@ucsbalum.com>

--- spec/api-app-spec.ts
+++ spec/api-app-spec.ts
@@ -913,6 +913,16 @@ describe('app module', () => {
         }).to.throw(/'name' is required when type is not mainAppService/);
       });
 
+      ifit(isVenturaOrHigher)('does not crash when the service name is not valid UTF-8', () => {
+        expect(() => {
+          app.setLoginItemSettings({
+            openAtLogin: false,
+            type: 'daemonService',
+            serviceName: '\uD800'
+          });
+        }).to.not.throw();
+      });
+
       ifit(isVenturaOrHigher)('can unset a login item', () => {
         app.setLoginItemSettings({
           openAtLogin: true,
