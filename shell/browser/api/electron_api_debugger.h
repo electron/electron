23be0a95b3662f9652c5f5d962fccf6b6f440c54
deepak1556 <hop2deep@gmail.com>
2026-09-19 05:02:54 +0900

    fix: isolate debugger native state from cppgc lifetime

--- shell/browser/api/electron_api_debugger.h
+++ shell/browser/api/electron_api_debugger.h
@@ -5,12 +5,11 @@
 #ifndef ELECTRON_SHELL_BROWSER_API_ELECTRON_API_DEBUGGER_H_
 #define ELECTRON_SHELL_BROWSER_API_ELECTRON_API_DEBUGGER_H_
 
-#include <map>
 #include <memory>
+#include <string>
 
-#include "base/containers/span.h"
+#include "base/task/sequenced_task_runner.h"
 #include "base/values.h"
-#include "content/public/browser/web_contents_observer.h"
 #include "gin/wrappable.h"
 #include "shell/browser/event_emitter_mixin.h"
 
@@ -23,16 +22,9 @@ namespace gin {
 class Arguments;
 }  // namespace gin
 
-namespace gin_helper {
-template <typename T>
-class Promise;
-}  // namespace gin_helper
-
 namespace electron::api {
-
 class Debugger final : public gin::Wrappable<Debugger>,
-                       public gin_helper::EventEmitterMixin<Debugger>,
-                       private content::WebContentsObserver {
+                       public gin_helper::EventEmitterMixin<Debugger> {
  public:
   static Debugger* Create(v8::Isolate* isolate,
                           content::WebContents* web_contents);
@@ -54,27 +46,20 @@ class Debugger final : public gin::Wrappable<Debugger>,
   Debugger(const Debugger&) = delete;
   Debugger& operator=(const Debugger&) = delete;
 
- protected:
-  // content::WebContentsObserver:
-  void RenderFrameHostChanged(content::RenderFrameHost* old_rfh,
-                              content::RenderFrameHost* new_rfh) override;
-
  private:
-  class AgentHostClient;
-  using PendingRequestMap = std::map<int, gin_helper::Promise<base::DictValue>>;
+  class AgentHostLifecycle;
 
   void AgentHostClosed();
-  void DispatchProtocolMessage(base::span<const uint8_t> message);
+  void EmitProtocolMessage(const std::string& method,
+                           base::DictValue params,
+                           const std::string& session_id);
   void Attach(gin::Arguments* args);
   bool IsAttached();
   void Detach();
   v8::Local<v8::Promise> SendCommand(gin::Arguments* args);
-  void ClearPendingRequests();
 
-  std::unique_ptr<AgentHostClient> agent_host_client_;
-
-  PendingRequestMap pending_requests_;
-  int previous_request_id_ = 0;
+  std::unique_ptr<AgentHostLifecycle, base::OnTaskRunnerDeleter>
+      agent_host_lifecycle_;
 };
 
 }  // namespace electron::api
