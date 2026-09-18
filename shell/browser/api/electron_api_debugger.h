5481faf1ef5da019da46f55a71f0e00f4034560c
deepak1556 <hop2deep@gmail.com>
2026-09-19 03:13:41 +0900

    fix: stop debugger callbacks after cppgc collection
    Route DevToolsAgentHost callbacks through a native proxy that holds a weak
    cppgc reference to the debugger wrapper.

--- shell/browser/api/electron_api_debugger.h
+++ shell/browser/api/electron_api_debugger.h
@@ -6,9 +6,10 @@
 #define ELECTRON_SHELL_BROWSER_API_ELECTRON_API_DEBUGGER_H_
 
 #include <map>
+#include <memory>
 
+#include "base/containers/span.h"
 #include "base/values.h"
-#include "content/public/browser/devtools_agent_host_client.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "gin/wrappable.h"
 #include "shell/browser/event_emitter_mixin.h"
@@ -31,14 +32,13 @@ namespace electron::api {
 
 class Debugger final : public gin::Wrappable<Debugger>,
                        public gin_helper::EventEmitterMixin<Debugger>,
-                       public content::DevToolsAgentHostClient,
                        private content::WebContentsObserver {
  public:
   static Debugger* Create(v8::Isolate* isolate,
                           content::WebContents* web_contents);
 
   // Make public for cppgc::MakeGarbageCollected.
-  explicit Debugger(content::WebContents* web_contents);
+  Debugger(v8::Isolate* isolate, content::WebContents* web_contents);
   ~Debugger() override;
 
   // gin_helper::Wrappable
@@ -55,25 +55,23 @@ class Debugger final : public gin::Wrappable<Debugger>,
   Debugger& operator=(const Debugger&) = delete;
 
  protected:
-  // content::DevToolsAgentHostClient:
-  void AgentHostClosed(content::DevToolsAgentHost* agent_host) override;
-  void DispatchProtocolMessage(content::DevToolsAgentHost* agent_host,
-                               base::span<const uint8_t> message) override;
-
   // content::WebContentsObserver:
   void RenderFrameHostChanged(content::RenderFrameHost* old_rfh,
                               content::RenderFrameHost* new_rfh) override;
 
  private:
+  class AgentHostClient;
   using PendingRequestMap = std::map<int, gin_helper::Promise<base::DictValue>>;
 
+  void AgentHostClosed();
+  void DispatchProtocolMessage(base::span<const uint8_t> message);
   void Attach(gin::Arguments* args);
   bool IsAttached();
   void Detach();
   v8::Local<v8::Promise> SendCommand(gin::Arguments* args);
   void ClearPendingRequests();
 
-  scoped_refptr<content::DevToolsAgentHost> agent_host_;
+  std::unique_ptr<AgentHostClient> agent_host_client_;
 
   PendingRequestMap pending_requests_;
   int previous_request_id_ = 0;
