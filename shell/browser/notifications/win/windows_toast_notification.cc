ee573533c27800a6c6499913e135312de0bd8d85
Bill Shen <15865969+cucbin@users.noreply.github.com>
2026-09-22 21:48:33 -0800

    fix: Use SHA256 hash to generate notification tags

--- shell/browser/notifications/win/windows_toast_notification.cc
+++ shell/browser/notifications/win/windows_toast_notification.cc
@@ -13,6 +13,7 @@
 #include <shlobj.h>
 #include <wrl\wrappers\corewrappers.h>
 
+#include "base/base64.h"
 #include "base/containers/fixed_flat_map.h"
 #include "base/logging.h"
 #include "base/no_destructor.h"
@@ -24,6 +25,7 @@
 #include "base/task/thread_pool.h"
 #include "base/win/scoped_hstring.h"
 #include "content/public/browser/browser_thread.h"
+#include "crypto/hash.h"
 #include "shell/browser/notifications/notification_delegate.h"
 #include "shell/browser/notifications/win/notification_presenter_win.h"
 #include "shell/browser/notifications/win/windows_toast_activator.h"
@@ -76,7 +78,10 @@ void DebugLog(std::string_view log_msg) {
 }
 
 std::wstring GetTag(const std::string_view notification_id) {
-  return base::UTF8ToWide(notification_id);
+  // The tag has a max length of 63 characters (plus null terminator).
+  // Base64Encode yields 44 chars.
+  return base::ASCIIToWide(
+      base::Base64Encode(crypto::hash::Sha256(notification_id)));
 }
 
 // See https://www.hresult.info for HRESULT error codes.
